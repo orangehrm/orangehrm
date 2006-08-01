111930021346409168ef6b3e9b21828877d40d1c
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2006-08-01 08:38:07 +0000

    Fixed -----
    [1] bug - 1530268 - PIM - Memberships - Cannot add a membership after viewing a membership

--- php/orangehrm/templates/hrfunct/hrempmembership.php
+++ php/orangehrm/templates/hrfunct/hrempmembership.php
@@ -313,6 +313,11 @@ if(isset($this->popArr['editMembershipArr'])) {
   </tr>
   <tr>
   <td>
+<?	if($locRights['add']) { ?>
+		<img border="0" title="Add" onClick="resetAdd(13);" onmouseout="this.src='../../themes/beyondT/pictures/btn_add.jpg';" onmouseover="this.src='../../themes/beyondT/pictures/btn_add_02.jpg';" src="../../themes/beyondT/pictures/btn_add.jpg">
+					<? 	} else { ?>
+		<img onClick="alert('<?=$sysConst->accessDenied?>');" src="../../themes/beyondT/pictures/btn_add.jpg">
+<?	} ?>
 <?	if($locRights['delete']) { ?>
         <img title="Delete" onclick="delEXTMembership();" onmouseout="this.src='../../themes/beyondT/pictures/btn_delete.jpg';" onmouseover="this.src='../../themes/beyondT/pictures/btn_delete_02.jpg';" src="../../themes/beyondT/pictures/btn_delete.jpg">
 <? 	} else { ?>
