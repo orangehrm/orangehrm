cb02557def1f55fa800c9b1f0efeb48966e9cde2
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2006-08-08 07:40:21 +0000

    Fixed -----
    [2] bug - 1530269 - PIM - Memberships - improper error message if the subscription amount is empty

--- php/orangehrm/lib/common/xajax/xajaxElementFiller.php
+++ php/orangehrm/lib/common/xajax/xajaxElementFiller.php
@@ -4,14 +4,17 @@ class xajaxElementFiller {
 	function xajaxElementFiller() { 
 	}
 	
-	function cmbFiller ($objResponse,$fillArr,$fele,$form,$element,$defSel=1) {
-		$objResponse->addScript("document.".$form.".".$element.".options.length = 0;");
+	function cmbFiller ($objResponse,$fillArr,$fele,$form,$element,$defSel=1) {		
 
-		if($defSel == 1)
+		if($defSel == 1) {
 	 		$objResponse->addScript("document.".$form.".".$element.".options[0] = new Option('--Select--','0');");
+			$objResponse->addScript("document.".$form.".".$element.".options.length = 0;");
+		} else {
+			$objResponse->addScript("document.".$form.".".$element.".options.length = 1;");
+		}
 	 		
 	 	for($i=0;$fillArr && count($fillArr)>$i;$i++)
-	 		$objResponse->addScript("document.".$form.".".$element.".options[".($defSel == 1 ? $i+1 : $i)."] = new Option('" .$fillArr[$i][($fele+1)]. "','".$fillArr[$i][$fele]."');");
+	 		$objResponse->addScript("document.".$form.".".$element.".options[".($defSel == 1 ? $i+1 : $i+1)."] = new Option('" .$fillArr[$i][($fele+1)]. "','".$fillArr[$i][$fele]."');");
 		
 		return $objResponse;
 	}
