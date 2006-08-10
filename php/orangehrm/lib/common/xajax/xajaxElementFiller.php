4f9791ede30689f94cbf67d40188a2ffd8ce1c11
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2006-08-10 13:28:30 +0000

    Fixed -----
    [3] bug - 1530243 - PIM - Immigration - The location of the calendar square should be consistent
    
    [3] bug - 1530245 - PIM - Immigration - there should be a space between labels and their field

--- php/orangehrm/lib/common/xajax/xajaxElementFiller.php
+++ php/orangehrm/lib/common/xajax/xajaxElementFiller.php
@@ -9,12 +9,14 @@ class xajaxElementFiller {
 		if($defSel == 1) {
 	 		$objResponse->addScript("document.".$form.".".$element.".options[0] = new Option('--Select--','0');");
 			$objResponse->addScript("document.".$form.".".$element.".options.length = 0;");
+		} else if($defSel == 0) {
+			$objResponse->addScript("document.".$form.".".$element.".options.length = 0;");
 		} else {
 			$objResponse->addScript("document.".$form.".".$element.".options.length = 1;");
 		}
 	 		
 	 	for($i=0;$fillArr && count($fillArr)>$i;$i++)
-	 		$objResponse->addScript("document.".$form.".".$element.".options[".($defSel == 1 ? $i+1 : $i+1)."] = new Option('" .$fillArr[$i][($fele+1)]. "','".$fillArr[$i][$fele]."');");
+	 		$objResponse->addScript("document.".$form.".".$element.".options[".($defSel == 3 ? $i+1 : $i)."] = new Option('" .$fillArr[$i][($fele+1)]. "','".$fillArr[$i][$fele]."');");
 		
 		return $objResponse;
 	}
