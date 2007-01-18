e889f772b72f36b9a64d88feb087a9930f709a26
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-01-18 08:05:51 +0000

    Changed to hide errors in mail() but failed

--- php/orangehrm/lib/common/htmlMimeMail5/htmlMimeMail5.php
+++ php/orangehrm/lib/common/htmlMimeMail5/htmlMimeMail5.php
@@ -736,13 +736,13 @@ class htmlMimeMail5
                 }
 
                 $to = $this->encodeHeader(implode(', ', $recipients), $this->build_params['head_charset']);
-
+				
                 if (!empty($this->return_path)) {
-                    $result = mail($to, $subject, $this->output, implode(CRLF, $headers), '-f' . $this->return_path);
+                    $result = @mail($to, $subject, $this->output, implode(CRLF, $headers), '-f' . $this->return_path);
                 } else {
-                    $result = mail($to, $subject, $this->output, implode(CRLF, $headers));
+                    $result = @mail($to, $subject, $this->output, implode(CRLF, $headers));
                 }
-
+                
                 // Reset the subject in case mail is resent
                 if ($subject !== '') {
                     $this->headers['Subject'] = $subject;
