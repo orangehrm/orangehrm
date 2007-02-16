fbc1a614a4c4e16a1ac6c7defa9109261b1c2266
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-02-16 07:49:35 +0000

    Report View translation complete

--- php/orangehrm/language/ru/lang_ru_repview.php
+++ php/orangehrm/language/ru/lang_ru_repview.php
@@ -1,34 +1,33 @@
 <?php
 $srchlist[0] = array( -1 , 0 , 1 );
-$srchlist[1] = array( '-Select-' , 'ID' , 'Name' );
+$srchlist[1] = array( "-$lang_Leave_Common_Select-" , $lang_view_ID , $lang_Commn_name);
 
-$searchby = 'Search by:';
-$searchfor = 'Search for:';
+$searchby = $lang_empview_searchby;
+$searchfor = $lang_empview_description;
 
-$Previous = 'Previous';
-$Next     = 'Next';
-$dispMessage = "No Records to Display !";
-$SearchBy = 'Search By:';
-$description = 'Description';
-$search = 'Search';
+$Previous = $lang_empview_previous;
+$Next     = $lang_empview_next;
+$dispMessage = "$lang_empview_norecorddisplay !";
+$SearchBy = $lang_empview_searchby;
+$description = $lang_Commn_description ;
+$search = $lang_empview_search;
 
-$ADD_SUCCESS = 'Successfully Added';
-$UPDATE_SUCCESS = 'Successfully Updated';
-$DELETE_SUCCESS = 'Successfully Deleted';
+$ADD_SUCCESS = $lang_empview_ADD_SUCCESS;
+$UPDATE_SUCCESS = $lang_empview_UPDATE_SUCCESS ;
+$DELETE_SUCCESS = $lang_empview_DELETE_SUCCESS ;
 
-$ADD_FAILURE = 'Failed to Add';
-$UPDATE_FAILURE = 'Failed to Update';
-$DELETE_FAILURE = 'Failed to Delete';
-
+$ADD_FAILURE = $lang_empview_ADD_FAILURE;
+$UPDATE_FAILURE = $lang_empview_UPDATE_FAILURE;
+$DELETE_FAILURE = $lang_empview_DELETE_FAILURE;
 
 switch ($_GET['repcode']) {
 
 		case 'EMPVIEW' :
-			$heading = array ('Report ID','Report Name',1,'View Employee Reports','Deletion might affect Company Hierarchy');
+			$heading = array ($lang_repview_ReportID, $lang_repview_ReportName, 1, $lang_repview_ViewEmployeeReports, $lang_repview_message);
 			break;
 
 		case 'EMPDEF' :
-			$heading = array ('Report ID','Report Name',0,'Define Employee Reports','Deletion might affect viewing of reports.');
+			$heading = array ($lang_repview_ReportID, $lang_repview_ReportName, 0, $lang_repview_DefineEmployeeReports , $lang_repview_message2);
 			break;
 }
-?>	
+?>
--- php/orangehrm/repview.php
+++ php/orangehrm/repview.php
@@ -1,15 +1,15 @@
 <?php
 /*
-OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures 
-all the essential functionalities required for any enterprise. 
+OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+all the essential functionalities required for any enterprise.
 Copyright (C) 2006 hSenid Software International Pvt. Ltd, http://www.hsenid.com
 
 OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
 the GNU General Public License as published by the Free Software Foundation; either
 version 2 of the License, or (at your option) any later version.
 
-OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program;
@@ -21,14 +21,16 @@ Boston, MA  02110-1301, USA
 require_once ROOT_PATH . '/lib/confs/sysConf.php';
 
 
-	$sysConst = new sysConf(); 
+	$sysConst = new sysConf();
 	$locRights=$_SESSION['localRights'];
-	 	
+
 	$headingInfo=$this->popArr['headinginfo'];
-	
+
 	$currentPage = $this->popArr['currentPage'];
-    
+
 	$message= $this->popArr['message'];
+
+//$headingInfo = $heading;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
@@ -38,7 +40,7 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 <title>Untitled Document</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
-<script>		
+<script>
 
 	function nextPage() {
 		var i=eval(document.standardView.pageNO.value);
@@ -63,7 +65,7 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 
 		location.href = "./CentralController.php?repcode=<?php echo $this->getArr['repcode']?>&capturemode=addmode";
 	}
-	
+
 <?php  }	?>
 
 	function returnDelete() {
@@ -75,38 +77,38 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 				}
 			}
 		}
-	
+
 		if ( $check == 1 ){
-			
-			document.standardView.delState.value = 'DeleteMode';		
+
+			document.standardView.delState.value = 'DeleteMode';
 			document.standardView.pageNO.value=1;
 			document.standardView.submit();
 		} else {
-			alert("Select at least one record to delete");
-		}		
+			alert("<?php echo $lang_Error_SelectAtLeastOneRecordToDelete; ?>");
+		}
 	}
-	
-	function returnSearch() {		
-		document.standardView.captureState.value = 'SearchMode';		
+
+	function returnSearch() {
+		document.standardView.captureState.value = 'SearchMode';
 		document.standardView.pageNO.value=1;
 		document.standardView.submit();
 	}
-	
+
 	function doHandleAll()
 	{
-		with (document.standardView) {		
+		with (document.standardView) {
 			if(elements['allCheck'].checked == false){
 				doUnCheckAll();
 			}
 			else if(elements['allCheck'].checked == true){
 				doCheckAll();
 			}
-		}	
+		}
 	}
-	
+
 	function doCheckAll()
 	{
-		with (document.standardView) {		
+		with (document.standardView) {
 			for (var i=0; i < elements.length; i++) {
 				if (elements[i].type == 'checkbox') {
 					elements[i].checked = true;
@@ -114,10 +116,10 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 			}
 		}
 	}
-	
+
 	function doUnCheckAll()
 	{
-		with (document.standardView) {		
+		with (document.standardView) {
 			for (var i=0; i < elements.length; i++) {
 				if (elements[i].type == 'checkbox') {
 					elements[i].checked = false;
@@ -136,21 +138,21 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 <table width='100%' cellpadding='0' cellspacing='0' border='0' class='moduleTitle'><tr><td valign='top'>
 <form name="standardView" method="post" action="<?php echo $_SERVER['PHP_SELF']?>?repcode=<?php echo $this->getArr['repcode']?>&VIEW=MAIN">
   </td>
-  <td width='100%'><h2> 
-      <?php echo $headingInfo[3]?>
+  <td width='100%'><h2>
+      <?php echo $heading[3]?>
     </h2></td>
   <td valign='top' align='right' nowrap style='padding-top:3px; padding-left: 5px;'></td></tr>
 </table></p>
-</p> 
+</p>
 <p>
 <table width="100%" cellpadding="0" cellspacing="0" border="0">
   <tr>
-    <td width="22%" nowrap><h3> 
+    <td width="22%" nowrap><h3>
         <input type="hidden" name="captureState" value="<?php echo isset($this->postArr['captureState'])?$this->postArr['captureState']:''?>">
         <input type="hidden" name="delState" value="">
-        
+
         <input type="hidden" name="pageNO" value="<?php echo isset($this->postArr['pageNO'])?$this->postArr['pageNO']:'1'?>">
-    
+
 <?php	if($locRights['repDef'] && $headingInfo[2] == 1) { ?>
         <img border="0" title="Add" onClick="returnAdd();" onMouseOut="this.src='../../themes/beyondT/pictures/btn_add.jpg';" onMouseOver="this.src='../../themes/beyondT/pictures/btn_add_02.jpg';" src="../../themes/beyondT/pictures/btn_add.jpg">
         <img title="Delete" onClick="returnDelete();" onMouseOut="this.src='../../themes/beyondT/pictures/btn_delete.jpg';" onMouseOver="this.src='../../themes/beyondT/pictures/btn_delete_02.jpg';" src="../../themes/beyondT/pictures/btn_delete.jpg">
@@ -163,26 +165,26 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 <p>
 <table width="100%" cellpadding="0" cellspacing="0" border="0">
   <tr>
-    <td width="22%" nowrap><h3>Search</h3></td>
-    <td width='78%' align="right"><IMG height='1' width='1' src='../../pictures/blank.gif' alt=''> 
+    <td width="22%" nowrap><h3><?php echo $search; ?></h3></td>
+    <td width='78%' align="right"><IMG height='1' width='1' src='../../pictures/blank.gif' alt=''>
      <?php
 		if (isset($this->getArr['message'])) {
-		
+
 			$expString  = $this->getArr['message'];
 			$expString = explode ("_",$expString);
-			$length = count($expString);		
-			
+			$length = count($expString);
+
 			$col_def=$expString[$length-1];
-			
+
 			$expString=$this->getArr['message'];
 	?>
-			<font class="<?php echo $col_def?>" size="-1" face="Verdana, Arial, Helvetica, sans-serif">	
+			<font class="<?php echo $col_def?>" size="-1" face="Verdana, Arial, Helvetica, sans-serif">
 	<?php
 				echo $$expString;
 	?>
 			</font>
 	<?php
-		}		
+		}
 		?>&nbsp;&nbsp;&nbsp;&nbsp;</td>
   </tr>
 </table>
@@ -206,10 +208,10 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 								   echo "<option selected value='" . $srchlist[0][$c] ."'>".$srchlist[1][$c] ."</option>";
 								else
 								   echo "<option value='" . $srchlist[0][$c] ."'>".$srchlist[1][$c] ."</option>";
-?>								   
+?>
                         </select>
                       </slot></td>
-                      <td width="200" class="dataLabel" noWrap><slot><?php echo $searchfor?></slot>&nbsp;&nbsp;<slot>
+                      <td width="200" class="dataLabel" noWrap><slot><?php echo $searchfor; ?></slot>&nbsp;&nbsp;<slot>
                         <input type=text size="20" name="loc_name" class=dataField  value="<?php echo isset($this->postArr['loc_name'])? stripslashes($this->postArr['loc_name']):''?>">
                      </slot></td>
                     <td align="right" width="180" class="dataLabel"><img title="Search" onClick="returnSearch();" onMouseOut="this.src='../../themes/beyondT/pictures/btn_search.jpg';" onMouseOver="this.src='../../themes/beyondT/pictures/btn_search_02.jpg';" src="../../themes/beyondT/pictures/btn_search.jpg">&nbsp;&nbsp;<img title="Clear" onClick="clear_form();" onMouseOut="this.src='../../themes/beyondT/pictures/btn_clear.jpg';" onMouseOver="this.src='../../themes/beyondT/pictures/btn_clear_02.jpg';" src="../../themes/beyondT/pictures/btn_clear.jpg"></td>
@@ -237,45 +239,45 @@ require_once ROOT_PATH . '/lib/confs/sysConf.php';
 			  <table border="0" width="100%">
 			  <tr>
 			  <td height="40" valign="bottom" align="right">
-			  
+
 <?php
-$temp = $this->popArr['temp']; 
-if($temp)    
+$temp = $this->popArr['temp'];
+if($temp)
     $recCount=$temp;
-else 
+else
 	$recCount=0;
-	
+
 	$noPages=(int)($recCount/$sysConst->itemsPerPage);
 
 	if($recCount%$sysConst->itemsPerPage)
 	   $noPages++;
 
 	if ($noPages > 1) {
-		
-	
+
+
 		if ($currentPage==1)
 			echo "<font color='Gray'>$Previous</font>";
 		else
     		echo "<a href='#' onClick='prevPage()'>$Previous</a>";
-    	
+
     	echo "  ";
-    	
+
 		for( $c = 1 ; $noPages >= $c ; $c++) {
 	    	if($c == $currentPage)
 				echo "<font color='Gray'>" .$c. "</font>";
 			else
 	    		echo "<a href='#' onClick='chgPage(" .$c. ")'>" .$c. "</a>";
-	    	
+
 	    	echo "  ";
 		}
-		
+
 		if($currentPage == $noPages)
 			echo "<font color='Gray'>$Next</font>";
 		else
     		echo "<a href='#' onClick='nextPage()'>$Next</a>";
-	
+
 	}
-?> 
+?>
 		</td>
 		<td width="25"></td>
 		</tr>
@@ -291,12 +293,12 @@ else
                   <td background="../../themes/beyondT/pictures/table_r2_c1.gif"><img name="table_r2_c1" src="../../themes/beyondT/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
                   <td><table width="100%" border="0" cellpadding="5" cellspacing="0" class="">
 						  <td width="50" NOWRAP class="listViewThS1" scope="col">
-<?php				if($headingInfo[2]==1) { ?>	  
+<?php				if($headingInfo[2]==1) { ?>
 						  <input type='checkbox' class='checkbox' name='allCheck' value='' onClick="doHandleAll();">
-<?php				}	?>						  
+<?php				}	?>
 						  </td>
-						  <td scope="col" width="250" class="listViewThS1"><?php echo $headingInfo[0]?></td>
-						  <td scope="col" width="400" class="listViewThS1"><?php echo $headingInfo[1]?></td>
+						  <td scope="col" width="250" class="listViewThS1"><?php echo $heading[0]?></td>
+						  <td scope="col" width="400" class="listViewThS1"><?php echo $heading[1]?></td>
                   </table></td>
                   <td background="../../themes/beyondT/pictures/table_r2_c3.gif"><img name="table_r2_c3" src="../../themes/beyondT/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
                   <td><img src="../../themes/beyondT/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
@@ -304,53 +306,53 @@ else
 
         <?php
 			if ((isset($message)) && ($message !='')) {
-	 
+
 			 for ($j=0; $j<count($message);$j++) {
-			 	
+
 			 	$descField=$message[$j][1];
-			 	
+
 			 	if($sysConst->viewDescLen <= strlen($descField)) {
 			 	   $descField = substr($descField,0,$sysConst->viewDescLen);
 			 	   $descField .= "....";
 			 	}
-			
+
 		?>
                 <tr>
                   <td background="../../themes/beyondT/pictures/table_r2_c1.gif"><img name="table_r2_c1" src="../../themes/beyondT/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
                   <td><table width="100%" border="0" cellpadding="5" cellspacing="0" class="">
-		<?php	if(!($j%2)) { 
-				if($headingInfo[2]==1) { ?>	  
+		<?php	if(!($j%2)) {
+				if($headingInfo[2]==1) { ?>
                   <td width="50"><input type='checkbox' class='checkbox' name='chkLocID[]' value='<?php echo $message[$j][0]?>'></td>
 		<?php 		} else { ?>
                   <td width="50"></td>
 		<?php 		}  ?>
-                  
+
 				  <td width="250"><a href="./CentralController.php?id=<?php echo $message[$j][0]?>&repcode=<?php echo $this->getArr['repcode']?>&capturemode=updatemode" class="listViewTdLinkS1"><?php echo $message[$j][0]?></a>
 		  		  <td width="400" ><?php echo $descField?></td>
-		 <?php } else {  
-				if($headingInfo[2]==1) { ?>	  
+		 <?php } else {
+				if($headingInfo[2]==1) { ?>
                   <td bgcolor="#EEEEEE" width="50"><input type='checkbox' class='checkbox' name='chkLocID[]' value='<?php echo $message[$j][0]?>'></td>
 		<?php 		} else { ?>
                   <td bgcolor="#EEEEEE" width="50"></td>
 		<?php 		}  ?>
 				  <td bgcolor="#EEEEEE" width="250"><a href="./CentralController.php?id=<?php echo $message[$j][0]?>&repcode=<?php echo $this->getArr['repcode']?>&capturemode=updatemode" class="listViewTdLinkS1"><?php echo $message[$j][0]?></a>
 		  		  <td bgcolor="#EEEEEE" width="400" ><?php echo $descField?></td>
-		 <?php } ?>		 
+		 <?php } ?>
                   </table></td>
                   <td background="../../themes/beyondT/pictures/table_r2_c3.gif"><img name="table_r2_c3" src="../../themes/beyondT/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
                   <td><img src="../../themes/beyondT/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
                 </tr>
 
-         <?php } 
+         <?php }
         	  } else if ((isset($message)) && ($message =='')) {
-        		
-        		 $dispMessage = "No Records to Display !";
+
+        		 $dispMessage = $dispMessage;
         		 echo '<font color="#FF0000" size="-1" face="Verdana, Arial, Helvetica, sans-serif">';
         		 echo $dispMessage;
         		 echo '</font>';
         	}
-         
-         ?> 
+
+         ?>
 
                 <tr>
                   <td><img name="table_r3_c1" src="../../themes/beyondT/pictures/table_r3_c1.gif" width="13" height="16" border="0" alt=""></td>
@@ -358,7 +360,7 @@ else
                   <td><img name="table_r3_c3" src="../../themes/beyondT/pictures/table_r3_c3.gif" width="13" height="16" border="0" alt=""></td>
                   <td><img src="../../themes/beyondT/pictures/spacer.gif" width="1" height="16" border="0" alt=""></td>
                 </tr>
-      
+
 <!--  newtable -->
 
 </form>
