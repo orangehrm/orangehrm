d9c94f46bb9f6acf92768811f1b66ca446dc18c8
koshika <koshika@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-02-23 11:14:20 +0000

    Error when MySQL ext. was absent with System check corrected

--- php/orangehrm/installer/checkSystem.php
+++ php/orangehrm/installer/checkSystem.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 function check_php_version($sys_php_version = '') {
 
 	$sys_php_version = empty($sys_php_version) ? constant('PHP_VERSION') : $sys_php_version;
 	// versions below $min_considered_php_version considered invalid by default,
 	// versions equal to or above this ver will be considered depending
-	// on the rules that follow 
+	// on the rules that follow
 	$min_considered_php_version = '5.1.0';
 
 	// only the supported versions,
 	// should be mutually exclusive with $invalid_php_versions
-	$supported_php_versions = array (		
+	$supported_php_versions = array (
 		'5.0.1', '5.0.2', '5.0.3', '5.0.4',
 		'5.0.5', '5.1.0', '5.1.1', '5.1.2',
 		'5.1.3', '5.1.4', '5.1.5', '5.1.6',
 		'5.1.7', '5.2.0'
 	);
-	
+
 	sort($supported_php_versions);
-	
+
 	// invalid versions above the $min_considered_php_version,
 	// should be mutually exclusive with $supported_php_versions
 	$invalid_php_versions = array('5.0.0');
@@ -37,9 +37,9 @@ function check_php_version($sys_php_version = '') {
 			break;
 		}
 	}
-		
+
 	if (($retval != 1) && (1 == version_compare($sys_php_version, $ver, '>'))) {
-		$retval = 1;				
+		$retval = 1;
 	}
 
 	// invalid version check overrides default unsupported
@@ -57,43 +57,43 @@ function chk_memory($limit=9, $recommended=16) {
 
 	$msg = '';
 	$type = '';
-	
+
 	$max_memory = ini_get('memory_limit');
-	
+
 	if ($max_memory == "") {
-	
+
 		$msg = "OK (No Limit)";
 		$type = "done";
-		
+
 	} else if ($max_memory === "-1") {
-	
+
 		$msg = "OK (Unlimited)";
 		$type = "done";
-		
+
 	} else {
-	
+
 		$max_memory = rtrim($max_memory, "M");
 		$max_memory_int = (int) $max_memory;
-		
+
 		if ($max_memory_int < $limit) {
-			
+
 			$msg = "Warning at least $limit M required ($max_memory M available, Recommended $recommended M)";
 			$type = "error";
-			
+
 		} elseif ($max_memory_int < $recommended) {
-		
+
 				$msg = "OK (Recommended $recommended M)";
 				$type = "pending";
-		
-		} else {			
+
+		} else {
 				$msg = "OK";
-				$type = "done";			
+				$type = "done";
 		}
-		
+
 	}
-		
-	$msg = "<b class='$type'>".$msg."</b>";	
-		
+
+	$msg = "<b class='$type'>".$msg."</b>";
+
 return $msg;
 }
 
@@ -110,8 +110,8 @@ function sysCheckPassed() {
 
 <div id="content">
 
-  <h2>Step 2: System Check</h2>  
-  
+  <h2>Step 2: System Check</h2>
+
   <p>In order for your OrangeHRM installation to function properly,
   please ensure that all of the system check items listed below are green. If
   any are red, please take the necessary steps to fix them.</p>
@@ -155,10 +155,9 @@ function sysCheckPassed() {
             <td align="right" class="tdValues"><strong>
             <?php
 
-            	$mysqlClient = mysql_get_client_info();
-
                if(function_exists('mysql_connect')) {
-                  
+	            	$mysqlClient = mysql_get_client_info();
+
                   if(intval(substr($mysqlClient,0,1)) < 4 || substr($mysqlClient,0,3) == '4.0') {
 	                  echo "<b><font color='#C4C781'>ver 4.1.x or later recommended (reported ver " .$mysqlClient. ')</font></b>';
                   } else echo "<b><font color='green'>OK (ver " .$mysqlClient. ')</font></b>';
@@ -204,7 +203,7 @@ function sysCheckPassed() {
 		  <tr>
             <td class="tdComponent">Memory allocated for PHP script</td>
             <td align="right" class="tdValues"><?php echo chk_memory(9, 16)?></td>
-          </tr>		 
+          </tr>
 		</table>
 		<br />
         <input class="button" type="button" value="Back" onclick="back();" tabindex="4">
