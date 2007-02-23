b3dae1857a9bb3e1638577485551053729cb6755
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-02-23 13:07:43 +0000

    Getting ready to release Nicolaj

--- php/orangehrm/installer/applicationSetup.php
+++ php/orangehrm/installer/applicationSetup.php
@@ -1,81 +1,81 @@
 <?php
 // Installing
-function createDB() {	
-	connectDB();							
+function createDB() {
+	connectDB();
 	mysql_query("CREATE DATABASE " . $_SESSION['dbInfo']['dbName']);
-	
+
 	if(!@mysql_select_db($_SESSION['dbInfo']['dbName'])) {
 		$_SESSION['error'] = 'Unable to create Database!';
 		return;
-	}								
-								
-								
+	}
+
+
 }
 
 function connectDB() {
 
 	if(!@mysql_connect($_SESSION['dbInfo']['dbHostName'].':'.$_SESSION['dbInfo']['dbHostPort'], 		$_SESSION['dbInfo']['dbUserName'], $_SESSION['dbInfo']['dbPassword'])) {
-		$_SESSION['error'] =  'Database Connection Error!';		
+		$_SESSION['error'] =  'Database Connection Error!';
 		return;
 	}
-	
+
 }
 
 function fillData($phase=1, $source='/dbscript/dbscript-') {
 	$source .= $phase.'.sql';
 	connectDB();
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Connected to the DB Server\n",3, "installer/log.txt");
-	
+
 	if(!mysql_select_db($_SESSION['dbInfo']['dbName'])) {
 		$_SESSION['error'] = 'Unable to create Database!';
 		error_log (date("r")." Fill Data Phase $phase - Error - Unable to create Database\n",3, "installer/log.txt");
 		return;
 	}
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Selected the DB\n",3, "installer/log.txt");
 	error_log (date("r")." Fill Data Phase $phase - Reading DB Script\n",3, "installer/log.txt");
-	
+
 	$queryFile = ROOT_PATH . $source;
 	$fp    = fopen($queryFile, 'r');
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Opened DB Script\n",3, "installer/log.txt");
-	
+
 	$query = fread($fp, filesize($queryFile));
 	fclose($fp);
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Read DB script\n",3, "installer/log.txt");
-								
+
 	$dbScriptStatements = explode(";", $query);
-	
+
 	error_log (date("r")." Fill Data Phase $phase - There are ".count($dbScriptStatements)." Statements in the DB script\n",3, "installer/log.txt");
-								
+
 	for($c=0;(count($dbScriptStatements)-1)>$c;$c++)
-		if(!@mysql_query($dbScriptStatements[$c])) {  
+		if(!@mysql_query($dbScriptStatements[$c])) {
 			$_SESSION['error'] = mysql_error();
 			$error = mysql_error();
 			error_log (date("r")." Fill Data Phase $phase - Error Statement # $c \n",3, "installer/log.txt");
 			error_log (date("r")." ".$dbScriptStatements[$c]."\n",3, "installer/log.txt");
 			return;
 		}
-									
+
 	if(isset($error))
 		return;
-}	
+}
 
 function createDBUser() {
 
 	connectDB();
-	
+
 	if(isset($_SESSION['dbInfo']['dbOHRMUserName'])) {
-	
+
 		$dbName = $_SESSION['dbInfo']['dbName'];
 		$dbOHRMUser = $_SESSION['dbInfo']['dbOHRMUserName'];
 		$dbOHRMPassword = $_SESSION['dbInfo']['dbOHRMPassword'];
-	
+
 		$querryIdentifiedBy = (isset($dbOHRMPassword) && ($dbOHRMPassword !== ''))? "IDENTIFIED BY '$dbOHRMPassword'": '';
-		
-		
+
+
       	$query = <<< USRSQL
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, INDEX
 ON `$dbName`.*
@@ -99,11 +99,11 @@ USRSQL;
          		return;
       		}
 		}
-      
+
 	  	$dbName = $_SESSION['dbInfo']['dbName'];
 	  	$dbOHRMUser = $_SESSION['dbInfo']['dbOHRMUserName'];
 		$dbOHRMPassword = $_SESSION['dbInfo']['dbOHRMPassword'];
-	
+
       	$query = <<< USRSQL
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, INDEX
 ON `$dbName`.*
@@ -120,7 +120,7 @@ USRSQL;
 set password for "$dbOHRMUser"@"%"
  = old_password('$dbOHRMPassword');
 USRSQL;
-		
+
 		if (isset($dbOHRMPassword) && ($dbOHRMPassword !== '')) {
       		if(!@mysql_query($query)) {
          		$_SESSION['error'] = mysql_error() or die();
@@ -128,18 +128,18 @@ USRSQL;
       		}
 		}
 	}
-	
+
 }
 
 function createUser() {
 
 	connectDB();
-			
+
 	if(!@mysql_select_db($_SESSION['dbInfo']['dbName'])) {
 		$_SESSION['error'] = 'Unable to access OrangeHRM Database!';
 		return;
 	}
-								
+
 	$query = "INSERT INTO `hs_hr_users` VALUES ('USR001','" .$_SESSION['defUser']['AdminUserName']. "','".md5($_SESSION['defUser']['AdminPassword'])."','Admin','',null,'','Yes','1','','0000-00-00 00:00:00','0000-00-00 00:00:00',null,null,'','','','','','','','','','Enabled','','','','','','',0,'','USG001')";
 
 	if(!mysql_query($query)) {
@@ -154,11 +154,11 @@ function writeConfFile() {
 	$dbHost = $_SESSION['dbInfo']['dbHostName'];
 	$dbHostPort = $_SESSION['dbInfo']['dbHostPort'];
 	$dbName = $_SESSION['dbInfo']['dbName'];
-							  
+
 	if(isset($_SESSION['dbInfo']['dbOHRMUserName'])) {
 		$dbOHRMUser = $_SESSION['dbInfo']['dbOHRMUserName'];
 		$dbOHRMPassword = $_SESSION['dbInfo']['dbOHRMPassword'];
-	} else {	
+	} else {
 		$dbOHRMUser = $_SESSION['dbInfo']['dbUserName'];
 		$dbOHRMPassword = $_SESSION['dbInfo']['dbPassword'];
 	}
@@ -175,125 +175,125 @@ class Conf {
 	var \$version;
 
 	function Conf() {
-		
+
 		\$this->dbhost	= '$dbHost';
 		\$this->dbport 	= '$dbHostPort';
 		\$this->dbname	= '$dbName';
 		\$this->dbuser	= '$dbOHRMUser';
 		\$this->dbpass	= '$dbOHRMPassword';
-		\$this->version = '2.1_alpha_4';		
+		\$this->version = '2.1_alpha_6';
 	}
 }
 ?>
 CONFCONT;
-						      
+
 	$filename = ROOT_PATH . '/lib/confs/Conf.php';
 	$handle = fopen($filename, 'w');
 	fwrite($handle, $confContent);
-	 
+
     fclose($handle);
 
 }
 
 function writeLog() {
 	$Content = "Client Info\n\n";
-	
+
 	$Content .= "User Agent : ".$_SERVER['HTTP_USER_AGENT']."\n";
 	$Content .= "Remote Address : ".$_SERVER['REMOTE_ADDR']."\n\n";
-	
+
 	$Content .= "Server Info\n\n";
-	$Content .= "Host : ".$_SERVER['HTTP_HOST']."\n";	
+	$Content .= "Host : ".$_SERVER['HTTP_HOST']."\n";
 	$Content .= "PHP Version : ".constant('PHP_VERSION')."\n";
 	$Content .= "Server : ".$_SERVER['SERVER_SOFTWARE']."\n";
 	$Content .= "Admin : ".$_SERVER['SERVER_ADMIN']."\n\n";
-	
+
 	$Content .= "Document Root : ".$_SERVER['DOCUMENT_ROOT']."\n";
 	$Content .= "ROOT_PATH : ".ROOT_PATH."\n\n";
-	
+
 	$Content .= "OrangeHRM Installation Log\n\n";
-	
+
 	$filename = 'installer/log.txt';
 	$handle = fopen($filename, 'w');
 	fwrite($handle, $Content);
 	fclose($handle);
-}		
-   
+}
+
    if (isset($_SESSION['INSTALLING'])) {
-	switch ($_SESSION['INSTALLING']) {		
+	switch ($_SESSION['INSTALLING']) {
 		case 0	:	writeLog();
 					error_log (date("r")." DB Creation - Starting\n",3, "installer/log.txt");
 					createDB();
 					error_log (date("r")." DB Creation - Done\n",3, "installer/log.txt");
-					if (!isset($error) || !isset($_SESSION['error'])) {	
+					if (!isset($error) || !isset($_SESSION['error'])) {
 						$_SESSION['INSTALLING'] = 1;
-						error_log (date("r")." DB Creation - No Errors\n",3, "installer/log.txt");																										
+						error_log (date("r")." DB Creation - No Errors\n",3, "installer/log.txt");
 					} else {
 						error_log (date("r")." DB Creation - Errors\n",3, "installer/log.txt");
 						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");
 					}
-					
+
 					break;
-								
-		case 1	:	error_log (date("r")." Fill Data Phase 1 - Starting\n",3, "installer/log.txt");		
+
+		case 1	:	error_log (date("r")." Fill Data Phase 1 - Starting\n",3, "installer/log.txt");
 					fillData();
-					error_log (date("r")." Fill Data Phase 1 - Done\n",3, "installer/log.txt");		
-					if (!isset($error) || !isset($_SESSION['error'])) {								
+					error_log (date("r")." Fill Data Phase 1 - Done\n",3, "installer/log.txt");
+					if (!isset($error) || !isset($_SESSION['error'])) {
 						$_SESSION['INSTALLING'] = 2;
-						error_log (date("r")." Fill Data Phase 1 - No Errors\n",3, "installer/log.txt");		
+						error_log (date("r")." Fill Data Phase 1 - No Errors\n",3, "installer/log.txt");
 					} else {
-						error_log (date("r")." Fill Data Phase 1 - Errors\n",3, "installer/log.txt");		
+						error_log (date("r")." Fill Data Phase 1 - Errors\n",3, "installer/log.txt");
 						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");
-					}																				
+					}
 					break;
-					
-		case 2	:	error_log (date("r")." Fill Data Phase 2 - Starting\n",3, "installer/log.txt");		
+
+		case 2	:	error_log (date("r")." Fill Data Phase 2 - Starting\n",3, "installer/log.txt");
 					fillData(2);
-					error_log (date("r")." Fill Data Phase 2 - Done\n",3, "installer/log.txt");		
-					if (!isset($error) || !isset($_SESSION['error'])) {								
+					error_log (date("r")." Fill Data Phase 2 - Done\n",3, "installer/log.txt");
+					if (!isset($error) || !isset($_SESSION['error'])) {
 						$_SESSION['INSTALLING'] = 3;
-						error_log (date("r")." Fill Data Phase 2 - No Errors\n",3, "installer/log.txt");		
+						error_log (date("r")." Fill Data Phase 2 - No Errors\n",3, "installer/log.txt");
 					} else {
-						error_log (date("r")." Fill Data Phase 2 - Errors\n",3, "installer/log.txt");		
+						error_log (date("r")." Fill Data Phase 2 - Errors\n",3, "installer/log.txt");
 						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");
-					}																				
+					}
 					break;
-					
-		case 3	:	error_log (date("r")." Create DB user - Starting\n",3, "installer/log.txt");		
+
+		case 3	:	error_log (date("r")." Create DB user - Starting\n",3, "installer/log.txt");
 					createDBUser();
-					error_log (date("r")." Create DB user - Done\n",3, "installer/log.txt");		
+					error_log (date("r")." Create DB user - Done\n",3, "installer/log.txt");
 					if (!isset($error) || !isset($_SESSION['error'])) {
 						$_SESSION['INSTALLING'] = 4;
-						error_log (date("r")." Create DB user - No Errors\n",3, "installer/log.txt");		
+						error_log (date("r")." Create DB user - No Errors\n",3, "installer/log.txt");
 					} else {
-						error_log (date("r")." Create DB user - Errors\n",3, "installer/log.txt");	
-						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");	
-					}												
+						error_log (date("r")." Create DB user - Errors\n",3, "installer/log.txt");
+						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");
+					}
 					break;
-									
-		case 4	:	error_log (date("r")." Create OrangeHRM user - Starting\n",3, "installer/log.txt");		
+
+		case 4	:	error_log (date("r")." Create OrangeHRM user - Starting\n",3, "installer/log.txt");
 					createUser();
 					error_log (date("r")." Create OrangeHRM user - Done\n",3, "installer/log.txt");
 					if (!isset($error) || !isset($_SESSION['error'])) {
 						$_SESSION['INSTALLING'] = 5;
 						error_log (date("r")." Create OrangeHRM user - No Errors\n",3, "installer/log.txt");
 					} else {
-						error_log (date("r")." Create OrangeHRM user - Errors\n",3, "installer/log.txt");	
+						error_log (date("r")." Create OrangeHRM user - Errors\n",3, "installer/log.txt");
 						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");
-					}											
+					}
 					break;
-								
+
 		case 5 :	error_log (date("r")." Write Conf - Starting\n",3, "installer/log.txt");
 					writeConfFile();
 					error_log (date("r")." Write Conf - Done\n",3, "installer/log.txt");
 					if (!isset($error) || !isset($_SESSION['error'])) {
-						$_SESSION['INSTALLING'] = 6;	
-						error_log (date("r")." Write Conf - No Errors\n",3, "installer/log.txt");															
+						$_SESSION['INSTALLING'] = 6;
+						error_log (date("r")." Write Conf - No Errors\n",3, "installer/log.txt");
 					} else {
 						error_log (date("r")." Write Conf - Errors\n",3, "installer/log.txt");
 						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "installer/log.txt");
 					}
-					break;						
-		
+					break;
+
 	}
   }
 ?>
--- php/orangehrm/upgrader/restore/restoringData.php
+++ php/orangehrm/upgrader/restore/restoringData.php
@@ -3,7 +3,7 @@ if(!isset($_SESSION))
 	session_start();
 if (isset($_SESSION['error'])) {
 	unset($_SESSION['error']);
-}	
+}
 
 unset($error);
 
@@ -12,22 +12,22 @@ require_once ROOT_PATH.'/upgrader/restore/Restore.php';
 require_once ROOT_PATH.'/upgrader/backup/Backup.php';
 
 function createDB() {
-	
-	connectDB();							
+
+	connectDB();
 	mysql_query("CREATE DATABASE " . $_SESSION['dbInfo']['dbName']);
-	
+
 	if(!@mysql_select_db($_SESSION['dbInfo']['dbName'])) {
 		$_SESSION['error'] = 'Unable to create Database!';
 		return;
-	}								
-								
-								
+	}
+
+
 }
 
 function connectDB() {
 
 	if(!$connect = @mysql_connect($_SESSION['dbInfo']['dbHostName'].':'.$_SESSION['dbInfo']['dbHostPort'], 		$_SESSION['dbInfo']['dbUserName'], $_SESSION['dbInfo']['dbPassword'])) {
-		$_SESSION['error'] =  'Database Connection Error!';		
+		$_SESSION['error'] =  'Database Connection Error!';
 		return false;
 	}
 	return $connect;
@@ -38,18 +38,18 @@ function alterOldData() {
 	$sqlQString = "UPDATE `hs_hr_employee` SET `employee_id` = `emp_number` WHERE `employee_id` IS NULL";
 
 	$res = mysql_query($sqlQString);
-	
+
 	$err = mysql_errno();
 	error_log (date("r")." Alter Old Data failed  with $err\n",3, "log.txt");
-	
+
 	if ($res) {
 		return true;
 	}
-	
+
 	if (empty($err)) {
-		return true;	
-	}	
-	
+		return true;
+	}
+
 	return $err;
 }
 
@@ -57,41 +57,41 @@ function alterOldData() {
 function fillData($phase=1, $source='/dbscript/dbscript-u') {
 	$source .= $phase.'.sql';
 	connectDB();
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Connected to the DB Server\n",3, "log.txt");
-	
+
 	if(!mysql_select_db($_SESSION['dbInfo']['dbName'])) {
 		$_SESSION['error'] = 'Unable to create Database!';
 		error_log (date("r")." Fill Data Phase $phase - Error - Unable to create Database\n",3, "log.txt");
 		return;
 	}
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Selected the DB\n",3, "log.txt");
 	error_log (date("r")." Fill Data Phase $phase - Reading DB Script\n",3, "log.txt");
-	
+
 	$queryFile = ROOT_PATH . $source;
 	$fp    = fopen($queryFile, 'r');
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Opened DB Script\n",3, "log.txt");
-	
+
 	$query = fread($fp, filesize($queryFile));
 	fclose($fp);
-	
+
 	error_log (date("r")." Fill Data Phase $phase - Read DB script\n",3, "log.txt");
-								
+
 	$dbScriptStatements = explode(";", $query);
-	
+
 	error_log (date("r")." Fill Data Phase $phase - There are ".count($dbScriptStatements)." Statements in the DB script\n",3, "log.txt");
-								
+
 	for($c=0;(count($dbScriptStatements)-1)>$c;$c++)
-		if(!@mysql_query($dbScriptStatements[$c])) {  
+		if(!@mysql_query($dbScriptStatements[$c])) {
 			$_SESSION['error'] = mysql_error();
 			$error = mysql_error();
 			error_log (date("r")." Fill Data Phase $phase - Error Statement # $c \n",3, "log.txt");
 			error_log (date("r")." ".$dbScriptStatements[$c]."\n",3, "log.txt");
 			return;
 		}
-									
+
 	if(isset($error))
 		return;
 }
@@ -101,11 +101,11 @@ function writeConfFile() {
 	$dbHost = $_SESSION['dbInfo']['dbHostName'];
 	$dbHostPort = $_SESSION['dbInfo']['dbHostPort'];
 	$dbName = $_SESSION['dbInfo']['dbName'];
-							  
+
 	if(isset($_SESSION['dbInfo']['dbOHRMUserName'])) {
 		$dbOHRMUser = $_SESSION['dbInfo']['dbOHRMUserName'];
 		$dbOHRMPassword = $_SESSION['dbInfo']['dbOHRMPassword'];
-	} else {	
+	} else {
 		$dbOHRMUser = $_SESSION['dbInfo']['dbUserName'];
 		$dbOHRMPassword = $_SESSION['dbInfo']['dbPassword'];
 	}
@@ -122,23 +122,23 @@ class Conf {
 	var \$dbpass;
 
 	function Conf() {
-		
+
 	\$this->dbhost	= '$dbHost';
 	\$this->dbport 	= '$dbHostPort';
 	\$this->dbname	= '$dbName';
 	\$this->dbuser	= '$dbOHRMUser';
 	\$this->dbpass	= '$dbOHRMPassword';
-	\$this->version	= '2.1_alpha_4';
+	\$this->version	= '2.1_alpha_6';
 	\$this->upgrade	= true;
 	}
 }
 ?>
 CONFCONT;
-						      
+
 	$filename = ROOT_PATH . '/lib/confs/Conf.php';
 	$handle = fopen($filename, 'w');
 	fwrite($handle, $confContent);
-	 
+
     fclose($handle);
 
 }
@@ -146,21 +146,21 @@ CONFCONT;
 
 function writeLog() {
 	$Content = "Client Info\n\n";
-	
+
 	$Content .= "User Agent : ".$_SERVER['HTTP_USER_AGENT']."\n";
 	$Content .= "Remote Address : ".$_SERVER['REMOTE_ADDR']."\n\n";
-	
+
 	$Content .= "Server Info\n\n";
-	$Content .= "Host : ".$_SERVER['HTTP_HOST']."\n";	
+	$Content .= "Host : ".$_SERVER['HTTP_HOST']."\n";
 	$Content .= "PHP Version : ".constant('PHP_VERSION')."\n";
 	$Content .= "Server : ".$_SERVER['SERVER_SOFTWARE']."\n";
 	$Content .= "Admin : ".$_SERVER['SERVER_ADMIN']."\n\n";
-	
+
 	$Content .= "Document Root : ".$_SERVER['DOCUMENT_ROOT']."\n";
 	$Content .= "ROOT_PATH : ".ROOT_PATH."\n\n";
-	
+
 	$Content .= "OrangeHRM Upgrading Log\n\n";
-	
+
 	$filename = 'upgrader/log.txt';
 	$handle = fopen($filename, 'a');
 	fwrite($handle, $Content);
@@ -168,31 +168,31 @@ function writeLog() {
 }
 
 if (isset($_SESSION['RESTORING'])) {
-	
+
 	$conn = connectDB();
-	
-	if ($conn)	
-	switch ($_SESSION['RESTORING']) {	
+
+	if ($conn)
+	switch ($_SESSION['RESTORING']) {
 		case -1 : $db = mysql_select_db($_SESSION['dbInfo']['dbName']);
 					if ($db) {
-						
+
 						$dump = new Backup();
 						$connec = connectDB();
 						$dump->setConnection($conn);
 						$dump->setDatabase($_SESSION['dbInfo']['dbName']);
 						$_SESSION['DATABASE_BACKUP']=$dump->dumpDatabase(true);
-						
-						$_SESSION['DATABASE_CONSTRAINTS']=$dump->getConstraints();					
-						
+
+						$_SESSION['DATABASE_CONSTRAINTS']=$dump->getConstraints();
+
 						error_log (date("r")." Going to drop existing database- \n",3, "log.txt");
-										  
+
 						@mysql_query('DROP DATABASE `'.$_SESSION['dbInfo']['dbName']."`");
-										  
+
 						error_log (date("r")."database ".$_SESSION['dbInfo']['dbName']." is droped".mysql_errno()."- \n",3, "log.txt");
-										 
+
 						$_SESSION['RESTORING'] = 0;
 				    };
-				    
+
 		case 0	:	$db = mysql_select_db($_SESSION['dbInfo']['dbName']);
 					writeLog();
 					error_log (date("r")." DB ".$_SESSION['dbInfo']['dbName']." selected".$db." - Starting\n",3, "log.txt");
@@ -200,46 +200,46 @@ if (isset($_SESSION['RESTORING'])) {
 						error_log (date("r")." DB Creation - Starting\n",3, "log.txt");
 						createDB();
 						error_log (date("r")." DB Creation - Done\n",3, "log.txt");
-						if (!isset($error) || !isset($_SESSION['error'])) {	
+						if (!isset($error) || !isset($_SESSION['error'])) {
 							$_SESSION['RESTORING'] = 1;
-							error_log (date("r")." DB Creation - No Errors\n",3, "log.txt");																										
+							error_log (date("r")." DB Creation - No Errors\n",3, "log.txt");
 						} else {
 							error_log (date("r")." DB Creation - Errors\n",3, "log.txt");
 							error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "log.txt");
 						}
-					} 
+					}
 				   error_log (date("r")." Next step".$_SESSION['RESTORING']." - Starting\n",3, "log.txt");
 					break;
-		case 1 	:	error_log (date("r")." Fill Data Phase 1 - Starting\n",3, "log.txt");		
+		case 1 	:	error_log (date("r")." Fill Data Phase 1 - Starting\n",3, "log.txt");
 					fillData();
-					error_log (date("r")." Fill Data Phase 1 - Done\n",3, "log.txt");		
-					if (!isset($error) || !isset($_SESSION['error'])) {								
+					error_log (date("r")." Fill Data Phase 1 - Done\n",3, "log.txt");
+					if (!isset($error) || !isset($_SESSION['error'])) {
 						$_SESSION['RESTORING'] = 2;
-						error_log (date("r")." Fill Data Phase 1 - No Errors\n",3, "log.txt");		
+						error_log (date("r")." Fill Data Phase 1 - No Errors\n",3, "log.txt");
 					} else {
-						error_log (date("r")." Fill Data Phase 1 - Errors\n",3, "log.txt");		
+						error_log (date("r")." Fill Data Phase 1 - Errors\n",3, "log.txt");
 						error_log (date("r")." ".(isset($error)? $error: $_SESSION['error'])."\n",3, "log.txt");
-					}																				
-					break;			
-	    
+					}
+					break;
+
 		case 2	:	error_log (date("r")." Getting the file content  - \n",3, "log.txt");
 					error_log (date("r")." File content ok  - \n",3, "log.txt");
 					$restorex = new Restore();
-					//$connection = mysql_connect($_SESSION['dbInfo']['dbHostName'], $_SESSION['dbInfo']['dbUserName'], $_SESSION['dbInfo']['dbPassword']);	 			//						
-					//mysql_close();					
+					//$connection = mysql_connect($_SESSION['dbInfo']['dbHostName'], $_SESSION['dbInfo']['dbUserName'], $_SESSION['dbInfo']['dbPassword']);	 			//
+					//mysql_close();
 					$restorex->setConnection($conn);
 					$restorex->setDatabase($_SESSION['dbInfo']['dbName']);
 					$restorex->setFileSource($_SESSION['FILEDUMP']);
 					error_log (date("r")." Fill Data  - Starting\n",3, "log.txt");
-					$res = $restorex->fillDatabase();				
-					if ($res) {	
+					$res = $restorex->fillDatabase();
+					if ($res) {
 						$_SESSION['RESTORING'] = 3;
 						error_log (date("r")." Fill Data - Finished \n",3, "log.txt");
-						
+
 					} else {
 						$_SESSION['error'] = mysql_error();
 						error_log (date("r")." Fill Data - Failed \n",3, "log.txt");
-						error_log (date("r")." Fill Data - Error \n ".mysql_error()."\n" ,3, "log.txt");						
+						error_log (date("r")." Fill Data - Error \n ".mysql_error()."\n" ,3, "log.txt");
 					}
 					break;
 		case 3 	:	fillData(2);
@@ -252,6 +252,6 @@ if (isset($_SESSION['RESTORING'])) {
 					break;
 	}
 }
-									
-	    	
+
+
 ?>
