a13e0b5e5d97cc539c66f01ccc0b8fff0ecdc25f
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-03-28 12:14:46 +0000

    New module rights

--- php/orangehrm/dbscript/dbscript-2.sql
+++ php/orangehrm/dbscript/dbscript-2.sql
@@ -505,9 +505,17 @@ INSERT INTO `hs_hr_user_group` VALUES ('USG001','Admin','1');
 INSERT INTO `hs_hr_db_version` VALUES ('DVR001','mysql4.1','initial DB','2005-10-10 00:00:00','2005-12-20 00:00:00',null,null);
 INSERT INTO `hs_hr_file_version` VALUES ('FVR001',NULL,'Release 1','2006-03-15 00:00:00','2006-03-15 00:00:00',null,null,'file_ver_01');
 INSERT INTO `hs_hr_versions` VALUES ('VER001','Release 1','2006-03-15 00:00:00','2006-03-15 00:00:00',null,null,0,'DVR001','FVR001','version 1.0');
-INSERT INTO `hs_hr_module` VALUES ('MOD001','Admin','Koshika','koshika@beyondm.net','VER001','HR Admin'),('MOD002','PIM','Koshika','koshika@beyondm.net','VER001','HR Functions'),('MOD004','Report','Koshika','koshika@beyondm.net','VER001','Reporting'),('MOD005', 'Leave', 'Mohanjith', 'mohanjith@beyondm.net', 'VER001', 'Leave Tracking');
+INSERT INTO `hs_hr_module` VALUES ('MOD001','Admin','Koshika','koshika@beyondm.net','VER001','HR Admin'),
+								  ('MOD002','PIM','Koshika','koshika@beyondm.net','VER001','HR Functions'),
+								  ('MOD004','Report','Koshika','koshika@beyondm.net','VER001','Reporting'),
+								  ('MOD005', 'Leave', 'Mohanjith', 'mohanjith@beyondm.net', 'VER001', 'Leave Tracking'),
+								  ('MOD006', 'Time', 'Mohanjith', 'mohanjith@orangehrm.com', 'VER001', 'Time Tracking');
 INSERT INTO `hs_hr_rights` ( `userg_id` , `mod_id` , `addition` , `editing` , `deletion` , `viewing` )
-VALUES ('USG001', 'MOD001', '1', '1', '1', '1'), ('USG001', 'MOD002', '1', '1', '1', '1'), ('USG001', 'MOD004', '1', '1', '1', '1');
+VALUES  ('USG001', 'MOD001', '1', '1', '1', '1'),
+		('USG001', 'MOD002', '1', '1', '1', '1'),
+		('USG001', 'MOD004', '1', '1', '1', '1'),
+		('USG001', 'MOD005', '1', '1', '1', '1'),
+		('USG001', 'MOD006', '1', '1', '1', '1');
 INSERT INTO `hs_hr_compstructtree` VALUES ('', 'Parent Company', null , 1, 2, 1, 0);
 
 INSERT INTO `hs_hr_leavetype` VALUES ('LTY001', 'Casual', 1);
