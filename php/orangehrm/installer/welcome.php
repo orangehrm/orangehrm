5b99977d27e8697089dc574d8b138cf907b30ddf
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-04-17 03:01:49 +0000

    update version numbers to 2.2_beta_2

--- php/orangehrm/installer/welcome.php
+++ php/orangehrm/installer/welcome.php
@@ -5,12 +5,12 @@ function welcomeSubmit() {
 }
 </script>
 	<div id="content">
-		<h2>Welcome to the OrangeHRM ver 2.1 Setup Wizard</h2>
+		<h2>Welcome to the OrangeHRM ver 2.2 Setup Wizard</h2>
    
       
 		<p>This installer creates the OrangeHRM database tables and sets the
         configuration files that you need to start.</p>
-        <p><b>N.B.</b><br/>If you have OrangeHRM 1.2 or OrangeHRM 2.0 already running and would like to upgrade to OrangeHRM 2.1, please click <a href="../upgrade.php">here</a>.</p>
+        <p><b>N.B.</b><br/>If you have OrangeHRM 1.2 or OrangeHRM 2.0 already running and would like to upgrade to OrangeHRM 2.2, please click <a href="../upgrade.php">here</a>.</p>
         <p>
 		Click <b>[Next]</b> to Start the Wizard.</p>
         <input class="button" type="button" value="Back" onclick="back();" disabled="disabled">
