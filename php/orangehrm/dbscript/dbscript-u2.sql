0355589c6cd01241ac273950839519cb242b74f5
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-06-14 06:09:10 +0000

    EST000 duplicate

--- php/orangehrm/dbscript/dbscript-u2.sql
+++ php/orangehrm/dbscript/dbscript-u2.sql
@@ -270,7 +270,8 @@ alter table `hs_hr_time_event`
 
 INSERT INTO `hs_hr_empstat`
   (`estat_code`, `estat_name`)
-  VALUES ('EST000', 'Terminated');
+  VALUES ('EST000', 'Terminated')
+  ON DUPLICATE KEY UPDATE `estat_name`='Terminated';
 
 INSERT INTO `hs_hr_jobtit_empstat`
   (`jobtit_code`, `estat_code`)
