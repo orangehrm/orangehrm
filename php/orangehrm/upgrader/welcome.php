d221978fdec1d8f12987ea5d0cdacaf491f3bf02
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-06-25 14:33:19 +0000

    Updated version number from 2.1 to 2.2

--- php/orangehrm/upgrader/welcome.php
+++ php/orangehrm/upgrader/welcome.php
@@ -27,9 +27,9 @@ function welcomeSubmit() {
 	<div id="content">
 		<h2>Welcome to the OrangeHRM Web Upgrader Wizard</h2>
 
-		<p>This upgrader upgrades your exsisting OrangeHRM 1.2/OrangeHRM 2.0 database to run OrangeHRM 2.1 and set the configuration files that you need to start using OrangeHRM 2.1.</p>
+		<p>This upgrader upgrades your exsisting OrangeHRM 1.2/OrangeHRM 2.0 database to run OrangeHRM 2.2 and set the configuration files that you need to start using OrangeHRM 2.2.</p>
 
-        <p><b>N.B.</b><br/>If you don't have OrangeHRM 1.2/OrangeHRM 2.0 running already you should install OrangeHRM 2.1, please click <a href="../install.php">here</a> to do so.</p>
+        <p><b>N.B.</b><br/>If you don't have OrangeHRM 1.2/OrangeHRM 2.0 running already you should install OrangeHRM 2.2, please click <a href="../install.php">here</a> to do so.</p>
         <p>
 	  Click <b>[Next]</b> to Start the Wizard.</p>
         <input class="button" type="button" value="Back" onclick="back();" disabled="disabled">
