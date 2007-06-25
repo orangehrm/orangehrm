0cf31f2c524e1f6da897bb1c55a2ea899e691de2
mohanjith <mohanjith@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-06-25 17:31:28 +0000

    Version change

--- php/orangehrm/upgrade.php
+++ php/orangehrm/upgrade.php
@@ -21,7 +21,7 @@
 
 function needToUpgrade() {
 
-	$currentVersion = '2.2_RC_3';
+	$currentVersion = '2.2_RC_4';
 
 	if (is_file(ROOT_PATH . '/lib/confs/Conf.php') && !isset($_SESSION['RESTORING'])) {
 
