ccab0f9ae967b0d6da3f248dffad8f5af975588a
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-07-16 05:07:03 +0000

    Undoing previous commit which checked in local settings in testConf.php

--- php/orangehrm/build/testConf.php
+++ php/orangehrm/build/testConf.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Change the following to match your environment
-	$rootPath = "/var/www/html/orangehrm";
+	$rootPath = "/var/www/orangehrm";
 	$webPath = "http://localhost/orangehrm";
 
 	if (!defined('ROOT_PATH')) {
