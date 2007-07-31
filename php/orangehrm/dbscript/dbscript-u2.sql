439644e3eeb73a3dc6fcac94a719b94f0485e05d
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2007-07-31 04:05:59 +0000

    r1220@localhost:  ruchira | 2007-07-31 09:24:45 +0530  When upgrading, add missing modules to hs_hr_module. Delete the maintenance module if it exists.

--- php/orangehrm/dbscript/dbscript-u2.sql
+++ php/orangehrm/dbscript/dbscript-u2.sql
@@ -288,3 +288,10 @@ INSERT INTO `hs_hr_jobtit_empstat`
   (`jobtit_code`, `estat_code`)
   SELECT `jobtit_code`, 'EST000' FROM `hs_hr_job_title`
   ON DUPLICATE KEY UPDATE `estat_code`='EST000';
+
+DELETE FROM `hs_hr_module` WHERE mod_id = 'MOD003';
+INSERT IGNORE INTO `hs_hr_module` VALUES ('MOD001','Admin','Koshika','koshika@beyondm.net','VER001','HR Admin');
+INSERT IGNORE INTO `hs_hr_module` VALUES ('MOD002','PIM','Koshika','koshika@beyondm.net','VER001','HR Functions');
+INSERT IGNORE INTO `hs_hr_module` VALUES ('MOD004','Report','Koshika','koshika@beyondm.net','VER001','Reporting');
+INSERT IGNORE INTO `hs_hr_module` VALUES ('MOD005', 'Leave', 'Mohanjith', 'mohanjith@beyondm.net', 'VER001', 'Leave Tracking');
+INSERT IGNORE INTO `hs_hr_module` VALUES ('MOD006', 'Time', 'Mohanjith', 'mohanjith@orangehrm.com', 'VER001', 'Time Tracking');
