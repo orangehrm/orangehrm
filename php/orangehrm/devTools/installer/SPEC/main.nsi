b1275332362cbf64fd08be70963dfd5392a31ff4
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2008-04-18 03:55:07 +0000

    Changed the version to 2.3-alpha.6

--- php/orangehrm/devTools/installer/SPEC/main.nsi
+++ php/orangehrm/devTools/installer/SPEC/main.nsi
@@ -30,7 +30,7 @@
 ; Product Details
 
   !define ProductName "OrangeHRM"
-  !define ProductVersion "2.3-alpha.5"
+  !define ProductVersion "2.3-alpha.6"
 
   !define Organization "OrangeHRM Inc."
 
--- php/orangehrm/upgrade.php
+++ php/orangehrm/upgrade.php
@@ -21,7 +21,7 @@
 
 function needToUpgrade() {
 
-	$currentVersion = '2.3-alpha.5';
+	$currentVersion = '2.3-alpha.6';
 
 	if (is_file(ROOT_PATH . '/lib/confs/Conf.php') && !isset($_SESSION['RESTORING'])) {
 
