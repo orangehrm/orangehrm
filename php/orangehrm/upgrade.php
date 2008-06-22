7e8f150e04988d6673e2906fbb93f51b534377f2
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2008-06-22 19:46:24 +0000

    Updated version number to 2.3-beta.3

--- php/orangehrm/upgrade.php
+++ php/orangehrm/upgrade.php
@@ -21,7 +21,7 @@
 
 function needToUpgrade() {
 
-	$currentVersion = '2.3-beta.2';
+	$currentVersion = '2.3-beta.3';
 
 	if (is_file(ROOT_PATH . '/lib/confs/Conf.php') && !isset($_SESSION['RESTORING'])) {
 
--- php/orangehrm/upgrader/welcome.php
+++ php/orangehrm/upgrader/welcome.php
@@ -27,9 +27,9 @@ function welcomeSubmit() {
 	<div id="content">
 		<h2>Welcome to the OrangeHRM Web Upgrader Wizard</h2>
 
-		<p>This upgrader upgrades your exsisting OrangeHRM database to run OrangeHRM 2.3-beta.2 and set the configuration files that you need to start using OrangeHRM 2.3-beta.2</p>
+		<p>This upgrader upgrades your exsisting OrangeHRM database to run OrangeHRM 2.3-beta.3 and set the configuration files that you need to start using OrangeHRM 2.3-beta.3</p>
 
-        <p><b>N.B.</b><br/>If you don't have previous versions of OrangeHRM running already you should install OrangeHRM 2.3-beta.2, please click <a href="../install.php">here</a> to do so.</p>
+        <p><b>N.B.</b><br/>If you don't have previous versions of OrangeHRM running already you should install OrangeHRM 2.3-beta.3, please click <a href="../install.php">here</a> to do so.</p>
         <p>
 	  Click <b>[Next]</b> to Start the Wizard.</p>
         <input class="button" type="button" value="Back" onclick="back();" disabled="disabled">
