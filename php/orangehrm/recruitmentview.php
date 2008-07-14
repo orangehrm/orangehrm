342c9d1cb2a474701a344927fd519af07a6df88d
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2008-07-14 07:32:17 +0000

    Introduced <div> and <span> tags for layout formatting instead of HTML tables & changed the code in the file recruitmentview.php; Display errors in Firefox 3 is resolved

--- php/orangehrm/recruitmentview.php
+++ php/orangehrm/recruitmentview.php
@@ -81,16 +81,30 @@ if (isset($this->postArr['loc_code'])) {
 
 $themeDir = '../../themes/' . $styleSheet;
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link href="<?php echo $themeDir;?>/css/style.css" rel="stylesheet" type="text/css">
 <style type="text/css">@import url("<?php echo $themeDir;?>/css/style.css"); </style>
+<style type="text/css">
+
+    .roundbox {
+        margin-top: 10px;
+        margin-left: 0px;
+        width: 98%;
+    }
+
+    .roundbox_content {
+        padding:15px;
+    }
+
+</style>
 <title></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <script type="text/javascript" src="../../scripts/archive.js"></script>
+<script type="text/javascript" src="../../scripts/octopus.js"></script>
 </head>
-<script>
+<script type="text/javascript">
 
 	// Maps to allow searching of mapped values
     var maps = new Array();
@@ -230,219 +244,203 @@ for ($i = 0; $i < count($valueMap); $i++) {
 	}
 </script>
 <body>
-<p>
-<table width='100%' cellpadding='0' cellspacing='0' border='0' class='moduleTitle'><tr><td valign='top'>
 <form name="standardView" id="standardView" method="post" action="<?php echo $baseURL;?>&action=<?php echo $this->getArr['action'];?>&sortField=<?php echo $this->getArr['sortField']?>&sortOrder<?php echo $this->getArr['sortField']?>=<?php echo $this->getArr['sortOrder'.$this->getArr['sortField']];?>">
-  </td>
-  <td width='100%'><h2>
-      <?php echo $title?>
-    </h2></td>
-  <td valign='top' align='right' nowrap style='padding-top:3px; padding-left: 5px;'></td></tr>
-</table></p>
-</p>
-<p>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-  <tr>
-    <td width="22%" nowrap><h3>
-        <input type="hidden" name="captureState" value="<?php echo isset($this->postArr['captureState'])?$this->postArr['captureState']:''?>">
+	<div class="moduleTitle" style="padding: 6px;"><h2><?php echo$title; ?></h2></div>
+	<div>
+		<input type="hidden" name="captureState" value="<?php echo isset($this->postArr['captureState'])?$this->postArr['captureState']:''?>" />
+		<input type="hidden" name="pageNO" value="<?php echo isset($this->postArr['pageNO'])?$this->postArr['pageNO']:'1'?>" />
+		<div style="padding: 6px;">
+		<?php	if($allowAdd) { ?>
+		<img 
+			style="border: none" 
+			title="Add" 
+			alt="Add" 
+			src="<?php echo $themeDir;?>/pictures/btn_add.gif" 
+			onclick="returnAdd();" 
+			onmouseout="this.src='<?php echo $themeDir;?>/pictures/btn_add.gif';" 
+			onmouseover="this.src='<?php echo $themeDir;?>/pictures/btn_add_02.gif';" />
+		<?php	} ?>
 
-        <input type="hidden" name="pageNO" value="<?php echo isset($this->postArr['pageNO'])?$this->postArr['pageNO']:'1'?>">
+		<?php if($allowDelete) { ?>
+		<img 
+			style="border: none" 
+	   		title="Delete" 
+	   		alt="Delete" 
+			src="<?php echo $themeDir;?>/pictures/btn_delete.gif"
+			onclick="returnDelete();" 
+			onmouseout="this.src='<?php echo $themeDir;?>/pictures/btn_delete.gif';" 
+			onmouseover="this.src='<?php echo $themeDir;?>/pictures/btn_delete_02.gif';" />
+		<?php } ?>
+		</div>
+	</div>
 
-<?php	if($allowAdd) { ?>
-        <img border="0" title="Add" onClick="returnAdd();" onMouseOut="this.src='<?php echo $themeDir;?>/pictures/btn_add.gif';" onMouseOver="this.src='<?php echo $themeDir;?>/pictures/btn_add_02.gif';" src="<?php echo $themeDir;?>/pictures/btn_add.gif">
-<?php	}
-
-if($allowDelete) {
-?>
-       <img title="Delete" onClick="returnDelete();" onMouseOut="this.src='<?php echo $themeDir;?>/pictures/btn_delete.gif';" onMouseOver="this.src='<?php echo $themeDir;?>/pictures/btn_delete_02.gif';" src="<?php echo $themeDir;?>/pictures/btn_delete.gif">
-<?php
-    }
-?>
-
-      </h3></td>
-    <td width='78%'><IMG height='1' width='1' src='../../pictures/blank.gif' alt=''></td>
-  </tr>
-</table>
-<p>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-  <tr>
-    <td width="22%" nowrap><h3><?php echo $search?></h3></td>
-    <td width='78%' align="right"><IMG height='1' width='1' src='../../pictures/blank.gif' alt=''>
-     <?php
-		if (isset($this->getArr['message'])) {
-
-			$expString = $this->getArr['message'];
-			$col_def = CommonFunctions::getCssClassForMessage($expString);
-	?>
-			<font class="<?php echo $col_def?>" size="-1" face="Verdana, Arial, Helvetica, sans-serif">
-	<?php
-				echo $$expString;
-	?>
-			</font>
-	<?php
-		}
-		?>
-      &nbsp;&nbsp;&nbsp;&nbsp;</td>
-  </tr>
-</table>
-
-
-      <table border="0" cellpadding="0" cellspacing="0" width="100%">
-        <tr>
-          <td width="13"><img name="table_r1_c1" src="<?php echo $themeDir;?>/pictures/table_r1_c1.gif" width="13" height="12" border="0" alt=""></td>
-          <td width="339" background="<?php echo $themeDir;?>/pictures/table_r1_c2.gif"><img name="table_r1_c2" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-          <td width="13"><img name="table_r1_c3" src="<?php echo $themeDir;?>/pictures/table_r1_c3.gif" width="13" height="12" border="0" alt=""></td>
-          <td width="11"><img src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="12" border="0" alt=""></td>
-        </tr>
-        <tr>
-          <td background="<?php echo $themeDir;?>/pictures/table_r2_c1.gif"><img name="table_r2_c1" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-          <td><table  border="0" cellpadding="5" cellspacing="0" class="">
-            <tr>
-              <td width="200" class="dataLabel"><slot><?php echo $SearchBy?></slot>&nbsp;&nbsp;<slot>
-                <select style="z-index: 99;" name="loc_code" id="loc_code">
-<?php               for($c=-1;count($srchlist)-1>$c;$c++) {
-					    if(isset($this->postArr['loc_code']) && $this->postArr['loc_code']==$c) {
-						   echo "<option selected value='" . $c ."'>".$srchlist[$c+1] ."</option>";
-					    } else {
-						   echo "<option value='" . $c ."'>".$srchlist[$c+1] ."</option>";
-					    }
-                    }
-?>
-                </select>
-              </slot></td>
-              <td width="300" class="dataLabel" noWrap><slot><?php echo $description?></slot>&nbsp;&nbsp;<slot>
-                <input type=text size="20" name="loc_name" id="loc_name" class=dataField  value="<?php echo $searchStr;?>">
-             </slot></td>
-            <td align="right" width="180" class="dataLabel"><img tabindex=3 title="Search" onClick="returnSearch();" onMouseOut="this.src='<?php echo $themeDir;?>/pictures/btn_search.gif';" onMouseOver="this.src='<?php echo $themeDir;?>/pictures/btn_search_02.gif';" src="<?php echo $themeDir;?>/pictures/btn_search.gif">&nbsp;&nbsp;<img title="Clear" onClick="clear_form();" onMouseOut="this.src='<?php echo $themeDir;?>/pictures/btn_clear.gif';" onMouseOver="this.src='<?php echo $themeDir;?>/pictures/btn_clear_02.gif';" src="<?php echo $themeDir;?>/pictures/btn_clear.gif"></td>
-
-          </table></td>
-          <td background="<?php echo $themeDir;?>/pictures/table_r2_c3.gif"><img name="table_r2_c3" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-          <td><img src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-        </tr>
-
-        <tr>
-          <td background="<?php echo $themeDir;?>/pictures/table_r2_c1.gif"><img name="table_r2_c1" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-          <td><table  border="0" cellpadding="5" cellspacing="0" class="">
-
-          </table></td>
-          <td background="<?php echo $themeDir;?>/pictures/table_r2_c3.gif"><img name="table_r2_c3" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-          <td><img src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-        </tr>
-        <tr>
-          <td><img name="table_r3_c1" src="<?php echo $themeDir;?>/pictures/table_r3_c1.gif" width="13" height="16" border="0" alt=""></td>
-          <td background="<?php echo $themeDir;?>/pictures/table_r3_c2.gif"><img name="table_r3_c2" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-          <td><img name="table_r3_c3" src="<?php echo $themeDir;?>/pictures/table_r3_c3.gif" width="13" height="16" border="0" alt=""></td>
-          <td><img src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="16" border="0" alt=""></td>
-        </tr>
-      </table>
-	  <table border="0" width="100%">
-	  <tr>
-	  <td height="40" valign="bottom" align="right">
-
-<?php
-$count = $this->popArr['count'];
-$commonFunc = new CommonFunctions();
-$pageStr = $commonFunc->printPageLinks($count, $currentPage);
-$pageStr = preg_replace(array('/#first/', '/#previous/', '/#next/', '/#last/'), array($lang_empview_first, $lang_empview_previous, $lang_empview_next, $lang_empview_last), $pageStr);
-
-echo $pageStr;
-
-	if (empty($list)) { ?>
-		<span ><?php echo $dispMessage; ?></span>
-<?php  } ?>
-		</td>
-		<td width="25"></td>
-		</tr>
-		</table>
-		<table width="100%" border="0" cellpadding="5" cellspacing="0" class="">
-		<thead>
-		<tr>
-          <td class="r1_c1" width="12"></td>
-          <td class="tableTopMiddle" width="50"></td>
-          <?php for ($j=0; $j < count($headings); $j++) {?>
-          <td width="200" class="tableTopMiddle"></td>
-          <?php } ?>
-          <td class="tableTopMiddle" width="300"></td>
-          <td class="tableTopRight"></td>
-         </tr>
-		 </thead>
-			<tr nowrap>
-				<td class="r2_c1"><img name="table_r2_c1" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-				<td width="50" NOWRAP class="listViewThS1" scope="col">
-				<?php	if($allowDelete) { ?>
-					<input type='checkbox' class='checkbox' name='allCheck' value='' onClick="doHandleAll();">
-				<?php	}	?>
-				</td>
+	<div style="width: 98%">
+		<table width="100%" cellpadding="0" cellspacing="0" border="0">
+			<tr>
+				<td width="22%" style="white-space: nowrap"><h3><?php echo $search?></h3></td>
+				<td width='78%' align="right">
 				<?php
-					for ($j=0; $j < count($headings); $j++) {
-						if (!isset($this->getArr['sortOrder'.$j])) {
-							$this->getArr['sortOrder'.$j] = 'null';
-						}
-						$nextSortOrder = getNextSortOrder($this->getArr['sortOrder'.$j]);
-						$nextSortInWords = getSortOrderInWords($nextSortOrder);
+					if (isset($this->getArr['message'])) {
+					
+						$expString  = $this->getArr['message'];
+						$col_def = CommonFunctions::getCssClassForMessage($expString);
 				?>
-				<td scope="col" width="250" class="listViewThS1">
-					<a href="#" onclick="sortAndSearch(<?php echo $j?>, '<?php echo $nextSortOrder;?>');"
-						title="Sort in <?php echo $nextSortInWords; ?> order"><?php echo $headings[$j]?></a>
-					<img src="<?php echo $themeDir;?>/icons/<?php echo $this->getArr['sortOrder'.$j]?>.png"
-					width="8" height="10" border="0" alt="" style="vertical-align: middle"></td>
+						<span class="<?php echo $col_def?>" style="font-family: Verdana, Arial, Helvetica, sans-serif;"><?php echo $$expString; ?></span>
+				<?php
+					}
+				?>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+	<div class="roundbox">
+		<table  border="0" cellpadding="5" cellspacing="0" class="">
+			<tr>
+				<td width="200" class="dataLabel">
+					<label for="loc_code" style="float: left; padding-right: 10px;"><?php echo $SearchBy?></label>
+					<select style="z-index: 99;" name="loc_code" id="loc_code">
+					<?php 
+						for($c=-1;count($srchlist)-1>$c;$c++) {
+							if(isset($this->postArr['loc_code']) && $this->postArr['loc_code']==$c) {
+								echo "<option selected value='" . $c ."'>".$srchlist[$c+1] ."</option>";
+							} else {
+								echo "<option value='" . $c ."'>".$srchlist[$c+1] ."</option>";
+							}
+						}
+					?>
+					</select>
+				</td>
+				<td width="300" class="dataLabel" style="white-space: nowrap">
+					<label for="loc_name" style="float: left; padding-right: 10px;"><?php echo $description?></label>
+					<input type="text" size="20" name="loc_name" id="loc_name" class="dataField"  value="<?php echo $searchStr;?>" />
+				</td>
+				<td align="right" width="180" class="dataLabel">
+					<img 
+						title="Search" 
+						alt="Search" 
+						src="<?php echo $themeDir;?>/pictures/btn_search.gif" 
+						onclick="returnSearch();" 
+						onmouseover="this.src='<?php echo $themeDir;?>/pictures/btn_search_02.gif';" 
+						onmouseout="this.src='<?php echo $themeDir;?>/pictures/btn_search.gif';" />
+					<img 
+						title="Clear" 
+						alt="Clear" 
+						src="<?php echo $themeDir;?>/pictures/btn_clear.gif" 
+						onclick="clear_form();" 
+						onmouseover="this.src='<?php echo $themeDir;?>/pictures/btn_clear_02.gif';" 
+						onmouseout="this.src='<?php echo $themeDir;?>/pictures/btn_clear.gif';" />
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+	<div style="padding-top: 4px; width: 98%">
+		<span id="messageDisplay">
+			<?php
+			if (empty($list)) { 
+					echo $dispMessage; 
+			} 
+			?>&nbsp;
+		</span>
+	</div>
+	
+	<div style="text-align: right; padding-top: 4px; width: 98%">
+		<?php
+		$temp = $this->popArr['count'];
+		$commonFunc = new CommonFunctions();
+		$pageStr = $commonFunc->printPageLinks($temp, $currentPage);
+		$pageStr = preg_replace(array('/#first/', '/#previous/', '/#next/', '/#last/'), array($lang_empview_first, $lang_empview_previous, $lang_empview_next, $lang_empview_last), $pageStr);
+		
+		echo $pageStr;
+		?>&nbsp;
+	</div>
+
+	<div class="roundbox">
+		<table width="100%" border="0" cellpadding="5" cellspacing="0" class="">
+		  <tr style="white-space: nowrap">
+			<td width="50" nowrap="nowrap" class="listViewThS1" scope="col">
+				<?php	if($allowDelete) { ?>
+					<input type='checkbox' class='checkbox' name='allCheck' value='' onclick="doHandleAll();" />
+				<?php	} else {	?>
+					&nbsp;
 				<?php } ?>
-      		<td class="listViewThS1"><img name="table_r2_c3" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-      		<td class="r2_c3"><img src="<?php echo $themeDir;?>/pictures/spacer.gif" width="13" height="1" border="0" alt=""></td>
-    		</tr>
-    		<?php
+			</td>
+			<?php
+				for ($j=0; $j < count($headings); $j++) {
+					if (!isset($this->getArr['sortOrder'.$j])) {
+						$this->getArr['sortOrder'.$j] = 'null';
+					}
+					$nextSortOrder = getNextSortOrder($this->getArr['sortOrder'.$j]);
+					$nextSortInWords = getSortOrderInWords($nextSortOrder);
+			?>
+					<td scope="col" width="250" class="listViewThS1">
+						<a 
+							href="#" 
+							onclick="sortAndSearch(<?php echo $j?>, '<?php echo $nextSortOrder;?>');" 
+							title="Sort in <?php echo $nextSortInWords; ?> order">
+								<?php echo $headings[$j]?>
+						</a> 
+						<img 
+							src="<?php echo $themeDir;?>/icons/<?php echo $this->getArr['sortOrder'.$j]?>.png" 
+							style="width: 8px; height:10px; border: none; vertical-align: middle" />
+					</td>
+			<?php 
+				} 
+			?>
+			</tr>
+			<?php
 				if ((isset($list)) && ($list !='')) {
 					for ($j=0; $j < count($list);$j++) {
-	 		?>
-    		<tr>
-       		<td class="r2_c1"><img name="table_r2_c1" src="<?php echo $themeDir;?>/pictures/spacer.gif" width="1" height="1" border="0" alt=""></td>
-       		<?php
-	 	 	 			$cssClass = ($j%2) ? 'even' : 'odd';
-
-		 	 			if($allowDelete) {
-		 		?>
-       		<td class="<?php echo $cssClass?>" width="50">
-       				<?php if (CommonFunctions::extractNumericId($list[$j][0]) > 0) { ?>
-       					<input type='checkbox' class='checkbox' name='chkID[]' value='<?php echo $list[$j][0]?>' /></td>
-       				<?php } ?>
-		 		<?php 	} else { ?>
-       		<td class="<?php echo $cssClass?>" width="50"></td>
-		 		<?php 	}  ?>
-		 		<td class="<?php echo $cssClass?>" width="250"><a href="<?php echo $baseURL . '&id='. $list[$j][0];?>&action=View" class="listViewTdLinkS1"><?php echo $list[$j][0]?></a></td>
-		 		<?php
-		 			  $k=1;
-		 			  if ($k < count($headings)) {
-		 			  	    $descField = getDisplayValue($list[$j][$k], $valueMap[$k], $maxDispLen);
-		 			  }
-			 	?>
-		 		<td class="<?php echo $cssClass?>" width="400" ><a href="<?php echo $baseURL . '&id='. $list[$j][0];?>&action=View" class="listViewTdLinkS1"><?php echo $descField?></a></td>
-		 		<?php
-		 				for ($k=2; $k < count($headings); $k++) {
-		 			  	    $descField = getDisplayValue($list[$j][$k], $valueMap[$k], $maxDispLen);
-		 		?>
-		 		<td class="<?php echo $cssClass?>" width="400" ><?php echo $descField?></td>
-		 		<?php } ?>
-				<td class="<?php echo $cssClass?>" width="400" >&nbsp;</td>
-		 		<td class="r2_c3"><img src="<?php echo $themeDir;?>/pictures/spacer.gif" width="13" height="1" border="0" alt=""></td>
-		 	</tr>
-		 	<?php
-		 		 }
-		 		}
-		  ?>
-		  <tr>
-          <td class="r3_c1" height="16"></td>
-          <td class="r3_c2" height="16"></td>
-          <?php for ($j=0; $j < count($headings); $j++) {?>
-          <td width="250" class="r3_c2" height="16"</td>
-          <?php } ?>
-          <td class="r3_c2" height="16"></td>
-          <td class="r3_c3" height="16"></td>
-         </tr>
- 		</table>
-
-
-<!--  newtable -->
-
+						$cssClass = ($j%2) ? 'even' : 'odd';
+			?>
+						<tr>
+							<td class="<?php echo $cssClass?>" width="50">
+							<?php 
+								if($allowDelete) { 
+									if (CommonFunctions::extractNumericId($list[$j][0]) > 0) { 
+							?>
+										<input type='checkbox' class='checkbox' name='chkID[]' value='<?php echo $list[$j][0]?>' />
+							<?php 	} 
+								} else {  ?>
+									&nbsp;
+							<?php 	}  ?>
+							</td>
+						<td class="<?php echo $cssClass?>" width="250">
+							<a href="<?php echo $baseURL . '&id='. $list[$j][0];?>&amp;action=View" class="listViewTdLinkS1"><?php echo $list[$j][0]?></a>
+						</td>
+						<?php
+							$k =	1;
+							if ($k < count($headings)) {
+								$descField = getDisplayValue($list[$j][$k], $valueMap[$k], $maxDispLen);
+							}
+						?>
+						<td class="<?php echo $cssClass?>" width="400" >
+							<a href="<?php echo $baseURL . '&id='. $list[$j][0];?>&amp;action=View" class="listViewTdLinkS1"><?php echo $descField?></a>
+						</td>
+						<?php
+							for ($k=2; $k < count($headings); $k++) {
+								$descField = getDisplayValue($list[$j][$k], $valueMap[$k], $maxDispLen);
+						?>
+							<td class="<?php echo $cssClass?>" width="400" ><?php echo $descField?></td>
+						<?php } ?>
+						</tr>
+					<?php
+					}
+				}
+			?>
+		</table>
+	</div>
 </form>
+
+<script type="text/javascript">
+<!--
+   	if (document.getElementById && document.createElement) {
+		initOctopus();
+	}
+-->
+</script>
+
 </body>
 </html>
