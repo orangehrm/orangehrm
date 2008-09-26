be75e4ecd6280a17126e21e5828abfc83d5d60f2
idhanu <idhanu@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2008-09-26 07:59:36 +0000

    fixed [ 2129732 ] HTML error in job application, fixed [ 2129793 ] Error in IE when country selected in Job Application

--- php/orangehrm/templates/recruitment/applicant/viewJobApplication.php
+++ php/orangehrm/templates/recruitment/applicant/viewJobApplication.php
@@ -197,12 +197,12 @@ $clearImgPressed = $picDir . 'btn_clear_02.gif';
 	}
 
 	function hideLoading() {
-		status = $('status');
+		var status = $('status');
 		status.style.display = 'none';
 	}
 
 	function showLoading() {
-		status = $('status');
+		var status = $('status');
 		status.style.display = 'block';
 	}
 
@@ -376,7 +376,7 @@ $clearImgPressed = $picDir . 'btn_clear_02.gif';
 				onMouseOut="this.src='<?php echo $saveImg;?>';"
             	onMouseOver="this.src='<?php echo $saveImgPressed;?>';"
             	src="<?php echo $saveImg;?>">
-			<img onClick="reset(); id="resetBtn"
+			<img onClick="reset();" id="resetBtn"
 				onMouseOut="this.src='<?php echo $clearImg;?>';"
             	onMouseOver="this.src='<?php echo $clearImgPressed;?>';"
             	src="<?php echo $clearImg;?>">
