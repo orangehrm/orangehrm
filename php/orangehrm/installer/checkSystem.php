b92024f11ac9159b58a4c2484d15e0b17f016fe1
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2009-01-07 17:31:22 +0000

    fixed code formatting

--- php/orangehrm/installer/checkSystem.php
+++ php/orangehrm/installer/checkSystem.php
@@ -170,26 +170,22 @@ function sysCheckPassed() {
 
             <td align="right" class="tdValues"><strong>
             <?php
-               if(function_exists('mysql_connect') && (@mysql_connect($dbInfo['dbHostName'].':'.$dbInfo['dbHostPort'], $dbInfo['dbUserName'], $dbInfo['dbPassword']))) {
+                if(function_exists('mysql_connect') && (@mysql_connect($dbInfo['dbHostName'].':'.$dbInfo['dbHostPort'], $dbInfo['dbUserName'], $dbInfo['dbPassword']))) {
 
-	              $mysqlServer = mysql_query("show engines");
-                    while ($engines = mysql_fetch_assoc($mysqlServer)){
-                        if($engines['Engine'] == 'InnoDB'){
-                            if($engines['Support'] == 'DISABLED'){
+                    $mysqlServer = mysql_query("show engines");
+                    while ($engines = mysql_fetch_assoc($mysqlServer)) {
+                        if ($engines['Engine'] == 'InnoDB') {
+                            if ($engines['Support'] == 'DISABLED') {
                                 echo "<b><font color='red'>Disabled!</font></b>";
                                 $error_found = true;
-                            }
-                            elseif($engines['Support'] == 'DEFAULT'){
+                            } elseif ($engines['Support'] == 'DEFAULT') {
                                 echo "<b><font color='green'>Default</font></b>";
-                            }
-                            elseif($engines['Support'] == 'YES'){
+                            } elseif ($engines['Support'] == 'YES') {
                                 echo "<b><font color='green'>Enabled</font></b>";
-                            }
-                            elseif($engines['Support'] == 'NO'){
+                            } elseif ($engines['Support'] == 'NO') {
                                 echo "<b><font color='red'>Not available!</font></b>";
                                 $error_found = true;
-                            }
-                            else{
+                            } else {
                                 echo "<b><font color='red'>Unknown Error!</font></b>";
                                 $error_found = true;
                             }
