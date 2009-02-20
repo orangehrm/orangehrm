f6ad225b05d7605a85d0eb49e9feec31c87d6642
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2009-02-20 09:02:24 +0000

    Changed version numbers for 2.5-alpha.8

--- php/orangehrm/login.php
+++ php/orangehrm/login.php
@@ -385,7 +385,7 @@ body {
 <!-- End ImageReady Slices -->
 <table width="100%">
 <tr>
-<td align="center"><a href="http://www.orangehrm.com" target="_blank">OrangeHRM</a> ver 2.5-alpha.7 &copy; OrangeHRM Inc. 2005 - 2009 All rights reserved.</td>
+<td align="center"><a href="http://www.orangehrm.com" target="_blank">OrangeHRM</a> ver 2.5-alpha.8 &copy; OrangeHRM Inc. 2005 - 2009 All rights reserved.</td>
 </tr>
 </table>
 
