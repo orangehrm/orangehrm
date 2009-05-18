06b7ca080c30d544afc8c54d0040af2d11609e5e
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2009-05-18 13:24:10 +0000

    Changed version numbers for 2.5-beta.6

--- php/orangehrm/login.php
+++ php/orangehrm/login.php
@@ -444,7 +444,7 @@ body {
 <!-- End ImageReady Slices -->
 <table width="100%">
 <tr>
-<td align="center"><a href="http://www.orangehrm.com" target="_blank">OrangeHRM</a> ver 2.5-beta.5 &copy; OrangeHRM Inc. 2005 - 2009 All rights reserved.</td>
+<td align="center"><a href="http://www.orangehrm.com" target="_blank">OrangeHRM</a> ver 2.5-beta.6 &copy; OrangeHRM Inc. 2005 - 2009 All rights reserved.</td>
 </tr>
 </table>
 
