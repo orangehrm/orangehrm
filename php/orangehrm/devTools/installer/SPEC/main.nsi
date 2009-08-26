4a09c0ca7a0f8081a50f9d911d033196a617dd78
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2009-08-26 12:54:33 +0000

    Changed version number to 2.5-beta.15

--- php/orangehrm/devTools/installer/SPEC/main.nsi
+++ php/orangehrm/devTools/installer/SPEC/main.nsi
@@ -30,7 +30,7 @@
 ; Product Details
 
   !define ProductName "OrangeHRM"
-  !define ProductVersion "2.5-beta.14"
+  !define ProductVersion "2.5-beta.15"
 
   !define Organization "OrangeHRM Inc."
 
--- php/orangehrm/sysinfo.php
+++ php/orangehrm/sysinfo.php
@@ -120,7 +120,7 @@ function chk_memory($limit=9, $recommended=16) {
 return $msg;
 }
 
-$ohrmVersion = "2.5-beta.14";
+$ohrmVersion = "2.5-beta.15";
 
 if (@include_once ROOT_PATH."/lib/confs/Conf.php-distribution") {
 	$conf = new Conf();
