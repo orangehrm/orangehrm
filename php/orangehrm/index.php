07cf633c80cf34f8d64fe27d02781348d3e16735
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2009-08-26 06:39:59 +0000

    Made an improvement in the main menu for the menu items of import and export

--- php/orangehrm/index.php
+++ php/orangehrm/index.php
@@ -339,10 +339,10 @@ if ( (($_SESSION['isAdmin']=='Yes') && $arrAllRights[Admin]['view']) || $_SESSIO
 
 		$sub = new MenuItem("importexport",$lang_Menu_Admin_DataImportExport,   "#");
 		$subsubs = array();
-		$subsubs[] = new MenuItem("importexport",$lang_Menu_Admin_DataExport, "index.php?uniqcode=CSE&menu_no_top=eim" );
 		$subsubs[] = new MenuItem("importexport",$lang_Menu_Admin_DataExportDefine, "index.php?uniqcode=CEX&menu_no_top=eim" );
-		$subsubs[] = new MenuItem("importexport",$lang_Menu_Admin_DataImport, "index.php?uniqcode=IMP&menu_no_top=eim" );
+		$subsubs[] = new MenuItem("importexport",$lang_Menu_Admin_DataExport, "index.php?uniqcode=CSE&menu_no_top=eim" );
 		$subsubs[] = new MenuItem("importexport",$lang_Menu_Admin_DataImportDefine, "index.php?uniqcode=CIM&menu_no_top=eim" );
+		$subsubs[] = new MenuItem("importexport",$lang_Menu_Admin_DataImport, "index.php?uniqcode=IMP&menu_no_top=eim" );
 		$sub->setSubMenuItems($subsubs);
 		$subs[] = $sub;
 
