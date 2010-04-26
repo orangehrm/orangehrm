97398cb16b8595f33330871dcc99010b16e00ea8
isamantha <isamantha@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2010-04-26 12:21:45 +0000

    add test for Kpi service class

--- php/orangehrm/symfony/test/model/performance/service/KpiServiceTest.php
+++ php/orangehrm/symfony/test/model/performance/service/KpiServiceTest.php
@@ -68,12 +68,74 @@ class KpiServiceTest extends PHPUnit_Framework_TestCase{
             $this->kpiService->setKpiDao($this->kpiDao );
             $result 	=	$this->kpiService->readKpi( 1 );
             $this->assertEquals( $kpi , $result ); 
-            
     	}
     	
-    	
     }
-    
-    
+   
+    /**
+     * Tests Delete Kpi
+     */
+   public function testDeleteKpi() {
+      $this->kpiDao		=	$this->getMock('KpiDao');
+      $this->kpiDao->expects($this->once())
+                    ->method('deleteKpi')
+                    ->will($this->returnValue(true));
+      $this->kpiService->setKpiDao($this->kpiDao );
+      $result 	=	$this->kpiService->deleteKpi(array(1));
+      $this->assertTrue($result);
+   }
+
+    /**
+     * Test Kpi Default Rate
+     */
+   public function testGetKpiDefaultRate() {
+      foreach ($this->testCases['Kpi'] as $key=>$testCase) {
+    		$kpi	=	new DefineKpi();
+			$kpi->setJobtitlecode ($this->testCases['JobTitle']['jobtitlecode']);
+			$kpi->setDesc ( $testCase['desc']);
+			$kpi->setMin ( $testCase['min'] );
+			$kpi->setMax ( $testCase['max'] );
+			$kpi->setDefault ( $testCase['default'] );
+			$kpi->setIsactive ( $testCase['isactive'] );
+
+			$this->kpiDao		=	$this->getMock('KpiDao');
+			$this->kpiDao->expects($this->once())
+                       ->method('getKpiDefaultRate')
+                       ->will($this->returnValue($kpi));
+         $this->kpiService->setKpiDao($this->kpiDao );
+         $result 	=	$this->kpiService->getKpiDefaultRate();
+         $this->assertTrue($result instanceof DefineKpi);
+    	}
+   }
+
+    /**
+     * Test OverRideKpiDefaultRate
+     */
+   public function testOverRideKpiDefaultRate() {
+      foreach ($this->testCases['Kpi'] as $key=>$testCase) {
+         $kpi	=	new DefineKpi();
+			$kpi->setJobtitlecode ($this->testCases['JobTitle']['jobtitlecode']);
+			$kpi->setDesc ( $testCase['desc']);
+			$kpi->setMin ( $testCase['min'] );
+			$kpi->setMax ( $testCase['max'] );
+			$kpi->setDefault ( $testCase['default'] );
+			$kpi->setIsactive ( $testCase['isactive'] );
+
+         //reading the KPI
+         $this->kpiDao		=	$this->getMock('KpiDao');
+			$this->kpiDao->expects($this->once())
+                       ->method('readKpi')
+                       ->will($this->returnValue($kpi));
+         $this->kpiService->setKpiDao($this->kpiDao );
+         $kpi = $this->kpiService->readKpi(1);
+
+         //overriding
+         $this->kpiDao->expects($this->once())
+                       ->method('overRideKpiDefaultRate')
+                       ->will($this->returnValue(true));
+         $result	=	$this->kpiDao->overRideKpiDefaultRate($kpi);
+         $this->assertTrue($result);
+      }
+   }
 }
 ?>
