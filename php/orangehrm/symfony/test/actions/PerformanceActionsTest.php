67608578514be215053ea67ed0b2ded16e41c5b7
isamantha <isamantha@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2010-05-06 04:17:24 +0000

    performance action test

--- php/orangehrm/symfony/test/actions/PerformanceActionsTest.php
+++ php/orangehrm/symfony/test/actions/PerformanceActionsTest.php
@@ -23,7 +23,7 @@ class PerformanceActionsTest extends PHPUnit_Framework_TestCase {
    protected function setUp() {
       $this->context = MockContext::getInstance();
       $request = new MockWebRequest();
-
+      
       // In sfConfigCache, we just need checkConfig method
       $configCache = $this->getMock('sfConfigCache', array('checkConfig'), array(), '', false);
 
@@ -33,6 +33,7 @@ class PerformanceActionsTest extends PHPUnit_Framework_TestCase {
       $this->context->request       = $request;
       $this->context->configCache   = $configCache;
       $this->context->controller    = $controller;
+      $this->context->user          = $this->getMock('sfUser', array('setFlash'), array(), '', false);
       //$this->deleteDefineKpi        = false;
    }
 
@@ -207,7 +208,6 @@ class PerformanceActionsTest extends PHPUnit_Framework_TestCase {
 
       $postParam = array('chkKpiID' => 1);
       $request = $this->context->request;
-      $request = $this->context->request;
       $request->setPostParameters($postParam);
       $request->setMethod(sfRequest::POST);
 
@@ -225,14 +225,66 @@ class PerformanceActionsTest extends PHPUnit_Framework_TestCase {
    /**
     * Test ViewReview
     */
-   function testViewReview() {
-      $prService = $this->getMock('PerformanceReviewService');
-      $arrReview = array('review1', 'review2', 'review3');
-      
+   public function testViewReview() {
+     $prService = $this->getMock('PerformanceReviewService');
+     $arrReview = array('review1', 'review2', 'review3');
+
       //Set up mock object to call callback method
       $prService->expects($this->once())
                     ->method('searchPerformanceReview')
                     ->will($this->returnCallback(array(&$this, 'searchPerformanceReviewCallback')));
+
+      $prService->searchPerformanceReview($arrReview);
+
+      $performanceAction = new performanceActions($this->context, "performance", "viewReview");
+      $performanceAction->setPerformanceReviewService($prService);
+
+
+
+      try {
+         $performanceAction->executeViewReview();
+         $this->fail("Expected to redirect");
+      } catch(Exception $e) {
+
+      }
+
+      $this->assertTrue(isset($this->performanceReviews));
+   }
+
+   /**
+    * Test DeleteReview
+    */
+   public function testDeleteReview() {
+      $prService = $this->getMock('PerformanceReviewService');
+
+      $prService->expects($this->once())
+              ->method('deletePerformanceReview')
+              ->will($this->returnValue(true));
+      $prService->deletePerformanceReview(array(1));
+
+      $this->context->user->expects($this->exactly(2))
+          ->method('setFlash')
+          ->will($this->returnCallback(array(&$this, 'setFlash')));
+
+      //finally should redirect to list page
+      $this->context->controller->expects($this->once())
+                         ->method('redirect')
+                         ->with($this->equalTo('performance/viewReview'));
+
+      $performanceAction = new performanceActions($this->context, "performance", "deleteReview");
+
+      $postParam = array('chkReview' => 1);
+      $request = $this->context->request;
+      $request->setPostParameters($postParam);
+      $request->setMethod(sfRequest::POST);
+
+      try {
+         $performanceAction->executeDeleteReview($request);
+         $this->fail("Expected to redirect");
+      } catch(Exception $e) {
+         
+      }
+      $this->assertTrue(isset($this->flashMessages['templateMessage']));
    }
 
     /**
@@ -266,128 +318,7 @@ class PerformanceActionsTest extends PHPUnit_Framework_TestCase {
     */
    public function searchPerformanceReviewCallback() {
       $args = func_get_args();
-      return $args[0];
+      $this->performanceReviews = $args[0];
    }
-}
-?>
-<?php
-
-require_once 'PHPUnit/Framework.php';
-define('SYMFONY_ROOT', dirname(__FILE__) . '/../../');
-
-require_once SYMFONY_ROOT . 'test/util/MockContext.class.php';
-require_once SYMFONY_ROOT . 'test/util/MockWebRequest.class.php';
-require_once SYMFONY_ROOT . 'apps/orangehrm/modules/performance/actions/actions.class.php';
-
-/**
- * PerformanceActionsTest class tests Performance Controller
- *
- * @author Sujith T
- */
-class PerformanceActionsTest extends PHPUnit_Framework_TestCase {
-
-   private $context;
-
-   /**
-    * SetUp Function
-    */
-   protected function setUp() {
-      $this->context = MockContext::getInstance();
-      $request = new MockWebRequest();
-
-      // In sfConfigCache, we just need checkConfig method
-      $configCache = $this->getMock('sfConfigCache', array('checkConfig'), array(), '', false);
-
-      // Mock of controller, with redirect method mocked.
-      $controller = $this->getMock('sfController', array('redirect'), array(), '', false);
-
-      $this->context->request       = $request;
-      $this->context->configCache   = $configCache;
-      $this->context->controller    = $controller;
-   }
-
-   /**
-    * Test ListDefineKpi
-    */
-   public function testListDefineKpi() {
-      $kpi        = array('test1', 'kpi1', 'kpi2');
-
-      $kpiService = $this->getMock('KpiService');
-      /*$kpiService->expects($this->once())
-               ->method('getKpiList')
-               ->will($this->returnValue($kpi));*/
-
-      //create action class
-      $performanceAction = new performanceActions($this->context, "performance", "listDefineKpi");
-      //$performanceAction->setJobService($jobService);
-      $performanceAction->setKpiService($kpiService);
-
-      $request = $this->context->request;
-      $request->setMethod(sfRequest::GET);
-      $performanceAction->executeListDefineKpi($request);
-
-      $this->assertTrue(is_object($performanceAction->kpiList));
-   }
-
-   /**
-    * Test SaveKpi
-    */
-   public function testSaveKpi() {
-      $kpiService = $this->getMock('KpiService');
-
-      //mock the User class -> expects 'SUCCESS' flash to be set -> see action class
-      $user = $this->getMock('sfUser', array('setFlash'), array(), '', false);
-      $user->expects($this->exactly(2))
-          ->method('setFlash')
-          ->will($this->returnCallback(array(&$this, 'setFlash')));
-      $this->context->user = $user;
-
-      //finally should redirect to list page
-      $this->context->controller->expects($this->once())
-                         ->method('redirect')
-                         ->with($this->equalTo('performance/saveKpi'));
-
-      //create action class
-      $performanceAction = new performanceActions($this->context, "performance", "saveKpi");
-      $performanceAction->setKpiService($kpiService);
-
-      $postParam = array(
-         'txtJobTitle' => 'JOB001',
-         'txtDescription' => 'Sample Test',
-         'txtMinRate' => 10,
-         'txtMaxRate' => 100,
-         'chkDefaultScale' => 1
-      );
-
-     $request = $this->context->request;
-     $request->setPostParameters($postParam);
-
-     // Set request to POST method
-     $request->setMethod(sfRequest::POST);
-
-     try{
-        $performanceAction->executeSaveKpi($request);
-     } catch(Exception $e) {
-        
-     }
-     //make assertion
-     $this->assertTrue(isset($this->flashMessages['message']));
-     $this->assertEquals('SUCCESS', $this->flashMessages['messageType']);
-   }
-
-    /**
-     * Call back method for setFlash
-     * @return string
-     */
-    public function setFlash() {
-
-        // verify 2 parameters
-        $args = func_get_args();
-        $this->assertEquals(2, count($args), 'flash should receive 2 parameters');
-        $name = $args[0];
-        $value = $args[1];
-
-        $this->flashMessages[$name] = $value;
-    }
 }
 ?>
