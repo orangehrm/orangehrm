bc0d9763f60d34c0277ad21213ff7ccaa8296d5e
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2010-12-17 05:45:18 +0000

    config files upgraded to symfony 1.4

--- php/orangehrm/symfony/config/ProjectConfiguration.class.php
+++ php/orangehrm/symfony/config/ProjectConfiguration.class.php
@@ -11,8 +11,8 @@ class ProjectConfiguration extends sfProjectConfiguration
     // for compatibility / remove and enable only the plugins you want 
     $this->enablePlugins(array('sfDoctrinePlugin'));
     $this->enablePlugins(array('sfPhpunitPlugin'));
-    $this->enablePlugins(array('sfJqueryPlugin'));
-    $this->disablePlugins(array('sfPropelPlugin'));
+    //$this->enablePlugins(array('sfJqueryPlugin'));
+    //$this->disablePlugins(array('sfPropelPlugin'));
 
     // Set up logging 
     Logger::configure(dirname(__FILE__) . '/log4php.properties');
