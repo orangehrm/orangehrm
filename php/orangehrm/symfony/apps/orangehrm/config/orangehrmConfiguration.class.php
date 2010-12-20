92df79a433fd51de66d6ee642e8572544f47d108
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2010-12-20 04:47:52 +0000

    Enabled doctrine validations

--- php/orangehrm/symfony/apps/orangehrm/config/orangehrmConfiguration.class.php
+++ php/orangehrm/symfony/apps/orangehrm/config/orangehrmConfiguration.class.php
@@ -10,6 +10,7 @@ class orangehrmConfiguration extends sfApplicationConfiguration
    * Configure doctrine connections to use tablename prefix hs_hr_
    */
    public function configureDoctrine(Doctrine_Manager $manager) {
+	$manager->setAttribute(Doctrine_Core::ATTR_VALIDATE, Doctrine_Core::VALIDATE_ALL);
        //$manager->setAttribute(Doctrine::ATTR_TBLNAME_FORMAT, 'hs_hr_%s');
    }
    
