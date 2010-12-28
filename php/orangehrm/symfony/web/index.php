128086014e210c41c1ef10bf6376310d5dc40e3f
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2010-12-28 16:14:03 +0000

    Reverting changes to Symfony front controler

--- php/orangehrm/symfony/web/index.php
+++ php/orangehrm/symfony/web/index.php
@@ -9,5 +9,5 @@ define('WPATH', $scriptPath . "/../../");
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('orangehrm', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('orangehrm', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();
