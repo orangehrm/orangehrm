24a033438222ded9bb0a81b455413ad4e01dd292
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-01-06 15:09:44 +0000

    Changing version numbers for 2.6.1-rc.1

--- php/orangehrm/installer/welcome.php
+++ php/orangehrm/installer/welcome.php
@@ -25,7 +25,7 @@ function welcomeSubmit() {
 }
 </script>
 	<div id="content">
-		<h2>Welcome to the OrangeHRM ver 2.6.1-beta.1 Setup Wizard</h2>
+		<h2>Welcome to the OrangeHRM ver 2.6.1-rc.1 Setup Wizard</h2>
 
 
 		<p>This installer creates the OrangeHRM database tables and sets the
