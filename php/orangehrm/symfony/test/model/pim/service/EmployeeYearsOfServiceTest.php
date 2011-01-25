60baee4dd51e6eada7b436ff02f0323264c1b1f6
sujitht <sujitht@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-01-25 12:43:44 +0000

    Apply Leave

---
+++ php/orangehrm/symfony/test/model/pim/service/EmployeeYearsOfServiceTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * EmployeeYearsOfService
+ *
+ * @author sujith
+ */
+class EmployeeYearsOfServiceTest extends PHPUnit_Framework_TestCase {
+    private $employeeService;
+
+    /**
+     * Setup Function
+     */
+    public function setup() {
+        $this->employeeService = new EmployeeService();
+    }
+
+    public function testGetDurationInYears1() {
+        $years = $this->employeeService->getDurationInYears('2006-01-01', '2011-01-01');
+        $this->assertEquals($years, 5);
+    }
+
+    public function testGetDurationInYears2() {
+        $years = $this->employeeService->getDurationInYears('2006-01-01', '2006-06-30');
+        $this->assertEquals($years, 0.5);
+    }
+
+    public function testGetDurationInYears3() {
+        $years = $this->employeeService->getDurationInYears('2007-02-28', '2007-05-28');
+        $this->assertEquals($years, 0.25);
+    }
+
+    public function testGetDurationInYears4() {
+        $years = $this->employeeService->getDurationInYears('2009-06-30', '2012-02-29');
+        $this->assertEquals(round($years, 2), 2.67);
+    }
+
+    public function testGetDurationInYears5() {
+        $years = $this->employeeService->getDurationInYears('2012-02-29', '2009-06-30');
+        $this->assertEquals($years, 0);
+    }
+
+    public function testGetDurationInYears6() {
+        $years = $this->employeeService->getDurationInYears('2010-03-15', '2012-09-05');
+        $this->assertEquals($years, 2.5);
+    }
+
+    public function testGetEmployeeYearsOfService() {
+        $employee = new Employee();
+        $employee->setJoinedDate('2007-06-22');
+        $employeeDao    = $this->getMock('EmployeeDao', array('getEmployee'));
+        $employeeDao->expects($this->once())
+                    ->method('getEmployee')
+                    ->will($this->returnValue($employee));
+
+        $this->employeeService->setEmployeeDao($employeeDao);
+        $years = $this->employeeService->getDurationInYears('2007-06-22', date("Y-m-d"));
+
+        $employeeYearsOfService = $this->employeeService->getEmployeeYearsOfService(1, date("Y-m-d"));
+        $this->assertEquals($employeeYearsOfService, $years);
+    }
+}
+?>
