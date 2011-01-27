1a8bafd7577b4447e46a8d7eb6adc7b0843366d0
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-01-27 11:42:34 +0000

    Improved Leave Type server side validation

--- php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/lib/form/LeaveTypeForm.php
+++ php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/lib/form/LeaveTypeForm.php
@@ -22,7 +22,15 @@ class LeaveTypeForm extends orangehrmForm {
     protected function loadInitialWidgets() {
 
         $this->setWidget('txtLeaveTypeName', new sfWidgetFormInput(array(), array('size' => 30)));
-        $this->setValidator('txtLeaveTypeName', new sfValidatorPass());
+        $this->setValidator('txtLeaveTypeName', new sfValidatorString(array(
+                            'min_length' => 2,
+                            'max_length' => 30
+                            ),
+                            array(
+                            'min_length' => 'Leave type name should be at least 2 characters',
+                            'max_length' => 'Leave type name should be less than 30 characters'
+                            )));
+
 
         $this->setWidget('hdnSavingMode', new sfWidgetFormInputHidden(array('default' => self::SAVING_MODE_NEW)));
         $this->setValidator('hdnSavingMode', new sfValidatorPass());
