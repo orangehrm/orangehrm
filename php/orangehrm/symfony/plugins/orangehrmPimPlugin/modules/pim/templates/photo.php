19419baf47d438c68f014dc08a9631d0beb67ea2
sujitht <sujitht@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-02-24 11:12:07 +0000

    Deleting unnecessary files

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/modules/pim/templates/photo.php
+++
@@ -1,9 +0,0 @@
-<div id="currentImage">
-    <center>
-        <a href="../../../../lib/controllers/CentralController.php?menu_no_top=hr&id=<?php echo $empNumber;?>&amp;capturemode=updatemode&amp;reqcode=EMP&pane=21">
-            <img style="width:100px; height:120px;" alt="Employee Photo" src="<?php echo url_for("pim/viewPhoto?empNumber=". $empNumber); ?>" border="0"/>
-        </a>
-    <br />
-    <span class="smallHelpText"><strong><?php echo $form->fullName; ?></strong></span>
-    </center>
-</div>
