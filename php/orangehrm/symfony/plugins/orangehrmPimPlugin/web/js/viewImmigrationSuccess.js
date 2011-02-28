fe835567d07a182a9fcdcd7b4977d3ae987d499d
sujitht <sujitht@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-02-28 11:11:28 +0000

    Immigration Tab

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/web/js/viewImmigrationSuccess.js
+++ php/orangehrm/symfony/plugins/orangehrmPimPlugin/web/js/viewImmigrationSuccess.js
@@ -23,17 +23,17 @@ $(document).ready(function() {
     $("#frmEmpImmigration").validate({
         rules: {
             'immigration[number]': {required: true},
-            'immigration[passport_issue_date]': {required: true},
-            'immigration[passport_expire_date]' : {required: true},
+            'immigration[passport_issue_date]': {required: true, dateISO: true, validdate: true, validIssuedDate: true},
+            'immigration[passport_expire_date]' : {required: true, dateISO: true, validdate:true},
             'immigration[country]' : {required: true},
-            'immigration[i9_review_date]' : { required: true}
+            'immigration[i9_review_date]' : { required: true, dateISO: true, validdate: true}
         },
         messages: {
             'immigration[number]': {required: lang_numberRequired},
-            'immigration[passport_issue_date]': {required: lang_issueDateRequired},
-            'immigration[passport_expire_date]' : {required: lang_expireDateRequired},
+            'immigration[passport_issue_date]': {required: lang_issueDateRequird, dateISO: lang_dateFormatIssue, validdate: lang_invalidIssueDate, validIssuedDate: lang_issuedGreaterExpiry},
+            'immigration[passport_expire_date]' : {required: lang_expireDateRequired, dateISO: lang_dateFormatIssue, validdate: lang_invalidExpireDate},
             'immigration[country]' : {required: lang_countryRequired},
-            'immigration[i9_review_date]' : { required: lang_reviewDateRequired}
+            'immigration[i9_review_date]' : { required: lang_reviewDateRequired, dateISO: lang_dateFormatIssue, validdate: lang_invalidExpireDate}
         },
 
         errorElement : 'label',
@@ -80,7 +80,35 @@ $(document).ready(function() {
         $("#frmImmigrationDelete").submit();
     });
 
+    $.validator.addMethod("validdate", function(value, element) {
+        if(value == "") {
+            return true;
+        }
+        var dt = value.split("-");
+        return validateDate(parseInt(dt[2], 10), parseInt(dt[1], 10), parseInt(dt[0], 10));
+    });
 
+    /* Valid From Date */
+    $.validator.addMethod("validIssuedDate", function(value, element) {
+
+        var fromdate	=	$('#immigration_passport_issue_date').val();
+        fromdate = (fromdate).split("-");
+        if(!validateDate(parseInt(fromdate[2],10), parseInt(fromdate[1],10), parseInt(fromdate[0],10))) {
+           return false;
+        }
+        var fromdateObj = new Date(parseInt(fromdate[0],10), parseInt(fromdate[1],10) - 1, parseInt(fromdate[2],10));
+        var todate		=	$('#immigration_passport_expire_date').val();
+        todate = (todate).split("-");
+        var todateObj	=	new Date(parseInt(todate[0],10), parseInt(todate[1],10) - 1, parseInt(todate[2],10));
+
+        if(fromdateObj > todateObj){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    });
 
 });
 
@@ -99,4 +127,36 @@ function fillDataToImmigrationDataPane(seqno) {
 
     $("#btnAdd").hide();
     $("#immigrationDataPane").show();
+}
+
+function validateDate(day, month, year) {
+    var days31 = new Array(1,3,5,7,8,10,12);
+
+    if(month > 12 || month < 1) {
+        return false;
+    }
+
+    if(day == 29 && month == 2) {
+        if(year % 4 == 0) {
+            return true;
+        }
+    }
+
+    if(month == 2 && day < 29) {
+        return true;
+    }
+    if(day < 32 && month != 2) {
+        if(day == 31) {
+            flag = false;
+            for(i=0; i < days31.length; i++) {
+                if(days31[i] == month) {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        }
+        return true;
+    }
+    return false;
 }
