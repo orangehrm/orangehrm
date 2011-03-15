ef221f1d2b6a84bb54b3620e52fa4948f2ecd5d0
sujitht <sujitht@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-03-15 06:48:44 +0000

    Duplicate Username Message Change

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/modules/pim/actions/addEmployeeAction.class.php
+++ php/orangehrm/symfony/plugins/orangehrmPimPlugin/modules/pim/actions/addEmployeeAction.class.php
@@ -86,7 +86,7 @@ class addEmployeeAction extends sfAction {
 
             if($user instanceof Users) {
 
-                $this->getUser()->setFlash('templateMessage', array('warning', __('Employee Adding Failed, User Name Already Exists')));
+                $this->getUser()->setFlash('templateMessage', array('warning', __('Adding Employee Failed, User Name Already Exists')));
                 $this->redirect('pim/addEmployee');
             }
 
