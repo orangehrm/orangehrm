e3fecdbb24734834a3a2aa6782fee7da927719f6
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-03-31 11:34:27 +0000

    Changed version numbers for 2.6.3

--- php/orangehrm/devTools/installer/SPEC/main.nsi
+++ php/orangehrm/devTools/installer/SPEC/main.nsi
@@ -30,7 +30,7 @@
 ; Product Details
 
   !define ProductName "OrangeHRM"
-  !define ProductVersion "2.6.2"
+  !define ProductVersion "2.6.3"
 
   !define Organization "OrangeHRM Inc."
 
--- php/orangehrm/installer/applicationSetup.php
+++ php/orangehrm/installer/applicationSetup.php
@@ -279,7 +279,7 @@ class Conf {
 		}
 		\$this->dbuser    = '$dbOHRMUser';
 		\$this->dbpass	= '$dbOHRMPassword';
-		\$this->version = '2.6.2';
+		\$this->version = '2.6.3';
 
 		\$this->emailConfiguration = dirname(__FILE__).'/mailConf.php';
 		\$this->errorLog =  realpath(dirname(__FILE__).'/../logs/').'/';
