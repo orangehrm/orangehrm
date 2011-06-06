2b4c256480cbebbefe19bb4ddb7ca806207d9482
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-06-06 01:24:52 +0000

    Added encryption support for basic_salary

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/model/doctrine/PluginEmpBasicsalary.class.php
+++ php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/model/doctrine/PluginEmpBasicsalary.class.php
@@ -10,7 +10,15 @@
  * @author     ##NAME## <##EMAIL##>
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-abstract class PluginEmpBasicsalary extends BaseEmpBasicsalary
-{
+abstract class PluginEmpBasicsalary extends BaseEmpBasicsalary {
+    
+    public function setUp() {
+        parent::setup();
+
+        if (KeyHandler::keyExists()) {
+            $key = KeyHandler::readKey();
+            $this->addListener(new EncryptionListener('basic_salary', $key));
+        }
+    }    
 
 }
