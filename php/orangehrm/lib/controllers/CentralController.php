b6d610b5c2ca3e781efb95f04e8b14235309416c
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-06-12 03:37:08 +0000

    Correcting an error in CentralController.php due to a wrong line added in revision 7389

--- php/orangehrm/lib/controllers/CentralController.php
+++ php/orangehrm/lib/controllers/CentralController.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION['fname'])) {
 
 set_magic_quotes_runtime(0); // Turning off magic quotes runtime
 
-definer('ROOT_PATH', $_SESSION['path']);
+define('ROOT_PATH', $_SESSION['path']);
 define("SALT", '$2a$'.str_pad($_SESSION['empID'].session_id(), 24, session_id()).'$');
 
 require_once ROOT_PATH . '/lib/exception/ExceptionHandler.php';
