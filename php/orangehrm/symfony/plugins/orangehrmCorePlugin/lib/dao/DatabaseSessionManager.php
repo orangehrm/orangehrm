a242b9970f24b9aa2cd64d059aef6f47ca5a65fe
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-06-23 06:22:51 +0000

    Added DatabaseSessionManager class

---
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/dao/DatabaseSessionManager.php
@@ -0,0 +1,19 @@
+<?php
+
+class DatabaseSessionManager {
+
+    protected $sessionVariables = array();
+
+    public function setSessionVariables(array $sessionVariables) {
+        $this->sessionVariables = $sessionVariables;
+    }
+
+    public function  registerVarables() {
+        $conn = Doctrine_Manager::getInstance()->getCurrentConnection();
+        foreach ($this->sessionVariables as $key => $value) {
+            $conn->execute("SET @{$key} = '{$value}';");
+        }
+    }
+    
+}
+
