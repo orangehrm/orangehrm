e964eb6d1b45547eb4775a5f5d62a5e534ec5421
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-07-12 09:21:21 +0000

    Improved ohrmLustHeaderFactory to get and set individual headers

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/list/ohrmListHeaderFactory.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/list/ohrmListHeaderFactory.php
@@ -12,9 +12,31 @@ abstract class ohrmListConfigurationFactory implements ListConfigurationFactory 
 
         return $this->headers;
     }
-    
+
     public function setHeaders(array $headers) {
         $this->headers = $headers;
+    }
+
+    public function setHeader($index, ListHeader $header) {
+
+        if (empty($this->headers)) {
+            $this->init();
+        }
+
+        $this->headers[$index] = $header;
+    }
+
+    public function getHeader($index) {
+
+        if (empty($this->headers)) {
+            $this->init();
+        }
+
+        if (isset($this->headers[$index])) {
+            return $this->headers[$index];
+        } else {
+            throw new Exception('No headers set at index ' . $index);
+        }
     }
 
     protected function init() {
