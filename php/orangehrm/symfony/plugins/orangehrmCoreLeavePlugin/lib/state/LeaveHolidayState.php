f17db3313c0acfd67dfca60ac66c6be35eb0afe4
nadeeth <nadeeth@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-08-09 09:11:11 +0000

    LeaveList->Holidays doesn't not shown as non working day - ID: 3388347

--- php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/lib/state/LeaveHolidayState.php
+++ php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/lib/state/LeaveHolidayState.php
@@ -8,15 +8,15 @@ class LeaveHolidayState implements LeaveState {
 	}
 
 	public function approve() {
-		$this->leaveStateManger->setState($this->leaveStateManger->getLeaveApprovedState());
+		//$this->leaveStateManger->setState($this->leaveStateManger->getLeaveApprovedState());
 	}
 	
 	public function reject() {
-		$this->leaveStateManger->setState($this->leaveStateManger->getLeaveRejectedState());
+		//$this->leaveStateManger->setState($this->leaveStateManger->getLeaveRejectedState());
 	}
 	
 	public function cancel() {
-		$this->leaveStateManger->setState($this->leaveStateManger->getLeaveCancelledState());
+		//$this->leaveStateManger->setState($this->leaveStateManger->getLeaveCancelledState());
 	}
 	
 	public function take() {
