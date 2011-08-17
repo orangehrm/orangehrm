9c3ab16421aebe9323177433462280d242c60b31
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-08-17 08:39:54 +0000

    Changes to PluginTimesheetItem.class.php

--- php/orangehrm/symfony/plugins/orangehrmTimePlugin/lib/model/doctrine/PluginTimesheetItem.class.php
+++ php/orangehrm/symfony/plugins/orangehrmTimePlugin/lib/model/doctrine/PluginTimesheetItem.class.php
@@ -12,5 +12,37 @@
  */
 abstract class PluginTimesheetItem extends BaseTimesheetItem
 {
+    
+	public function getConvertTime() {
+		$timesheetService = new TimesheetService();
+		$timeInSecs = $this->getDuration();
+		if ($timeInSecs == null) {
+			return null;
+		} else {
+			return $timesheetService->convertDurationToHours($this->getDuration());
+		}
+	}
+
+	public function addConvertedTime($oldPlayTime, $PlayTimeToAdd) {
+
+		$old = explode(":", $oldPlayTime);
+		$play = explode(":", $PlayTimeToAdd);
+		print_r($old[0]."-".$old[1]);die;
+		$hours = $old[0] + $play[0];
+		$minutes = $old[1] + $play[1];
+		if ($minutes > 59) {
+
+			$minutes = $minutes - 60;
+			$hours++;
+		}
+		if ($minutes < 10) {
+			$minutes = "0" . $minutes;
+		}
+		if ($minutes == 0) {
+			$minutes = "00";
+		}
+		$sum = $hours . ":" . $minutes;
+		return $sum;
+	}    
 
 }
