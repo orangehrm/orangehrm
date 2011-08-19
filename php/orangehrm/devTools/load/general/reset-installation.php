bb6e27a8ea77ea4e581ed6418d066a544b681185
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-08-19 03:52:51 +0000

    Adding General and Recruitment load scripts

---
+++ php/orangehrm/devTools/load/general/reset-installation.php
@@ -0,0 +1,86 @@
+<?php
+
+@require_once "../../../lib/confs/Conf.php";
+
+$c = new Conf();
+mysql_connect("$c->dbhost:$c->dbport", $c->dbuser, $c->dbpass);
+
+if (mysql_query("DROP DATABASE {$c->dbname}")) {
+    echo "Database '{$c->dbname}' was deleted.<br /><br />\n";
+} else {
+    echo "Couldn't delete datanse '{$c->dbname}'.<br /><br />\n";
+}  
+
+$file = "../../../lib/confs/Conf.php";
+
+if (file_exists($file)) {
+
+    if (@unlink($file)) {
+        echo "File '$file' was deleted.<br /><br />\n";
+    } else {
+        echo "Couldn't delete file '$file'.<br /><br />\n";
+    }
+
+}
+
+$file = "../../../lib/confs/cryptokeys/key.ohrm";
+
+if (file_exists($file)) {
+
+    if (@unlink($file)) {
+        echo "File '$file' was deleted.<br /><br />\n";
+    } else {
+        echo "Couldn't delete file '$file'.<br /><br />\n";
+    }
+
+}
+
+$file = "../../../lib/logs/notification_mails.log";
+
+if (file_exists($file)) {
+
+    if (@unlink($file)) {
+        echo "File '$file' was deleted.<br /><br />\n";
+    } else {
+        echo "Couldn't delete file '$file'.<br /><br />\n";
+    }
+
+}
+
+$file = "../../../symfony/apps/orangehrm/config/emailConfiguration.yml";
+
+if (file_exists($file)) {
+
+    if (@unlink($file)) {
+        echo "File '$file' was deleted.<br /><br />\n";
+    } else {
+        echo "Couldn't delete file '$file'.<br /><br />\n";
+    }
+
+}
+
+$file = "../../../symfony/apps/orangehrm/config/parameters.yml";
+
+if (file_exists($file)) {
+
+    if (@unlink($file)) {
+        echo "File '$file' was deleted.<br /><br />\n";
+    } else {
+        echo "Couldn't delete file '$file'.<br /><br />\n";
+    }
+
+}
+
+$file = "../../../symfony/config/databases.yml";
+
+if (file_exists($file)) {
+
+    if (@unlink($file)) {
+        echo "File '$file' was deleted.<br /><br />\n";
+    } else {
+        echo "Couldn't delete file '$file'.<br /><br />\n";
+    }
+
+}
+
+?>
