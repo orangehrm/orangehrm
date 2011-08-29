17797b0870a2e6bb2c2ff9dfba1cd46d14d128d7
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-08-29 05:43:12 +0000

    Added support for partials in PluginPannel component

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/modules/core/templates/_ohrmPluginPannel.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/modules/core/templates/_ohrmPluginPannel.php
@@ -1,7 +1,11 @@
 <?php
 if (isset($subComponents[$location])) {
     foreach ($subComponents[$location] as $subComponent) {
-        include_component($subComponent['module'], $subComponent['component']);
+        if (array_key_exists('component', $subComponent)) {
+            include_component($subComponent['module'], $subComponent['component']);
+        } elseif (array_key_exists('partial', $subComponent)) {
+            include_partial("{$subComponent['module']}/{$subComponent['partial']}");
+        }
     }
 }
 
