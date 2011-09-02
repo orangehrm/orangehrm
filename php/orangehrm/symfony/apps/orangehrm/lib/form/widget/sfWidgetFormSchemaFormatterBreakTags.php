fb46d339372bea88df4d1956be490ed4d059fca7
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-09-02 12:33:39 +0000

    Reverted commits: 8077:8079 which added a login action, which was work-in-progress.

--- php/orangehrm/symfony/apps/orangehrm/lib/form/widget/sfWidgetFormSchemaFormatterBreakTags.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class sfWidgetFormSchemaFormatterBreakTags extends sfWidgetFormSchemaFormatter {
-    protected $rowFormat       = "%error%%label%\n  %field%%help%\n%hidden_fields%<br class=\"clear\">\n";
-    protected $errorRowFormat  = "<span>\n%errors%</span>\n";
-    protected $helpFormat      = '<br />%help%';
-    protected $decoratorFormat = "<form>\n  %content%</form>";
-}
--- php/orangehrm/symfony/apps/orangehrm/modules/auth/actions/actions.class.php
+++ php/orangehrm/symfony/apps/orangehrm/modules/auth/actions/actions.class.php
@@ -9,9 +9,28 @@
  * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
  */
 class authActions extends sfActions {
+    
+ /**
+  * Login action. Forwards to OrangeHRM login page if not already logged in.
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeLogin(sfWebRequest $request) {
+      
+      $this->getContext()->getConfiguration()->loadHelpers('Url');
+      $this->redirect(public_path('../../login.php'));
+  }
+  
+    /**
+     * Show not authorized message
+     * @return boolean true if successfully deleted, false otherwise
+     */
+    public function executeUnauthorized(sfWebRequest $request) {
+        sfConfig::set('sf_web_debug', false);
+        sfConfig::set('sf_debug', false);
 
-    public function executeIndex(sfWebRequest $request) {
-        $this->forward('auth', 'login');
-    }
-
+        $response = $this->getResponse();
+        $response->setStatusCode(401, 'Not authorized');        
+        return $this->renderText("You do not have the proper credentials to access this page!");
+    }   
 }
