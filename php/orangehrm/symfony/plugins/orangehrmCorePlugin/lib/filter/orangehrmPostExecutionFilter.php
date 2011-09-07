f671b2f5d3b044b03cd00d49655d3d9fa1afd036
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-09-07 02:26:48 +0000

    Changed code in orangehrmPostExecutionFilter to add messages from extended pre & post execute methods

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/filter/orangehrmPostExecutionFilter.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/filter/orangehrmPostExecutionFilter.php
@@ -1,20 +1,28 @@
 <?php
 
 /* TODO: Rename this class with a proper name as it hangle both pre & post levels together  */
+
 class orangehrmPostExecutionFilter extends sfFilter {
 
     protected static $servicePool = array();
-    
+
     public function execute(sfFilterChain $filterChain) {
+
         $module = $this->getContext()->getModuleName();
         $action = $this->getContext()->getActionName();
-        
+
         $actionsStack = PluginExecutionManager::instance()->getPreExecuteMethodStack($module, $action);
         foreach ($actionsStack as $methodCall) {
-            list($service, $method) = explode('.', $methodCall);
+            $service = $methodCall['service'];
+            $method = $methodCall['method'];
+            $messages = $methodCall['messages'];
+
             $serviceInstance = $this->getServiceClassInstance($service);
             if (method_exists($serviceInstance, $method)) {
                 $serviceInstance->$method($this->getContext()->getRequest());
+                if (isset($messages[$serviceInstance::getState()])) {
+                    MessageRegistry::instance()->addMessage($messages[$serviceInstance::getState()], $module, $action);
+                }
             }
         }
 
@@ -22,15 +30,19 @@ class orangehrmPostExecutionFilter extends sfFilter {
 
         $actionsStack = PluginExecutionManager::instance()->getPostExecuteMethodStack($module, $action);
         foreach ($actionsStack as $methodCall) {
-            list($service, $method) = explode('.', $methodCall);
+            $service = $methodCall['service'];
+            $method = $methodCall['method'];
+            $messages = $methodCall['messages'];
+
             $serviceInstance = $this->getServiceClassInstance($service);
             if (method_exists($serviceInstance, $method)) {
                 $form = $this->getContext()->getActionStack()->getFirstEntry()->getActionInstance()->getForm();
-
                 $serviceInstance->$method($form);
+                if (isset($messages[$serviceInstance::getState()])) {
+                    MessageRegistry::instance()->addMessage($messages[$serviceInstance::getState()], $module, $action);
+                }
             }
         }
-
     }
 
     /**
@@ -44,5 +56,6 @@ class orangehrmPostExecutionFilter extends sfFilter {
         }
         return self::$servicePool[$className];
     }
+
 }
 
