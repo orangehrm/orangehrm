946bf838d60bf582541f78750803fac6a4631b13
chaturanganamal <chaturanganamal@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-09-15 06:06:18 +0000

    [2] - ID 200 - The From and Two date validation messages should be improved time module bug resolved

--- php/orangehrm/symfony/plugins/orangehrmTimePlugin/modules/time/templates/displayEmployeeReportCriteriaSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmTimePlugin/modules/time/templates/displayEmployeeReportCriteriaSuccess.php
@@ -17,17 +17,22 @@ use_javascript('../../../scripts/jquery/jquery.autocomplete.js');
     <div class="mainHeading"><h2 id="reportToHeading"><?php echo __($reportName); ?></h2></div>
     <form action="<?php echo url_for("time/displayEmployeeReportCriteria?reportId=2"); ?>" id="reportForm" method="post">
 
-        <table  border="0" cellpadding="5" cellspacing="0" class="employeeTable">
+<!--        <table  border="0" cellpadding="5" cellspacing="0" class="employeeTable">-->
+        <div class="employeeTable">
+            <br class="clear"/>
+
              <?php foreach($sf_data->getRaw('runtimeFilterFieldWidgetNamesAndLabelsList')  as $label):?>
-            <tr>
-            <th><?php echo $reportForm[$label['labelName']]->renderLabel();?></th>
-			<td><?php echo $reportForm[$label['labelName']]->render();?><?php echo $reportForm[$label['labelName']]->renderError();?></td>
-            </tr>
+            <div>
+            <?php echo $reportForm[$label['labelName']]->renderLabel();?>
+			<?php echo $reportForm[$label['labelName']]->render();?><?php echo $reportForm[$label['labelName']]->renderError();?>
+            </div>
+            <br class="clear"/>
             <?php endforeach;?>
             <?php echo $reportForm->renderHiddenFields(); ?>
-        </table>
+<!--        </table>--></div>
+
         <div class="formbuttons">
-            <td colspan="2"><input type="submit" id="viewbutton" class="viewbutton" value="<?php echo __('View') ?>"/></td>
+            <td colspan="2"><input type="button" id="viewbutton" class="viewbutton" value="<?php echo __('View') ?>"/></td>
         </div>
     </form>
 </div>
@@ -49,4 +54,89 @@ use_javascript('../../../scripts/jquery/jquery.autocomplete.js');
         padding-left: 15px;
         padding-top: 5px;
     }
+    html body div#outerbox.outerbox div.maincontent form#reportForm div.employeeTable div label label.error{
+        width: 300px !important;
+}
 </style>
+
+<script type="text/javascript">
+        $(document).ready(function() {
+            $('#viewbutton').click(function() {
+                if(isValidForm()) {
+                    $('#reportForm').submit();
+                }
+            });
+        });
+
+        function isValidForm(){
+
+            var dateFormat	= '<?php echo $sf_user->getDateFormat(); ?>';
+            var jsDateFormat = '<?php echo get_js_date_format($sf_user->getDateFormat()); ?>';
+            var dateDisplayFormat = dateFormat.toUpperCase();
+            var lang_dateError = '<?php echo __("From date should be less than To date") ?>';
+            var lang_validDateMsg = '<?php echo __("Please enter a valid date in %format% format", array('%format%' => strtoupper($sf_user->getDateFormat()))) ?>'
+
+        $.validator.addMethod("dateComparison", function(value, element, params) {
+            var temp = false;
+
+            var fromdate	=	$('#project_date_range_from_date').val();
+            var todate	=	$('#project_date_range_to_date').val();
+
+            if(fromdate.trim() == "" || todate.trim() == "" || fromdate == dateDisplayFormat || todate == dateDisplayFormat){
+                temp = true;
+            }else{
+                fromdate = (fromdate).split("-");
+                var fromdateObj = new Date(parseInt(fromdate[0],10), parseInt(fromdate[1],10) - 1, parseInt(fromdate[2],10));
+
+                todate = (todate).split("-");
+                var todateObj	=	new Date(parseInt(todate[0],10), parseInt(todate[1],10) - 1, parseInt(todate[2],10));
+
+                if ( fromdate <= todate){
+                    temp = true;
+                }
+            }
+            return temp;
+
+        });
+
+        var validator = $("#reportForm").validate({
+
+            rules: {
+                'time[project_date_range][from]' : {
+                    valid_date: function() {
+                        return {
+                            format:jsDateFormat,
+                            displayFormat:dateDisplayFormat,
+                            required:false
+                        }
+                    },
+                    dateComparison:true
+                },
+                'time[project_date_range][to]' : {
+                    valid_date: function() {
+                        return {
+                            format:jsDateFormat,
+                            displayFormat:dateDisplayFormat,
+                            required:false
+                        }
+                    }
+                }
+            },
+            messages: {
+                'time[project_date_range][from]' : {
+                    dateComparison : lang_dateError,
+                    valid_date: lang_validDateMsg
+                },
+                'time[project_date_range][to]' : {
+                    valid_date: lang_validDateMsg
+                }
+
+            },
+            errorPlacement: function(error, element) {
+                error.appendTo( element.prev('label') );
+            }
+
+        });
+        return true;
+    }
+</script>
--- php/orangehrm/symfony/plugins/orangehrmTimePlugin/modules/time/templates/displayProjectReportCriteriaSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmTimePlugin/modules/time/templates/displayProjectReportCriteriaSuccess.php
@@ -8,38 +8,132 @@
     <div class="mainHeading"><h2 id="reportToHeading"><?php echo __($reportName); ?></h2></div>
     <form action="<?php echo url_for("time/displayProjectReportCriteria?reportId=1"); ?>" id="reportForm" method="post">
 
-        <table  border="0" cellpadding="5" cellspacing="0" class="employeeTable">
+        <div class="employeeTable">
+            <br class="clear"/>
             <?php foreach ($sf_data->getRaw('runtimeFilterFieldWidgetNamesAndLabelsList') as $label): ?>
-                <tr>
-                    <th><?php echo $reportForm[$label['labelName']]->renderLabel(); ?></th>
-                    <td><?php echo $reportForm[$label['labelName']]->render(); ?><?php echo $reportForm[$label['labelName']]->renderError(); ?></td>
-                </tr>
+                <div>
+                <?php echo $reportForm[$label['labelName']]->renderLabel(); ?>
+                <?php echo $reportForm[$label['labelName']]->render(); ?>
+            </div>
+            <br class="clear"/>
             <?php endforeach; ?>
             <?php echo $reportForm->renderHiddenFields(); ?>
-            </table>
+            </div>
             <div class="formbuttons">
-                <td colspan="2"><input type="submit" class="viewbutton" value="<?php echo __('View') ?>"/></td>
-        </div>
-    </form>
-</div>
-<div class="paddingLeftRequired">Fields marked with an asterisk <span class="required"> * </span> are required.</div>
+                <td colspan="2"><input type="button" id="viewbutton" class="viewbutton" value="<?php echo __('View') ?>"/></td>
+            </div>
+        </form>
+    </div>
+    <div class="paddingLeftRequired">Fields marked with an asterisk <span class="required"> * </span> are required.</div>
 
-<style type="text/css">
+    <style type="text/css">
 
-    form#reportForm label {
-        margin-top: 6px;
-        width: 140px;
-        font-weight: normal;
+        form#reportForm label {
+            margin-top: 6px;
+            width: 140px;
+            font-weight: normal;
+        }
+
+        .viewbutton {
+            margin-left: 20px;
+        }
+
+        .paddingLeftRequired{
+            font-size: 8pt;
+            padding-left: 15px;
+            padding-top: 5px;
+        }
+        html body div#outerbox.outerbox div.maincontent form#reportForm div.employeeTable div label label.error{
+            width: 300px !important;
+        }
+
+    </style>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#viewbutton').click(function() {
+                if(isValidForm()) {
+                    $('#reportForm').submit();
+                }
+            });
+        });
+
+        function isValidForm(){
+
+            var dateFormat	= '<?php echo $sf_user->getDateFormat(); ?>';
+            var jsDateFormat = '<?php echo get_js_date_format($sf_user->getDateFormat()); ?>';
+            var dateDisplayFormat = dateFormat.toUpperCase();
+            var lang_dateError = '<?php echo __("From date should be less than To date") ?>';
+            var lang_validDateMsg = '<?php echo __("Please enter a valid date in %format% format", array('%format%' => strtoupper($sf_user->getDateFormat()))) ?>'
+            var lang_required = '<?php echo __("Project name is required") ?>';
+
+        $.validator.addMethod("dateComparison", function(value, element, params) {
+            var temp = false;
+
+            var fromdate	=	$('#project_date_range_from_date').val();
+            var todate	=	$('#project_date_range_to_date').val();
+
+            if(fromdate.trim() == "" || todate.trim() == "" || fromdate == dateDisplayFormat || todate == dateDisplayFormat){
+                temp = true;
+            }else{
+                fromdate = (fromdate).split("-");
+                var fromdateObj = new Date(parseInt(fromdate[0],10), parseInt(fromdate[1],10) - 1, parseInt(fromdate[2],10));
+
+                todate = (todate).split("-");
+                var todateObj	=	new Date(parseInt(todate[0],10), parseInt(todate[1],10) - 1, parseInt(todate[2],10));
+
+                if ( fromdate <= todate){
+                    temp = true;
+                }
+            }
+            return temp;
+
+        });
+
+        var validator = $("#reportForm").validate({
+
+            rules: {
+                'time[project_name]' : {
+                    required:true
+                },
+                'time[project_date_range][from]' : {
+                    valid_date: function() {
+                        return {
+                            format:jsDateFormat,
+                            displayFormat:dateDisplayFormat,
+                            required:false
+                        }
+                    },
+                    dateComparison:true
+                },
+                'time[project_date_range][to]' : {
+                    valid_date: function() {
+                        return {
+                            format:jsDateFormat,
+                            displayFormat:dateDisplayFormat,
+                            required:false
+                        }
+                    }
+                }
+            },
+            messages: {
+                'time[project_name]': {
+                    required: lang_required
+                },
+                'time[project_date_range][from]' : {
+                    dateComparison : lang_dateError,
+                    valid_date: lang_validDateMsg
+                },
+                'time[project_date_range][to]' : {
+                    valid_date: lang_validDateMsg
+                }
+
+            },
+            errorPlacement: function(error, element) {
+                error.appendTo( element.prev('label') );
+            }
+
+        });
+        return true;
     }
-
-    .viewbutton {
-        margin-left: 20px;
-    }
-
-    .paddingLeftRequired{
-        font-size: 8pt;
-        padding-left: 15px;
-        padding-top: 5px;
-    }
-
-</style>
+</script>
