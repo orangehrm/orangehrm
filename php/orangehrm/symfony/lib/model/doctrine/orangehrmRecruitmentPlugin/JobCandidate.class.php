439f63cb50cf36d3b2b27f414ef33b06f493f8e9
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-09-26 08:10:44 +0000

    Removed additional spaces between first name and last name when the middle name was missing

--- php/orangehrm/symfony/lib/model/doctrine/orangehrmRecruitmentPlugin/JobCandidate.class.php
+++ php/orangehrm/symfony/lib/model/doctrine/orangehrmRecruitmentPlugin/JobCandidate.class.php
@@ -11,7 +11,6 @@
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
 class JobCandidate extends PluginJobCandidate {
-
     const ACTIVE = 1;
     const ARCHIVED = 2;
     const TYPE = "CANDIDATE";
@@ -19,8 +18,9 @@ class JobCandidate extends PluginJobCandidate {
     const MODE_OF_APPLICATION_MANUAL = 1;
     const MODE_OF_APPLICATION_ONLINE = 2;
 
-    public function getFullName(){
-        return $this->getFirstName()." ".$this->getMiddleName()." ".$this->getLastName();
+    public function getFullName() {
+        $middleName = trim($this->getMiddleName());
+        return $this->getFirstName() . ' ' . (empty($middleName) ? '' : $middleName . ' ') . $this->getLastName();
     }
 
 }
