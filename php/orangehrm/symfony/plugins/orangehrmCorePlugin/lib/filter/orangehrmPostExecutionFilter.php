b5202d083992f5b869bf85e09ed44d8ada2cea06
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-10-06 13:00:45 +0000

    Changes related to PHP 5.2.x compatibility in orangehrmPostExecutionFilter.php

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/filter/orangehrmPostExecutionFilter.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/filter/orangehrmPostExecutionFilter.php
@@ -20,8 +20,8 @@ class orangehrmPostExecutionFilter extends sfFilter {
             $serviceInstance = $this->getServiceClassInstance($service);
             if (method_exists($serviceInstance, $method)) {
                 $serviceInstance->$method($this->getContext()->getRequest());
-                if (isset($messages[$serviceInstance::getState()])) {
-                    MessageRegistry::instance()->addMessage($messages[$serviceInstance::getState()], $module, $action);
+                if (isset($messages[$serviceInstance->getState()])) {
+                    MessageRegistry::instance()->addMessage($messages[$serviceInstance->getState()], $module, $action);
                 }
             }
         }
@@ -38,8 +38,8 @@ class orangehrmPostExecutionFilter extends sfFilter {
             if (method_exists($serviceInstance, $method)) {
                 $form = $this->getContext()->getActionStack()->getFirstEntry()->getActionInstance()->getForm();
                 $serviceInstance->$method($form);
-                if (isset($messages[$serviceInstance::getState()])) {
-                    MessageRegistry::instance()->addMessage($messages[$serviceInstance::getState()], $module, $action);
+                if (isset($messages[$serviceInstance->getState()])) {
+                    MessageRegistry::instance()->addMessage($messages[$serviceInstance->getState()], $module, $action);
                 }
             }
         }
