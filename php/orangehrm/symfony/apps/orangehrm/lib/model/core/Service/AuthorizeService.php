eb5d2573fc3ba02b901d603f9e651ec9c98fad89
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-10-18 04:28:40 +0000

    Made some indentation corrections to AuthorizeService

--- php/orangehrm/symfony/apps/orangehrm/lib/model/core/Service/AuthorizeService.php
+++ php/orangehrm/symfony/apps/orangehrm/lib/model/core/Service/AuthorizeService.php
@@ -1,5 +1,6 @@
 <?php
-/* 
+
+/*
  * 
  * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
  * all the essential functionalities required for any enterprise.
@@ -25,148 +26,144 @@
  * @author orange
  */
 class AuthorizeService extends BaseService {
-	
-	const AUTHORIZE_ROLE_ADMIN 			= 'Admin';
-	const AUTHORIZE_ROLE_SUPERVISOR 	= 'Supervisor';
-	const AUTHORIZE_ROLE_ESS 			= 'ESS';
-	const AUTHORIZE_ROLE_PROJECT_ADMIN 	= "ProjectAdmin";
-    const AUTHORIZE_ROLE_MANAGER 		= 'Manager';
-    const AUTHORIZE_ROLE_DIRECTOR 		= 'Director';
+    const AUTHORIZE_ROLE_ADMIN = 'Admin';
+    const AUTHORIZE_ROLE_SUPERVISOR = 'Supervisor';
+    const AUTHORIZE_ROLE_ESS = 'ESS';
+    const AUTHORIZE_ROLE_PROJECT_ADMIN = "ProjectAdmin";
+    const AUTHORIZE_ROLE_MANAGER = 'Manager';
+    const AUTHORIZE_ROLE_DIRECTOR = 'Director';
 
     const YES = 'Yes';
     const NO = 'No';
-	
-    
-	public $roleAdmin 			= "Admin";
-	public $roleSupervisor 		= "Supervisor";
-	public $roleESS 			= "ESS";
-	public $roleProjectAdmin 	= "ProjectAdmin";
-    public $roleManager 		= "Manager";
-    public $roleDirector 		= "Director";
-    public $roleAcceptor 		= "Acceptor";
-    public $roleOfferer 		= "Offerer";
 
-        private $employeeService;
 
-	private $employeeID;
-	private $isAdmin;
-	private $roles;
+    public $roleAdmin = "Admin";
+    public $roleSupervisor = "Supervisor";
+    public $roleESS = "ESS";
+    public $roleProjectAdmin = "ProjectAdmin";
+    public $roleManager = "Manager";
+    public $roleDirector = "Director";
+    public $roleAcceptor = "Acceptor";
+    public $roleOfferer = "Offerer";
+    private $employeeService;
+    private $employeeID;
+    private $isAdmin;
+    private $roles;
 
-        /**
-         * Get EmployeeService
-         * @returns EmployeeService
-         */
-        public function getEmployeeService() {
-            if (is_null($this->employeeService)) {
-                $this->employeeService = new EmployeeService();
-                $this->employeeService->setEmployeeDao(new EmployeeDao());
-            }
-            return $this->employeeService;
+    /**
+     * Get EmployeeService
+     * @returns EmployeeService
+     */
+    public function getEmployeeService() {
+        if (is_null($this->employeeService)) {
+            $this->employeeService = new EmployeeService();
+            $this->employeeService->setEmployeeDao(new EmployeeDao());
+        }
+        return $this->employeeService;
+    }
+
+    /**
+     * Set EmployeeService
+     * @param EmployeeService $employeeService
+     */
+    public function setEmployeeService(EmployeeService $employeeService) {
+        $this->employeeService = $employeeService;
+    }
+
+    public function setEmployeeId($employeeId) {
+        $this->employeeID = $employeeId;
+    }
+
+    public function getEmployeeId() {
+        return $this->employeeID;
+    }
+
+    public function setIsAdmin($isAdmin) {
+        $this->isAdmin = $isAdmin;
+    }
+
+    public function getIsAdmin() {
+        return $this->isAdmin;
+    }
+
+    public function setRoles($roles) {
+        $this->roles = $roles;
+    }
+
+    public function getRoles() {
+        return $this->roles;
+    }
+
+    /**
+     * Class contructor
+     *
+     * @param String $employeeId
+     * @param String $isAdmin
+     */
+    public function __construct($employeeId, $isAdmin) {
+        $this->setEmployeeId($employeeId);
+        $this->setIsAdmin($isAdmin);
+
+        $this->setRoles($this->_roles());
+    }
+
+    /**
+     * Constructs roles
+     *
+     * @return boolean[]
+     */
+    private function _roles() {
+        $roles = null;
+        $isAdmin = $this->getIsAdmin();
+        $empId = $this->getEmployeeId();
+
+        if ($isAdmin === self::YES) {
+            $roles[$this->roleAdmin] = true;
+        } else {
+            $roles[$this->roleAdmin] = false;
         }
 
-        /**
-         * Set EmployeeService
-         * @param EmployeeService $employeeService
-         */
-        public function setEmployeeService(EmployeeService $employeeService) {
-            $this->employeeService = $employeeService;
+        $roles[$this->roleSupervisor] = $this->_checkIsSupervisor();
+        $roles[$this->roleProjectAdmin] = $this->_checkIsProjectAdmin();
+        $roles[$this->roleManager] = $this->_checkIsManager();
+        $roles[$this->roleDirector] = $this->_checkIsDirector();
+        $roles[$this->roleAcceptor] = $this->_checkIsAcceptor();
+        $roles[$this->roleOfferer] = $this->_checkIsOfferer();
+
+        if (!empty($empId)) {
+            $roles[$this->roleESS] = true;
+        } else {
+            $roles[$this->roleESS] = false;
         }
 
-	public function setEmployeeId($employeeId) {
-		$this->employeeID = $employeeId;
-	}
+        return $roles;
+    }
 
-	public function getEmployeeId() {
-		return $this->employeeID;
-	}
+    /**
+     * Check whether there are any subordinates
+     *
+     * @return boolean
+     */
+    private function _checkIsSupervisor() {
+        $isSupervisor = false;
 
-	public function setIsAdmin($isAdmin) {
-		$this->isAdmin = $isAdmin;
-	}
+        if (!empty($this->employeeID)) {
+            $isSupervisor = $this->getEmployeeService()->isSupervisor($this->employeeID);
+        }
 
-	public  function getIsAdmin() {
-		return $this->isAdmin;
-	}
+        return $isSupervisor;
+    }
 
-	public function setRoles($roles) {
-		$this->roles = $roles;
-	}
+    /**
+     * Check whether the user is a project admin
+     *
+     * @param int $projectId Project for which to check. If not given, all projects are checked.
+     * @return boolean
+     */
+    private function _checkIsProjectAdmin($projectId = null) {
 
-	public  function getRoles() {
-		return $this->roles;
-	}
-
-	/**
-	 * Class contructor
-	 *
-	 * @param String $employeeId
-	 * @param String $isAdmin
-	 */
-	public function __construct($employeeId, $isAdmin) {
-		$this->setEmployeeId($employeeId);
-		$this->setIsAdmin($isAdmin);
-
-		$this->setRoles($this->_roles());
-	}
-
-	/**
-	 * Constructs roles
-	 *
-	 * @return boolean[]
-	 */
-	private function _roles() {
-		$roles 		= null;
-		$isAdmin 	= $this->getIsAdmin();
-		$empId 		= $this->getEmployeeId();
-
-		if ($isAdmin === self::YES) {
-			$roles[$this->roleAdmin] = true;
-		} else {
-			$roles[$this->roleAdmin] = false;
-		}
-
-		$roles[$this->roleSupervisor] 	= $this->_checkIsSupervisor();
-		$roles[$this->roleProjectAdmin] = $this->_checkIsProjectAdmin();
-        $roles[$this->roleManager] 		= $this->_checkIsManager();
-        $roles[$this->roleDirector] 	= $this->_checkIsDirector();
-        $roles[$this->roleAcceptor] 	= $this->_checkIsAcceptor();
-        $roles[$this->roleOfferer] 		= $this->_checkIsOfferer();
-
-		if (!empty($empId)) {
-			$roles[$this->roleESS] = true;
-		} else {
-			$roles[$this->roleESS] = false;
-		}
-
-		return $roles;
-	}
-
-	/**
-	 * Check whether there are any subordinates
-	 *
-	 * @return boolean
-	 */
-	private function _checkIsSupervisor() {
-                $isSupervisor = false;
-
-                if (!empty($this->employeeID)) {
-                    $isSupervisor = $this->getEmployeeService()->isSupervisor($this->employeeID);
-                }
-
-                return $isSupervisor;
-
-	}
-
-	/**
-	 * Check whether the user is a project admin
-	 *
-	 * @param int $projectId Project for which to check. If not given, all projects are checked.
-	 * @return boolean
-	 */
-	private function _checkIsProjectAdmin($projectId = null) {
-
-		return false;
-	}
+        return false;
+    }
 
     /**
      * Check whether the user is a Manager
@@ -174,7 +171,7 @@ class AuthorizeService extends BaseService {
      * @return boolean
      */
     private function _checkIsManager() {
-    	 
+
         return false;
     }
 
@@ -185,7 +182,7 @@ class AuthorizeService extends BaseService {
      */
     private function _checkIsDirector() {
 
-		return false;
+        return false;
     }
 
     /**
@@ -195,8 +192,7 @@ class AuthorizeService extends BaseService {
      */
     private function _checkIsAcceptor() {
 
-		return false;
-
+        return false;
     }
 
     /**
@@ -206,37 +202,35 @@ class AuthorizeService extends BaseService {
      */
     private function _checkIsOfferer() {
 
-		return false;
-
+        return false;
     }
 
+    /**
+     * Checks whether an admin
+     *
+     * @return boolean
+     */
+    public function isAdmin() {
+        return $this->_chkRole($this->roleAdmin);
+    }
 
-	/**
-	 * Checks whether an admin
-	 *
-	 * @return boolean
-	 */
-	public function isAdmin() {
-		return $this->_chkRole($this->roleAdmin);
-	}
+    /**
+     * Checks whether an supervisor
+     *
+     * @return boolean
+     */
+    public function isSupervisor() {
+        return $this->_chkRole($this->roleSupervisor);
+    }
 
-	/**
-	 * Checks whether an supervisor
-	 *
-	 * @return boolean
-	 */
-	public function isSupervisor() {
-		return $this->_chkRole($this->roleSupervisor);
-	}
-
-	/**
-	 * Checks whether a project admin
-	 *
-	 * @return boolean true if a project admin. False otherwise
-	 */
-	public function isProjectAdmin() {
-		return $this->_chkRole($this->roleProjectAdmin);
-	}
+    /**
+     * Checks whether a project admin
+     *
+     * @return boolean true if a project admin. False otherwise
+     */
+    public function isProjectAdmin() {
+        return $this->_chkRole($this->roleProjectAdmin);
+    }
 
     /**
      * Checks whether a Manager
@@ -274,123 +268,122 @@ class AuthorizeService extends BaseService {
         return $this->_chkRole($this->roleOfferer);
     }
 
-	/**
-	 * Checks whether an ESS
-	 *
-	 * @return boolean
-	 */
-	public function isESS() {
-		return $this->_chkRole($this->roleESS);
-	}
+    /**
+     * Checks whether an ESS
+     *
+     * @return boolean
+     */
+    public function isESS() {
+        return $this->_chkRole($this->roleESS);
+    }
 
-	/**
-	 * Checks whether the particular employee is
-	 * the supervisor of the subordinate concerned
-	 *
-	 * @param unknown_type $subordinateId
-	 * @return boolean
-	 */
-	public function isTheSupervisor($subordinateId) {
+    /**
+     * Checks whether the particular employee is
+     * the supervisor of the subordinate concerned
+     *
+     * @param unknown_type $subordinateId
+     * @return boolean
+     */
+    public function isTheSupervisor($subordinateId) {
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Checks whether employee is a project admin of the
-	 * given project.
-	 *
-	 * @param int $projectId The project id
-	 * @return bool true if a project admin, false otherwise
-	 */
-	public function isProjectAdminOf($projectId) {
-		return $this->_checkIsProjectAdmin($projectId);
-	}
+    /**
+     * Checks whether employee is a project admin of the
+     * given project.
+     *
+     * @param int $projectId The project id
+     * @return bool true if a project admin, false otherwise
+     */
+    public function isProjectAdminOf($projectId) {
+        return $this->_checkIsProjectAdmin($projectId);
+    }
 
-	/**
-	 * Test whether element at pos of the array is equal to match
-	 *
-	 * @param Array array
-	 * @param String match
-	 * @param int pos
-	 */
-	private function searchArray($array, $match, $pos) {
-		if ($array[$pos] == $match) {
-				return true;
-		}
-		return false;
-	}
+    /**
+     * Test whether element at pos of the array is equal to match
+     *
+     * @param Array array
+     * @param String match
+     * @param int pos
+     */
+    private function searchArray($array, $match, $pos) {
+        if ($array[$pos] == $match) {
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Delegates all checks for all is<Role>
-	 * functions
-	 *
-	 * @param String $role
-	 * @return boolean
-	 */
-	private function _chkRole($role) {
-		$roles = $this->getRoles();
+    /**
+     * Delegates all checks for all is<Role>
+     * functions
+     *
+     * @param String $role
+     * @return boolean
+     */
+    private function _chkRole($role) {
+        $roles = $this->getRoles();
 
-		if (isset($roles[$role]) && $roles[$role]) {
-			return true;
-		}
+        if (isset($roles[$role]) && $roles[$role]) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Returns the first role out of the array of
-	 * roles sent
-	 *
-	 * @param String[] $roleArr
-	 * @return String/boolean
-	 */
-	public function firstRole($roleArr) {
-		for ($i=0; $i<count($roleArr); $i++) {
-			if ($this->_chkRole($roleArr[$i])){
-				return $roleArr[$i];
-			}
-		}
+    /**
+     * Returns the first role out of the array of
+     * roles sent
+     *
+     * @param String[] $roleArr
+     * @return String/boolean
+     */
+    public function firstRole($roleArr) {
+        for ($i = 0; $i < count($roleArr); $i++) {
+            if ($this->_chkRole($roleArr[$i])) {
+                return $roleArr[$i];
+            }
+        }
 
-		return false;
-	}
-	
-	public function isActionPermitted($action) {
-	    
-	    $permitted = false;
-	    
-	    switch ($action) {
-	    	
-	    	case 'TCP' :	if (!$this->isAdmin() && !$this->isSupervisor()) {
-	    	    				$permitted = false;
-	    					} else {
-	    					    $permitted = true;
-	    					}
-	    					break;
-	    					
-	    	case 'CST' :	if (!$this->isAdmin() && !$this->isSupervisor()) {
-	    	    				$permitted = false;
-	    					} else {
-	    					    $permitted = true;
-	    					}
-	    					break;
-	    					
-	    	case 'PAC' :	if (!$this->isAdmin() && !$this->isProjectAdmin()) {
-	    	    				$permitted = false;
-	    					} else {
-	    					    $permitted = true;
-	    					}
-	    					break;
-	        
-	        default : 		if ($this->isAdmin()) {
-	            	        	$permitted = true;
-	        		  		} else {
-	        		      		$permitted = false;
-	        		  		}
-	        		  		break;
-	        
-	    }
-	    
-	    return $permitted;	    
-	    
-	}
+        return false;
+    }
+
+    public function isActionPermitted($action) {
+
+        $permitted = false;
+
+        switch ($action) {
+
+            case 'TCP' : if (!$this->isAdmin() && !$this->isSupervisor()) {
+                    $permitted = false;
+                } else {
+                    $permitted = true;
+                }
+                break;
+
+            case 'CST' : if (!$this->isAdmin() && !$this->isSupervisor()) {
+                    $permitted = false;
+                } else {
+                    $permitted = true;
+                }
+                break;
+
+            case 'PAC' : if (!$this->isAdmin() && !$this->isProjectAdmin()) {
+                    $permitted = false;
+                } else {
+                    $permitted = true;
+                }
+                break;
+
+            default : if ($this->isAdmin()) {
+                    $permitted = true;
+                } else {
+                    $permitted = false;
+                }
+                break;
+        }
+
+        return $permitted;
+    }
+
 }
