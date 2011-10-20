9e5eb07070556695f1bfdea1710ab007718a7ffc
chaturanganamal <chaturanganamal@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-10-20 10:51:33 +0000

    date localization bug fixed

--- php/orangehrm/symfony/plugins/orangehrmRecruitmentPlugin/modules/recruitment/templates/addCandidateSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmRecruitmentPlugin/modules/recruitment/templates/addCandidateSuccess.php
@@ -240,7 +240,7 @@
                                     var lang_firstNameRequired = "<?php echo __("First name is required"); ?>";
                                     var lang_lastNameRequired = "<?php echo __("Last name is required"); ?>";
                                     var lang_emailRequired = "<?php echo __("E-mail is required"); ?>";
-                                    var lang_validDateMsg = '<?php echo __("Please enter a valid date in %format% format", array('%format%' => strtoupper($sf_user->getDateFormat()))); ?>'
+                                    var lang_validDateMsg = '<?php echo __("Please enter a valid date in %format% format", array('%format%' => get_datepicker_date_format($sf_user->getDateFormat()))); ?>';
                                     var lang_validEmail = "<?php echo __("Email address should contain at least one '.' and one '@' Example:user@example.com"); ?>";
                                     var list = <?php echo json_encode($allVacancylist); ?>;
                                     var allowedVacancylistWithClosedVacancies = <?php echo json_encode($allowedVacancylistWithClosedVacancies); ?>;
