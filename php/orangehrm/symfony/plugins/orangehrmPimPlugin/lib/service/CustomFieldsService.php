dc225c10b9879839eb1b10e861bc4c7526671191
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-11-02 04:10:13 +0000

    Restored changes related to reports lost in merge (rev: 8582) Fixes custom fields not appearing in pim reports.

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/service/CustomFieldsService.php
+++ php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/service/CustomFieldsService.php
@@ -58,7 +58,11 @@ class CustomFieldsService extends BaseService {
     */
    public function saveCustomField(CustomFields $customFields) {
       try {
-         return $this->customFieldsDao->saveCustomField($customFields);
+          $reportGeneratorService = new ReportGeneratorService();
+          $customFields = $this->customFieldsDao->saveCustomField($customFields);
+          $reportGeneratorService->saveCustomDisplayField($customFields, "3");
+          return $customFields;
+//         return $this->customFieldsDao->saveCustomField($customFields);
       } catch(DataDuplicationException $e) {
          //this is for backward compatibility, need to identify the references and update them
          throw new DuplicateNameException($e->getMessage());
@@ -75,6 +79,8 @@ class CustomFieldsService extends BaseService {
     */
    public function deleteCustomField($customFieldList) {
       try {
+          $reportGeneratorService = new ReportGeneratorService();
+          $reportGeneratorService->deleteCustomDisplayFieldList($customFieldList);
          return $this->customFieldsDao->deleteCustomField($customFieldList);
       } catch(Exception $e) {
          throw new AdminServiceException($e->getMessage());
