030391fc274ae45ffaeb0fc066897a94f4d0db6c
yasithacp <yasithacp@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-12-05 10:16:16 +0000

    bug fixed

---
+++ php/orangehrm/symfony/plugins/orangehrmAdminPlugin/modules/admin/actions/deleteWorkShiftsAction.class.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+class deleteWorkShiftsAction extends sfAction {
+	
+	private $workShiftService;
+
+	public function getWorkShiftService() {
+		if (is_null($this->workShiftService)) {
+			$this->workShiftService = new WorkShiftService();
+			$this->workShiftService->setWorkShiftDao(new WorkShiftDao());
+		}
+		return $this->workShiftService;
+	}
+	
+	public function execute($request) {
+
+		$toBeDeletedShiftIds = $request->getParameter('chkSelectRow');
+
+		if (!empty($toBeDeletedShiftIds)) {
+
+			foreach ($toBeDeletedShiftIds as $toBeDeletedShiftId) {
+
+				$shift = $this->getWorkShiftService()->getWorkShiftById($toBeDeletedShiftId);
+				$shift->delete();
+			}
+			$this->getUser()->setFlash('templateMessage', array('success', __('Selected Work Shift(s) Deleted Successfully')));
+		}
+
+		$this->redirect('admin/workShift');
+	}
+}
+
+?>
