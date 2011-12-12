6642c2cb3ad94a8bbbb9cc85c00ae4beb38b78ec
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-12-12 07:54:40 +0000

    Changed version numbers for 2.6.12

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/modules/core/templates/_footer.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/modules/core/templates/_footer.php
@@ -1,7 +1,7 @@
 <?php
 $styleSheet = 'orange';
 $imagePath = public_path("../../themes/{$styleSheet}/images/login");
-$version = '2.6.12-beta.1';
+$version = '2.6.12';
 $copyrightYear = '2011';
 ?>
 
