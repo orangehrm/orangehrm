8895662dd431b774142127a40034f2fa2f3a183b
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2011-12-23 05:53:45 +0000

    Adding OrangeHRM community news section to index.php

--- php/orangehrm/index.php
+++ php/orangehrm/index.php
@@ -853,7 +853,44 @@ $menuObj->getJavascript($menu);
     </head>
 
     <body>
-        <div id="companyLogoHeader"></div><div id="rightHeaderImage"></div>
+        <div id="companyLogoHeader"></div>
+
+	<div align='right' style="height:0px; width:100%;">
+	<?php
+
+	try
+{
+if($xml = @simplexml_load_file("http://www.orangehrm.com/global_update/orangehrm_updates.xml"))
+	{
+	foreach($xml->children() as $child)
+	  {
+		$data[] = $child;
+	  }
+	for($i=0; $i<count($data); $i=$i+3)
+		{
+			echo "<div style='width: auto; float:right; margin: 12px 2px 0px 0px;'>
+				<table border='0'>
+					<tr><td><a href='".$data[$i]."' target='_blank' style='text-decoration:none;'><font style='color:green; font-weight:bold; font-size:12px;'>".$data[$i+1]."</font>&nbsp;&nbsp;</td></tr>
+					<tr><td align='center'><a href='".$data[$i]."' target='_blank' style='text-decoration:none;'><font style='font-size:10px; font-weight:bold;'>".$data[$i+2]."</font></a></td></tr>
+				</table>
+			      </div>";
+		}
+	}
+else
+	{
+		echo "";
+	}
+}
+catch(exception $e)
+{
+	echo "";
+}
+
+	?>	
+	</div>	
+
+
+	<div id="rightHeaderImage"></div>
 <?php $menuObj->getMenu($menu, $optionMenu, $welcomeMessage); ?>
 
         <div id="main-content" style="float:left;height:640px;text-align:center;padding-left:0px;">
