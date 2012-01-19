fb06c95657668951a0121eee16132d04d5a65518
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-01-19 17:47:38 +0000

    Corrected the place of error display when rendering form using BreakTags widget schema formatter

--- php/orangehrm/symfony/apps/orangehrm/lib/form/widget/sfWidgetFormSchemaFormatterBreakTags.php
+++ php/orangehrm/symfony/apps/orangehrm/lib/form/widget/sfWidgetFormSchemaFormatterBreakTags.php
@@ -2,7 +2,7 @@
 
 class sfWidgetFormSchemaFormatterBreakTags extends sfWidgetFormSchemaFormatter {
 
-    protected $rowFormat = "%error%%label%\n  %field%%help%\n%hidden_fields%<br class=\"clear\">\n";
+    protected $rowFormat = "%label%\n  %field%%help%\n%hidden_fields%<br class=\"clear\">%error%\n";
     protected $errorRowFormat = "<span>\n%errors%</span>\n";
     protected $helpFormat = '<br />%help%';
     protected $decoratorFormat = "<form>\n  %content%</form>";
