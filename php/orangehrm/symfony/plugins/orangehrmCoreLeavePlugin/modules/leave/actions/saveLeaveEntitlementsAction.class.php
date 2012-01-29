96dcdd338ab1ee92166f8947e40f1fadf85a9f80
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-01-29 10:33:40 +0000

    Temperorily removed CSRF protection when saving leave entitlements

--- php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/actions/saveLeaveEntitlementsAction.class.php
+++ php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/actions/saveLeaveEntitlementsAction.class.php
@@ -11,32 +11,29 @@ class saveLeaveEntitlementsAction extends baseLeaveAction {
 
         if ($request->isMethod(sfRequest::POST)) {
             $form->bind($request->getParameter($form->getName()));
-            if ($form->isValid()) {
-                $hdnEmpId = $request->getParameter('hdnEmpId');
-                $hdnLeaveTypeId = $request->getParameter('hdnLeaveTypeId');
-                $hdnLeavePeriodId = $request->getParameter('hdnLeavePeriodId');
-                $txtLeaveEntitled = $request->getParameter('txtLeaveEntitled');
-                $count = count($txtLeaveEntitled);
 
-                $leaveEntitlementService = $this->getLeaveEntitlementService();
-                $leaveSummaryData = $request->getParameter('leaveSummary');
+            $hdnEmpId = $request->getParameter('hdnEmpId');
+            $hdnLeaveTypeId = $request->getParameter('hdnLeaveTypeId');
+            $hdnLeavePeriodId = $request->getParameter('hdnLeavePeriodId');
+            $txtLeaveEntitled = $request->getParameter('txtLeaveEntitled');
+            $count = count($txtLeaveEntitled);
 
-                for ($i = 0; $i < $count; $i++) {
-                    $leavePeriodId = empty($hdnLeavePeriodId[$i]) ? $leaveSummaryData['hdnSubjectedLeavePeriod'] : $hdnLeavePeriodId[$i];
-                    try {
-                        $leaveEntitlementService->saveEmployeeLeaveEntitlement($hdnEmpId[$i], $hdnLeaveTypeId[$i], $leavePeriodId, $txtLeaveEntitled[$i], true);
-                    } catch (Exception $e) {
-                        $saveSuccess = false;
-                    }
+            $leaveEntitlementService = $this->getLeaveEntitlementService();
+            $leaveSummaryData = $request->getParameter('leaveSummary');
+
+            for ($i = 0; $i < $count; $i++) {
+                $leavePeriodId = empty($hdnLeavePeriodId[$i]) ? $leaveSummaryData['hdnSubjectedLeavePeriod'] : $hdnLeavePeriodId[$i];
+                try {
+                    $leaveEntitlementService->saveEmployeeLeaveEntitlement($hdnEmpId[$i], $hdnLeaveTypeId[$i], $leavePeriodId, $txtLeaveEntitled[$i], true);
+                } catch (Exception $e) {
+                    $saveSuccess = false;
                 }
+            }
 
-                if ($saveSuccess) {
-                    $this->getUser()->setFlash('templateMessage', array('SUCCESS', __('Leave Entitlements Successfully Saved')));
-                } else {
-                    $this->getUser()->setFlash('templateMessage', array('FAILURE', __('Failed to Save Leave Entitlements')));
-                }
+            if ($saveSuccess) {
+                $this->getUser()->setFlash('templateMessage', array('SUCCESS', __('Leave Entitlements Successfully Saved')));
             } else {
-                $this->getUser()->setFlash('templateMessage', array('WARNING', __('Invalid Data Passed')));
+                $this->getUser()->setFlash('templateMessage', array('FAILURE', __('Failed to Save Leave Entitlements')));
             }
 
             $this->forward('leave', 'viewLeaveSummary');
@@ -53,10 +50,10 @@ class saveLeaveEntitlementsAction extends baseLeaveAction {
             $formOptions = $this->getLoggedInUserDetails();
             $this->form = new LeaveSummaryForm($formDefaults, $formOptions, true);
         }
-        
+
         return $this->form;
     }
-    
+
     /**
      *
      * @param LeaveSummaryForm $form 
