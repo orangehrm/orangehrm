172ff70a9dd65aa643080621949316920c54081c
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-02 07:44:53 +0000

    Added the capability to include javascripts from forms to defineWorkWeekSuccess template

--- php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/templates/defineWorkWeekSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/templates/defineWorkWeekSuccess.php
@@ -20,6 +20,8 @@
 
 <?php use_stylesheet('../orangehrmCoreLeavePlugin/css/defineWorkWeekSuccess'); ?>
 <?php use_javascripts_for_form($workWeekForm); ?>
+<?php use_stylesheets_for_form($workWeekForm); ?>
+
 <div id="messageBalloonContainer" style="width:380px;">
     <?php echo isset($templateMessage) ? templateMessage($templateMessage) : ''; ?>
 </div>
