7171553a9f941a24bd210590e25076fedacfbcf4
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-14 09:23:24 +0000

    Made some indentation improvements to JobInterviewDaoTest

--- php/orangehrm/symfony/plugins/orangehrmRecruitmentPlugin/test/model/dao/JobInterviewDaoTest.php
+++ php/orangehrm/symfony/plugins/orangehrmRecruitmentPlugin/test/model/dao/JobInterviewDaoTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
  * all the essential functionalities required for any enterprise.
@@ -25,118 +24,118 @@ require_once sfConfig::get('sf_test_dir') . '/util/TestDataService.php';
  */
 class JobInterviewDaoTest extends PHPUnit_Framework_TestCase {
 
-	private $jobInterviewDao;
-	protected $fixture;
+    private $jobInterviewDao;
+    protected $fixture;
 
-	/**
-	 * Set up method
-	 */
-	protected function setUp() {
+    /**
+     * Set up method
+     */
+    protected function setUp() {
 
-		$this->jobInterviewDao = new JobInterviewDao();
-		$this->fixture = sfConfig::get('sf_plugins_dir') . '/orangehrmRecruitmentPlugin/test/fixtures/CandidateDao.yml';
-		TestDataService::populate($this->fixture);
-	}
+        $this->jobInterviewDao = new JobInterviewDao();
+        $this->fixture = sfConfig::get('sf_plugins_dir') . '/orangehrmRecruitmentPlugin/test/fixtures/CandidateDao.yml';
+        TestDataService::populate($this->fixture);
+    }
 
-	/**
-	 * Test getInterviewListByCandidateIdAndInterviewDate for check return objects
-	 */
-	public function testGetInterviewListByCandidateIdAndInterviewDateAndTimeForCorrectObjects() {
+    /**
+     * Test getInterviewListByCandidateIdAndInterviewDate for check return objects
+     */
+    public function testGetInterviewListByCandidateIdAndInterviewDateAndTimeForCorrectObjects() {
 
-		$interviewList = $this->jobInterviewDao->getInterviewListByCandidateIdAndInterviewDateAndTime(4, '2011-08-18', '9:00:00', '11:00:00');
-		$this->assertTrue($interviewList[0] instanceof JobInterview);
-	}
+        $interviewList = $this->jobInterviewDao->getInterviewListByCandidateIdAndInterviewDateAndTime(4, '2011-08-18', '9:00:00', '11:00:00');
+        $this->assertTrue($interviewList[0] instanceof JobInterview);
+    }
 
-	/**
-	 * Test getInterviewListByCandidateIdAndInterviewDate for existing results
-	 */
-	public function testGetInterviewListByCandidateIdAndInterviewDateAndTimeForExistingResults() {
+    /**
+     * Test getInterviewListByCandidateIdAndInterviewDate for existing results
+     */
+    public function testGetInterviewListByCandidateIdAndInterviewDateAndTimeForExistingResults() {
 
-		$interviewList = $this->jobInterviewDao->getInterviewListByCandidateIdAndInterviewDateAndTime(1, '2011-08-03', '8:30:00', '10:30:00');
-		$this->assertEquals(1, count($interviewList));
-	}
+        $interviewList = $this->jobInterviewDao->getInterviewListByCandidateIdAndInterviewDateAndTime(1, '2011-08-03', '8:30:00', '10:30:00');
+        $this->assertEquals(1, count($interviewList));
+    }
 
-	/**
-	 * Test getInterviewListByCandidateIdAndInterviewDate for not existing results
-	 */
-	public function testGetInterviewListByCandidateIdAndInterviewDateAndTimeForNotExistingResults() {
+    /**
+     * Test getInterviewListByCandidateIdAndInterviewDate for not existing results
+     */
+    public function testGetInterviewListByCandidateIdAndInterviewDateAndTimeForNotExistingResults() {
 
-		$interviewList = $this->jobInterviewDao->getInterviewListByCandidateIdAndInterviewDateAndTime(5, '2011-08-18', '9:00:00', '11:00:00');
-		$this->assertTrue(true, empty($interviewList));
-	}
+        $interviewList = $this->jobInterviewDao->getInterviewListByCandidateIdAndInterviewDateAndTime(5, '2011-08-18', '9:00:00', '11:00:00');
+        $this->assertTrue(true, empty($interviewList));
+    }
 
-	public function testGetInterviewById() {
-		$interviewId = 1;
-		$interview = $this->jobInterviewDao->getInterviewById($interviewId);
-		$this->assertTrue($interview instanceof JobInterview);
-		$this->assertEquals($interview->getInterviewDate(), '2011-08-03');
-		$this->assertEquals($interview->getInterviewTime(), '09:00:00');
-	}
+    public function testGetInterviewById() {
+        $interviewId = 1;
+        $interview = $this->jobInterviewDao->getInterviewById($interviewId);
+        $this->assertTrue($interview instanceof JobInterview);
+        $this->assertEquals($interview->getInterviewDate(), '2011-08-03');
+        $this->assertEquals($interview->getInterviewTime(), '09:00:00');
+    }
 
-	public function testGetInterviewersByInterviewId() {
-		$interviewId = 1;
-		$interviewers = $this->jobInterviewDao->getInterviewersByInterviewId($interviewId);
-		$this->assertTrue($interviewers[0] instanceof JobInterviewInterviewer);
-		$this->assertEquals($interviewers[0]->getInterviewId(), 1);
-		$this->assertEquals($interviewers[0]->getInterviewerId(), 3);
-	}
+    public function testGetInterviewersByInterviewId() {
+        $interviewId = 1;
+        $interviewers = $this->jobInterviewDao->getInterviewersByInterviewId($interviewId);
+        $this->assertTrue($interviewers[0] instanceof JobInterviewInterviewer);
+        $this->assertEquals($interviewers[0]->getInterviewId(), 1);
+        $this->assertEquals($interviewers[0]->getInterviewerId(), 3);
+    }
 
-	public function testGetInterviewsByCandidateVacancyId() {
-		$candidateVacancyId = 1;
-		$interviews = $this->jobInterviewDao->getInterviewsByCandidateVacancyId($candidateVacancyId);
-		$this->assertTrue($interviews[0] instanceof JobInterview);
-		$this->assertEquals($interviews[0]->getInterviewDate(), '2011-08-03');
-		$this->assertEquals($interviews[0]->getInterviewTime(), '09:00:00');
-	}
+    public function testGetInterviewsByCandidateVacancyId() {
+        $candidateVacancyId = 1;
+        $interviews = $this->jobInterviewDao->getInterviewsByCandidateVacancyId($candidateVacancyId);
+        $this->assertTrue($interviews[0] instanceof JobInterview);
+        $this->assertEquals($interviews[0]->getInterviewDate(), '2011-08-03');
+        $this->assertEquals($interviews[0]->getInterviewTime(), '09:00:00');
+    }
 
-	public function testGetInterviewScheduledHistoryByInterviewId() {
-		$interviewId = 1;
-		$history = $this->jobInterviewDao->getInterviewScheduledHistoryByInterviewId($interviewId);
-		$this->assertTrue($history instanceof CandidateHistory);
-		$this->assertEquals($history->getCandidateId(), 1);
-		$this->assertEquals($history->getNote(), 'This is my third note');
-	}
+    public function testGetInterviewScheduledHistoryByInterviewId() {
+        $interviewId = 1;
+        $history = $this->jobInterviewDao->getInterviewScheduledHistoryByInterviewId($interviewId);
+        $this->assertTrue($history instanceof CandidateHistory);
+        $this->assertEquals($history->getCandidateId(), 1);
+        $this->assertEquals($history->getNote(), 'This is my third note');
+    }
 
-	public function testSaveJobInterview() {
+    public function testSaveJobInterview() {
 
-		$interview = new JobInterview();
-		$interview->id = 12;
-		$interview->interviewName = '1st Interview';
-		$interview->interviewDate = '2011-05-05';
+        $interview = new JobInterview();
+        $interview->id = 12;
+        $interview->interviewName = '1st Interview';
+        $interview->interviewDate = '2011-05-05';
 
-		$this->jobInterviewDao->saveJobInterview($interview);
-		$this->assertNotNull($interview->getId());
-	}
+        $this->jobInterviewDao->saveJobInterview($interview);
+        $this->assertNotNull($interview->getId());
+    }
 
-	public function testSaveJobInterviewForNullId() {
-            
-            TestDataService::truncateTables(array(
-                'JobInterview',
-                'JobInterviewInterviewer',
-                'JobInterviewAttachment',
-            ));
+    public function testSaveJobInterviewForNullId() {
 
-		$interview = new JobInterview();
-		$interview->setId(null);
-		$interview->setInterviewName('1st Interview');
-		$interview->setInterviewDate('2011-05-05');
+        TestDataService::truncateTables(array(
+            'JobInterview',
+            'JobInterviewInterviewer',
+            'JobInterviewAttachment',
+        ));
 
-		$return = $this->jobInterviewDao->saveJobInterview($interview);
-		$this->assertTrue($return);
-	}
+        $interview = new JobInterview();
+        $interview->setId(null);
+        $interview->setInterviewName('1st Interview');
+        $interview->setInterviewDate('2011-05-05');
 
-	public function testUpdateJobInterview() {
-		$interview = new JobInterview();
-		$interview->id = 2;
-		$interview->candidateId = 1;
-		$interview->candidateVacancyId = 1;
-		$interview->interviewTime = '12:30';
-		$interview->interviewName = '1st Interview';
-		$interview->interviewDate = '2011-05-05';
-		$interview->note = 'Scheduled';
-		$result = $this->jobInterviewDao->updateJobInterview($interview);
-		$this->assertEquals($result, 1);
-	}
-        
+        $return = $this->jobInterviewDao->saveJobInterview($interview);
+        $this->assertTrue($return);
+    }
+
+    public function testUpdateJobInterview() {
+        $interview = new JobInterview();
+        $interview->id = 2;
+        $interview->candidateId = 1;
+        $interview->candidateVacancyId = 1;
+        $interview->interviewTime = '12:30';
+        $interview->interviewName = '1st Interview';
+        $interview->interviewDate = '2011-05-05';
+        $interview->note = 'Scheduled';
+        $result = $this->jobInterviewDao->updateJobInterview($interview);
+        $this->assertEquals($result, 1);
+    }
+
 }
 
