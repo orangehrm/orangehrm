9b5e9be753e9ee380398336ef630f4b072341f38
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-14 04:06:18 +0000

    Added defaults to constructor parameters to match constructor of Exception class

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/exception/CoreServiceException.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/exception/CoreServiceException.php
@@ -21,7 +21,7 @@
 class CoreServiceException extends ServiceException
 {
     
-    public function __construct($message, $code, $previous) {
+    public function __construct($message = "", $code = 0, $previous = NULL) {
         
         if (version_compare(PHP_VERSION, '5.3.0') < 0) {
             parent::__construct($message, $code);
