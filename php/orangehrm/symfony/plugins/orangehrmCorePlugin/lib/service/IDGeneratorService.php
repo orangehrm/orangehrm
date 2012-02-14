9f2ffee9ec5e8a3a8b1038a3b2f03417d974639f
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-14 09:02:38 +0000

    Made some indentation improvements to IDGeneratorService

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/service/IDGeneratorService.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/service/IDGeneratorService.php
@@ -1,214 +1,194 @@
 <?php
-class IDGeneratorService extends BaseService
-{
-	const MIN_LENGTH	=	3 ;
 
-	private $entity ;
+class IDGeneratorService extends BaseService {
+    const MIN_LENGTH = 3;
 
-	/**
-	 * Constructor of Generator Service class
-	 * @return unknown_type
-	 */
-	public function __construct()
-	{
+    private $entity;
 
-	}
+    /**
+     * Constructor of Generator Service class
+     * @return unknown_type
+     */
+    public function __construct() {
+        
+    }
 
+    /**
+     * Get the entity
+     * @return unknown_type
+     */
+    public function getEntity() {
+        return $this->entity;
+    }
 
-	/**
-	 * Get the entity
-	 * @return unknown_type
-	 */
-	public function getEntity( )
-	{
-		return $this->entity ;
-	}
+    /**
+     * Set the entity
+     * @param $entity
+     * @return unknown_type
+     */
+    public function setEntity($entity) {
+        $this->entity = $entity;
+    }
 
-	/**
-	 * Set the entity
-	 * @param $entity
-	 * @return unknown_type
-	 */
-	public function setEntity( $entity )
-	{
-		$this->entity	=	$entity ;
-	}
+    /**
+     * get the prefix
+     * @return unknown_type
+     */
+    private function getEntityPrefix() {
+        $prefix = '';
 
+        switch (get_class($this->entity)) {
+            case 'Location':
+                $prefix = 'LOC';
+                break;
 
-	/**
-	 * get the prefix
-	 * @return unknown_type
-	 */
-	private function getEntityPrefix()
-	{
-		$prefix	=	'';
+            case 'JobCategory':
+                $prefix = 'EEC';
+                break;
 
-		switch( get_class($this->entity))
-		{
-			case 'Location':
-				$prefix	=	'LOC';
-			break;
+            case 'CompanyProperty':
+                $prefix = 'EEC';
+                break;
 
-			case 'JobCategory':
-				$prefix	=	'EEC';
-			break;
+            case 'SalaryGrade':
+                $prefix = 'SAL';
+                break;
 
-			case 'CompanyProperty':
-				$prefix	=	'EEC';
-			break;
+            case 'EmployeeStatus':
+                $prefix = 'EST';
+                break;
 
-			case 'SalaryGrade':
-				$prefix	=	'SAL';
-			break;
+            case 'JobTitle':
+                $prefix = 'JOB';
+                break;
 
-			case 'EmployeeStatus':
-				$prefix	=	'EST';
-			break;
+            case 'Education':
+                $prefix = 'EDU';
+                break;
 
-			case 'JobTitle':
-				$prefix	=	'JOB';
-			break;
+            case 'Licenses':
+                $prefix = 'LIC';
+                break;
 
-			case 'Education':
-				$prefix	=	'EDU';
-			break;
+            case 'Skill':
+                $prefix = 'SKI';
+                break;
 
-			case 'Licenses':
-				$prefix	=	'LIC';
-			break;
+            case 'Skill':
+                $prefix = 'SKI';
+                break;
 
-			case 'Skill':
-				$prefix	=	'SKI';
-			break;
+            case 'Language':
+                $prefix = 'LAN';
+                break;
 
-			case 'Skill':
-				$prefix	=	'SKI';
-			break;
+            case 'MembershipType':
+                $prefix = 'MEM';
+                break;
 
-			case 'Language':
-				$prefix	=	'LAN';
-			break;
+            case 'Membership':
+                $prefix = 'MME';
+                break;
 
-			case 'MembershipType':
-				$prefix	=	'MEM';
-			break;
+            case 'UserGroup':
+                $prefix = 'USG';
+                break;
 
-			case 'Membership':
-				$prefix	=	'MME';
-			break;
+            case 'Users':
+                $prefix = 'USR';
+                break;
 
-			case 'UserGroup':
-				$prefix	=	'USG';
-			break;
+            case 'JobSpecifications':
+                $prefix = '';
+                break;
 
-			case 'Users':
-				$prefix	=	'USR';
-			break;
+            case 'ProjectActivity':
+                $prefix = '';
+                break;
 
-			case 'JobSpecifications':
-				$prefix	=	'';
-			break;
-
-			case 'ProjectActivity':
-				$prefix	=	'';
-			break;
-            
             case 'EmployeeReport':
-				$prefix	=	'REP';
-			break;
-			
-			case 'LeaveType':
-				$prefix	=	'LTY';
-			break;
-			
-			case 'LeaveRequest':
-				$prefix	=	'';
-			break;
-			
-			case 'Leave':
-				$prefix	=	'';
-			break;
-			
-			case 'EmployeeLeaveEntitlement':
-				$prefix	=	'';
-			break;
-			
+                $prefix = 'REP';
+                break;
 
-		}
+            case 'LeaveType':
+                $prefix = 'LTY';
+                break;
 
-		return $prefix ;
-	}
+            case 'LeaveRequest':
+                $prefix = '';
+                break;
 
+            case 'Leave':
+                $prefix = '';
+                break;
 
+            case 'EmployeeLeaveEntitlement':
+                $prefix = '';
+                break;
+        }
 
-	/**
-	 * Get next auto increment ID
-	 *
-	 * @param bool update - Update id in the database - defaults to true.
-	 *
-	 * @return next auto increment ID
-	 */
-	public function getNextID($update = true)
-	{
-		$prefix	= $this->getEntityPrefix();
+        return $prefix;
+    }
 
-		$currentId	= $this->getCurrentID();
+    /**
+     * Get next auto increment ID
+     *
+     * @param bool update - Update id in the database - defaults to true.
+     *
+     * @return next auto increment ID
+     */
+    public function getNextID($update = true) {
+        $prefix = $this->getEntityPrefix();
 
-		if ($update) {
-		    $this->updateNextId($currentId+1);
-		}
+        $currentId = $this->getCurrentID();
 
-      $minLength = self::MIN_LENGTH;
-      if(get_class($this->entity) == "Employee") {
-         $minLength = 4;
-      }
-		$nextId	= $prefix . str_pad($currentId+1, $minLength, "0", STR_PAD_LEFT);
+        if ($update) {
+            $this->updateNextId($currentId + 1);
+        }
 
-		return $nextId;
+        $minLength = self::MIN_LENGTH;
+        if (get_class($this->entity) == "Employee") {
+            $minLength = 4;
+        }
+        $nextId = $prefix . str_pad($currentId + 1, $minLength, "0", STR_PAD_LEFT);
 
+        return $nextId;
+    }
 
+    /**
+     * Get Current ID
+     */
+    private function getCurrentID() {
+        $tableName = $this->entity->getTable()->tableName;
 
-	}
+        $q = Doctrine_Query::create()
+                ->from('UniqueId')
+                ->where('table_name = ' . "'$tableName'");
 
-	/**
-	 * Get Current ID
-	 */
-	private function getCurrentID( )
-	{
-		$tableName=	$this->entity->getTable()->tableName ;
+        $uniqueId = $q->fetchOne();
+        if ($uniqueId instanceof UniqueId) {
+            return $uniqueId->getLastId();
+        } else {
+            return 0;
+        }
+    }
 
-		$q 			= Doctrine_Query::create()
-				    ->from('UniqueId')
-				    ->where('table_name = '."'$tableName'");
+    /**
+     * Update next ID
+     * @param int $nextId
+     */
+    private function updateNextId($nextId) {
+        try {
+            $tableName = $this->entity->getTable()->tableName;
+            $q = Doctrine_Query::create()
+                    ->update('UniqueId')
+                    ->set("last_id", "'" . $nextId . "'")
+                    ->where('table_name = ' . "'$tableName'");
 
-		$uniqueId = $q->fetchOne();
-		if( $uniqueId instanceof UniqueId) {
-			return $uniqueId->getLastId();
-		} else {
-			return 0;
-		}
-	}
-
-	/**
-	 * Update next ID
-	 * @param int $nextId
-	 */
-	private function updateNextId( $nextId)
-	{
-		try
-        {
-			$tableName=	$this->entity->getTable()->tableName ;
-        	$q = Doctrine_Query::create()
-				    ->update('UniqueId')
-					 ->set("last_id", "'".$nextId."'")
-				    ->where('table_name = '."'$tableName'");
-
-			$q->execute();
-
-         }catch( Exception $e)
-        {
+            $q->execute();
+        } catch (Exception $e) {
             throw new AdminServiceException($e->getMessage());
         }
-	}
+    }
 
 }
