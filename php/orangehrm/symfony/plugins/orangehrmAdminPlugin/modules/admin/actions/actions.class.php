d2b2631e72f0d935550f31978452860fe75f1cca
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-20 17:22:17 +0000

    Fixed the message issues on mail configuration

--- php/orangehrm/symfony/plugins/orangehrmAdminPlugin/modules/admin/actions/actions.class.php
+++ php/orangehrm/symfony/plugins/orangehrmAdminPlugin/modules/admin/actions/actions.class.php
@@ -2098,9 +2098,9 @@ class adminActions extends sfActions {
             $result = $emailService->sendTestEmail($request->getParameter('txtTestEmail'));
 
             if ($result) {
-                $this->getUser()->setFlash('templateMessage', array('SUCCESS', __('Email configuration was saved. Test email was sent')));
+                $this->getUser()->setFlash('templateMessage', array('SUCCESS', __('Successfully Saved. Test Email Sent')));
             } else {
-                $this->getUser()->setFlash('templateMessage', array('WARNING', __("Email configuration was saved. Test email couldn't be sent")));
+                $this->getUser()->setFlash('templateMessage', array('WARNING', __("Successfully Saved. Test Email Not Sent")));
             }
         }
 
