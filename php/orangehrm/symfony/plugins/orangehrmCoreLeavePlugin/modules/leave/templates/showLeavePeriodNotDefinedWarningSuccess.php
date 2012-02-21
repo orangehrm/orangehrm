182ff850e107f28af4047d2cda889e50fd23efa4
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-21 04:26:30 +0000

    Refactored the message to Leave Period Not Defined message in leave

--- php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/templates/showLeavePeriodNotDefinedWarningSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/templates/showLeavePeriodNotDefinedWarningSuccess.php
@@ -1,3 +1,3 @@
 <div id='messageBalloon_notice' class='messageBalloon_notice'>
-<?php echo __("Leave Period is Not Defined Yet. Please Notify the Hr Admin to Define the Leave Period in Order to Access This Module"); ?>
+<?php echo __("Leave Period Not Defined"); ?>
 </div>
