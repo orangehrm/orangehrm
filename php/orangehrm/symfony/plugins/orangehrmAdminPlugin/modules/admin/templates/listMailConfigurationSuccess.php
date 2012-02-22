df9ad6fbde6b5b3dd6d33d53d3b5092431cf8581
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-22 09:48:43 +0000

    Fixed the error message alignment in mail configuration

--- php/orangehrm/symfony/plugins/orangehrmAdminPlugin/modules/admin/templates/listMailConfigurationSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmAdminPlugin/modules/admin/templates/listMailConfigurationSuccess.php
@@ -10,64 +10,96 @@
 	    <form action="<?php echo url_for('admin/saveMailConfiguration');?>" onsubmit="" method="post" id="frmSave" name="frmSave">
                 <?php echo $form['_csrf_token']; ?>
 	        <input type="hidden" value="UpdateRecord" name="sqlState"/>
-	        <label for="txtMailAddress"><?php echo __("Mail Sent As")?><span class="required">*</span></label>
-	        <input type="text"  value="<?php  echo $mailAddress;?>" class="formInputText" id="txtMailAddress" name="txtMailAddress"/>
-
-	        <label for="cmbMailSendingMethod"><?php echo __("Sending Method")?></label>
-	        <select id="cmbMailSendingMethod" name="cmbMailSendingMethod" class="formSelect">
-	            <option value="sendmail" <?php if($emailType == "sendmail"){ echo "selected";} ?> ><?php echo __("Sendmail") ?></option>
-	            <option value="smtp" <?php if($emailType == "smtp"){ echo "selected";} ?>><?php echo __("SMTP") ?></option>
-	        </select>
-	        <br class="clear"/>
-
-	        <div id="divsendmailControls" class="toggleDiv">
-	            <label for="txtSendmailPath"><?php echo __("Path to Sendmail")?></label>
-	            <input type="text" id="txtSendmailPath" name="txtSendmailPath" value="<?php echo $sendMailPath;?>" class="formInputText" />
-	            <br class="clear"/>
-	        </div>
-
-	        <div id="divsmtpControls" class="toggleDiv">
-
-                   <label for="txtSmtpHost"><?php echo __("SMTP Host") ?> <span class="required">*</span></label>
-                    <input type="text" name="txtSmtpHost" id="txtSmtpHost"  class="formInputText" value="<?php echo $smtpHost;?>" />
-                    <label for="txtSmtpPort"><?php echo __("SMTP Port"); ?> <span class="required">*</span></label>
-                    <input type="text" name="txtSmtpPort" id="txtSmtpPort" class="formInputText" value="<?php echo $smtpPort;?>"/>
+           <table cellspacing="0" cellpadding="0" border="0" width="650px">
+                <tr>
+                    <td class="mailTableColumn">
+                        <label for="txtMailAddress"><?php echo __("Mail Sent As")?><span class="required">*</span></label>
+                        <input type="text"  value="<?php  echo $mailAddress;?>" class="formInputText" id="txtMailAddress" name="txtMailAddress"/>
+                    </td>
+                    <td class="mailTableColumn">
+                        <label for="cmbMailSendingMethod"><?php echo __("Sending Method")?></label>
+                        <select id="cmbMailSendingMethod" name="cmbMailSendingMethod" class="formSelect">
+                            <option value="sendmail" <?php if($emailType == "sendmail"){ echo "selected";} ?> ><?php echo __("Sendmail") ?></option>
+                            <option value="smtp" <?php if($emailType == "smtp"){ echo "selected";} ?>><?php echo __("SMTP") ?></option>
+                        </select>
+                        <br class="clear"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div id="divsendmailControls" class="toggleDiv">
+                        <label for="txtSendmailPath"><?php echo __("Path to Sendmail")?></label>
+                        <input type="text" id="txtSendmailPath" name="txtSendmailPath" value="<?php echo $sendMailPath;?>" class="formInputText" />
+                        <br class="clear"/>
+                        </div>
+                    </td>
+                </tr>
+           </table>
+            <div id="divsmtpControls" class="toggleDiv">
+                <table cellspacing="0" cellpadding="0" border="0" width="650px">
+                    <tr>
+                        <td class="mailTableColumn">
+                           <label for="txtSmtpHost"><?php echo __("SMTP Host") ?> <span class="required">*</span></label>
+                            <input type="text" name="txtSmtpHost" id="txtSmtpHost"  class="formInputText" value="<?php echo $smtpHost;?>" />
+                        </td>
+                        <td class="mailTableColumn">
+                            <label for="txtSmtpPort"><?php echo __("SMTP Port"); ?> <span class="required">*</span></label>
+                            <input type="text" name="txtSmtpPort" id="txtSmtpPort" class="formInputText" value="<?php echo $smtpPort;?>"/>
+                            <br class="clear"/>
+                       </td>
+                   </tr>
+                   <tr>
+                       <td colspan="2">
+                            <label><?php echo __("Use SMTP Authentication"); ?></label>
+                            <input type="radio" name="optAuth" id="optAuthNONE" class="formRadio" value="none" <?php if($smtpAuth == '' || $smtpAuth == 'none') { echo "checked";}?> />
+                            <label for="optAuthNONE" class="optionlabel"><?php echo __("No") ?></label>
+                            <input type="radio" name="optAuth" id="optAuthLOGIN" class="formRadio" value="login" <?php if($smtpAuth == 'login') { echo "checked";}?>/>
+                            <label for="optAuthLOGIN" class="optionlabel"><?php echo __("Yes") ?></label>
+                            <br class="clear"/>
+                       </td>
+                   </tr>
+                   <tr>
+                        <td class="mailTableColumn">
+                            <label for="txtSmtpUser"><?php echo __("SMTP User") ?></label>
+                            <input type="text" name="txtSmtpUser" id="txtSmtpUser" class="formInputText" value="<?php echo $smtpUser;?>" />
+                        </td>
+                        <td class="mailTableColumn">
+                            <label for="txtSmtpPass"><?php echo __("SMTP Password") ?></label>
+                            <input type="password" name="txtSmtpPass" id="txtSmtpPass" class="formInputText" value="<?php echo $smtpPass;?>" />
+                            <br class="clear"/>
+                        </td>
+                  </tr>
+                  <tr>
+                        <td colspan="2">
+                            <label><?php echo __("User Secure Connection") ?></label>
+                            <input type="radio" name="optSecurity" id="optSecurityNONE" class="formRadio" value="none" <?php if($smtpSecurity == 'none') { echo "checked";}?> />
+                            <label for="optSecurityNONE"  class="optionlabel"><?php echo __("No"); ?></label>
+        
+                            <input type="radio" name="optSecurity" id="optSecuritySSL" class="formRadio" value="ssl" <?php if($smtpSecurity == 'ssl') { echo "checked";}?> />
+                            <label for="optSecuritySSL" class="optionlabel"><?php echo __("SSL") ?></label>
+        
+                            <input type="radio" name="optSecurity" id="optSecurityTLS" class="formRadio" value="tls" <?php if($smtpSecurity == 'tls') { echo "checked";}?> />
+                            <label for="optSecurityTLS" class="optionlabel"><?php echo __("TLS") ?></label>
+                            <br class="clear" />
+                        </td>
+                  </tr>
+              </table>
+        </div>
+        <table cellspacing="0" cellpadding="0" border="0" width="650px">
+            <tr>
+                <td class="mailTableColumn">
+                    <label for="chkSendTestEmail"><?php echo __("Send Test Email")?></label>
+                    <input type="checkbox" id="chkSendTestEmail" name="chkSendTestEmail"
+                        value="Yes" class="formInputText" />
+                </td>
+                <td class="mailTableColumn">
+                    <label for="txtTestEmail"><?php echo __("Test Email Address") ?></label>
+                    <input type="text" name="txtTestEmail" id="txtTestEmail" class="formInputText" />
                     <br class="clear"/>
-
-                    <label><?php echo __("Use SMTP Authentication"); ?></label>
-                    <input type="radio" name="optAuth" id="optAuthNONE" class="formRadio" value="none" <?php if($smtpAuth == '' || $smtpAuth == 'none') { echo "checked";}?> />
-                    <label for="optAuthNONE" class="optionlabel"><?php echo __("No") ?></label>
-                    <input type="radio" name="optAuth" id="optAuthLOGIN" class="formRadio" value="login" <?php if($smtpAuth == 'login') { echo "checked";}?>/>
-                    <label for="optAuthLOGIN" class="optionlabel"><?php echo __("Yes") ?></label>
-                    <br class="clear"/>
-
-                    <label for="txtSmtpUser"><?php echo __("SMTP User") ?></label>
-                    <input type="text" name="txtSmtpUser" id="txtSmtpUser" class="formInputText" value="<?php echo $smtpUser;?>" />
-
-                    <label for="txtSmtpPass"><?php echo __("SMTP Password") ?></label>
-                    <input type="password" name="txtSmtpPass" id="txtSmtpPass" class="formInputText" value="<?php echo $smtpPass;?>" />
-                    <br class="clear"/>
-
-                    <label><?php echo __("User Secure Connection") ?></label>
-                    <input type="radio" name="optSecurity" id="optSecurityNONE" class="formRadio" value="none" <?php if($smtpSecurity == 'none') { echo "checked";}?> />
-                    <label for="optSecurityNONE"  class="optionlabel"><?php echo __("No"); ?></label>
-
-                    <input type="radio" name="optSecurity" id="optSecuritySSL" class="formRadio" value="ssl" <?php if($smtpSecurity == 'ssl') { echo "checked";}?> />
-                    <label for="optSecuritySSL" class="optionlabel"><?php echo __("SSL") ?></label>
-
-                    <input type="radio" name="optSecurity" id="optSecurityTLS" class="formRadio" value="tls" <?php if($smtpSecurity == 'tls') { echo "checked";}?> />
-                    <label for="optSecurityTLS" class="optionlabel"><?php echo __("TLS") ?></label>
-                    <br class="clear" />
-	        </div>
-	        <br class="clear" />
-
-	        <label for="chkSendTestEmail"><?php echo __("Send Test Email")?></label>
-	        <input type="checkbox" id="chkSendTestEmail" name="chkSendTestEmail"
-	            value="Yes" class="formInputText" />
-	        <label for="txtTestEmail"><?php echo __("Test Email Address") ?></label>
-	        <input type="text" name="txtTestEmail" id="txtTestEmail" class="formInputText" />
-	        <br class="clear"/>
-
+                </td>
+                
+            </tr>
+         </table>
 	        <div class="formbuttons">
 	            <input type="button" value="<?php echo __("Edit")?>"   id="editBtn" class="editbutton"/>
 	            <input type="button" value="<?php echo __("Reset")?>" id="resetBtn"  tabindex="3"  class="clearbutton"/>
