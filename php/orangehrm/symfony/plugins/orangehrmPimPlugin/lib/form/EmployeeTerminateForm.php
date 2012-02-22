f05fee404a5648039c222f749bacf6f0a6a86c90
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-22 08:26:33 +0000

    Fixed the Employee termination note length issue

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/form/EmployeeTerminateForm.php
+++ php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/form/EmployeeTerminateForm.php
@@ -72,7 +72,7 @@ class EmployeeTerminateForm extends BaseForm {
             'date' => new ohrmDateValidator(array('date_format' => $inputDatePattern, 'required' => true),
                     array('invalid' => 'Date format should be ' . $inputDatePattern)),
             'reason' => new sfValidatorChoice(array('required' => true, 'choices' => array_keys($terminateReasons))),
-            'note' => new sfValidatorString(array('required' => false, 'max_length' => 250))
+            'note' => new sfValidatorString(array('required' => false, 'max_length' => 250, 'trim' => true))
         ));
 
         $this->setDefault('date', set_datepicker_date_format(date('Y-m-d')));
