2e6cae0010af9b4a534825040c692cc9d4c36bd3
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-02-24 11:33:44 +0000

    Added the Already exixts error message to the dictionary in leave holiday

--- php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/actions/defineHolidayAction.class.php
+++ php/orangehrm/symfony/plugins/orangehrmCoreLeavePlugin/modules/leave/actions/defineHolidayAction.class.php
@@ -134,7 +134,7 @@ class defineHolidayAction extends sfAction {
 
                 /* Error will not return if the date if not in the correct format */
                 if (!$allowToAdd && !is_null($date)) {
-                    $this->templateMessage = array('WARNING', __('Failed to Save: Date Alredy Assigned'));
+                    $this->templateMessage = array('WARNING', __('Failed to Save: Date Already Assigned'));
                 } else {
 
                     //first creating the leave period if the date belongs to next leave period
