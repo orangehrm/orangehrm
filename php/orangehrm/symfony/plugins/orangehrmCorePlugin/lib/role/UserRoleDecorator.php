49164473c98df3dd7154a044e4cf2187204f9083
nadeeeratkp <nadeeeratkp@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-03-03 09:37:43 +0000

    modification needed by attendance csv extractor

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/role/UserRoleDecorator.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/role/UserRoleDecorator.php
@@ -5,7 +5,7 @@ abstract class UserRoleDecorator extends User {
     protected function isPluginAvailable($pluginName){
         $file = sfConfig::get('sf_plugins_dir') . "/$pluginName/";
         
-        if (is_dir($file)){      
+        if (is_readable($file)){      
             return true;           
         } else {
             return false;
