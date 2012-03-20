2bda95bc648729fad328360f37b20c1d95048199
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-03-20 08:41:49 +0000

    Fixed the symfony lib path on project configuration class

--- php/orangehrm/upgrader/config/ProjectConfiguration.class.php
+++ php/orangehrm/upgrader/config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../symfony/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../../symfony/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
