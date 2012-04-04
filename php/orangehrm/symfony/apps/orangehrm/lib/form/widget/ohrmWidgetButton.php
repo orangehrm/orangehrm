6b992464ecd83aba9e1a97e0fdc9d15f5d0b723e
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-04-04 05:12:45 +0000

    Made some minor whitespace improvements

--- php/orangehrm/symfony/apps/orangehrm/lib/form/widget/ohrmWidgetButton.php
+++ php/orangehrm/symfony/apps/orangehrm/lib/form/widget/ohrmWidgetButton.php
@@ -1,62 +1,62 @@
 <?php
 
-class ohrmWidgetButton extends sfWidget
-{
-	protected $name;
-	protected $label;
-	protected $attributes;
+class ohrmWidgetButton extends sfWidget {
 
-	public function __construct($name, $label, $attributes = null) {
-		if (empty($attributes)) {
-			$attributes = array();
-		}
-		 
-		$this->name = $name;
-		$this->label = $label;
-		$this->attributes = empty($attributes) ? array() : $attributes;
-	}
+    protected $name;
+    protected $label;
+    protected $attributes;
 
-	/**
-	 * @return string An HTML tag string
-	 */
-	public function render($name, $value = null, $attributes = array(), $errors = array())
-	{
-		if (empty($name)) {
-			$name = $this->name;
-		}
-			
-		if (empty($value)) {
-			$value = $this->label;
-		}
-			
-		if (empty($attributes)) {
-			$attributes = $this->attributes;
-		}
+    public function __construct($name, $label, $attributes = null) {
+        if (empty($attributes)) {
+            $attributes = array();
+        }
 
-		if (is_array($attributes)) {
-			if (!array_key_exists('id', $attributes)) {
-				$attributes['id'] = $name;
-			}
-			if (!array_key_exists('class', $attributes)) {
-				$attributes['class'] = 'plainbtn';
-			}
-		} else {
-			$attributes['id'] = $name;
-			$attributes['class'] = 'plainbtn';
-		}
+        $this->name = $name;
+        $this->label = $label;
+        $this->attributes = empty($attributes) ? array() : $attributes;
+    }
 
-		return $this->renderTag('input', array_merge(array('type' => 'button', 'name' => $name, 'value' => __($value)), $attributes));
-	}
+    /**
+     * @return string An HTML tag string
+     */
+    public function render($name, $value = null, $attributes = array(), $errors = array()) {
+        if (empty($name)) {
+            $name = $this->name;
+        }
 
-	public function getDefault() {
-		if (!array_key_exists('value', $this->attributes)) {
-			$this->attributes['value'] = $this->label;
-		}
+        if (empty($value)) {
+            $value = $this->label;
+        }
 
-		return $this->attributes;
-	}
+        if (empty($attributes)) {
+            $attributes = $this->attributes;
+        }
 
-	public function getName() {
-		return $this->name;
-	}
+        if (is_array($attributes)) {
+            if (!array_key_exists('id', $attributes)) {
+                $attributes['id'] = $name;
+            }
+            if (!array_key_exists('class', $attributes)) {
+                $attributes['class'] = 'plainbtn';
+            }
+        } else {
+            $attributes['id'] = $name;
+            $attributes['class'] = 'plainbtn';
+        }
+
+        return $this->renderTag('input', array_merge(array('type' => 'button', 'name' => $name, 'value' => __($value)), $attributes));
+    }
+
+    public function getDefault() {
+        if (!array_key_exists('value', $this->attributes)) {
+            $this->attributes['value'] = $this->label;
+        }
+
+        return $this->attributes;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
 }
