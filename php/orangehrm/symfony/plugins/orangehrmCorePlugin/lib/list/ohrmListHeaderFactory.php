020d70125d54bc6dd84d5419732119c6eb34ab69
madhusanka <madhusanka@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-04-04 05:07:52 +0000

    Made some documentational improvements to ohrmListHeaderFactory

--- php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/list/ohrmListHeaderFactory.php
+++ php/orangehrm/symfony/plugins/orangehrmCorePlugin/lib/list/ohrmListHeaderFactory.php
@@ -1,9 +1,9 @@
 <?php
 
 abstract class ohrmListConfigurationFactory implements ListConfigurationFactory {
+
     const BEFORE_TABLE = 'before-data';
     const AFTER_TABLE = 'after-data';
-    
     const RECORD = 'record';
 
     protected $headerGroups;
@@ -15,36 +15,39 @@ abstract class ohrmListConfigurationFactory implements ListConfigurationFactory 
     protected static $permissions;
 
     public function getHeaderGroups() {
-        
+
         $this->__init();
         return $this->headerGroups;
     }
-    
+
     public function setHeaderGroups(array $headerGroups) {
         $this->headerGroups = $headerGroups;
     }
-    
+
     /**
      * Checks if group headers should be displayed in report.
-     * 
      * Returns true if at least one header group needs to be displayed.
      * 
      * @return type bool true if group headers need to be displayed, false if not.
      */
-    public function showGroupHeaders() {        
+    public function showGroupHeaders() {
         $this->__init();
-        
+
         foreach ($this->headerGroups as $group) {
-            
+
             if ($group->showHeader()) {
                 return true;
             }
         }
         return false;
     }
-    
+
+    /**
+     *
+     * @return array 
+     */
     public function getHeaders() {
-        
+
         $this->__init();
         $headers = array();
 
@@ -55,14 +58,24 @@ abstract class ohrmListConfigurationFactory implements ListConfigurationFactory 
         return $headers;
     }
 
+    /**
+     *
+     * @param array $headers 
+     */
     public function setHeaders(array $headers) {
         $this->headerGroups = array(new ListHeaderGroup($headers));
     }
 
+    /**
+     *
+     * @param string $index
+     * @return ListHeader
+     * @throws Exception 
+     */
     public function getHeader($index) {
 
         $this->__init();
-        
+
         $headers = $this->getHeaders();
 
         if (isset($headers[$index])) {
@@ -72,36 +85,52 @@ abstract class ohrmListConfigurationFactory implements ListConfigurationFactory 
         }
     }
 
+    /**
+     *
+     * @return array
+     */
     public function getRuntimeDefinitions() {
         return $this->runtimeDefinitions;
     }
-    
+
+    /**
+     *
+     * @param array $runtimeDefinitions 
+     */
     public function setRuntimeDefinitions(array $runtimeDefinitions) {
         $this->runtimeDefinitions = $runtimeDefinitions;
     }
 
+    /**
+     *
+     * @param type $userType 
+     */
     public static function setUserType($userType) {
         self::$userType = $userType;
     }
 
+    /**
+     *
+     * @param string $userId 
+     */
     public static function setUserId($userId) {
         self::$userId = $userId;
     }
-    
+
     public static function setPermissions($permissions) {
         self::$permissions = $permissions;
-    }    
+    }
 
     protected function init() {
         $this->headerGroups = array(new ListHeaderGroup(array()));
     }
-    
+
     public function __set($name, $value) {
         if ($name === 'headers') {
             $this->setHeaders($value);
         }
     }
-    
+
     public function __get($name) {
         if ($name === 'headers') {
             return $this->getHeaders();
@@ -109,9 +138,9 @@ abstract class ohrmListConfigurationFactory implements ListConfigurationFactory 
             return null;
         }
     }
-    
+
     private function __init() {
-        
+
         if (!$this->initialized) {
             $this->init();
             $this->initialized = true;
