cc9183242954b9fc323c4fb6867ea1fff310cce5
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-04-04 06:08:54 +0000

    Fixed the duplicate entry for salary currency details

--- php/orangehrm/upgrader/apps/upgrader/lib/schemaIncrementTasks/SchemaIncrementTask46.php
+++ php/orangehrm/upgrader/apps/upgrader/lib/schemaIncrementTasks/SchemaIncrementTask46.php
@@ -9,7 +9,9 @@ class SchemaIncrementTask46 extends SchemaIncrementTask {
         $this->incrementNumber = 46;
         parent::execute();
         
-        for($i = 0; $i <= 2; $i++) {
+        $result[] = $this->updateSalaryCurrencyDetail();
+        
+        for($i = 0; $i <= 1; $i++) {
             $result[] = $this->upgradeUtility->executeSql($this->sql[$i]);
         }
         
@@ -28,16 +30,46 @@ class SchemaIncrementTask46 extends SchemaIncrementTask {
     }
     
     public function loadSql() {
-    
-        $sql[0] = "UPDATE hs_pr_salary_currency_detail 
+        
+        $sql[0] = "UPDATE hs_hr_emp_basicsalary 
                         SET currency_id = 'ZAR' WHERE currency_id = 'SAR'";
         
-        $sql[1] = "UPDATE hs_hr_emp_basicsalary 
-                        SET currency_id = 'ZAR' WHERE currency_id = 'SAR'";
-        
-        $sql[2] = "UPDATE hs_hr_currency_type 
+        $sql[1] = "UPDATE hs_hr_currency_type 
                         SET code = '173', currency_name = 'Saudi Arabia Riyal' WHERE currency_id = 'SAR'";
         
         $this->sql = $sql;
+    }
+    
+    private function updateSalaryCurrencyDetail() {
+        $salaryCurrancyDetails = $this->upgradeUtility->executeSql("SELECT * FROM hs_pr_salary_currency_detail");
+        $success = true;
+        if($salaryCurrancyDetails) {
+            while($row = $this->upgradeUtility->fetchArray($salaryCurrancyDetails))
+            {
+                $salGrdCode = $row['sal_grd_code'];
+                $currencyId = $row['currency_id'];
+                if ($currencyId == 'SAR') {
+                    $duplicateSalaryCurrencyDetails = $this->upgradeUtility->executeSql("SELECT * FROM hs_pr_salary_currency_detail WHERE currency_id = 'ZAR' AND sal_grd_code = '$salGrdCode'");
+                    if ($this->upgradeUtility->fetchArray($duplicateSalaryCurrencyDetails)) {
+                        $sql = "DELETE FROM hs_pr_salary_currency_detail 
+                            WHERE currency_id = 'SAR' AND sal_grd_code = '$salGrdCode'";
+                        
+                        $result = $this->upgradeUtility->executeSql($sql);
+                        if(!$result) {
+                            $success = false;
+                        }
+                    } else {
+                        $sql = "UPDATE hs_pr_salary_currency_detail 
+                            SET currency_id = 'ZAR' WHERE currency_id = 'SAR' AND sal_grd_code = '$salGrdCode'";
+                        
+                        $result = $this->upgradeUtility->executeSql($sql);
+                        if(!$result) {
+                            $success = false;
+                        }
+                    }
+                }
+            }
+        }
+        return $success;
     }
 }
