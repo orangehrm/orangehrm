e3fc70e14b738c70b92aedf2a54093e7048275eb
lgtkaushalya <lgtkaushalya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-04-18 09:17:14 +0000

    Fixed the js issue in punch in and out in oromo language

--- php/orangehrm/symfony/plugins/orangehrmAttendancePlugin/modules/attendance/templates/punchTimeSuccess.php
+++ php/orangehrm/symfony/plugins/orangehrmAttendancePlugin/modules/attendance/templates/punchTimeSuccess.php
@@ -113,11 +113,11 @@
                                                     var linkForOverLappingValidation='<?php echo url_for('attendance/validatePunchOutOverLapping') ?>';
                                                     var linkForPunchInOverlappingValidation='<?php echo url_for('attendance/validatePunchInOverLapping') ?>';
                                                     var errorForInvalidTime='<?php echo __('Punch out Time Should Be Higher Than Punch in Time'); ?>';
-                                                    var errorForInvalidFormat='<?php echo __('Should Be a Valid Time in %format% Format', array('%format%' => 'HH:MM')) ?>';
-                                                    var errorForInvalidTimeFormat='<?php echo __('Should Be a Valid Time in %format% Format', array('%format%' => 'HH:MM')) ?>';
+                                                    var errorForInvalidFormat="<?php echo __('Should Be a Valid Time in %format% Format', array('%format%' => 'HH:MM')) ?>";
+                                                    var errorForInvalidTimeFormat="<?php echo __('Should Be a Valid Time in %format% Format', array('%format%' => 'HH:MM')) ?>";
                                                     var getCurrentTimeLink='<?php echo url_for('attendance/getCurrentTime') ?>';
                                                     var errorForInvalidDateFormat='<?php echo __('Should Be a Valid Date in %format% Format', array('%format%' => get_datepicker_date_format($sf_user->getDateFormat()))) ?>';
-                                                    var errorForOverLappingTime='<?php echo __('Overlapping Records Found'); ?>';
+                                                    var errorForOverLappingTime="<?php echo __('Overlapping Records Found'); ?>";
                                                     var errorForInvalidNote='<?php echo __(ValidationMessages::TEXT_LENGTH_EXCEEDS, array('%amount%' => 250)) ?>';
 
                                                     var actionPunchIn='<?php echo $actionPunchIn; ?>';
