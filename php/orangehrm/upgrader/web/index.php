e118a8a80408ae16f3eac4e3c2caf446f668a981
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-04-21 06:56:18 +0000

    Added check for log and cache directories' write permissions to the upgrader

--- php/orangehrm/upgrader/web/index.php
+++ php/orangehrm/upgrader/web/index.php
@@ -3,5 +3,9 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
+if (!is_writable(dirname(__FILE__).'/../cache') || !is_writable(dirname(__FILE__).'/../log')) {
+    die("'upgrader/cache' and 'upgrader/log' directories should be writable.");
+}
+
 $configuration = ProjectConfiguration::getApplicationConfiguration('upgrader', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
