2f72e44d2a9136402aca027a4b2595b41feaff63
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-07-02 09:56:46 +0000

    rebuilt model using doctrine:build-model. Base class now has unique constraint for field 'name'.

--- php/orangehrm/symfony/lib/model/doctrine/orangehrmCorePlugin/base/BaseUserRole.class.php
+++ php/orangehrm/symfony/lib/model/doctrine/orangehrmCorePlugin/base/BaseUserRole.class.php
@@ -47,6 +47,7 @@ abstract class BaseUserRole extends sfDoctrineRecord
         $this->hasColumn('name', 'string', 255, array(
              'type' => 'string',
              'notnull' => true,
+             'unique' => true,
              'length' => 255,
              ));
         $this->hasColumn('display_name', 'string', 255, array(
