35f0696a1b1fb6c54619f5d6a0392237713d3cf1
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-08-03 09:10:21 +0000

    Fixed IE 9 white spacing issue in Skills table

--- php/orangehrm/symfony/plugins/orangehrmAdminPlugin/web/js/viewSkillsSuccess.js
+++ php/orangehrm/symfony/plugins/orangehrmAdminPlugin/web/js/viewSkillsSuccess.js
@@ -2,6 +2,12 @@ var originalSkillName = '';
 
 $(document).ready(function() {
     
+    /* Fix for IE9 white space issue in tables: Begins */
+    var whiteSpaceExpr = new RegExp('>[ \t\r\n\v\f]*<', 'g');
+    var tableHtml = $('#recordsListTable').html();
+    $('#recordsListTable').html(tableHtml.replace(whiteSpaceExpr, '><'));
+    /* Fix for IE9 white space issue in tables: Ends */    
+    
     executeLoadtimeActions();
     loadCheckboxBehavior();
     loadAddForm();
