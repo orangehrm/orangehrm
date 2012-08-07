b618758a5d3bafd162f0a5e60f8007e14713da33
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-08-07 09:39:38 +0000

    Removing ie9 fix

--- php/orangehrm/symfony/web/js/orangehrm.ie9.hacks.js
+++ php/orangehrm/symfony/web/js/orangehrm.ie9.hacks.js
@@ -2,12 +2,7 @@ $(document).ready(function(){
 
     /* Fix for IE9 white space issue in tables: Begins */
 
-    var whiteSpaceExpr = new RegExp('>[ \t\r\n\v\f]*<', 'g');
 
-    $('table').each(function(){
-        var tableHtml = $(this).html();
-        $(this).html(tableHtml.replace(whiteSpaceExpr, '><'));
-    });
 
     /* Fix for IE9 white space issue in tables: Ends */        
 
