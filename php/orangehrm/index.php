c59166edfe84c4d5dd76f62c0400f8c14891f8fe
webgaya <webgaya@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-08-07 13:04:48 +0000

    Forcing IE8 mode

--- php/orangehrm/index.php
+++ php/orangehrm/index.php
@@ -853,6 +853,8 @@ if (isset($_SESSION['load.admin.localization'])) {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $cultureElements[0]; ?>" lang="<?php echo $cultureElements[0]; ?>">
     <head>
         <title>OrangeHRM</title>
+        <!-- Mimic Internet Explorer 8 -->  
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" >         
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <link href="themes/<?php echo $styleSheet; ?>/css/style.css" rel="stylesheet" type="text/css"/>
         <link href="favicon.ico" rel="icon" type="image/gif"/>
