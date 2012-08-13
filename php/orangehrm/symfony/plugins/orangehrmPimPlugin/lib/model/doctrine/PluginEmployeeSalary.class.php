4ed79ffdb9a3c614d3fbd46abc59a424df1f3715
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-08-13 04:59:27 +0000

    Re-added salary encryption listener which was removed in previous checkin

--- php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/model/doctrine/PluginEmployeeSalary.class.php
+++ php/orangehrm/symfony/plugins/orangehrmPimPlugin/lib/model/doctrine/PluginEmployeeSalary.class.php
@@ -10,7 +10,14 @@
  * @author     ##NAME## <##EMAIL##>
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-abstract class PluginEmployeeSalary extends BaseEmployeeSalary
-{
+abstract class PluginEmployeeSalary extends BaseEmployeeSalary {
+    
+    public function setUp() {
+        parent::setup();
 
+        if (KeyHandler::keyExists()) {
+            $key = KeyHandler::readKey();
+            $this->addListener(new EncryptionListener('amount', $key));
+        }
+    } 
 }
