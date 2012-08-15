dbe03330e333a14b8dcf7232b9878b7989118b98
chameeracd <chameeracd@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2012-08-15 09:54:45 +0000

    Community message in ajax

--- php/orangehrm/index.php
+++ php/orangehrm/index.php
@@ -875,38 +875,7 @@ $menuObj->getJavascript($menu);
     <body>
         <div id="companyLogoHeader"></div>
 
-	<div align='right' style="height:0px; width:100%;">
-	<?php
-
-	try
-{
-if($xml = @simplexml_load_file("http://www.orangehrm.com/global_update/orangehrm_updates.xml"))
-	{
-	foreach($xml->children() as $child)
-	  {
-		$data[] = $child;
-	  }
-	for($i=0; $i<count($data); $i=$i+3)
-		{
-			echo "<div style='width: auto; float:right; margin: 12px 2px 0px 0px;'>
-				<table border='0'>
-					<tr><td><a href='".$data[$i]."' target='_blank' style='text-decoration:none;'><font style='color:green; font-weight:bold; font-size:12px;'>".$i18n->__($data[$i+1])."</font>&nbsp;&nbsp;</td></tr>
-					<tr><td align='center'><a href='".$data[$i]."' target='_blank' style='text-decoration:none;'><font style='font-size:10px; font-weight:bold;'>".$i18n->__($data[$i+2])."</font></a></td></tr>
-				</table>
-			      </div>";
-		}
-	}
-else
-	{
-		echo "";
-	}
-}
-catch(exception $e)
-{
-	echo "";
-}
-
-	?>	
+        <div align='right' style="height:0px; width:100%;" id="orangehrm_updates">	
 	</div>	
 
 
@@ -941,6 +910,12 @@ catch(exception $e)
 
             exploitSpace();
             window.onresize = exploitSpace;
+            
+            var xhReq = new XMLHttpRequest();
+            xhReq.open("GET", "orangehrm_updates.php", false);
+            xhReq.send(null);
+            document.getElementById("orangehrm_updates").innerHTML = xhReq.responseText;
+
             //]]>
         </script>
 
