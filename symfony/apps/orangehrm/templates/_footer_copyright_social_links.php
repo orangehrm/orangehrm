0ecb8b66279431f6a778292c2651a534a63ff05b
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2013-09-26 08:54:37 +0000

    social links: update youtube link, escape & and specify alt attributes for images

--- symfony/apps/orangehrm/templates/_footer_copyright_social_links.php
+++ symfony/apps/orangehrm/templates/_footer_copyright_social_links.php
@@ -6,13 +6,13 @@ $imagePath = theme_path("images/login");
     <?php include_partial('global/copyright'); ?>
     </div>
     <div id="social-icons">
-        <a href="http://www.linkedin.com/groups?home=&gid=891077" target="_blank">
-            <img src="<?php echo "{$imagePath}/linkedin.png"; ?>" /></a>&nbsp;
+        <a href="http://www.linkedin.com/groups?home=&amp;gid=891077" target="_blank">
+            <img src="<?php echo "{$imagePath}/linkedin.png"; ?>" alt="LinkedIn OrangeHRM group"/></a>&nbsp;
         <a href="http://www.facebook.com/OrangeHRM" target="_blank">
-            <img src="<?php echo "{$imagePath}/facebook.png"; ?>" /></a>&nbsp;
+            <img src="<?php echo "{$imagePath}/facebook.png"; ?>" alt="OrangeHRM on Facebook"/></a>&nbsp;
         <a href="http://twitter.com/orangehrm" target="_blank">
-            <img src="<?php echo "{$imagePath}/twiter.png"; ?>" /></a>&nbsp;
-        <a href="http://www.youtube.com/results?search_query=orangehrm&search_type=" target="_blank">
-            <img src="<?php echo "{$imagePath}/youtube.png"; ?>" /></a>&nbsp;
+            <img src="<?php echo "{$imagePath}/twiter.png"; ?>" alt="OrangeHRM on twitter"/></a>&nbsp;
+        <a href="http://www.youtube.com/orangehrm" target="_blank">
+            <img src="<?php echo "{$imagePath}/youtube.png"; ?>" alt="OrangeHRM on youtube"/></a>&nbsp;
     </div>
 </div>
