90655bc67530bea553e952ce5abd4c3129ed604c
rtamarasinghe <rtamarasinghe@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2014-03-05 06:35:26 +0000

    fix test failure when run in 2014

--- symfony/plugins/orangehrmLeavePlugin/test/model/service/LeavePeriodHistoryServiceTest.php
+++ symfony/plugins/orangehrmLeavePlugin/test/model/service/LeavePeriodHistoryServiceTest.php
@@ -114,17 +114,26 @@ class LeavePeriodHistoryServiceTest extends PHPUnit_Framework_TestCase {
 
      /* Fails if run in 2014 */
      public function testGetGeneratedLeavePeriodListForLeapYear(){
+        $thisYear = date('Y');
+        
+        $startYear = $thisYear - 3;
+        $nextYear = $thisYear + 1;
+        
         $leavePeriodHistory = new LeavePeriodHistory();
         $leavePeriodHistory->setLeavePeriodStartMonth(3);
         $leavePeriodHistory->setLeavePeriodStartDay(1);
-        $leavePeriodHistory->setCreatedAt('2010-01-02');
+        $leavePeriodHistory->setCreatedAt($startYear . '-01-02');
         
         $this->leavePeriodService->saveLeavePeriodHistory( $leavePeriodHistory );
         
+        $expected = array();
+        
+        for ($year = $startYear - 1; $year <= $nextYear; $year++) {
+            $expected[] = array($year . '-03-01', $this->getLastDayInFebruary($year + 1));
+        }
         
         $result = $this->leavePeriodService->getGeneratedLeavePeriodList(null, true);
-        
-        $this->assertEquals(array(array('2009-03-01','2010-02-28'),array('2010-03-01','2011-02-28'),array('2011-03-01','2012-02-29'),array('2012-03-01','2013-02-28'),array('2013-03-01','2014-02-28'), array('2014-03-01','2015-02-28')),$result);                
+        $this->assertEquals($expected, $result);                
     }
     
     public function testGetGeneratedLeavePeriodListDefineAs2010Jan1stAnd2012Jan1st(){
@@ -175,35 +184,61 @@ class LeavePeriodHistoryServiceTest extends PHPUnit_Framework_TestCase {
     
     /* Fails if run in 2014 */
      public function testGetGeneratedLeavePeriodListCase1(){
+        
+        $thisYear = date('Y');
+        $expected = array();
+        
+        $threeYearsAgo = $thisYear - 3;
+        $twoYearsAgo = $thisYear - 2;
+        $oneYearAgo = $thisYear - 1;
+        $nextYear = $thisYear + 1;
+        $nextNextYear = $thisYear + 2;
+        
         $leavePeriodHistory = new LeavePeriodHistory();
         $leavePeriodHistory->setLeavePeriodStartMonth(1);
         $leavePeriodHistory->setLeavePeriodStartDay(1);
-        $leavePeriodHistory->setCreatedAt('2010-10-02');
+        $leavePeriodHistory->setCreatedAt($threeYearsAgo . '-10-02');
+        $expected[] = array($threeYearsAgo . '-01-01', $threeYearsAgo . '-12-31');
         
         $this->leavePeriodService->saveLeavePeriodHistory( $leavePeriodHistory );
         
         $leavePeriodHistory = new LeavePeriodHistory();
         $leavePeriodHistory->setLeavePeriodStartMonth(2);
         $leavePeriodHistory->setLeavePeriodStartDay(1);
-        $leavePeriodHistory->setCreatedAt('2011-08-04');
+        $leavePeriodHistory->setCreatedAt($twoYearsAgo . '-08-04');
+        $expected[] = array($twoYearsAgo . '-01-01', $oneYearAgo . '-01-31');
         
         $this->leavePeriodService->saveLeavePeriodHistory( $leavePeriodHistory );
         
         $leavePeriodHistory = new LeavePeriodHistory();
         $leavePeriodHistory->setLeavePeriodStartMonth(3);
         $leavePeriodHistory->setLeavePeriodStartDay(1);
-        $leavePeriodHistory->setCreatedAt('2012-08-02');
+        $leavePeriodHistory->setCreatedAt($oneYearAgo . '-08-02');
+        $expected[] = array($oneYearAgo . '-02-01', $this->getLastDayInFebruary($thisYear));
         
         $this->leavePeriodService->saveLeavePeriodHistory( $leavePeriodHistory );
+        
+        $expected[] = array($thisYear . '-03-01', $this->getLastDayInFebruary($nextYear));
+        $expected[] = array($nextYear . '-03-01', $this->getLastDayInFebruary($nextNextYear));
         
         // work around for cached generated leave period list
         $newLeavePeriodService = new LeavePeriodService();
         $newLeavePeriodService->setLeaveEntitlementService($this->leavePeriodService->getLeaveEntitlementService());                
         $result= $newLeavePeriodService->getGeneratedLeavePeriodList(null, true);
         
-        $this->assertEquals(array(array('2010-01-01','2010-12-31'),array('2011-01-01','2012-01-31'),array('2012-02-01','2013-02-28'),array('2013-03-01','2014-02-28'),array('2014-03-01','2015-02-28')),$result);
+        $this->assertEquals($expected, $result);
         
         
+    }
+    
+    /**
+     * Returns the last day of the given year in yyy-mm-dd format.
+     * 
+     * @param String $year Full year with 4 digits
+     */
+    protected function getLastDayInFebruary($year) {
+        $lastDay =  date('L', strtotime("$year-01-01")) ? '29' : '28';
+        return "{$year}-02-{$lastDay}"; 
     }
     
       public function testGetGeneratedLeavePeriodListCase2(){
