efdc3f573d88547d25413e9352f275a6c4de994b
damith88 <damith88@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2014-04-25 06:32:50 +0000

    fix a csrf vulnerability in deleting project activities

--- symfony/plugins/orangehrmAdminPlugin/modules/admin/actions/deleteProjectActivityAction.class.php
+++ symfony/plugins/orangehrmAdminPlugin/modules/admin/actions/deleteProjectActivityAction.class.php
@@ -27,30 +27,31 @@ class deleteProjectActivityAction extends sfAction {
 	 * @param <type> $request
 	 */
 	public function execute($request) {
-
 		$toBeDeletedActivityIds = $request->getParameter('chkSelectRow');
 		$projectId = $request->getParameter('projectId');
-		
-		if (!empty($toBeDeletedActivityIds)) {
-			$delete = true;
-			foreach ($toBeDeletedActivityIds as $toBeDeletedActivityId) {
-				$deletable = $this->getProjectService()->hasActivityGotTimesheetItems($toBeDeletedActivityId);
-				if ($deletable) {
-					$delete = false;
-					break;
-				}
-			}
-			if ($delete) {
-				foreach ($toBeDeletedActivityIds as $toBeDeletedActivityId) {
+		$form = new DefaultListForm();
+                $form->bind($request->getParameter($form->getName()));
+                if ($form->isValid()) {
+                    if (!empty($toBeDeletedActivityIds)) {
+                            $delete = true;
+                            foreach ($toBeDeletedActivityIds as $toBeDeletedActivityId) {
+                                    $deletable = $this->getProjectService()->hasActivityGotTimesheetItems($toBeDeletedActivityId);
+                                    if ($deletable) {
+                                            $delete = false;
+                                            break;
+                                    }
+                            }
+                            if ($delete) {
+                                    foreach ($toBeDeletedActivityIds as $toBeDeletedActivityId) {
 
-					$customer = $this->getProjectService()->deleteProjectActivities($toBeDeletedActivityId);
-				}
-				$this->getUser()->setFlash('success', __(TopLevelMessages::DELETE_SUCCESS));
-			} else {
-				$this->getUser()->setFlash('error', __('Not Allowed to Delete Project Activites Which Have Time Logged Against'));
-			}
-		}
-
+                                            $customer = $this->getProjectService()->deleteProjectActivities($toBeDeletedActivityId);
+                                    }
+                                    $this->getUser()->setFlash('success', __(TopLevelMessages::DELETE_SUCCESS));
+                            } else {
+                                    $this->getUser()->setFlash('error', __('Not Allowed to Delete Project Activites Which Have Time Logged Against'));
+                            }
+                    }
+                }
 		$this->redirect('admin/saveProject?projectId=' . $projectId . '#ProjectActivities');
 	}
 
