64bd1af1bcba436d1794fe7c7ce4df4713b31a05
hashan0314 <hashan0314@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2014-10-06 03:29:28 +0000

    remove subunit from kpis

--- symfony/lib/model/doctrine/orangehrmPerformancePlugin/base/BaseKpi.class.php
+++ symfony/lib/model/doctrine/orangehrmPerformancePlugin/base/BaseKpi.class.php
@@ -7,34 +7,28 @@
  * 
  * @property integer $id
  * @property string $jobTitleCode
- * @property string $departmentCode
  * @property varchar $kpi_indicators
  * @property integer $min_rating
  * @property integer $max_rating
  * @property integer $default_kpi
  * @property JobTitle $JobTitle
- * @property Subunit $Department
  * @property Doctrine_Collection $ReviewerRating
  * 
  * @method integer             getId()             Returns the current record's "id" value
  * @method string              getJobTitleCode()   Returns the current record's "jobTitleCode" value
- * @method string              getDepartmentCode() Returns the current record's "departmentCode" value
  * @method varchar             getKpiIndicators()  Returns the current record's "kpi_indicators" value
  * @method integer             getMinRating()      Returns the current record's "min_rating" value
  * @method integer             getMaxRating()      Returns the current record's "max_rating" value
  * @method integer             getDefaultKpi()     Returns the current record's "default_kpi" value
  * @method JobTitle            getJobTitle()       Returns the current record's "JobTitle" value
- * @method Subunit             getDepartment()     Returns the current record's "Department" value
  * @method Doctrine_Collection getReviewerRating() Returns the current record's "ReviewerRating" collection
  * @method Kpi                 setId()             Sets the current record's "id" value
  * @method Kpi                 setJobTitleCode()   Sets the current record's "jobTitleCode" value
- * @method Kpi                 setDepartmentCode() Sets the current record's "departmentCode" value
  * @method Kpi                 setKpiIndicators()  Sets the current record's "kpi_indicators" value
  * @method Kpi                 setMinRating()      Sets the current record's "min_rating" value
  * @method Kpi                 setMaxRating()      Sets the current record's "max_rating" value
  * @method Kpi                 setDefaultKpi()     Sets the current record's "default_kpi" value
  * @method Kpi                 setJobTitle()       Sets the current record's "JobTitle" value
- * @method Kpi                 setDepartment()     Sets the current record's "Department" value
  * @method Kpi                 setReviewerRating() Sets the current record's "ReviewerRating" collection
  * 
  * @package    orangehrm
@@ -54,10 +48,6 @@ abstract class BaseKpi extends sfDoctrineRecord
              'length' => 6,
              ));
         $this->hasColumn('job_title_code as jobTitleCode', 'string', 10, array(
-             'type' => 'string',
-             'length' => 10,
-             ));
-        $this->hasColumn('department_code as departmentCode', 'string', 10, array(
              'type' => 'string',
              'length' => 10,
              ));
@@ -84,10 +74,6 @@ abstract class BaseKpi extends sfDoctrineRecord
         parent::setUp();
         $this->hasOne('JobTitle', array(
              'local' => 'jobTitleCode',
-             'foreign' => 'id'));
-
-        $this->hasOne('Subunit as Department', array(
-             'local' => 'department_code',
              'foreign' => 'id'));
 
         $this->hasMany('ReviewerRating', array(
