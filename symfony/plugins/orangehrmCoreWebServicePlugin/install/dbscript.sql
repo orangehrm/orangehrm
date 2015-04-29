5f68ec8ef9516bfd19dd2ed713c589c00ace1176
emmafroster <emmafroster@e1132cf6-a30d-0410-af00-bc5eaeb76bce>
2015-04-29 04:33:17 +0000

    change orangehrmCoreWebServicePlugin dbscript to create table if not exists

--- symfony/plugins/orangehrmCoreWebServicePlugin/install/dbscript.sql
+++ symfony/plugins/orangehrmCoreWebServicePlugin/install/dbscript.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `ohrm_ws_consumer` (
+CREATE TABLE IF NOT EXISTS `ohrm_ws_consumer` (
     `app_id` INT UNSIGNED AUTO_INCREMENT,
     `app_token` VARCHAR(10) NOT NULL,
     `app_name` VARCHAR(50) DEFAULT NULL,
