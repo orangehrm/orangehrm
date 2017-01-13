c7a870fbc7f06faed35a12f53960da93949e97d2
Samantha Jayasinghe <isamantha@gmail.com>
2017-01-13 15:42:45 +1300

    initiate v1 rest apis

---
+++ symfony/plugins/orangehrmRESTPlugin/modules/baseapi/actions/baseGetAction.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of resourceAction
+ *
+ * @author orangehrm
+ */
+class baseGetAction extends baseOAuthAction {
+    public function execute($request) {
+        $server = $this->getOAuthServer();
+        $oauthRequest = $this->getOAuthRequest();
+        $oauthResponse = $this->getOAuthResponse();
+        
+        if (!$server->verifyResourceRequest($oauthRequest, $oauthResponse)) {
+            $server->getResponse()->send();
+            exit;
+        }
+        $api_response = array(
+            'friends' => array(
+                'john',
+                'matt',
+                'jane'
+            )
+        );
+        echo json_encode($api_response);
+        return sfView::NONE;
+    }
+}
+
