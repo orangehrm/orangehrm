8826fc5dbbc52a64e590221e45ea83a322d8b247
lakshithaorange <lakshitha@orangehrmlive.com>
2017-01-24 09:30:25 +0530

    OS:employeeAPI changes

---
+++ symfony/plugins/orangehrmRESTPlugin/lib/Api/Pim/EmployeeWrapper.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pola
+ * Date: 1/24/17
+ * Time: 6:53 AM
+ */
+
+namespace Orangehrm\Rest\Api\Pim;
+
+
+class EmployeeWrapper
+{
+    private $apiEmployee;
+
+    /**
+     * EmployeeWrapper constructor.
+     * @param $employee
+     */
+    public function __construct($apiEmployee)
+    {
+        $this->apiEmployee = $apiEmployee;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApiEmployee()
+    {
+        return $this->apiEmployee;
+    }
+
+    /**
+     * @param mixed $apiEmployee
+     */
+    public function setApiEmployee($apiEmployee)
+    {
+        $this->apiEmployee = $apiEmployee;
+    }
+
+    /**
+     * set api employee variables 
+     *
+     * @param $employee
+     */
+    public  function setEmployee($employee){
+
+        $this->apiEmployee->setFirstName($employee->getFirstName());
+        $this->apiEmployee->setMiddleName($employee->getLastName());
+        $this->apiEmployee->setLastName($employee->getMiddleName());
+        $this->apiEmployee->setAge(25); // temp
+        //TODO create all the needed wrapper variables
+     }
+
+     //TODO unwrap method
+
+}
