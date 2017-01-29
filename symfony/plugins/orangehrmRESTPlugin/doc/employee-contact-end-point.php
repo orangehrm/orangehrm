a84c1106be02186496e8432f2ce634441ba3c9d0
Samantha Jayasinghe <isamantha@gmail.com>
2017-01-29 15:57:21 +1300

    update api docs for employee endpoints

--- symfony/plugins/orangehrmRESTPlugin/doc/employee-contact-end-point.php
+++ symfony/plugins/orangehrmRESTPlugin/doc/employee-contact-end-point.php
@@ -1,5 +1,5 @@
 /**
-* @api {get} /employee/:id/contact Employee Contact detail
+* @api {get} /employee/:id/contact-detail Employee Contact detail
 * @apiName GetEmployeeContactDetails
 * @apiGroup Employee
 *
@@ -13,15 +13,21 @@
 *
 * @apiSuccessExample Success-Response:
 *     HTTP/1.1 200 OK
-*     [
+*
 *       {
-*       "firstname": "John",
-*       "lastname": "Doe",
-*       "address": "17, clifford rd, Wellington",
-*       "email": "john@orangehrm.com",
-*       "phone": "+762153413",
+*         "data":
+*         {
+*             "id": "001",
+*             "fullName": "John Doe",
+*             "telephone": "03131238",
+*             "email": "test@example.com",
+*             "address": "17 Clifford Rd, Wellington",
+*             "country": "New Zealand"
+*         },
+*       "rels": {
 *       }
-*     ]
+*     }
+
 *
 * @apiError UserNotFound The id of the employee was not found.
 *
