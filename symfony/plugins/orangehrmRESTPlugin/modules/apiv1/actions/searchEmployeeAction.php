d2f22908ee6f32992bf8707ad3b4e3673ed5c09a
lakshithaorange <lakshitha@orangehrmlive.com>
2017-01-30 11:18:18 +0530

    OS:orangehrmRESTPlugin:API routing changes

---
+++ symfony/plugins/orangehrmRESTPlugin/modules/apiv1/actions/searchEmployeeAction.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: pola
+ * Date: 1/23/17
+ * Time: 9:47 AM
+ */
+class searchEmployeeAction extends  baseGetAction
+{
+
+    /**
+     * @param \Orangehrm\Rest\Http\Request $request
+     * @return \Orangehrm\Rest\Http\Response
+     */
+    protected function handleRequest(\Orangehrm\Rest\Http\Request $request)
+    {
+       var_dump("search Employeeee");die();
+    }
+}
