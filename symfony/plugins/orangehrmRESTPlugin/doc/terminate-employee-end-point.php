be7c6f9fe4a392acfae1fec6a8571bc3c8bd337a
lakshithaorange <lakshitha@orangehrmlive.com>
2017-02-20 12:07:36 +0530

    OS:REST plugin : api doc changes for employee termination

---
+++ symfony/plugins/orangehrmRESTPlugin/doc/terminate-employee-end-point.php
@@ -0,0 +1,33 @@
+/**
+* @api {post} employee/:id/action/terminate Terminate employment
+* @apiName terminateEmployee
+* @apiGroup Employee
+*
+* @apiParam {Number}  employee id
+*
+* @apiParam {String} termination date.
+* @apiParam {String} termination reason.
+* @apiParam {String} note  termination note.
+* @apiSuccess {Object} data success response
+*
+* @apiSuccessExample Success-Response:
+*     HTTP/1.1 200 OK
+*
+*      {
+*        "success": "Successfully terminated"
+*      }
+*
+* @apiError Bad-Response Saving failed.
+*
+* @apiErrorExample Error-Response:
+*     HTTP/1.1 400 Bad Request
+*     {
+*       "error": ["Termination failed"]
+*     }
+*
+* @apiErrorExample Error-Response:
+*     HTTP/1.1 202 Invalid Parameter
+*     {
+*       "error": ["invalid Parameter"]
+*     }
+*/
