4065222f6911e90ffe486f42e784cd4ca0aa6cf1
lakshithaorange <lakshitha@orangehrmlive.com>
2017-03-20 04:26:13 +0530

    OS:RESTPlugin : Leave Request add comments entity

--- symfony/plugins/orangehrmRESTPlugin/lib/Api/Leave/Entity/LeaveRequest.php
+++ symfony/plugins/orangehrmRESTPlugin/lib/Api/Leave/Entity/LeaveRequest.php
@@ -219,7 +219,18 @@ class LeaveRequest implements Serializable
         $this->setDate($leaveRequest->getDateApplied());
         $this->setLeaveBalance($leaveRequest->getLeaveBalance());
         $this->setStatus($leaveRequest->getLeaveStatusId());
-        $this->setComments($leaveRequest->getLeaveRequestComment());
         $this->setNumberOfDays($leaveRequest->getNumberOfDays());
+        $this->setAction(\Leave::getLeaveStatusForText($leaveRequest->getLeaveStatusId()));
+
+        $commentsList = null;
+
+        if(!empty($leaveRequest->getLeaveRequestComment())) {
+            foreach ($leaveRequest->getLeaveRequestComment() as $comment){
+                $datetime = explode(" ", $comment->getCreated());
+                $leaveComment = new LeaveRequestComment($comment->getId(),$comment->getCreatedByName(),$datetime[0] , $datetime[1]  ,$comment->getComments());
+                $commentsList[] = $leaveComment->toArray();
+            }
+        }
+        $this->setComments($commentsList);
     }
 }
