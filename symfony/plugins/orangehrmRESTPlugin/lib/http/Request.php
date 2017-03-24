1bd335cbb0a597e18baad0c958f76d385f1bd0a0
lakshithaorange <lakshitha@orangehrmlive.com>
2017-03-24 17:22:36 +0530

    OS:RESTPlugin : request get method type ,

--- symfony/plugins/orangehrmRESTPlugin/lib/http/Request.php
+++ symfony/plugins/orangehrmRESTPlugin/lib/http/Request.php
@@ -57,4 +57,15 @@ class Request{
             );
 
     }
+    /**
+     * Checks if the request method is the given one.
+     *
+     * @param  string $method  The method name
+     *
+     * @return bool true if the current method is the given one, false otherwise
+     */
+    public function isMethod($method)
+    {
+        return strtoupper($method) == $this->getActionRequest()->getMethod();
+    }
 }
