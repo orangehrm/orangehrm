c388b69764dce5948650b020c2b049e51d0c6140
lakshithaorange <lakshitha@orangehrmlive.com>
2017-05-09 16:46:49 +0530

    Adding client app and api documentation links to auth page

--- symfony/plugins/orangehrmCoreOAuthPlugin/modules/admin/templates/registerOAuthClientSuccess.php
+++ symfony/plugins/orangehrmCoreOAuthPlugin/modules/admin/templates/registerOAuthClientSuccess.php
@@ -17,12 +17,27 @@
         <form id="oAuthClientRegistrationForm" method="post" action="<?php echo url_for('admin/registerOAuthClient'); ?>" 
               enctype="multipart/form-data">
             <fieldset>
+
                 <ol>
                     <?php echo $form->render(); ?>
+
                     <li class="required">
                         <em>*</em> <?php echo __(CommonMessages::REQUIRED_FIELD); ?>
                     </li>
+
+
+
+
                 </ol>
+                <ol>
+                    <dive style="float: left;width: 425px;margin-top: 9px;font-size: 12px;" >
+                        <p> <b>API Documentation : </b> <a  target="_blank"  href="https://orangehrm.github.io/orangehrm-api-doc/"> API Documentation</a>.</p>
+                        <br>
+                        <p><b>PHP Sample App :</b><a  target="_blank"  href="https://github.com/orangehrm/api-sample-app-php"> Notification Dashboard</a>.</p>
+                    </dive>
+
+                </ol>
+
                 <p>
                     <input type="submit" class="" id="btnSave" value="<?php echo __("Save"); ?>"  />
                 </p>
