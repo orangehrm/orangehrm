6882f987100a6ec78d1ddd4b00670d521f0cfa2e
lakshithaorange <lakshitha@orangehrmlive.com>
2017-06-13 12:37:39 +0530

    OS:RESTPlugin :API docs for Organization and employee picture

---
+++ symfony/plugins/orangehrmRESTPlugin/doc/34-get-employee-picture-end-point.php
@@ -0,0 +1,22 @@
+/**
+* @api {get} /custom-field 34.Employee Picture
+* @apiName getEmployeePicture
+* @apiGroup Employee
+* @apiVersion 0.1.0
+*
+* @apiSuccess {String} base64 Base64 encoded employee picture details.
+*
+*
+* @apiSuccessExample Success-Response:
+*     HTTP/1.1 200 OK
+*
+*		{
+*		  "data": [
+*		    {
+*		     "base64": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgICAgMCAgIDAwMDBAY"
+*		    }
+*		  ]
+*		 }
+*
+*
+*/
