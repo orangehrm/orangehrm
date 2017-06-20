85fc06fa51974308fcbf969378a844ff2d71b67f
Ruchira <rtamarasinghe@gmail.com>
2017-06-20 08:50:22 +0530

    Added Doctrine class for backwards compatibility. Can be removed after changing usages of Doctrine:: to Doctrine_Core::

---
+++ symfony/lib/model/Doctrine.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Kept for backwards compatibility.
+ *
+ * TODO: Change usages of Doctrine:: to Doctrine_Core::
+ * and remove this file.
+ */
+class Doctrine extends Doctrine_Core {
+
+}
