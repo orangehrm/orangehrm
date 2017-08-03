724b4ff7f4b0d17c72936b1ff266cd17f7250419
thulana <thulanakannangara@gmail.com>
2017-08-03 12:43:30 +0530

    fix seeding script

--- travis-config-files/seeddb.sh
+++ travis-config-files/seeddb.sh
@@ -4,8 +4,9 @@
 if [[ ${SEED} == true ]];
 then
     echo "if statement"
-    cd var/www/site/orangehrm/devTools/load/general; php load-employees.php
-	cd var/www/site/orangehrm/devTools/load/recruitment; php load-candidates.php
+    ls
+    cd ../devTools/load/general; php load-employees.php
+	cd ../devTools/load/recruitment; php load-candidates.php
 fi
 
 exit 0;
