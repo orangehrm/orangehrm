ccb7fea3b597c7eadcaf96c9eb26a48a49a1b24d
thulana <thulanakannangara@gmail.com>
2017-08-03 08:59:24 +0530

    change db seeding

--- travis-config-files/seeddb.sh
+++ travis-config-files/seeddb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-echo ${TRAVIS_BUILD_NUMBER}".3"
-echo ${TRAVIS_JOB_NUMBER}
-if [ ${TRAVIS_JOB_NUMBER} == ${TRAVIS_BUILD_NUMBER}".3" ]
+jobno="${TRAVIS_BUILD_NUMBER}.3"
+echo "${TRAVIS_BUILD_NUMBER}.3"
+echo "${TRAVIS_JOB_NUMBER}"
+if [ "${TRAVIS_JOB_NUMBER}" == "$jobno" ]
 then
     php var/www/site/orangehrm/devTools/load/genera/load-employees.php
 	php var/www/site/orangehrm/devTools/load/recruitment/load-candidates.php
