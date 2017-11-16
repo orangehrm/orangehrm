1a81ff4d3c5349f8e38a916a6e80012295d5764d
yasankarj <yasankarj@gmail.com>
2017-11-16 19:08:04 +0530

    Add Necessary dbscripts to installer.

--- dbscript/dbscript-2.sql
+++ dbscript/dbscript-2.sql
@@ -2829,3 +2829,8 @@ INSERT INTO hs_hr_config (`key`, `value`) VALUES
 </integrations>
 </xml>
 ');
+
+INSERT INTO `hs_hr_config` (`key`, `value`) VALUES
+('authentication.status', 'Enable'),
+('authentication.enforce_password_strength', 'on'),
+('authentication.default_required_password_strength', 'medium');
