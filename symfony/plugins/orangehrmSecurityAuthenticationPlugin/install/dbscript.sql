71498da2c47d7c0fdf6ee23611d7141c879c9c00
yasankarj <yasankarj@gmail.com>
2017-11-17 15:15:27 +0530

    Change Password Strength Level to Strong

--- symfony/plugins/orangehrmSecurityAuthenticationPlugin/install/dbscript.sql
+++ symfony/plugins/orangehrmSecurityAuthenticationPlugin/install/dbscript.sql
@@ -1,4 +1,4 @@
 INSERT INTO `hs_hr_config` (`key`, `value`) VALUES
 ('authentication.status', 'Enable'),
 ('authentication.enforce_password_strength', 'on'),
-('authentication.default_required_password_strength', 'medium');
+('authentication.default_required_password_strength', 'strong');
