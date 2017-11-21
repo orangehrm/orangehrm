4bd247291429456e31eecd95f65a59c0ce70c49a
lakshithaorange <lakshitha@orangehrmlive.com>
2017-11-21 16:50:03 +0530

    Customer Service changes

--- symfony/plugins/orangehrmAdminPlugin/lib/service/ProjectService.php
+++ symfony/plugins/orangehrmAdminPlugin/lib/service/ProjectService.php
@@ -19,63 +19,91 @@
  */
 class ProjectService extends BaseService {
 
-	private $projectDao;
+    private $projectDao;
 
-	/**
-	 * Construct
-	 */
-	public function __construct() {
-		$this->projectDao = new ProjectDao();
-	}
+    /**
+     * Construct
+     */
+    public function __construct() {
+        $this->projectDao = new ProjectDao();
+    }
 
-	/**
-	 *
-	 * @param ProjectDao $projectDao
-	 */
-	public function setTimesheetDao(ProjectDao $projectDao) {
+    /**
+     *
+     * @return <type>
+     */
+    public function getProjectDao() {
+        return $this->projectDao;
+    }
 
-		$this->projectDao = $projectDao;
-	}
+    /**
+     *
+     * @param UbCoursesDao $UbCoursesDao
+     */
+    public function setProjectDao(ProjectDao $projectDao) {
+        $this->projectDao = $projectDao;
+    }
 
-	/**
-	 *
-	 * get Project count
-	 *
-	 * Get Active Project count in default. Can get all project count by passing $activeOnly as false.
-	 *
-	 * @param type $activeOnly
-	 * @return type
-	 */
-	public function getProjectCount($activeOnly) {
-		return $this->projectDao->getProjectCount($activeOnly);
-	}
+    /**
+     *
+     * @param ProjectDao $projectDao
+     */
+    public function setTimesheetDao(ProjectDao $projectDao) {
 
-	/**
-	 *
-	 * Delete project
-	 *
-	 * Set project's is_deleted flag to 1. This will handled the deleting of corresponding
-	 * project activities and project admins under deleted project.
-	 *
-	 * @param type $projectId
-	 * @return type
-	 */
-	public function deleteProject($projectId) {
-		return $this->projectDao->deleteProject($projectId);
-	}
+        $this->projectDao = $projectDao;
+    }
 
-	/**
-	 *
-	 * Delete project activity
-	 *
-	 * Set project activity's is_deleted flag to 1.
-	 *
-	 * @param type $activityId
-	 * @return type
-	 */
-	public function deleteProjectActivities($activityId) {
-		return $this->projectDao->deleteProjectActivities($activityId);
-	}
+    /**
+     *
+     * get Project count
+     *
+     * Get Active Project count in default. Can get all project count by passing $activeOnly as false.
+     *
+     * @param type $activeOnly
+     * @return type
+     */
+    public function getProjectCount($activeOnly) {
+        return $this->projectDao->getProjectCount($activeOnly);
+    }
+
+    /**
+     *
+     * Delete project
+     *
+     * Set project's is_deleted flag to 1. This will handled the deleting of corresponding
+     * project activities and project admins under deleted project.
+     *
+     * @param type $projectId
+     * @return type
+     */
+    public function deleteProject($projectId) {
+        return $this->projectDao->deleteProject($projectId);
+    }
+
+    /**
+     *
+     * Delete project activity
+     *
+     * Set project activity's is_deleted flag to 1.
+     *
+     * @param type $activityId
+     * @return type
+     */
+    public function deleteProjectActivities($activityId) {
+        return $this->projectDao->deleteProjectActivities($activityId);
+    }
+
+    /**
+     *
+     * Gret project by id.
+     *
+     * @param type $projectId
+     * @return type
+     */
+    public function getProjectById($projectId) {
+        return $this->projectDao->getProjectById($projectId);
+    }
+
 
     /**
      * Return an array of Customer Ids for Lits of Project Ids
@@ -122,214 +150,187 @@ class ProjectService extends BaseService {
         return $this->projectDao->getProjectNameList($projectIdList, $excludeDeletedProjects);
     }
 
-	/**
-	 *
-	 * Get project activity by id.
-	 *
-	 * @param type $projectId
-	 * @return type
-	 */
-	public function getProjectActivityById($projectId) {
-		return $this->projectDao->getProjectActivityById($projectId);
-	}
+    /**
+     *
+     * Get project activity by id.
+     *
+     * @param type $projectId
+     * @return type
+     */
+    public function getProjectActivityById($projectId) {
+        return $this->projectDao->getProjectActivityById($projectId);
+    }
 
-	/**
-	 *
-	 * Get all projects
-	 *
-	 * Get all active projects as default. Can get all projects by passing $activeOnly parameter as false.
-	 *
-	 * @return type
-	 */
-	public function getAllProjects($activeOnly) {
-		return $this->projectDao->getAllProjects($activeOnly);
-	}
+    /**
+     *
+     * Get all projects
+     *
+     * Get all active projects as default. Can get all projects by passing $activeOnly parameter as false.
+     *
+     * @return type
+     */
+    public function getAllProjects($activeOnly) {
+        return $this->projectDao->getAllProjects($activeOnly);
+    }
 
-	/**
-	 *
-	 * Get active activity list for a project.
-	 *
-	 * @param type $projectId
-	 * @return type
-	 */
-	public function getActivityListByProjectId($projectId) {
-		return $this->projectDao->getActivityListByProjectId($projectId);
-	}
+    /**
+     *
+     * Get active activity list for a project.
+     *
+     * @param type $projectId
+     * @return type
+     */
+    public function getActivityListByProjectId($projectId) {
+        return $this->projectDao->getActivityListByProjectId($projectId);
+    }
 
-	/**
-	 * Will return wheather the activity has any timesheet records related.
-	 *
-	 * @param type $activityId
-	 * @return type
-	 */
-	public function hasActivityGotTimesheetItems($activityId) {
-		return $this->projectDao->hasActivityGotTimesheetItems($activityId);
-	}
+    /**
+     * Will return wheather the activity has any timesheet records related.
+     *
+     * @param type $activityId
+     * @return type
+     */
+    public function hasActivityGotTimesheetItems($activityId) {
+        return $this->projectDao->hasActivityGotTimesheetItems($activityId);
+    }
 
-	/**
-	 *
-	 * Will return wheather the project has any timesheet records related.
-	 *
-	 * @param type $projectId
-	 * @return type
-	 */
-	public function hasProjectGotTimesheetItems($projectId) {
-		return $this->projectDao->hasProjectGotTimesheetItems($projectId);
-	}
+    /**
+     *
+     * Will return wheather the project has any timesheet records related.
+     *
+     * @param type $projectId
+     * @return type
+     */
+    public function hasProjectGotTimesheetItems($projectId) {
+        return $this->projectDao->hasProjectGotTimesheetItems($projectId);
+    }
 
-	/**
-	 *
-	 * Get active project list for a customer.
-	 *
-	 * @param type $customerId
-	 * @return type
-	 */
-	public function getProjectsByCustomerId($customerId) {
-		return $this->projectDao->getProjectsByCustomerId($customerId);
-	}
+    /**
+     *
+     * Get active project list for a customer.
+     *
+     * @param type $customerId
+     * @return type
+     */
+    public function getProjectsByCustomerId($customerId) {
+        return $this->projectDao->getProjectsByCustomerId($customerId);
+    }
 
-	/**
-	 * Get project list for login user
-	 *
-	 * @param type $role
-	 * @param type $empNumber
-	 * @return type
-	 */
-	public function getProjectListForUserRole($role, $empNumber) {
-		return $this->projectDao->getProjectListForUserRole($role, $empNumber);
-	}
+    /**
+     * Get project list for login user
+     *
+     * @param type $role
+     * @param type $empNumber
+     * @return type
+     */
+    public function getProjectListForUserRole($role, $empNumber) {
+        return $this->projectDao->getProjectListForUserRole($role, $empNumber);
+    }
 
-	/**
-	 * Gets project name with customer name given project id.
-	 *
-	 * @param integer $projectId
-	 * @return string
-	 */
-	public function getProjectNameWithCustomerName($projectId, $glue = " - ") {
+    /**
+     * Gets project name with customer name given project id.
+     *
+     * @param integer $projectId
+     * @return string
+     */
+    public function getProjectNameWithCustomerName($projectId, $glue = " - ") {
 
-		$project = $this->getProjectById($projectId);
-		$projectName = $project->getCustomer()->getName() . $glue . $project->getName();
+        $project = $this->getProjectById($projectId);
+        $projectName = $project->getCustomer()->getName() . $glue . $project->getName();
 
-		return $projectName;
-	}
+        return $projectName;
+    }
 
-	/**
-	 *
-	 * Gret project by id.
-	 *
-	 * @param type $projectId
-	 * @return type
-	 */
-	public function getProjectById($projectId) {
-		return $this->projectDao->getProjectById($projectId);
-	}
+    /**
+     * Get active project list
+     *
+     * @return type
+     */
+    public function getActiveProjectList() {
+        return $this->getProjectDao()->getActiveProjectList();
+    }
 
-	/**
-	 * Get active project list
-	 *
-	 * @return type
-	 */
-	public function getActiveProjectList() {
-		return $this->getProjectDao()->getActiveProjectList();
-	}
+    /**
+     *Get list of active projects, ordered by customer name, project name.
+     *
+     * @return Doctrine_Collection of Project objects. Empty collection if no
+     *         active projects available.
+     */
+    public function getActiveProjectsOrderedByCustomer() {
+        return $this->getProjectDao()->getActiveProjectsOrderedByCustomer();
+    }
 
-	/**
-	 *
-	 * @return <type>
-	 */
-	public function getProjectDao() {
-		return $this->projectDao;
-	}
+    /**
+     * Get project list for a project admin
+     *
+     * @param type $empNo
+     * @param type $emptyIfNotAprojectAdmin
+     * @return type
+     */
+    public function getProjectListByProjectAdmin($empNo, $emptyIfNotAprojectAdmin = false) {
 
-	/**
-	 *
-	 * @param UbCoursesDao $UbCoursesDao
-	 */
-	public function setProjectDao(ProjectDao $projectDao) {
-		$this->projectDao = $projectDao;
-	}
+        $projectAdmins = $this->getProjectDao()->getProjectAdminByEmpNumber($empNo);
 
-        /**
-        *Get list of active projects, ordered by customer name, project name.
-        *
-        * @return Doctrine_Collection of Project objects. Empty collection if no
-        *         active projects available.
-        */
-	public function getActiveProjectsOrderedByCustomer() {
-		return $this->getProjectDao()->getActiveProjectsOrderedByCustomer();
-	}
+        $projectIdArray = array();
 
-	/**
-	 * Check wheather the user is a project admin
-	 *
-	 * @param int $empNumber
-	 * @return boolean
-	 */
-	public function isProjectAdmin($empNumber) {
-		$projects = $this->getProjectListByProjectAdmin($empNumber, true);
-		return (count($projects) > 0);
-	}
+        if (!is_null($projectAdmins)) {
+            foreach ($projectAdmins as $projectAdmin) {
+                $projectIdArray[] = $projectAdmin->getProjectId();
+            }
+        }
 
-	/**
-	 * Get project list for a project admin
-	 *
-	 * @param type $empNo
-	 * @param type $emptyIfNotAprojectAdmin
-	 * @return type
-	 */
-	public function getProjectListByProjectAdmin($empNo, $emptyIfNotAprojectAdmin = false) {
+        if (empty($projectIdArray)) {
+            return array();
+        }
 
-		$projectAdmins = $this->getProjectDao()->getProjectAdminByEmpNumber($empNo);
+        $projectList = $this->getProjectDao()->getProjectsByProjectIds($projectIdArray);
 
-		$projectIdArray = array();
+        return $projectList;
+    }
 
-		if (!is_null($projectAdmins)) {
-			foreach ($projectAdmins as $projectAdmin) {
-				$projectIdArray[] = $projectAdmin->getProjectId();
-			}
-		}
+    /**
+     * Check wheather the user is a project admin
+     *
+     * @param int $empNumber
+     * @return boolean
+     */
+    public function isProjectAdmin($empNumber) {
+        $projects = $this->getProjectListByProjectAdmin($empNumber, true);
+        return (count($projects) > 0);
+    }
 
-		if (empty($projectIdArray)) {
-			return array();
-		}
+    /**
+     * Get project admin list
+     *
+     * @return type
+     */
+    public function getProjectAdminList() {
+        return $this->getProjectDao()->getProjectAdminList();
+    }
 
-		$projectList = $this->getProjectDao()->getProjectsByProjectIds($projectIdArray);
+    /**
+     *
+     * Search project by project name, customer name and project admin.
+     *
+     * @param type $srchClues
+     * @param type $allowedProjectList
+     * @return type
+     */
+    public function searchProjects($srchClues, $allowedProjectList) {
+        return $this->getProjectDao()->searchProjects($srchClues, $allowedProjectList);
+    }
 
-		return $projectList;
-	}
-
-	/**
-	 * Get project admin list
-	 *
-	 * @return type
-	 */
-	public function getProjectAdminList() {
-		return $this->getProjectDao()->getProjectAdminList();
-	}
-
-	/**
-	 *
-	 * Search project by project name, customer name and project admin.
-	 *
-	 * @param type $srchClues
-	 * @param type $allowedProjectList
-	 * @return type
-	 */
-	public function searchProjects($srchClues, $allowedProjectList) {
-		return $this->getProjectDao()->searchProjects($srchClues, $allowedProjectList);
-	}
-
-	/**
-	 *
-	 * Get project count of the search results.
-	 *
-	 * @param type $srchClues
-	 * @param type $allowedProjectList
-	 * @return type
-	 */
-	public function getSearchProjectListCount($srchClues, $allowedProjectList) {
-		return $this->getProjectDao()->getSearchProjectListCount($srchClues, $allowedProjectList);
-	}
+    /**
+     *
+     * Get project count of the search results.
+     *
+     * @param type $srchClues
+     * @param type $allowedProjectList
+     * @return type
+     */
+    public function getSearchProjectListCount($srchClues, $allowedProjectList) {
+        return $this->getProjectDao()->getSearchProjectListCount($srchClues, $allowedProjectList);
+    }
 
 
     /**
@@ -348,7 +349,6 @@ class ProjectService extends BaseService {
      * To check project with the given name existed
      *
      * @param $projectName
-     * @param $customerId
      * @return int
      */
     public function getProjectByName($projectName,$customerId){
