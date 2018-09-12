0a81856a5112f05a5f5e18724500aaf6a600d2a1
rajithakumara <tmrkumara.15@cse.mrt.ac.lk>
2018-09-12 13:19:22 +0530

    Changed session name

--- symfony/plugins/orangehrmCoreOAuthPlugin/modules/oauth/actions/loginAction.class.php
+++ symfony/plugins/orangehrmCoreOAuthPlugin/modules/oauth/actions/loginAction.class.php
@@ -32,7 +32,7 @@ class loginAction extends sfAction {
 
         $this->form = $loginForm;
 
-        setcookie('Orangehrm', '', time() - 3600, '/');
+        setcookie('PHPSESSID', '', time() - 3600, '/');
     }
     
     public function isSecure() {
