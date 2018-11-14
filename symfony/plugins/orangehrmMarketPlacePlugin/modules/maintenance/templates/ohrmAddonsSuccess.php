9045b5627fb11271680b59ac8b5149b09f40fb76
AshanKooragoda <ashan.15@cse.mrt.ac.lk>
2018-11-14 15:47:48 +0530

    create plugin for marcket place

---
+++ symfony/plugins/orangehrmMarketPlacePlugin/modules/maintenance/templates/ohrmAddonsSuccess.php
@@ -0,0 +1,27 @@
+<?php
+use_stylesheet(plugin_web_path('orangehrmMarketPlacePlugin', 'css/materialize.min.css'));
+use_javascript(plugin_web_path('orangehrmMarketPlacePlugin', 'js/materialize.min.js'))
+?>
+<div class="inner">
+
+<div class="row">
+    <div class="col s12 m12">
+        <div class="card small">
+            <div class="card-image">
+                <img src="https://lorempixel.com/100/190/nature/6">
+                <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+                <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
---
+++ symfony/plugins/orangehrmMarketPlacePlugin/test/orangehrmMarketPlacePluginAllTests.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class orangehrmMarketPlacePluginAllTests
+ */
+class orangehrmMarketPlacePluginAllTests {
+
+    /**
+     * @return PHPUnit_Framework_TestSuite
+     */
+    public static function suite() {
+
+        $suite = new PHPUnit_Framework_TestSuite('orangehrmMaintenancePluginAllTest');
+
+        return $suite;
+    }
+
+}
