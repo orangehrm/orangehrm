71b31847f18e864c770f3754f8a38bc84f0abc76
rajithakumara <tmrkumara.15@cse.mrt.ac.lk>
2018-12-19 12:53:59 +0530

    OHRM-185:Update OrangeHRM version to 4.3

--- lib/confs/sysConf.php
+++ lib/confs/sysConf.php
@@ -77,7 +77,7 @@ class sysConf {
     $this->timeFormat = "H:i";
     $this->timeInputHint = "HH:MM";
     $this->styleSheet = "orange";
-    $this->version = "4.2.0.1";
+    $this->version = "4.3";
     $this->registrationUrl = "https://ospenguin.orangehrm.com";
     $this->mode = "dev";
   }
