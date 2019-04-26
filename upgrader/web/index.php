9969913adf82143034dae9b7f686a9ea03a01106
supunrandunurk <48504588+supunrandunurk@users.noreply.github.com>
2019-04-26 20:02:47 +0530

    OHRM-292: fixing upgrader blank page issue

--- upgrader/web/index.php
+++ upgrader/web/index.php
@@ -1,6 +1,8 @@
 <?php
 
+define('ROOT_PATH', realpath(__DIR__ . '/../..'));
 
+include_once(ROOT_PATH . '/lib/confs/log_settings.php');
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 if (!is_writable(dirname(__FILE__).'/../cache') || !is_writable(dirname(__FILE__).'/../log')) {
