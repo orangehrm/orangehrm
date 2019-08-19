99f42d774f1f17310d770aa4a0736d2fab1e57d6
Leshan Bashitha Wijegunawardana <bashithawije@gmail.com>
2019-08-19 12:14:22 +0530

    OHRM-407 : spinner related css changes, jquery auto complete css changes, dashboard related css changes (#553)
    * spinner related css changes, jquery auto complete css changes, dashboard related css changes
    * market place button color change when accessing marketplace screen

--- symfony/plugins/orangehrmMarketPlacePlugin/web/js/ohrmAddonSuccess.js
+++ symfony/plugins/orangehrmMarketPlacePlugin/web/js/ohrmAddonSuccess.js
@@ -7,6 +7,7 @@ var renewId;
 $(document).ready(function () {
     var acc = document.getElementsByClassName("accordion");
     var i;
+    $('#MP_link').addClass('current');
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
             this.classList.toggle("active");
