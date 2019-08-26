64604585ac7958f7de6f965d86ec7c679f78d055
Leshan Bashitha Wijegunawardana <bashithawije@gmail.com>
2019-08-26 16:00:06 +0530

    OHRM-445 : Footer version update and change log

--- installer/guide/index.js
+++ installer/guide/index.js
@@ -1,4 +1,4 @@
-var version = "4.3.2";
+var version = "4.3.3";
 var elements = document.getElementsByClassName('version');
 for (var i = 0; i < elements.length; i++){
 	elements[i].innerHTML = version;
