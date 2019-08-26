12830feaa9bc7232cd6b8308d1778431594e9b44
Damith Karunaratne <damithuom@gmail.com>
2019-08-26 18:38:03 +0530

    OHRM-427: fixing id auto increment test failures

--- upgrader/apps/upgrader/lib/schemaIncrementTasks/SchemaIncrementTask72.php
+++ upgrader/apps/upgrader/lib/schemaIncrementTasks/SchemaIncrementTask72.php
@@ -29,11 +29,14 @@ class SchemaIncrementTask72  extends SchemaIncrementTask
             0 => 'ALTER TABLE `hs_hr_employee` MODIFY `emp_number` int(7) not null AUTO_INCREMENT;',
             1 => 'ALTER TABLE `ohrm_timesheet` MODIFY `timesheet_id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
             2 => 'ALTER TABLE `ohrm_timesheet_item` MODIFY `timesheet_item_id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
-            3 => 'ALTER TABLE `ohrm_timesheet_action_log` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
+            3 => 'ALTER TABLE `ohrm_timesheet_action_log` MODIFY `timesheet_action_log_id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
             4 => 'ALTER TABLE `ohrm_attendance_record` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
             5 => 'ALTER TABLE `ohrm_job_candidate` MODIFY `id` int(13) not null AUTO_INCREMENT;',
             6 => 'ALTER TABLE `ohrm_job_candidate_vacancy` MODIFY `id` int(13) default null unique AUTO_INCREMENT;',
+            6 => 'ALTER TABLE `ohrm_job_vacancy` MODIFY `id` int(13) not null auto_increment;',
         );
+        array_unshift($sql, 'SET FOREIGN_KEY_CHECKS = 0;');
+        array_push($sql, 'SET FOREIGN_KEY_CHECKS = 1;');
         $this->sql = $sql;
     }
 
