7b9c0382c48693f4e15c6f9b3f2d3dd863ab1828
Damith Karunaratne <damithuom@gmail.com>
2019-10-16 16:40:27 +0530

    updating jquery libraries with production releases

--- symfony/apps/orangehrm/templates/_header.php
+++ symfony/apps/orangehrm/templates/_header.php
@@ -25,11 +25,7 @@ $cultureElements = explode('_', $sf_user->getCulture());
         <?php       
         // Library JavaScript files
 
-        //echo javascript_include_tag('jquery/jquery-1.7.2.min.js');
-        //echo javascript_include_tag('jquery/jquery-1.12.4.js');
-        echo javascript_include_tag('jquery/jquery-3.4.1.js');
-        //echo javascript_include_tag('jquery/jquery-migrate-1.4.1.js');
-        //echo javascript_include_tag('jquery/jquery-migrate-3.1.0.min.js');
+        echo javascript_include_tag('jquery/jquery-3.4.1.min.js');
 
         echo javascript_include_tag('jquery/validate/jquery.validate.js');
         
