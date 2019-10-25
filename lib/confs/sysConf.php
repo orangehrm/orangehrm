5e2d9a3145ad12a37af885a8897d04485a0ba104
Damith Karunaratne <damithuom@gmail.com>
2019-10-25 09:08:26 +0530

    bumping version

--- lib/confs/sysConf.php
+++ lib/confs/sysConf.php
@@ -77,7 +77,7 @@ class sysConf {
     $this->timeFormat = "H:i";
     $this->timeInputHint = "HH:MM";
     $this->styleSheet = "orange";
-    $this->version = "4.3.3";
+    $this->version = "4.3.4-beta.1";
     $this->registrationUrl = "https://ospenguin.orangehrm.com";
     $this->mode = "dev";
   }
