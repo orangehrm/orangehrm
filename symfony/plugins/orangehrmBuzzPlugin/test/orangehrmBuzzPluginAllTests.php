fed200b18bb7889fa86f2a0db374c04a9a51639a
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2020-05-13 12:59:23 +0530

    OHRM-625:Improve Buzz unit tests

--- symfony/plugins/orangehrmBuzzPlugin/test/orangehrmBuzzPluginAllTests.php
+++ symfony/plugins/orangehrmBuzzPlugin/test/orangehrmBuzzPluginAllTests.php
@@ -13,6 +13,8 @@ class orangehrmBuzzPluginAllTests {
         $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzServiceTest.php');
         $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzConfigServiceTest.php');
         $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzTextParserServiceTest.php');
+        $suite->addTestFile(dirname(__FILE__) . '/model/dao/BuzzNotificationDaoTest.php');
+        $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzNotificationServiceTest.php');
            
         // utility test cases
         //$suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzWebServiceHelperTest.php');
