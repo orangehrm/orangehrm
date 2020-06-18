45e6748764effc5337a244a6f570c2e1cf2bf6d0
Rajitha Kumara <rajitha@orangehrm.us.com>
2020-06-18 15:08:27 +0530

    OHRM-720:Initiate my info API endpoint

--- symfony/plugins/orangehrmRESTPlugin/config/orangehrmRESTPluginConfiguration.class.php
+++ symfony/plugins/orangehrmRESTPlugin/config/orangehrmRESTPluginConfiguration.class.php
@@ -6,7 +6,7 @@
  */
 require_once __DIR__ . '/../../../lib/vendor/autoload.php';
 /**
- * Description of orangehrmCoreOAuthPluginConfiguration
+ * Description of orangehrmRESTPluginConfiguration
  *
  * @author orangehrm
  */
@@ -15,7 +15,7 @@ class orangehrmRESTPluginConfiguration extends sfPluginConfiguration {
         $enabledModules = sfConfig::get('sf_enabled_modules');  
         if (is_array($enabledModules)) {  
             sfConfig::set('sf_enabled_modules',
-                array_merge(sfConfig::get('sf_enabled_modules'), array('baseapi','apiv1pim','apiv1leave','apiv1admin','apiv1time','apiv1attendance','apiv1performance','apiv1integration'))
+                array_merge(sfConfig::get('sf_enabled_modules'), array('baseapi','apiv1pim','apiv1leave','apiv1admin','apiv1time','apiv1attendance','apiv1performance','apiv1integration','apiv1mobile'))
             );
         }  
     }
