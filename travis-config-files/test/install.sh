5f2a37239a4727b8b6661fa02c06970b9412a5d8
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-01-05 09:09:04 +0530

    OHRM-993:Fix Travis CI failing (#705)

--- travis-config-files/test/install.sh
+++ travis-config-files/test/install.sh
@@ -1,5 +1,6 @@
-composer install -d symfony/lib
-composer dump-autoload -o -d symfony/lib
+wget https://getcomposer.org/composer-1.phar -O composer.phar
+php composer.phar install -d symfony/lib
+php composer.phar dump-autoload -o -d symfony/lib
 php installer/cli_install.php 0
 php devTools/general/create-test-db.php root
 
