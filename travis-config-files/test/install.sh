b7da1640b047fa265b77819d849efedfc5477a96
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-04-10 12:44:51 +0530

    OHRM5X-67:Invalidate browser cache for new build (#729)

--- travis-config-files/test/install.sh
+++
@@ -1,6 +0,0 @@
-wget https://getcomposer.org/composer-1.phar -O composer.phar
-php composer.phar install -d symfony/lib
-php composer.phar dump-autoload -o -d symfony/lib
-php installer/cli_install.php 0
-php devTools/general/create-test-db.php root
-
