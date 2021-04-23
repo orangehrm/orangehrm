8a1686064fb65c02b3398969258a5824a76cacba
KaveeshaSilva1996 <kaveeshasilva@orangehrmlive.com>
2021-04-23 13:25:56 +0530

    OHRM5X-9: removed changes skill dao & service

--- symfony/plugins/orangehrmAdminPlugin/lib/dao/SkillDao.php
+++ symfony/plugins/orangehrmAdminPlugin/lib/dao/SkillDao.php
@@ -20,88 +20,88 @@
 class SkillDao extends BaseDao {
 
     public function saveSkill(Skill $skill) {
-
+        
         try {
-            $skill->save();
+            $skill->save();            
         } catch (Exception $e) {
             throw new DaoException($e->getMessage(), $e->getCode(), $e);
         }
-
+        
     }
-
+    
     public function getSkillById($id) {
-
+        
         try {
             return Doctrine::getTable('Skill')->find($id);
         } catch (Exception $e) {
             throw new DaoException($e->getMessage(), $e->getCode(), $e);
         }
-
+        
     }
-
+    
     public function getSkillByName($name) {
-
+        
         try {
-
+            
             $q = Doctrine_Query::create()
-                ->from('Skill')
-                ->where('name = ?', trim($name));
-
+                                ->from('Skill')
+                                ->where('name = ?', trim($name));
+            
             return $q->fetchOne();
-
+            
         } catch (Exception $e) {
             throw new DaoException($e->getMessage(), $e->getCode(), $e);
         }
-
-    }
-
+        
+    }    
+    
     public function getSkillList() {
-
+        
         try {
-
+            
             $q = Doctrine_Query::create()->from('Skill')
-                ->orderBy('name');
-
-            return $q->execute();
-
+                                         ->orderBy('name');
+            
+            return $q->execute();            
+            
         } catch (Exception $e) {
             throw new DaoException($e->getMessage(), $e->getCode(), $e);
-        }
-
+        }        
+        
     }
-
+    
     public function deleteSkills($toDeleteIds) {
-
+        
         try {
-
+            
             $q = Doctrine_Query::create()->delete('Skill')
-                ->whereIn('id', $toDeleteIds);
+                            ->whereIn('id', $toDeleteIds);
 
-            return $q->execute();
-
+            return $q->execute();            
+            
         } catch (Exception $e) {
             throw new DaoException($e->getMessage(), $e->getCode(), $e);
-        }
-
+        }        
+        
     }
-
+    
     public function isExistingSkillName($skillName) {
-
+        
         try {
-
+            
             $q = Doctrine_Query:: create()->from('Skill s')
-                ->where('s.name = ?', trim($skillName));
+                            ->where('s.name = ?', trim($skillName));
 
             if ($q->count() > 0) {
                 return true;
             }
-
+            
             return false;
-
+            
         } catch (Exception $e) {
             throw new DaoException($e->getMessage(), $e->getCode(), $e);
-        }
-
+        }       
+        
     }
 
 }
--- symfony/plugins/orangehrmAdminPlugin/lib/service/SkillService.php
+++ symfony/plugins/orangehrmAdminPlugin/lib/service/SkillService.php
@@ -18,18 +18,18 @@
  * Boston, MA  02110-1301, USA
  */
 class SkillService extends BaseService {
-
+    
     private $skillDao;
-
+    
     /**
      * @ignore
      */
     public function getSkillDao() {
-
+        
         if (!($this->skillDao instanceof SkillDao)) {
             $this->skillDao = new SkillDao();
         }
-
+        
         return $this->skillDao;
     }
 
@@ -39,77 +39,77 @@ class SkillService extends BaseService {
     public function setSkillDao($skillDao) {
         $this->skillDao = $skillDao;
     }
-
+    
     /**
      * Saves a skill
-     *
+     * 
      * Can be used for a new record or updating.
-     *
-     * @version 2.6.12
-     * @param Skill $skill
+     * 
+     * @version 2.6.12 
+     * @param Skill $skill 
      * @return NULL Doesn't return a value
      */
-    public function saveSkill(Skill $skill) {
-        $this->getSkillDao()->saveSkill($skill);
+    public function saveSkill(Skill $skill) {        
+        $this->getSkillDao()->saveSkill($skill);        
     }
-
+    
     /**
      * Retrieves a skill by ID
-     *
-     * @version 2.6.12
-     * @param int $id
+     * 
+     * @version 2.6.12 
+     * @param int $id 
      * @return Skill An instance of Skill or NULL
-     */
+     */    
     public function getSkillById($id) {
         return $this->getSkillDao()->getSkillById($id);
     }
-
+    
     /**
      * Retrieves a skill by name
-     *
+     * 
      * Case insensitive
-     *
-     * @version 2.6.12
-     * @param string $name
+     * 
+     * @version 2.6.12 
+     * @param string $name 
      * @return Skill An instance of Skill or false
-     */
+     */    
     public function getSkillByName($name) {
         return $this->getSkillDao()->getSkillByName($name);
-    }
-
+    }    
+  
     /**
      * Retrieves all skills ordered by name
-     *
-     * @version 2.6.12
-     * @return Doctrine_Collection A doctrine collection of Skill objects
-     */
+     * 
+     * @version 2.6.12 
+     * @return Doctrine_Collection A doctrine collection of Skill objects 
+     */        
     public function getSkillList() {
         return $this->getSkillDao()->getSkillList();
     }
-
+    
     /**
      * Deletes skills
-     *
-     * @version 2.6.12
+     * 
+     * @version 2.6.12 
      * @param array $toDeleteIds An array of IDs to be deleted
      * @return int Number of records deleted
-     */
+     */    
     public function deleteSkills($toDeleteIds) {
         return $this->getSkillDao()->deleteSkills($toDeleteIds);
     }
 
     /**
      * Checks whether the given skill name exists
-     *
+     * 
      * Case insensitive
-     *
-     * @version 2.6.12
+     * 
+     * @version 2.6.12 
      * @param string $skillName Skill name that needs to be checked
      * @return boolean
-     */
+     */    
     public function isExistingSkillName($skillName) {
         return $this->getSkillDao()->isExistingSkillName($skillName);
     }
-
+    
 
 }
