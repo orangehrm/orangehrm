1ddb2794ce2c3fb579fe3f82a1f4375d54fc5082
Samantha Jayasinghe <isamantha@gmail.com>
2021-05-24 18:16:55 +1200

    OHRM5X-192: Add cypress testing framework (#757)

---
+++ tests/cypress/integration/orangehrm/pim/add-employee.spce.js
@@ -0,0 +1,17 @@
+import user from '../../../fixtures/user.json'
+
+describe('Add Employee', function () {
+
+    it('check adding a employee', () => {
+        cy.login(user.admin.userName, user.admin.password)
+
+        cy.visit('/pim/addEmployee')
+
+        cy.get('input[name="First Name"]').type('John')
+
+        cy.get('input[name="Last Name"]').type('mike')
+    
+        cy.get('form').submit()
+
+    })
+})
