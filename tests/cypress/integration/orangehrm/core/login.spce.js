5221dbb0d802b0078bb790e75587672d4666da14
Samantha Jayasinghe <isamantha@gmail.com>
2021-05-24 18:16:55 +1200

    OHRM5X-192: Add cypress testing framework (#757)

---
+++ tests/cypress/integration/orangehrm/core/login.spce.js
@@ -0,0 +1,30 @@
+import user from '../../../fixtures/user.json'
+
+describe('Automation Test Suite - Fixtures', function () {
+
+    it('Visits the login page', () => {
+        cy.visit('/auth/login')
+
+        cy.get('input[name=username]').type(user.admin.userName)
+
+        cy.get('input[name=password]').type(user.admin.password)
+
+        cy.get('form').submit()
+
+        cy.get('.oxd-userdropdown').should('include.text', user.admin.fullName)
+
+    })
+      
+    it('Visits the login page and check validations', () => {
+        cy.visit('/auth/login')
+
+        cy.get('input[name=username]').type(' ')
+
+        cy.get('input[name=password]').type(' ')
+
+        cy.get('form').submit()
+
+        cy.get('.oxd-text').should('include.text', 'Required')
+
+    })  
+})
