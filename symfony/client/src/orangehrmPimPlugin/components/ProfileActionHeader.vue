ecae6768909d8171d08038c1f077e36b46259d6b
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-05-27 10:46:06 +0530

    OHRM5X-114: PIM - Emergency contact screens  (#771)
    * OHRM5X-113: Contact details FE API integration
    
    * OHRM5X-114: Em. contact UI, Attachment FE changes

---
+++ symfony/client/src/orangehrmPimPlugin/components/ProfileActionHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="orangehrm-action-header">
+    <oxd-text tag="h6">
+      <slot></slot>
+    </oxd-text>
+    <oxd-button
+      label="Add"
+      iconName="plus"
+      displayType="text"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profile-action-header',
+  inheritAttrs: false,
+};
+</script>
+
+<style lang="scss" scoped>
+.orangehrm-action {
+  &-header {
+    display: flex;
+    overflow-wrap: break-word;
+    align-items: center;
+    button {
+      margin-left: 1rem;
+      white-space: nowrap;
+    }
+  }
+}
+</style>
