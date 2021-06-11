991af7895ea7554226fac3d0d681f155d853de11
KaveeshaSilva1996 <72607374+KaveeshaSilva1996@users.noreply.github.com>
2021-06-11 16:01:10 +0530

    OHRM5X-123: Develop employee qualification skill API (#783)

---
+++ symfony/plugins/orangehrmPimPlugin/Dto/EmployeeSkillSearchFilterParams.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
+namespace OrangeHRM\Pim\Dto;
+
+use OrangeHRM\Core\Dto\FilterParams;
+
+class EmployeeSkillSearchFilterParams extends FilterParams
+{
+    public const ALLOWED_SORT_FIELDS = ['es.yearsOfExp'];
+
+    /**
+     * @var float|null
+     */
+    protected ?float $yearsOfExp = null;
+
+    /**
+     * @var string|null
+     */
+    protected ?string $comments = null;
+
+    /**
+     * @var string|null
+     */
+    protected ?string $empNumber = null;
+
+    /**
+     * EmployeeSkillSearchFilterParams constructor.
+     */
+    public function __construct()
+    {
+        $this->setSortField('es.yearsOfExp');
+    }
+    /**
+     * @return float|null
+     */
+    public function getYearsOfExp(): ?float
+    {
+        return $this->yearsOfExp;
+    }
+
+    /**
+     * @param string|null $yearsOfExp
+     */
+    public function setYearsOfExp(?string $yearsOfExp): void
+    {
+        $this->yearsOfExp = $yearsOfExp;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getComments(): ?string
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param string|null $comments
+     */
+    public function setComments(?string $comments): void
+    {
+        $this->comments = $comments;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getEmpNumber(): ?int
+    {
+        return $this->empNumber;
+    }
+
+    /**
+     * @param int|null $empNumber
+     */
+    public function setEmpNumber(?int $empNumber): void
+    {
+        $this->empNumber = $empNumber;
+    }
+
+}
