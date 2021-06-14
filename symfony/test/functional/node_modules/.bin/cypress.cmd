7c304fff7b28c8c0a49a9919f8ddc1db067379b9
Pavithra-orangehrm <pavithra@orangehrm.com>
2021-06-14 09:53:24 +0530

    OHRM5X-304: Test Automation: Admin -Job Title
     Delete commit to delete the 'node_modules'

--- symfony/test/functional/node_modules/.bin/cypress.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\cypress\bin\cypress" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\cypress\bin\cypress" %*
-)
