c49fc2ff69dafb8c2f6ae737e7bd05f40d1a71ce
yasirunilan <nilanyasiru@gmail.com>
2021-07-12 12:39:31 +0530

    fixed the issue with wrong use of datatable disable prop

--- symfony/client/src/orangehrmPimPlugin/components/EmployeeSupervisors.vue
+++ symfony/client/src/orangehrmPimPlugin/components/EmployeeSupervisors.vue
@@ -237,7 +237,7 @@ export default {
 
   computed: {
     isDisabled() {
-      return !(this.showSaveModal || this.showEditModal);
+      return this.showSaveModal || this.showEditModal;
     },
   },
 };
