dcd510fef39425d877801aa05ba5c470221fa072
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-10-05 09:16:19 +0530

    OHRM5X-695: PIM - List Employee report, Add report UI

---
+++ symfony/client/src/orangehrmPimPlugin/pages/reports/EmployeeReport.vue
@@ -0,0 +1,264 @@
+<!--
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+ -->
+
+<template>
+  <div class="orangehrm-background-container">
+    <oxd-table-filter filter-title="Employee Reports">
+      <oxd-form @submitValid="filterItems">
+        <oxd-form-row>
+          <oxd-grid :cols="2" class="orangehrm-full-width-grid">
+            <oxd-grid-item>
+              <!-- todo: switch to autocomplete -->
+              <oxd-input-field label="Report Name" v-model="filters.name" />
+            </oxd-grid-item>
+          </oxd-grid>
+        </oxd-form-row>
+
+        <oxd-divider />
+
+        <oxd-form-actions>
+          <oxd-button displayType="ghost" label="Reset" @click="onClickReset" />
+          <oxd-button
+            class="orangehrm-left-space"
+            displayType="secondary"
+            label="Search"
+            type="submit"
+          />
+        </oxd-form-actions>
+      </oxd-form>
+    </oxd-table-filter>
+
+    <br />
+
+    <div class="orangehrm-paper-container">
+      <div class="orangehrm-header-container">
+        <oxd-button
+          label="Add"
+          iconName="plus"
+          displayType="secondary"
+          @click="onClickAdd"
+        />
+      </div>
+      <table-header
+        :selected="checkedItems.length"
+        :total="total"
+        :loading="isLoading"
+        @delete="onClickDeleteSelected"
+      ></table-header>
+      <div class="orangehrm-container">
+        <oxd-card-table
+          :headers="headers"
+          :items="items?.data"
+          :selectable="true"
+          :clickable="false"
+          :loading="isLoading"
+          v-model:selected="checkedItems"
+          v-model:order="sortDefinition"
+          rowDecorator="oxd-table-decorator-card"
+        />
+      </div>
+      <div class="orangehrm-bottom-container">
+        <oxd-pagination
+          v-if="showPaginator"
+          :length="pages"
+          v-model:current="currentPage"
+        />
+      </div>
+    </div>
+
+    <delete-confirmation ref="deleteDialog"></delete-confirmation>
+  </div>
+</template>
+
+<script>
+import {computed, ref} from 'vue';
+import {APIService} from '@/core/util/services/api.service';
+import useSort from '@orangehrm/core/util/composable/useSort';
+import {navigate} from '@orangehrm/core/util/helper/navigation';
+import usePaginate from '@orangehrm/core/util/composable/usePaginate';
+import DeleteConfirmationDialog from '@orangehrm/components/dialogs/DeleteConfirmationDialog.vue';
+
+const defaultFilters = {
+  name: '',
+};
+
+const defaultSortOrder = {
+  'location.name': 'ASC',
+};
+
+const locationDataNormalizer = data => {
+  return data.map(location => {
+    return {
+      id: location.id,
+      name: location.name,
+    };
+  });
+};
+
+export default {
+  props: {
+    countries: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      headers: [
+        {
+          name: 'name',
+          slot: 'title',
+          title: 'Name',
+          style: {flex: 1},
+          sortField: 'location.name',
+        },
+        {
+          name: 'actions',
+          title: 'Actions',
+          slot: 'action',
+          style: {'justify-content': 'flex-end'},
+          cellType: 'oxd-table-cell-actions',
+          cellConfig: {
+            delete: {
+              onClick: this.onClickDelete,
+              component: 'oxd-icon-button',
+              props: {
+                name: 'trash',
+              },
+            },
+            edit: {
+              onClick: this.onClickEdit,
+              props: {
+                name: 'pencil-fill',
+              },
+            },
+          },
+        },
+      ],
+      checkedItems: [],
+    };
+  },
+
+  components: {
+    'delete-confirmation': DeleteConfirmationDialog,
+  },
+
+  setup() {
+    const {sortDefinition, sortField, sortOrder, onSort} = useSort({
+      sortDefinition: defaultSortOrder,
+    });
+    const filters = ref({...defaultFilters});
+    const serializedFilters = computed(() => {
+      return {
+        name: filters.value.name,
+        sortField: sortField.value,
+        sortOrder: sortOrder.value,
+      };
+    });
+    const http = new APIService(
+      window.appGlobal.baseUrl,
+      '/api/v2/admin/locations',
+    );
+    const {
+      showPaginator,
+      currentPage,
+      total,
+      pages,
+      pageSize,
+      response,
+      isLoading,
+      execQuery,
+    } = usePaginate(http, {
+      query: serializedFilters,
+      normalizer: locationDataNormalizer,
+    });
+
+    onSort(execQuery);
+
+    return {
+      http,
+      showPaginator,
+      currentPage,
+      isLoading,
+      total,
+      pages,
+      pageSize,
+      execQuery,
+      items: response,
+      filters,
+      sortDefinition,
+    };
+  },
+
+  methods: {
+    onClickAdd() {
+      navigate('/pim/definePredefinedReport');
+    },
+    onClickEdit(item) {
+      navigate('/pim/definePredefinedReport/{id}', {id: item.id});
+    },
+    onClickDeleteSelected() {
+      const ids = [];
+      this.checkedItems.forEach(index => {
+        ids.push(this.items?.data[index].id);
+      });
+      this.$refs.deleteDialog.showDialog().then(confirmation => {
+        if (confirmation === 'ok') {
+          this.deleteItems(ids);
+        }
+      });
+    },
+    onClickDelete(item) {
+      this.$refs.deleteDialog.showDialog().then(confirmation => {
+        if (confirmation === 'ok') {
+          this.deleteItems([item.id]);
+        }
+      });
+    },
+    deleteItems(items) {
+      if (items instanceof Array) {
+        this.isLoading = true;
+        this.http
+          .deleteAll({
+            ids: items,
+          })
+          .then(() => {
+            return this.$toast.deleteSuccess();
+          })
+          .then(() => {
+            this.isLoading = false;
+            this.resetDataTable();
+          });
+      }
+    },
+    async resetDataTable() {
+      this.checkedItems = [];
+      await this.execQuery();
+    },
+    async filterItems() {
+      await this.execQuery();
+    },
+    onClickReset() {
+      this.filters = {...defaultFilters};
+      this.filterItems();
+    },
+  },
+};
+</script>
