d5829e35e6ca9645d4b92523aa5b2ae69bf08aa5
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-10-13 19:23:01 +0530

    OHRM5X-275: Admin - Develop Email Subscription Screens

---
+++ symfony/client/src/orangehrmAdminPlugin/pages/emailSubscription/EmailSubscription.vue
@@ -0,0 +1,175 @@
+<!--
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+ -->
+
+<template>
+  <div class="orangehrm-background-container">
+    <div class="orangehrm-paper-container">
+      <div class="orangehrm-header-container">
+        <oxd-text tag="h6" class="orangehrm-main-title">
+          Email Subscriptions
+        </oxd-text>
+      </div>
+      <table-header
+        :selected="0"
+        :total="total"
+        :loading="isLoading"
+      ></table-header>
+      <div class="orangehrm-container">
+        <oxd-card-table
+          :loading="isLoading"
+          :headers="headers"
+          :items="items?.data"
+          :selectable="false"
+          :clickable="false"
+          rowDecorator="oxd-table-decorator-card"
+        />
+      </div>
+      <div class="orangehrm-bottom-container"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import usePaginate from '@orangehrm/core/util/composable/usePaginate';
+import {navigate} from '@orangehrm/core/util/helper/navigation';
+import {APIService} from '@orangehrm/core/util/services/api.service';
+import SwitchInput from '@orangehrm/oxd/core/components/Input/SwitchInput';
+import Spinner from '@orangehrm/oxd/core/components/Loader/Spinner';
+
+const subscribersNormalizer = data => {
+  return data.map(item => {
+    return {
+      id: item.id,
+      type: item.name,
+      subscribers: Array.isArray(item.subscribers)
+        ? item.subscribers
+            .map(sub => {
+              return `${sub.name} <${sub.email}>`;
+            })
+            .join(', ')
+        : '',
+      enabled: item.enabled,
+      _loading: false,
+    };
+  });
+};
+
+export default {
+  setup() {
+    const http = new APIService(
+      // window.appGlobal.baseUrl,
+      'https://884b404a-f4d0-4908-9eb5-ef0c8afec15c.mock.pstmn.io',
+      '/api/v2/admin/email-subscriptions',
+    );
+    const {total, response, isLoading} = usePaginate(http, {
+      normalizer: subscribersNormalizer,
+    });
+    return {
+      http,
+      isLoading,
+      total,
+      items: response,
+    };
+  },
+
+  data() {
+    return {
+      headers: [
+        {
+          name: 'type',
+          slot: 'title',
+          title: 'Notification Type',
+          style: {flex: '20%'},
+        },
+        {name: 'subscribers', title: 'Subscribers', style: {flex: '65%'}},
+        {
+          name: 'actions',
+          title: 'Actions',
+          slot: 'action',
+          style: {flex: '15%'},
+          cellType: 'oxd-table-cell-actions',
+          cellRenderer: this.cellRenderer,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    cellRenderer(...args) {
+      const [index, , , row] = args;
+
+      const addSubscriber = {
+        component: 'oxd-icon-button',
+        props: {
+          name: 'person-plus-fill',
+          onClick: () => {
+            navigate('/admin/saveSubscriber/{id}', {id: row.id});
+          },
+        },
+      };
+
+      const switchSubscription = {
+        component: SwitchInput,
+        props: {
+          modelValue: row.enabled,
+          'onUpdate:modelValue': $event => {
+            this.items.data[index]._loading = true;
+            this.http
+              .update(row.id, {
+                enabled: $event,
+              })
+              .then(response => {
+                const {data} = response.data;
+                this.items.data[index].enabled = data.enabled;
+                this.$toast.updateSuccess();
+              })
+              .finally(() => {
+                this.items.data[index]._loading = false;
+              });
+          },
+          style: {
+            'margin-left': '1rem',
+            'text-align': 'left',
+          },
+        },
+      };
+
+      const loader = {
+        component: Spinner,
+        props: {
+          withContainer: false,
+        },
+      };
+
+      return {
+        props: {
+          header: {
+            cellConfig: {
+              ...(row._loading
+                ? {loader}
+                : {addSubscriber, switchSubscription}),
+            },
+          },
+        },
+      };
+    },
+  },
+};
+</script>
