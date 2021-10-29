fc4675af26ea2d1115ea057dcfd1087fb9cfd474
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-10-29 08:13:00 +0530

    OHRM5X-790: Develop about page (#979)

--- symfony/client/src/core/components/layout/Layout.vue
+++ symfony/client/src/core/components/layout/Layout.vue
@@ -1,26 +1,73 @@
 <template>
-  <oxd-layout>
+  <oxd-layout v-bind="$attrs">
     <template v-for="(_, name) in $slots" v-slot:[name]="slotData">
       <slot :name="name" v-bind="slotData" />
     </template>
+    <template v-slot:user-actions>
+      <li>
+        <a
+          href="#"
+          role="menuitem"
+          class="oxd-userdropdown-link"
+          @click="openAboutModel"
+        >
+          About
+        </a>
+      </li>
+      <li>
+        <a :href="supportUrl" role="menuitem" class="oxd-userdropdown-link">
+          Support
+        </a>
+      </li>
+      <li>
+        <a :href="logoutUrl" role="menuitem" class="oxd-userdropdown-link">
+          Logout
+        </a>
+      </li>
+    </template>
   </oxd-layout>
+  <about v-if="showAboutModel" @close="closeAboutModel"></about>
 </template>
 
 <script>
-import {provide, readonly} from 'vue';
+import {provide, readonly, ref} from 'vue';
+import About from '@/core/pages/About.vue';
 import Layout from '@orangehrm/oxd/src/core/components/Layout/Layout.vue';
 
 export default {
+  inheritAttrs: false,
   components: {
+    about: About,
     'oxd-layout': Layout,
   },
   props: {
     permissions: {
       type: Object,
     },
+    logoutUrl: {
+      type: String,
+    },
+    supportUrl: {
+      type: String,
+    },
   },
   setup(props) {
+    const showAboutModel = ref(false);
     provide('permissions', readonly(props.permissions));
+
+    const openAboutModel = () => {
+      showAboutModel.value = true;
+    };
+
+    const closeAboutModel = () => {
+      showAboutModel.value = false;
+    };
+
+    return {
+      showAboutModel,
+      openAboutModel,
+      closeAboutModel,
+    };
   },
 };
 </script>
