0b6f81c223eb83bfc9b1ea127413b01046421dd0
VageeshOrangeHRM <89253437+VageeshOrangeHRM@users.noreply.github.com>
2021-11-01 11:04:58 +0530

    OHRM5X-823: Develop update password screen (#981)

---
+++ symfony/client/src/orangehrmPimPlugin/pages/updatePassword/UpdatePassword.vue
@@ -0,0 +1,150 @@
+<!--
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+ -->
+
+<template>
+  <div class="orangehrm-background-container">
+    <div class="orangehrm-card-container">
+      <oxd-text tag="h6" class="orangehrm-main-title">{{
+        $t('pim.update_password')
+      }}</oxd-text>
+      <oxd-divider />
+
+      <oxd-form :loading="isLoading" @submitValid="onSave">
+        <oxd-form-row>
+          <oxd-grid :cols="2" class="orangehrm-full-width-grid">
+            <oxd-grid-item>
+              <oxd-input-group :label="$t('pim.user_name')">
+                <oxd-text class="orangehrm-user-name" tag="p">
+                  {{ userName }}
+                </oxd-text>
+              </oxd-input-group>
+            </oxd-grid-item>
+
+            <oxd-grid-item>
+              <oxd-input-field
+                type="password"
+                :label="$t('pim.current_password')"
+                v-model="user.currentPassword"
+                :rules="rules.currentPassword"
+                required
+              />
+            </oxd-grid-item>
+          </oxd-grid>
+        </oxd-form-row>
+
+        <password-input
+          v-model:password="user.password"
+          v-model:passwordConfirm="user.passwordConfirm"
+        />
+
+        <oxd-divider />
+        <oxd-form-actions>
+          <required-text />
+          <oxd-button
+            type="button"
+            displayType="ghost"
+            label="Cancel"
+            @click="onCancel"
+          />
+          <submit-button />
+        </oxd-form-actions>
+      </oxd-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import {APIService} from '@/core/util/services/api.service';
+import {navigate} from '@orangehrm/core/util/helper/navigation';
+import PasswordInput from '@/core/components/inputs/PasswordInput';
+import {
+  required,
+  shouldNotExceedCharLength,
+} from '@orangehrm/core/util/validation/rules';
+
+const userModel = {
+  currentPassword: '',
+  password: '',
+  passwordConfirm: '',
+};
+
+export default {
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const http = new APIService(
+      window.appGlobal.baseUrl,
+      'api/v2/core/update-password',
+    );
+    return {
+      http,
+    };
+  },
+  components: {
+    'password-input': PasswordInput,
+  },
+
+  data() {
+    return {
+      isLoading: false,
+      user: {...userModel},
+      rules: {
+        currentPassword: [required, shouldNotExceedCharLength(64)],
+      },
+    };
+  },
+
+  methods: {
+    onCancel() {
+      navigate('/pim/viewEmployeeList');
+    },
+    onSave() {
+      this.isLoading = true;
+      this.http.http
+        .put('api/v2/pim/update-password', {
+          currentPassword: this.user.currentPassword,
+          newPassword: this.user.password,
+        })
+        .then(response => {
+          if (response.status === 200) {
+            this.$toast.saveSuccess();
+          } else {
+            this.isLoading = false;
+            this.$toast.error({
+              title: 'Error',
+              message: 'Current Password is Incorrect',
+            });
+            return Promise.reject();
+          }
+        })
+        .then(() => {
+          this.user = {...userModel};
+          this.onCancel();
+        });
+    },
+  },
+};
+</script>
+<style src="../../../orangehrmAdminPlugin/pages/systemUser/system-user.scss" lang="scss" scoped></style>
+<style src="./update-password.scss" lang="scss" scoped></style>
