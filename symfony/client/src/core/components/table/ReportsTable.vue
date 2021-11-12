d1877981fb71897da53f3ad0be89b2e6d36ab9c7
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-11-12 20:08:40 +0530

    OHRM5X-890: PIM Reports show multiple rows per cell issue fix

--- symfony/client/src/core/components/table/ReportsTable.vue
+++ symfony/client/src/core/components/table/ReportsTable.vue
@@ -129,6 +129,23 @@ export default {
       });
     });
 
+    const setupTableHeaders = header => {
+      delete header['size'];
+      const {type, ...rest} = header.cellProperties ?? {};
+      const cellProperties = function({prop, model}) {
+        const url = model?._url ? model?._url[prop] : undefined;
+        return {
+          ...rest,
+          onClick: url ? () => navigate(url) : undefined,
+        };
+      };
+      return {
+        ...header,
+        cellProperties,
+        cellTemplate: type === 'list' ? CellAdapter(MultilineCell) : undefined,
+      };
+    };
+
     const fetchTableHeaders = async () => {
       isLoading.value = true;
       http
@@ -143,22 +160,14 @@ export default {
         .then(response => {
           const {data, meta} = response.data;
           headers.value = data.headers.map(header => {
-            delete header['size'];
-            const cellProps = header.cellProperties ?? {};
-            const {type, ...rest} = cellProps;
-            const cellProperties = function({prop, model}) {
-              const url = model?._url ? model?._url[prop] : undefined;
-              return {
-                ...rest,
-                onClick: url ? () => navigate(url) : undefined,
-              };
-            };
-            return {
-              ...header,
-              cellProperties,
-              cellTemplate:
-                type === 'list' ? CellAdapter(MultilineCell) : undefined,
-            };
+            if (header?.children && Array.isArray(header.children)) {
+              header.children = header.children.map(child =>
+                setupTableHeaders(child),
+              );
+              return header;
+            } else {
+              return setupTableHeaders(header);
+            }
           });
           if (meta.headers?.columnCount) {
             colCount.value = meta.headers.columnCount;
