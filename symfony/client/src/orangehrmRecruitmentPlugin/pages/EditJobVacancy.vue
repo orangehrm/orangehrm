6e8c30564b59471475c520fa26cbbae6b59f7423
SandeepaOHRM <sandeepa@orangehrmlive.com>
2021-11-18 00:56:59 +0530

    Vacancy attachment list view done

--- symfony/client/src/orangehrmRecruitmentPlugin/pages/EditJobVacancy.vue
+++ symfony/client/src/orangehrmRecruitmentPlugin/pages/EditJobVacancy.vue
@@ -110,11 +110,49 @@
         </oxd-form-actions>
       </oxd-form>
     </div>
+    <br />
+    <div class="orangehrm-paper-container">
+      <div class="orangehrm-header-container orangehrm-attachment-header">
+        <oxd-text
+          tag="h6"
+          class="orangehrm-main-title orangehrm-attachment-header__title"
+          >Attachments</oxd-text
+        >
+        <oxd-button
+          label="Add"
+          iconName="plus"
+          displayType="text"
+          @click="onClickAdd"
+        />
+      </div>
+      <table-header
+        :selected="checkedItems.length"
+        :loading="isLoading"
+        :total="attachments.length"
+        @delete="onClickDeleteSelected"
+      ></table-header>
+      <div class="orangehrm-container">
+        <oxd-card-table
+          :headers="headers"
+          :items="attachments"
+          :selectable="true"
+          :clickable="false"
+          v-model:selected="checkedItems"
+          :loading="isLoading"
+          rowDecorator="oxd-table-decorator-card"
+        />
+      </div>
+      <delete-confirmation ref="deleteDialog"></delete-confirmation>
+      <br />
+      <br />
+    </div>
   </div>
 </template>
 <script>
+import {ref} from 'vue';
 import {APIService} from '@/core/util/services/api.service';
 import {navigate} from '@orangehrm/core/util/helper/navigation';
+import DeleteConfirmationDialog from '@orangehrm/components/dialogs/DeleteConfirmationDialog';
 import SwitchInput from '@orangehrm/oxd/core/components/Input/SwitchInput';
 
 import {
@@ -137,6 +175,20 @@ const vacancyModel = {
   isPublished: false,
 };
 
+const attachmentNormalizer = data => {
+  return data.map(item => {
+    return {
+      id: item.id,
+      vacancyId: item.vacancyId,
+      fileName: item.attachment.fileName,
+      fileSize: +(item.attachment.fileSize / 1024).toFixed(2) + ' kb',
+      fileType: item.attachment.fileType,
+      comment: item.comment,
+      attachmentType: item.attachmentType,
+    };
+  });
+};
+
 export default {
   props: {
     vacancyId: {
@@ -150,6 +202,7 @@ export default {
     'employee-autocomplete': EmployeeAutocomplete,
     'jobtitle-dropdown': JobtitleDropdown,
     'vacancy-link-card': VacancyLinkCard,
+    'delete-confirmation': DeleteConfirmationDialog,
   },
 
   data() {
@@ -168,6 +221,59 @@ export default {
         status: [required],
         isPublished: [required],
       },
+      headers: [
+        {
+          name: 'fileName',
+          slot: 'title',
+          title: 'File Name',
+          style: {flex: 3},
+        },
+        {
+          name: 'fileSize',
+          title: 'Size',
+          style: {flex: 2},
+        },
+        {
+          name: 'fileType',
+          title: 'Type',
+          style: {flex: 2},
+        },
+        {
+          name: 'comment',
+          title: 'Comment',
+          style: {flex: 4},
+        },
+        {
+          name: 'actions',
+          slot: 'action',
+          title: 'Actions',
+          style: {flex: 2},
+          cellType: 'oxd-table-cell-actions',
+          cellConfig: {
+            delete: {
+              onClick: this.onClickDelete,
+              component: 'oxd-icon-button',
+              props: {
+                name: 'trash',
+              },
+            },
+            download: {
+              onClick: this.onClickEdit,
+              props: {
+                name: 'download',
+              },
+            },
+            edit: {
+              onClick: this.onClickEdit,
+              props: {
+                name: 'pencil-fill',
+              },
+            },
+          },
+        },
+      ],
+      attachments: [],
+      checkedItems: [],
     };
   },
   setup() {
@@ -175,8 +281,13 @@ export default {
       window.appGlobal.baseUrl,
       'api/v2/recruitment/vacancies',
     );
+    const httpAttachments = new APIService(
+      window.appGlobal.baseUrl,
+      'api/v2/recruitment/vacancyAttachments',
+    );
     return {
       http,
+      httpAttachments,
     };
   },
 
@@ -204,14 +315,54 @@ export default {
           this.onCancel();
         });
     },
-    getUrls() {
-      const baseUrl = window.location.origin;
-      console.log(baseUrl);
+    async getAttachments() {
+      const result = await this.httpAttachments.get(this.vacancyId);
+      const {data} = result.data;
+      this.attachments = attachmentNormalizer(data);
+    },
+    onClickDelete(item) {
+      this.$refs.deleteDialog.showDialog().then(confirmation => {
+        if (confirmation === 'ok') {
+          this.deleteData([item.id]);
+        }
+      });
+    },
+    onClickDeleteSelected() {
+      const ids = this.checkedItems.map(index => {
+        return this.attachments[index]?.id;
+      });
+      this.$refs.deleteDialog.showDialog().then(confirmation => {
+        if (confirmation === 'ok') {
+          this.deleteData(ids);
+        }
+      });
+    },
+
+    async deleteData(items) {
+      console.log(items);
+      if (items instanceof Array) {
+        this.isLoading = true;
+        this.httpAttachments
+          .deleteAll({
+            ids: items,
+          })
+          .then(() => {
+            return this.$toast.deleteSuccess();
+          })
+          .then(() => {
+            this.resetDataTable();
+            this.isLoading = false;
+          });
+      }
+    },
+    async resetDataTable() {
+      this.checkedItems = [];
+      this.getAttachments();
     },
   },
   created() {
     this.isLoading = true;
-    this.getUrls();
+    this.getAttachments();
     this.http
       .get(this.vacancyId)
       .then(response => {
