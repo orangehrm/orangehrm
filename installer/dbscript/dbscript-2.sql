dd312b542bfb9fc85cac8b6434cc75eb525d2e5e
VageeshOrangeHRM <vageeshan@orangehrm.com>
2021-11-26 17:12:57 +0530

    OHRM5X-554: API Integration test added

--- installer/dbscript/dbscript-2.sql
+++ installer/dbscript/dbscript-2.sql
@@ -4235,3 +4235,20 @@ UPDATE `ohrm_email_template` SET `subject` = '/orangehrmLeavePlugin/Mail/templat
 UPDATE `ohrm_email_template` SET `subject` = '/orangehrmLeavePlugin/Mail/templates/en_US/reject/leaveRejectionSubscriberSubject.txt.twig', `body` = '/orangehrmLeavePlugin/Mail/templates/en_US/reject/leaveRejectionSubscriberBody.html.twig' WHERE `email_id` = @leave_reject_email_id AND `recipient_role` = 'subscriber';
 
 INSERT INTO `ohrm_registration_event_queue` (`event_type`,`published`,`event_time`) VALUES (0, 0, now());
+
+-- ------------------------------
+--        OrangeHRM 5.0
+-- ------------------------------
+
+INSERT INTO ohrm_data_group (`name`, `description`, `can_read`, `can_create`, `can_update`, `can_delete`)
+VALUES ('apiv2_project_activities', 'API-V2 Project - project Activities', 1, 1, 1, 1);
+
+SET @time_module_id := (SELECT `id` FROM ohrm_module WHERE name = 'time' LIMIT 1);
+
+SET @apiv2_project_activities_data_group_id := (SELECT `id` FROM ohrm_data_group WHERE name = 'apiv2_project_activities' LIMIT 1);
+
+INSERT INTO ohrm_api_permission (`api_name`, `module_id`, `data_group_id`)
+VALUES ('OrangeHRM\\Time\\Api\\ProjectActivityAPI', @time_module_id, @apiv2_project_activities_data_group_id);
+
+INSERT INTO ohrm_user_role_data_group (`can_read`, `can_create`, `can_update`, `can_delete`, `self`, `data_group_id`,`user_role_id`)
+VALUES (1, 1, 1, 1, 0, @apiv2_project_activities_data_group_id, @admin_role_id);
