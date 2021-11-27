775e1b7cb2c24848e59d9d2693753fcf4fca801c
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-11-27 21:40:44 +0530

    OHRM5X-1016: Add vue3-recommended lint rules

--- symfony/client/.eslintrc.js
+++ symfony/client/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
+    'plugin:vue/vue3-recommended',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
