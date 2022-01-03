985d50878d5defff47a98dc7199a98750c9eca86
VageeshOrangeHRM <vageeshan@orangehrm.com>
2022-01-03 17:52:10 +0530

    OHRM5X-1000: Code refactor added

--- symfony/plugins/orangehrmTimePlugin/Service/ProjectService.php
+++ symfony/plugins/orangehrmTimePlugin/Service/ProjectService.php
@@ -73,10 +73,8 @@ class ProjectService
     ): void {
         $fromProjectActivities = $this->getProjectActivityDao()
             ->getProjectActivitiesByActivityIds($fromProjectActivityIds);
-
         $duplicatedActivities = $this->getProjectActivityDao()
             ->getDuplicatedActivities($fromProjectId, $toProjectId);
-
         $fetchedFromProjectActivityIds = array_map(
             fn(ProjectActivity $projectActivity) => $projectActivity->getId(),
             $fromProjectActivities
@@ -85,13 +83,11 @@ class ProjectService
         if (!empty(array_diff($fromProjectActivityIds, $fetchedFromProjectActivityIds))) {
             throw ProjectServiceException::projectActivityNotFound();
         }
-
         $duplicatedActivitiesMap = $this->getProjectActivityAsMap($duplicatedActivities);
         foreach ($fromProjectActivities as $fromProjectActivity) {
             if ($fromProjectActivity->getProject()->getId() !== $fromProjectId) {
                 throw ProjectServiceException::projectActivityNotFound();
             }
-
             $name = $fromProjectActivity->getName();
             if (isset($duplicatedActivitiesMap[$name])) {
                 throw ProjectServiceException::duplicateProjectActivityNameFound();
