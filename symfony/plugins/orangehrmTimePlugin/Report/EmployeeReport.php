a864bd700d214ccb9677636eca6514ea2ea5738e
SandeepaOHRM <sandeepa@orangehrmlive.com>
2022-01-19 00:23:40 +0530

    Bug Fixes for OHRM5X-1010

--- symfony/plugins/orangehrmTimePlugin/Report/EmployeeReport.php
+++ symfony/plugins/orangehrmTimePlugin/Report/EmployeeReport.php
@@ -35,6 +35,8 @@ use OrangeHRM\Core\Report\Filter\FilterDefinition;
 use OrangeHRM\Core\Report\Header\Column;
 use OrangeHRM\Core\Report\Header\Header;
 use OrangeHRM\Core\Traits\UserRoleManagerTrait;
+use OrangeHRM\Entity\Project;
+use OrangeHRM\Entity\ProjectActivity;
 use OrangeHRM\Time\Dto\EmployeeReportsSearchFilterParams;
 
 class EmployeeReport implements EndpointAwareReport
@@ -119,13 +121,13 @@ class EmployeeReport implements EndpointAwareReport
             $endpoint->getValidationDecorator()->notRequiredParamRule(
                 new ParamRule(
                     self::FILTER_PARAMETER_PROJECT_ID,
-                    new Rule(Rules::POSITIVE)
+                    new Rule(Rules::ENTITY_ID_EXISTS, [Project::class])
                 )
             ),
             $endpoint->getValidationDecorator()->notRequiredParamRule(
                 new ParamRule(
                     self::FILTER_PARAMETER_ACTIVITY_ID,
-                    new Rule(Rules::POSITIVE)
+                    new Rule(Rules::ENTITY_ID_EXISTS, [ProjectActivity::class])
                 )
             ),
             $endpoint->getValidationDecorator()->notRequiredParamRule(
