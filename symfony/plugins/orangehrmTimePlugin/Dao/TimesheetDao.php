49122f8fcc36876f8a221ef64324aafe787db61e
G.Vageeshan <vageeshan@orangehrm.com>
2022-01-28 14:13:17 +0530

    OHRM5X-1071: Fix no record found issue in edit timesheet screen (#1102)
    * OHRM5X-1067: Fix timesheet pending action list sorting issue
    
    * OHRM5X-1071: Fix edit timesheet no record found issue

--- symfony/plugins/orangehrmTimePlugin/Dao/TimesheetDao.php
+++ symfony/plugins/orangehrmTimePlugin/Dao/TimesheetDao.php
@@ -371,6 +371,7 @@ class TimesheetDao extends BaseDao
         }
 
         $this->setSortingAndPaginationParams($q, $employeeTimesheetActionSearchFilterParams);
+        $q->addOrderBy('employee.lastName');
         return $this->getPaginator($q);
     }
 
