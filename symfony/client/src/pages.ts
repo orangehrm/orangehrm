1cd6bf9d3fc5da3d74c534cbc786555953f2876e
Anika Saparamadu <anika@orangehrm.us.com>
2022-02-09 20:56:34 +0530

    OHRM5X-1104: Develop performance - my trackers list screen (#1109)

--- symfony/client/src/pages.ts
+++ symfony/client/src/pages.ts
@@ -69,6 +69,7 @@ import EditEmailSubscription from '@/orangehrmAdminPlugin/pages/emailSubscriptio
 import HelpPages from '@/orangehrmHelpPlugin';
 import TimePages from '@/orangehrmTimePlugin';
 import AttendancePages from '@/orangehrmAttendancePlugin';
+import MyTracker from '@/orangehrmPerformancePlugin/pages/MyTracker.vue';
 
 export default {
   'job-title-list': JobTitle,
@@ -125,4 +126,5 @@ export default {
   ...HelpPages,
   ...TimePages,
   ...AttendancePages,
+  'my-tracker': MyTracker,
 };
