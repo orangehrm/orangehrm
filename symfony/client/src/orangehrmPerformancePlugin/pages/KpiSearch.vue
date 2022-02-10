5eccc3e889dcb1b718372d210c6205042fa01005
devishke-orange <devishke@orangehrmlive.com>
2022-02-10 17:20:52 +0530

    OHRM5X-1102: Added spacing to ternary operator

--- symfony/client/src/orangehrmPerformancePlugin/pages/KpiSearch.vue
+++ symfony/client/src/orangehrmPerformancePlugin/pages/KpiSearch.vue
@@ -101,7 +101,7 @@ const kpiNormalizer = data => {
       jobTitleId: item.jobTitle.id,
       minRating: item.minRating,
       maxRating: item.maxRating,
-      isDefault: item.isDefault? 'Yes' : '',
+      isDefault: item.isDefault ? 'Yes' : '',
     };
   });
 };
