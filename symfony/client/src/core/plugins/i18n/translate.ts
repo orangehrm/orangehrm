910d5f3190372c3f57ad63202a3c3f3da1c437de
Chamara Abesinghe <chamara@orangehrmlive.com>
2022-02-28 18:33:30 +0530

    OHRM5X-1123: Initiate front-end i18n support (#1122)

--- symfony/client/src/core/plugins/i18n/translate.ts
+++ symfony/client/src/core/plugins/i18n/translate.ts
@@ -16,56 +16,105 @@
  * Boston, MA  02110-1301, USA
  */
 
-import {App, ComponentOptions} from 'vue';
+import {AxiosResponse} from 'axios';
+import {App, ComponentOptions, ref} from 'vue';
+import IntlMessageFormat from 'intl-messageformat';
+import {APIService} from '@/core/util/services/api.service';
+import {mergeConfig} from '@ohrm/oxd/services/store';
 
 export type Language = {
-  source_language: string;
-  target_language: string;
-  date: string;
-  modules: {
-    [moduleName: string]: {
-      version: string;
-      messages: {
-        [key: string]: {
-          source: string;
-          target: string;
-          description: string;
-        };
-      };
-    };
-  };
+  [key: string]: IntlMessageFormat;
 };
 
+export interface LanguageResponse {
+  [key: string]: {
+    source: string;
+    target: string;
+    description: string;
+  };
+}
+
 export interface LanguageOptions {
-  langugePack: Language;
+  baseUrl: string;
+  resourceUrl: string;
 }
 
 export type TranslateAPI = (key: string, fallback?: string) => string;
 
-const translate = (language: Language) => {
-  return (key: string, fallback = ''): string => {
-    const [moduleName, messageKey] = key.split('.');
-    if (moduleName && messageKey) {
-      return language.modules[moduleName]?.messages[messageKey]?.target;
+export const langStrings = ref<Language>({});
+
+/**
+ * A factory function that will return translator function
+ * @return {function(key, parameters): string}
+ */
+export const translate = () => (
+  key: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  parameters: {[key: string]: any} = {},
+): string => {
+  // IntlMessageFormat.format method will throw error if not every argument in the message pattern
+  // has been provided. sourrounded by try catch to fallback incase of param resolution
+  try {
+    if (!langStrings.value[key]) return key;
+    const translatedString = langStrings.value[key].format<string>(parameters);
+    if (Array.isArray(translatedString)) {
+      return typeof translatedString[0] === 'string'
+        ? translatedString[0]
+        : key;
     }
-    return fallback;
+    return translatedString;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return key;
+  }
+};
+
+const defineMixin = (): ComponentOptions => {
+  return {
+    beforeCreate(): void {
+      this.$t = translate();
+    },
   };
 };
 
-function defineMixin(language: Language): ComponentOptions {
+function createI18n(options: LanguageOptions) {
+  const http = new APIService(options.baseUrl, options.resourceUrl);
   return {
-    beforeCreate(): void {
-      this.$t = translate(language);
+    init: function() {
+      http
+        .request({
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control':
+              'public, only-if-cached, stale-while-revalidate, max-age=60',
+          },
+        })
+        .then((response: AxiosResponse<LanguageResponse>) => {
+          const {data} = response;
+          const language: {[key: string]: string} = {};
+          for (const key in data) {
+            // https://formatjs.io/docs/intl-messageformat#intlmessageformat-constructor
+            language[key] = data[key].target || data[key].source;
+            langStrings.value[key] = new IntlMessageFormat(
+              data[key].target || data[key].source,
+              undefined,
+              undefined,
+              {ignoreTag: true}, // no html/xml markup parsing
+            );
+          }
+          mergeConfig({
+            language,
+          });
+        });
+      return this;
+    },
+    install: function(app: App) {
+      app.mixin(defineMixin());
     },
   };
 }
 
-export default {
-  install: (app: App, options: LanguageOptions) => {
-    const translations = options.langugePack;
-    if (!translations) {
-      throw new Error('Language pack not found!');
-    }
-    app.mixin(defineMixin(translations));
-  },
-};
+export default createI18n;
