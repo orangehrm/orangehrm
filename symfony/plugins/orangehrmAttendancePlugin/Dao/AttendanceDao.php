09ad095b0ed4f7b715d25151298164993a2c6016
SharukaNP <sharuka@orangehrmlive.com>
2022-03-25 08:30:19 +0530

    OHRM5X-1224 : Fix get one restriction for attendance record

--- symfony/plugins/orangehrmAttendancePlugin/Dao/AttendanceDao.php
+++ symfony/plugins/orangehrmAttendancePlugin/Dao/AttendanceDao.php
@@ -275,10 +275,7 @@ class AttendanceDao extends BaseDao
     public function getAttendanceRecordById(int $attendanceRecordId): ?AttendanceRecord
     {
         $attendanceRecord = $this->getRepository(AttendanceRecord::class)->find($attendanceRecordId);
-        if (is_null($attendanceRecord->getEmployee()->getPurgedAt())) {
-            return ($attendanceRecord instanceof AttendanceRecord) ? $attendanceRecord : null;
-        }
-        return null;
+        return ($attendanceRecord instanceof AttendanceRecord) ? $attendanceRecord : null;
     }
 
     /**
