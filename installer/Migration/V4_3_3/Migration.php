d9f0ebd4017cd6f88fc7d7ca3ce8fe44908f393b
SharukaNP <sharuka@orangehrmlive.com>
2022-04-18 13:10:43 +0530

    OHRM5X-1260 : Replace raw sql

--- installer/Migration/V4_3_3/Migration.php
+++ installer/Migration/V4_3_3/Migration.php
@@ -19,6 +19,8 @@
 
 namespace OrangeHRM\Installer\Migration\V4_3_3;
 
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use OrangeHRM\Installer\Util\V1\AbstractMigration;
 
 class Migration extends AbstractMigration
@@ -30,17 +32,31 @@ class Migration extends AbstractMigration
     {
         $this->getSchemaHelper()->disableConstraints();
 
-        $sql = [
-            0 => 'ALTER TABLE `hs_hr_employee` MODIFY `emp_number` int(7) not null AUTO_INCREMENT;',
-            1 => 'ALTER TABLE `ohrm_timesheet` MODIFY `timesheet_id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
-            2 => 'ALTER TABLE `ohrm_timesheet_item` MODIFY `timesheet_item_id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
-            3 => 'ALTER TABLE `ohrm_timesheet_action_log` MODIFY `timesheet_action_log_id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
-            4 => 'ALTER TABLE `ohrm_attendance_record` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT;',
-            5 => 'ALTER TABLE `ohrm_job_candidate` MODIFY `id` int(13) not null AUTO_INCREMENT;',
-            6 => 'ALTER TABLE `ohrm_job_candidate_vacancy` MODIFY `id` int(13) default null unique AUTO_INCREMENT;',
-            7 => 'ALTER TABLE `ohrm_job_vacancy` MODIFY `id` int(13) not null auto_increment;',
-        ];
-        $this->getSchemaHelper()->execSql($sql);
+        $this->getSchemaHelper()->changeColumn('hs_hr_employee', 'emp_number', [
+            'Type' => Type::getType(Types::INTEGER), 'Length' => 7, 'Notnull' => true, 'Default' => null,'Autoincrement' => true
+        ]);
+
+        $this->getSchemaHelper()->changeColumn('ohrm_timesheet', 'timesheet_id', [
+            'Type' => Type::getType(Types::BIGINT), 'Length' => 20, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
+        $this->getSchemaHelper()->changeColumn('ohrm_timesheet_item', 'timesheet_item_id', [
+            'Type' => Type::getType(Types::BIGINT), 'Length' => 20, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
+        $this->getSchemaHelper()->changeColumn('ohrm_timesheet_action_log', 'timesheet_action_log_id', [
+            'Type' => Type::getType(Types::BIGINT), 'Length' => 20, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
+        $this->getSchemaHelper()->changeColumn('ohrm_attendance_record', 'id', [
+            'Type' => Type::getType(Types::BIGINT), 'Length' => 20, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
+        $this->getSchemaHelper()->changeColumn('ohrm_job_candidate', 'id', [
+            'Type' => Type::getType(Types::INTEGER), 'Length' => 13, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
+        $this->getSchemaHelper()->changeColumn('ohrm_job_candidate_vacancy', 'id', [
+            'Type' => Type::getType(Types::INTEGER), 'Length' => 13, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
+        $this->getSchemaHelper()->changeColumn('ohrm_job_vacancy', 'id', [
+            'Type' => Type::getType(Types::INTEGER), 'Length' => 13, 'Notnull' => true, 'Default' => null, 'Autoincrement' => true
+        ]);
         $this->getSchemaHelper()->enableConstraints();
     }
 
