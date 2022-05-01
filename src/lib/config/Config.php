00a8b9515b17ace5d1033f5f1db3745a81bf8343
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-05-01 12:29:33 +0530

    OHRM5X-1302: Change product mode to dev

--- src/lib/config/Config.php
+++ src/lib/config/Config.php
@@ -40,7 +40,7 @@ class Config
 
     public const PRODUCT_NAME = 'OrangeHRM OS';
     public const PRODUCT_VERSION = '5.0';
-    public const PRODUCT_MODE = self::MODE_PROD;
+    public const PRODUCT_MODE = self::MODE_DEV;
     public const REGISTRATION_URL = 'https://ospenguin.orangehrm.com';
 
     public const MAX_SESSION_IDLE_TIME = 1800;
