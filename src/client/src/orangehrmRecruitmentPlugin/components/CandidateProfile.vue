2d600bbb5b6e2f9019547c0ef753ba399b5d1ae2
Ajiharan007 <baskaran@orangehrmlive.com>
2022-05-16 20:38:54 +0530

    OHRM5X-1362: file validation added

--- src/client/src/orangehrmRecruitmentPlugin/components/CandidateProfile.vue
+++ src/client/src/orangehrmRecruitmentPlugin/components/CandidateProfile.vue
@@ -192,7 +192,10 @@ export default {
       allowedFileTypes: [
         'application/pdf',
         'application/msword',
-        'application/vnd.ms-excel',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'text/plain',
+        'application/rtf',
+        'application/vnd.oasis.opendocument.text',
       ],
       profile: {
         firstName: '',
