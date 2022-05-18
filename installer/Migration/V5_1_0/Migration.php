45abf77edeefcd6607688be3f436353ae81ae1c6
Ajiharan007 <baskaran@orangehrmlive.com>
2022-05-18 17:37:35 +0530

    OHRM5X-1282: Validate reset password link already used (#1273)

--- installer/Migration/V5_1_0/Migration.php
+++ installer/Migration/V5_1_0/Migration.php
@@ -20,6 +20,7 @@
 namespace OrangeHRM\Installer\Migration\V5_1_0;
 
 use Doctrine\DBAL\Schema\ForeignKeyConstraint;
+use Doctrine\DBAL\Types\Types;
 use OrangeHRM\Installer\Util\V1\AbstractMigration;
 
 class Migration extends AbstractMigration
@@ -31,6 +32,7 @@ class Migration extends AbstractMigration
     public function up(): void
     {
         $this->getDataGroupHelper()->insertApiPermissions(__DIR__ . '/permission/api.yaml');
+        $this->addValidColumnToRequestResetPassword();
 
         $this->getConnection()->executeStatement(
             'ALTER TABLE ohrm_kpi CHANGE job_title_code job_title_code INT(13) NOT NULL'
@@ -81,6 +83,22 @@ class Migration extends AbstractMigration
         }
     }
 
+    /**
+     * @return void
+     */
+    private function addValidColumnToRequestResetPassword(): void
+    {
+        $this->getSchemaHelper()->addColumn(
+            'ohrm_reset_password',
+            'expired',
+            Types::BOOLEAN,
+            ['Default' => true, 'Notnull' => true]
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function getVersion(): string
     {
         return '5.1.0';
