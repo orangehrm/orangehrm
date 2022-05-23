2354b77e3194cacaf4188dfb9105e8da2b074ea9
OvaraPerera <ovara@orangehrmlive.com>
2022-05-23 20:44:32 +0530

    added requested changes

--- src/test/functional/cypress/integration/feature/admin/jobCategories.spec.js
+++ src/test/functional/cypress/integration/feature/admin/jobCategories.spec.js
@@ -1,7 +1,27 @@
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
 describe('Admin - Job Category', function () {
   beforeEach(function () {
     cy.task('db:reset');
-    cy.viewport(1280, 720);
+    cy.fixture('viewport').then(({HD}) => {
+      cy.viewport(HD.width, HD.height);
+    });
     cy.fixture('chars').as('strings');
     cy.intercept('GET', '**/api/v2/admin/job-categories?*').as(
       'getJobCategories',
@@ -73,6 +93,7 @@ describe('Admin - Job Category', function () {
       cy.toast('success', 'Successfully Updated');
     });
   });
+  //Delete
   describe('Delete job Category', function () {
     it('Delete a single job category', function () {
       cy.loginTo(this.user, '/admin/jobCategory');
