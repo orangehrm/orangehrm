276186758864211c7f16871f32414b7e830ca6ba
SharukaNP <sharuka@orangehrmlive.com>
2022-06-03 10:37:40 +0530

    OHRM5X-1444 : Toast messages for past employee

--- src/plugins/orangehrmPerformancePlugin/Service/PerformanceReviewService.php
+++ src/plugins/orangehrmPerformancePlugin/Service/PerformanceReviewService.php
@@ -23,37 +23,46 @@ use OrangeHRM\Entity\JobTitle;
 use OrangeHRM\Entity\PerformanceReview;
 use OrangeHRM\Performance\Dao\PerformanceReviewDao;
 use OrangeHRM\Performance\Exception\ReviewServiceException;
+use OrangeHRM\Pim\Dao\EmployeeDao;
 
 class PerformanceReviewService
 {
     private ?PerformanceReviewDao $performanceReviewDao = null;
+    private ?EmployeeDao $employeeDao = null;
 
     /**
      * @return PerformanceReviewDao
      */
     public function getPerformanceReviewDao(): PerformanceReviewDao
     {
-        if (! ($this->performanceReviewDao instanceof PerformanceReviewDao)) {
+        if (!($this->performanceReviewDao instanceof PerformanceReviewDao)) {
             $this->performanceReviewDao = new PerformanceReviewDao();
         }
         return $this->performanceReviewDao;
     }
 
     /**
+     * @return EmployeeDao
+     */
+    public function getEmployeeDao(): EmployeeDao
+    {
+        if (!($this->employeeDao instanceof EmployeeDao)) {
+            $this->employeeDao = new EmployeeDao();
+        }
+        return $this->employeeDao;
+    }
+
+
+    /**
      * @param PerformanceReview $performanceReview
-     * @param int $reviewerId
+     * @param int $reviewerEmpNumber
      * @return PerformanceReview
      * @throws ReviewServiceException
      */
-    public function activateReview(PerformanceReview $performanceReview, int $reviewerId): PerformanceReview
+    public function activateReview(PerformanceReview $performanceReview, int $reviewerEmpNumber): PerformanceReview
     {
-        if (! $performanceReview->getEmployee()->getJobTitle() instanceof JobTitle) {
-            throw ReviewServiceException::activateWithoutJobTitle();
-        }
-        if ($this->getPerformanceReviewDao()->getReviewKPI($performanceReview) == null) {
-            throw ReviewServiceException::activateWithoutKPI();
-        }
-        return $this->getPerformanceReviewDao()->createReview($performanceReview, $reviewerId);
+        $this->activateReviewCommonExceptions($performanceReview);
+        return $this->getPerformanceReviewDao()->createReview($performanceReview, $reviewerEmpNumber);
     }
 
     /**
@@ -63,12 +72,33 @@ class PerformanceReviewService
      */
     public function updateActivateReview(PerformanceReview $performanceReview, int $reviewerEmpNumber): PerformanceReview
     {
-        if (! $performanceReview->getEmployee()->getJobTitle() instanceof JobTitle) {
+        $this->activateReviewCommonExceptions($performanceReview);
+        if (!($this->getEmployeeDao()->getEmployeeByEmpNumber($reviewerEmpNumber)->getEmployeeTerminationRecord()
+            == null)) {
+            throw ReviewServiceException::pastEmployeeForReviewer();
+        }
+        if ($this->getPerformanceReviewDao()->getSupervisorRecord(
+            $performanceReview->getEmployee()->getEmpNumber(),
+            $reviewerEmpNumber
+        ) == null){
+            throw ReviewServiceException::invalidSupervisor();
+        }
+
+        return $this->getPerformanceReviewDao()->updateReview($performanceReview, $reviewerEmpNumber);
+    }
+
+    /**
+     * @param PerformanceReview $performanceReview
+     * @return void
+     * @throws ReviewServiceException
+     */
+    private function activateReviewCommonExceptions(PerformanceReview $performanceReview): void
+    {
+        if (!$performanceReview->getEmployee()->getJobTitle() instanceof JobTitle) {
             throw ReviewServiceException::activateWithoutJobTitle();
         }
         if ($this->getPerformanceReviewDao()->getReviewKPI($performanceReview) == null) {
             throw ReviewServiceException::activateWithoutKPI();
         }
-        return $this->getPerformanceReviewDao()->updateReview($performanceReview, $reviewerEmpNumber);
     }
 }
