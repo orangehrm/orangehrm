61c5e197eb758bc54d02c0c5f5657bb1f0b08838
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-06-17 20:25:48 +0530

    OHRM5X-1347: Improve code coverage

--- src/plugins/orangehrmCorporateBrandingPlugin/test/Service/ThemeServiceTest.php
+++ src/plugins/orangehrmCorporateBrandingPlugin/test/Service/ThemeServiceTest.php
@@ -57,4 +57,100 @@ class ThemeServiceTest extends KernelTestCase
         $this->assertTrue($themeService->showSocialMediaImages()); // check default theme
         $this->assertTrue($themeService->showSocialMediaImages()); // call twice to check caching
     }
+
+    public function testGetCurrentThemeVariables(): void
+    {
+        $this->createKernelWithMockServices([
+            Services::CACHE => MockCacheService::getCache(),
+        ]);
+        $customThemeCssVariables = [
+            '--oxd-primary-one-color' => '#FD7B1D',
+            '--oxd-primary-font-color' => '#FFFFFD',
+            '--oxd-secondary-four-color' => '#76AC21',
+            '--oxd-secondary-font-color' => '#FFFFFD',
+            '--oxd-primary-gradient-start-color' => '#FD920B',
+            '--oxd-primary-gradient-end-color' => '#D35C17',
+            '--oxd-primary-one-lighten-5-color' => '#fd8a36',
+            '--oxd-primary-one-lighten-30-color' => '#fed4b5',
+            '--oxd-primary-one-darken-5-color' => '#fd6c04',
+            '--oxd-primary-one-alpha-10-color' => 'rgba(253, 123, 29, 0.1)',
+            '--oxd-primary-one-alpha-15-color' => 'rgba(253, 123, 29, 0.15)',
+            '--oxd-primary-one-alpha-20-color' => 'rgba(253, 123, 29, 0.2)',
+            '--oxd-primary-one-alpha-50-color' => 'rgba(253, 123, 29, 0.5)',
+            '--oxd-secondary-four-lighten-5-color' => '#85c125',
+            '--oxd-secondary-four-darken-5-color' => '#67971d',
+            '--oxd-secondary-four-alpha-10-color' => 'rgba(118, 172, 33, 0.1)',
+            '--oxd-secondary-four-alpha-15-color' => 'rgba(118, 172, 33, 0.15)',
+            '--oxd-secondary-four-alpha-20-color' => 'rgba(118, 172, 33, 0.2)',
+            '--oxd-secondary-four-alpha-50-color' => 'rgba(118, 172, 33, 0.5)',
+        ];
+        $themeService = new ThemeService();
+        $this->assertEquals($customThemeCssVariables, $themeService->getCurrentThemeVariables()); // check custom theme
+        $this->assertEquals(
+            $customThemeCssVariables,
+            $themeService->getCurrentThemeVariables()
+        ); // call twice to check caching
+
+        ThemeAPITest::keepOnlyDefaultThemePreHook();
+        $this->createKernelWithMockServices([
+            Services::CACHE => MockCacheService::getCache(),
+        ]);
+
+        $customThemeCssVariables = [
+            '--oxd-primary-one-color' => '#FF7B1D',
+            '--oxd-primary-font-color' => '#FFFFFF',
+            '--oxd-secondary-four-color' => '#76BC21',
+            '--oxd-secondary-font-color' => '#FFFFFF',
+            '--oxd-primary-gradient-start-color' => '#FF920B',
+            '--oxd-primary-gradient-end-color' => '#F35C17',
+            '--oxd-primary-one-lighten-5-color' => '#ff8a37',
+            '--oxd-primary-one-lighten-30-color' => '#ffd4b6',
+            '--oxd-primary-one-darken-5-color' => '#ff6c03',
+            '--oxd-primary-one-alpha-10-color' => 'rgba(255, 123, 29, 0.1)',
+            '--oxd-primary-one-alpha-15-color' => 'rgba(255, 123, 29, 0.15)',
+            '--oxd-primary-one-alpha-20-color' => 'rgba(255, 123, 29, 0.2)',
+            '--oxd-primary-one-alpha-50-color' => 'rgba(255, 123, 29, 0.5)',
+            '--oxd-secondary-four-lighten-5-color' => '#84d225',
+            '--oxd-secondary-four-darken-5-color' => '#68a61d',
+            '--oxd-secondary-four-alpha-10-color' => 'rgba(118, 188, 33, 0.1)',
+            '--oxd-secondary-four-alpha-15-color' => 'rgba(118, 188, 33, 0.15)',
+            '--oxd-secondary-four-alpha-20-color' => 'rgba(118, 188, 33, 0.2)',
+            '--oxd-secondary-four-alpha-50-color' => 'rgba(118, 188, 33, 0.5)',
+        ];
+        $this->assertEquals($customThemeCssVariables, $themeService->getCurrentThemeVariables()); // check default theme
+        $this->assertEquals(
+            $customThemeCssVariables,
+            $themeService->getCurrentThemeVariables()
+        ); // call twice to check caching
+    }
+
+    public function testGetImageETagWithNullImage(): void
+    {
+        ThemeAPITest::keepOnlyDefaultThemePreHook();
+        $this->createKernelWithMockServices([
+            Services::CACHE => MockCacheService::getCache(),
+        ]);
+        $themeService = new ThemeService();
+        $this->assertNull($themeService->getImageETag('client_logo'));
+    }
+
+    public function testGetImage(): void
+    {
+        $this->createKernelWithMockServices([
+            Services::CACHE => MockCacheService::getCache(),
+        ]);
+        $themeService = new ThemeService();
+        $image = $themeService->getImage('client_logo');
+        $this->assertEquals('ohrm_logo.png', $image->getFilename());
+    }
+
+    public function testGetImageWithNullImage(): void
+    {
+        ThemeAPITest::keepOnlyDefaultThemePreHook();
+        $this->createKernelWithMockServices([
+            Services::CACHE => MockCacheService::getCache(),
+        ]);
+        $themeService = new ThemeService();
+        $this->assertNull($themeService->getImage('client_logo'));
+    }
 }
