abdcc1d008f963820f0627bb2112d13bf8b48670
shenali <shenali@orangehrmlive.com>
2022-06-29 14:12:14 +0530

    test leave holidays code- fix 3

--- src/test/functional/cypress/integration/feature/leave/holidays.spec.js
+++ src/test/functional/cypress/integration/feature/leave/holidays.spec.js
@@ -1,3 +1,20 @@
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
 describe('Leave - Holidays', function () {
   beforeEach(function () {
     cy.task('db:reset');
@@ -39,7 +56,7 @@ describe('Leave - Holidays', function () {
   });
 
   describe('create snapshot with holiday', function () {
-    it.only('create snapshot with holiday', function () {
+    it('create snapshot with holiday', function () {
       cy.loginTo(this.user, '/leave/saveHolidays');
       cy.getOXD('form').within(() => {
         cy.getOXDInput('Name').type(this.strings.chars10.text);
