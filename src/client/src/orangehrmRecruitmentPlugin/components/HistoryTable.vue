afd767838e3aa854cad1b5878e76ddded72cba51
Chamara Abesinghe <chamara@orangehrmlive.com>
2022-07-08 10:17:06 +0530

    OHRM5X-1541: Recruitment history table change action button order

--- src/client/src/orangehrmRecruitmentPlugin/components/HistoryTable.vue
+++ src/client/src/orangehrmRecruitmentPlugin/components/HistoryTable.vue
@@ -253,12 +253,12 @@ export default {
           title: 'Performed Date',
           style: {flex: '20%'},
         },
-        {name: 'description', title: 'Description', style: {flex: '60%'}},
+        {name: 'description', title: 'Description', style: {flex: '65%'}},
         {
           name: 'actions',
           slot: 'action',
           title: this.$t('general.actions'),
-          style: {flex: '20%'},
+          style: {flex: '15%'},
           cellType: 'oxd-table-cell-actions',
           cellRenderer: this.cellRenderer,
         },
@@ -270,19 +270,6 @@ export default {
       const cellConfig = {};
 
       if (
-        row.action?.id === ACTION_INTERVIEW_SCHEDULED ||
-        row.action?.id === ACTION_INTERVIEW_PASSED ||
-        row.action?.id === ACTION_INTERVIEW_FAILED
-      ) {
-        cellConfig.attachment = {
-          onClick: this.onClickAttachment,
-          props: {
-            name: 'paperclip',
-          },
-        };
-      }
-
-      if (
         row.action?.id != ACTION_ASSIGNED_VACANCY &&
         row.action?.id != ACTION_ADDED &&
         row.action?.id != ACTION_REMOVED
@@ -291,6 +278,19 @@ export default {
           onClick: this.onClickEdit,
           props: {
             name: 'pencil-fill',
+          },
+        };
+      }
+
+      if (
+        row.action?.id === ACTION_INTERVIEW_SCHEDULED ||
+        row.action?.id === ACTION_INTERVIEW_PASSED ||
+        row.action?.id === ACTION_INTERVIEW_FAILED
+      ) {
+        cellConfig.attachment = {
+          onClick: this.onClckAttachment,
+          props: {
+            name: 'paperclip',
           },
         };
       }
