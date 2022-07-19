f469b7abd6b90578f0ff7fdb09fc5ec76b545bcb
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-07-19 10:51:15 +0530

    OHRM5X-1590: Block admin - language packages related screens & APIs

--- src/plugins/orangehrmAdminPlugin/Api/I18NTranslationAPI.php
+++ src/plugins/orangehrmAdminPlugin/Api/I18NTranslationAPI.php
@@ -108,6 +108,7 @@ class I18NTranslationAPI extends Endpoint implements CollectionEndpoint
      */
     public function getValidationRuleForGetAll(): ParamRuleCollection
     {
+        throw $this->getNotImplementedException(); // TODO:: this line should remove in 5.2 release
         return new ParamRuleCollection(
             new ParamRule(
                 self::PARAMETER_LANGUAGE_ID,
