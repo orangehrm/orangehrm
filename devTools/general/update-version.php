8936a202ae4051c12fa2c9a799b353968f6f9460
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-07-28 16:06:10 +0530

    OHRM5X-1636: Update version to 5.2

--- devTools/general/update-version.php
+++ devTools/general/update-version.php
@@ -22,6 +22,7 @@ $rootPath = dirname(__FILE__) . "/../../";
 $files = [
     'build/build.xml',
     'src/lib/config/Config.php',
+    'src/plugins/orangehrmCorePlugin/test/fixtures/testcases/AboutOrganizationTestCase.yaml',
 ];
 
 if ($argc != 3) {
