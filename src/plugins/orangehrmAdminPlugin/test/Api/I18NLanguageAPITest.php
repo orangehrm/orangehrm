a95c77c05afd1c02ebf32c01939222e0a4608186
Manul <manul@orangehrm.us.com>
2022-08-05 17:39:08 +0530

    Fix Test Cases

--- src/plugins/orangehrmAdminPlugin/test/Api/I18NLanguageAPITest.php
+++ src/plugins/orangehrmAdminPlugin/test/Api/I18NLanguageAPITest.php
@@ -55,6 +55,7 @@ class I18NLanguageAPITest extends EndpointIntegrationTestCase
         $this->populateFixtures('I18NLanguagesAPI.yml');
         $this->createKernelWithMockServices([Services::AUTH_USER => $this->getMockAuthUser($testCaseParams)]);
         $this->registerServices($testCaseParams);
+        $this->registerMockDateTimeHelper($testCaseParams);
         $api = $this->getApiEndpointMock(I18NLanguageAPI::class, $testCaseParams);
         $this->assertValidTestCase($api, 'update', $testCaseParams);
     }
