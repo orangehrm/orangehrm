47bb4f37b84752bf2a43368f1d4557e33b7998a6
Sandeepa Ranathunga <sandeepa@orangehrmlive.com>
2022-08-19 08:32:13 +0530

    OHRM5X-1661: Develop admin - LDAP config API (#1415)

---
+++ src/plugins/orangehrmLDAPAuthenticationPlugin/test/Api/LDAPConfigAPITest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
+namespace OrangeHRM\Tests\LDAP\Api;
+
+use OrangeHRM\Framework\Services;
+use OrangeHRM\LDAP\Api\LDAPConfigAPI;
+use OrangeHRM\Tests\Util\EndpointIntegrationTestCase;
+use OrangeHRM\Tests\Util\Integration\TestCaseParams;
+
+/**
+ * @group LDAP
+ * @group APIv2
+ */
+class LDAPConfigAPITest extends EndpointIntegrationTestCase
+{
+    public function testGetAll(): void
+    {
+        $api = new LDAPConfigAPI($this->getRequest());
+        $this->expectNotImplementedException();
+        $api->getAll();
+    }
+
+    public function testGetValidationRuleForGetAll(): void
+    {
+        $api = new LDAPConfigAPI($this->getRequest());
+        $this->expectNotImplementedException();
+        $api->getValidationRuleForGetAll();
+    }
+
+    /**
+     * @dataProvider dataProviderForTestCreate
+     */
+    public function testCreate(TestCaseParams $testCaseParams): void
+    {
+        $this->populateFixtures('LDAPConfig.yaml', null, true);
+        $this->createKernelWithMockServices([Services::AUTH_USER => $this->getMockAuthUser($testCaseParams)]);
+        $this->registerServices($testCaseParams);
+        $this->registerMockDateTimeHelper($testCaseParams);
+        $api = $this->getApiEndpointMock(LDAPConfigAPI::class, $testCaseParams);
+        $this->assertValidTestCase($api, 'create', $testCaseParams);
+    }
+
+    public function dataProviderForTestCreate(): array
+    {
+        return $this->getTestCases('LDAPConfigTestCases.yaml', 'Create');
+    }
+
+    public function testDelete(): void
+    {
+        $api = new LDAPConfigAPI($this->getRequest());
+        $this->expectNotImplementedException();
+        $api->delete();
+    }
+
+    public function testGetValidationRuleForDelete(): void
+    {
+        $api = new LDAPConfigAPI($this->getRequest());
+        $this->expectNotImplementedException();
+        $api->getValidationRuleForDelete();
+    }
+}
