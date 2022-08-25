fb6857dce3a75184061d7e6a3223436e35a44647
Manul <manul@orangehrm.us.com>
2022-08-25 11:59:06 +0530

    Add Default Navigation

--- src/plugins/orangehrmDashboardPlugin/Controller/DashboardController.php
+++ src/plugins/orangehrmDashboardPlugin/Controller/DashboardController.php
@@ -23,7 +23,7 @@ use OrangeHRM\Core\Controller\AbstractVueController;
 use OrangeHRM\Core\Vue\Component;
 use OrangeHRM\Framework\Http\Request;
 
-class DashboardWidgetController extends AbstractVueController
+class DashboardController extends AbstractVueController
 {
     public function preRender(Request $request): void
     {
