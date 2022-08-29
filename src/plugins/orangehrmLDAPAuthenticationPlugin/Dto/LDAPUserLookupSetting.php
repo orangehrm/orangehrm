81c8120536282e832cd968d6373de9ab5ff6aa93
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-08-29 13:03:30 +0530

    OHRM5X-1663: Initiate ldap user sync

---
+++ src/plugins/orangehrmLDAPAuthenticationPlugin/Dto/LDAPUserLookupSetting.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA
+ */
+
+namespace OrangeHRM\LDAP\Dto;
+
+use InvalidArgumentException;
+use Symfony\Component\Ldap\Adapter\QueryInterface;
+
+class LDAPUserLookupSetting
+{
+    private string $baseDN;
+    private string $searchScope = QueryInterface::SCOPE_SUB;
+
+    private string $userNameAttribute = 'cn';
+    private ?string $userUniqueIdAttribute = 'entryUUID';
+    private string $userSearchFilter = '(objectClass=inetOrgPerson)';
+
+    /**
+     * @param string $baseDN
+     */
+    public function __construct(string $baseDN)
+    {
+        $this->baseDN = $baseDN;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseDN(): string
+    {
+        return $this->baseDN;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSearchScope(): string
+    {
+        return $this->searchScope;
+    }
+
+    /**
+     * @param string $searchScope
+     * @return LDAPUserLookupSetting
+     */
+    public function setSearchScope(string $searchScope): LDAPUserLookupSetting
+    {
+        if (!in_array($searchScope, [QueryInterface::SCOPE_SUB, QueryInterface::SCOPE_ONE])) {
+            throw new InvalidArgumentException("Invalid search scope: `$searchScope`");
+        }
+        $this->searchScope = $searchScope;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserNameAttribute(): string
+    {
+        return $this->userNameAttribute;
+    }
+
+    /**
+     * @param string $userNameAttribute
+     * @return LDAPUserLookupSetting
+     */
+    public function setUserNameAttribute(string $userNameAttribute): LDAPUserLookupSetting
+    {
+        $this->userNameAttribute = $userNameAttribute;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserUniqueIdAttribute(): ?string
+    {
+        return $this->userUniqueIdAttribute;
+    }
+
+    /**
+     * @param string|null $userUniqueIdAttribute
+     * @return LDAPUserLookupSetting
+     */
+    public function setUserUniqueIdAttribute(?string $userUniqueIdAttribute): LDAPUserLookupSetting
+    {
+        $this->userUniqueIdAttribute = $userUniqueIdAttribute;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserSearchFilter(): string
+    {
+        return $this->userSearchFilter;
+    }
+
+    /**
+     * @param string $userSearchFilter
+     * @return LDAPUserLookupSetting
+     */
+    public function setUserSearchFilter(string $userSearchFilter): LDAPUserLookupSetting
+    {
+        $this->userSearchFilter = $userSearchFilter;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'baseDN' => $this->getBaseDN(),
+            'searchScope' => $this->getSearchScope(),
+            'userNameAttribute' => $this->getUserNameAttribute(),
+            'userUniqueIdAttribute' => $this->getUserUniqueIdAttribute(),
+            'userSearchFilter' => $this->getUserSearchFilter(),
+        ];
+    }
+
+    /**
+     * @param array $userLookupSetting
+     * @return static
+     */
+    public static function createFromArray(array $userLookupSetting): self
+    {
+        $lookupSetting = new LDAPUserLookupSetting($userLookupSetting['baseDN']);
+        $lookupSetting->setSearchScope($userLookupSetting['searchScope']);
+        $lookupSetting->setUserNameAttribute($userLookupSetting['userNameAttribute']);
+        $lookupSetting->setUserUniqueIdAttribute($userLookupSetting['userUniqueIdAttribute']);
+        $lookupSetting->setUserSearchFilter($userLookupSetting['userSearchFilter']);
+        return $lookupSetting;
+    }
+}
