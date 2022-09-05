c682b37b234317e53b9152632b6a5f6d4fac0bec
Sandeepa Ranathunga <sandeepa@orangehrmlive.com>
2022-09-05 14:26:07 +0530

    OHRM5X-1666: Develop admin - LDAP test connection API (#1429)

---
+++ src/plugins/orangehrmLDAPAuthenticationPlugin/Api/Model/LDAPTestConnectionModel.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
+namespace OrangeHRM\LDAP\Api\Model;
+
+use OrangeHRM\Core\Api\V2\Serializer\Normalizable;
+use OrangeHRM\LDAP\Dto\LDAPSetting;
+use OrangeHRM\LDAP\Service\LDAPTestService;
+
+class LDAPTestConnectionModel implements Normalizable
+{
+    /**
+     * @var LDAPSetting
+     */
+    private LDAPSetting $ldapSetting;
+
+    /**
+     * @param LDAPSetting $ldapSetting
+     */
+    public function __construct(LDAPSetting $ldapSetting)
+    {
+        $this->ldapSetting = $ldapSetting;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        $ldapTestService = new LDAPTestService($this->ldapSetting);
+        return [
+            [
+                "category" => "Login",
+                "checks" => [
+                    "label" => "Authentication",
+                    "value" => $ldapTestService->testAuthentication(),
+                ]
+            ]
+        ];
+    }
+}
