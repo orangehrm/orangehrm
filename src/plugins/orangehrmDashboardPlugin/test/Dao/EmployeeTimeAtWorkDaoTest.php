a5b5d403f8edeaea542350e54f6be624b6df8190
SandeepaOHRM <sandeepa@orangehrmlive.com>
2022-09-06 05:56:26 +0530

    OHRM5X-1653 - Implement Dao Testing

---
+++ src/plugins/orangehrmDashboardPlugin/test/Dao/EmployeeTimeAtWorkDaoTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
+namespace OrangeHRM\Tests\Dashboard\Dao;
+
+use DateTime;
+use Exception;
+use OrangeHRM\Config\Config;
+use OrangeHRM\Dashboard\Dao\EmployeeTimeAtWorkDao;
+use OrangeHRM\Entity\AttendanceRecord;
+use OrangeHRM\Tests\Util\KernelTestCase;
+use OrangeHRM\Tests\Util\TestDataService;
+
+/**
+ * @group Dashboard
+ * @group Dao
+ */
+class EmployeeTimeAtWorkDaoTest extends KernelTestCase
+{
+    protected string $fixture;
+    protected EmployeeTimeAtWorkDao $employeeTimeAtWorkDao;
+
+    /**
+     * Set up method
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $this->employeeTimeAtWorkDao = new EmployeeTimeAtWorkDao();
+        $this->fixture = Config::get(
+            Config::PLUGINS_DIR
+        ) . '/orangehrmDashboardPlugin/test/fixtures/EmployeeTimeAtWork.yaml';
+        TestDataService::populate($this->fixture);
+    }
+
+    public function testGetLatestAttendanceRecordByEmpNumber()
+    {
+        $lastAttendanceRecord = $this->employeeTimeAtWorkDao->getLatestAttendanceRecordByEmpNumber(2);
+        $this->assertEquals('PUNCHED OUT', $lastAttendanceRecord->getState());
+        $this->assertEquals(new DateTime('2022-09-06 18:15:00'), $lastAttendanceRecord->getPunchInUtcTime());
+        $this->assertEquals(new DateTime('2022-09-06 23:45:00'), $lastAttendanceRecord->getPunchInUserTime());
+        $this->assertEquals('5.5', $lastAttendanceRecord->getPunchInTimeOffset());
+        $this->assertEquals(new DateTime('2022-09-06 20:29:00'), $lastAttendanceRecord->getPunchOutUtcTime());
+        $this->assertEquals(new DateTime('2022-09-07 01:59:00'), $lastAttendanceRecord->getPunchOutUserTime());
+        $this->assertEquals('5.5', $lastAttendanceRecord->getPunchOutTimeOffset());
+
+        /**
+         * No last attendance record found
+         */
+        $lastAttendanceRecord = $this->employeeTimeAtWorkDao->getLatestAttendanceRecordByEmpNumber(4);
+        $this->assertNotInstanceOf(AttendanceRecord::class, $lastAttendanceRecord);
+    }
+
+    public function testGetAttendanceRecordsByEmployeeAndDate()
+    {
+        $attendanceRecords = $this->employeeTimeAtWorkDao->getAttendanceRecordsByEmployeeAndDate(2, new DateTime('2022-09-05'));
+        $this->assertCount(3, $attendanceRecords);
+        $this->assertEquals('PUNCHED OUT', $attendanceRecords[0]->getState());
+        $this->assertEquals(new DateTime('2022-09-05 17:00:00'), $attendanceRecords[0]->getPunchInUtcTime());
+        $this->assertEquals(new DateTime('2022-09-05 22:30:00'), $attendanceRecords[0]->getPunchInUserTime());
+        $this->assertEquals('5.5', $attendanceRecords[0]->getPunchInTimeOffset());
+        $this->assertEquals(new DateTime('2022-09-05 18:00:00'), $attendanceRecords[0]->getPunchOutUtcTime());
+        $this->assertEquals(new DateTime('2022-09-05 23:30:00'), $attendanceRecords[0]->getPunchOutUserTime());
+        $this->assertEquals('5.5', $attendanceRecords[0]->getPunchOutTimeOffset());
+
+        /**
+         * No attendance records found
+         */
+
+        $attendanceRecords = $this->employeeTimeAtWorkDao->getAttendanceRecordsByEmployeeAndDate(2, new DateTime('2022-09-22'));
+        $this->assertTrue(!$attendanceRecords);
+        $this->assertCount(0, $attendanceRecords);
+    }
+}
