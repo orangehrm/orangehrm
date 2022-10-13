a322f44a48511d85f1227bb3e62a1e213aefefc5
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-10-14 00:02:21 +0530

    Remove include log_settings.php from installer

--- installer/index.php
+++ installer/index.php
@@ -17,8 +17,6 @@
  * Boston, MA  02110-1301, USA
  */
 
-include_once('./../lib/log_settings.php');
-
 use OrangeHRM\Framework\Http\Request;
 use OrangeHRM\Installer\Framework\HttpKernel;
 use Symfony\Component\ErrorHandler\Debug;
