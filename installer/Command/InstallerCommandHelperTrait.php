f744dc52823e0ed81734da4ed6303cad76ef3e50
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-12-16 20:41:51 +0530

    OHRM5X-1856: Refactor code

--- installer/Command/InstallerCommandHelperTrait.php
+++ installer/Command/InstallerCommandHelperTrait.php
@@ -17,7 +17,7 @@
  * Boston, MA 02110-1301, USA
  */
 
-namespace OrangeHRM\Installer\cli;
+namespace OrangeHRM\Installer\Command;
 
 use OrangeHRM\Installer\Exception\InvalidArgumentException;
 use OrangeHRM\Installer\Util\SystemCheck;
--- installer/Command/UpgradeCommand.php
+++ installer/Command/UpgradeCommand.php
@@ -17,7 +17,7 @@
  * Boston, MA 02110-1301, USA
  */
 
-namespace OrangeHRM\Installer\cli;
+namespace OrangeHRM\Installer\Command;
 
 use InvalidArgumentException;
 use OrangeHRM\Authentication\Dto\UserCredential;
