9e97c469f079e58d9f933245b8077d850f922905
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-01-04 14:19:01 +0530

    OHRM5X-1928: Upgrade app vue dependencies

--- src/client/.eslintrc.js
+++ src/client/.eslintrc.js
@@ -9,11 +9,11 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
