3e806c6676037bba797ef17906d6b4244274fda5
DulsaraNethmin <nethmin@orangehrm.us.com>
2023-01-23 11:47:48 +0530

    OHRM5X-1940: Alter the claim event table

--- installer/Migration/V5_4_0/Migration.php
+++ installer/Migration/V5_4_0/Migration.php
@@ -23,6 +23,7 @@ namespace OrangeHRM\Installer\Migration\V5_4_0;
 use Doctrine\DBAL\Schema\ForeignKeyConstraint;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Types\Types;
+use OrangeHRM\Installer\Util\Logger;
 use OrangeHRM\Installer\Util\V1\AbstractMigration;
 
 class Migration extends AbstractMigration
@@ -39,7 +40,7 @@ class Migration extends AbstractMigration
                 ->addColumn('name', Types::STRING, ['Notnull' => true, 'Length' => 100])
                 ->addColumn('description', Types::STRING, ['Notnull' => false, 'Length' => 1000])
                 ->addColumn('added_by', Types::INTEGER, ['Notnull' => false])
-                ->addColumn('status', Types::BOOLEAN, ['Notnull' => false])
+                ->addColumn('status', Types::STRING, ['Notnull' => false, 'Length' => 64])
                 ->addColumn('is_deleted', Types::SMALLINT, ['Notnull' => true, 'Default' => 0])
                 ->setPrimaryKey(['id'])
                 ->create();
@@ -68,7 +69,8 @@ class Migration extends AbstractMigration
             ->executeQuery();
 
         $this->getDataGroupHelper()->insertApiPermissions(__DIR__ . '/permission/api.yaml');
-        //$this->modifyClaimEventTables();
+        $this->convertStatusOldValuesToBoolean('ohrm_claim_event');
+        $this->modifyClaimEventTables();
 
         if (!$this->getSchemaHelper()->tableExists(['ohrm_expense_type'])) {
             $this->getSchemaHelper()->createTable('ohrm_expense_type')
@@ -234,6 +236,55 @@ class Migration extends AbstractMigration
                 Types::BOOLEAN
             )
             ->executeStatement();
+    }
+
+    /**
+     * @return void
+     */
+    private function modifyClaimEventTables()
+    {
+        $this->getSchemaHelper()->addOrChangeColumns('ohrm_claim_event', [
+            'is_deleted' => ['Type' => Type::getType(Types::BOOLEAN), 'Notnull' => true, 'Default' => 0],
+            'status' => ['Type' => Type::getType(Types::BOOLEAN), 'Notnull' => false, 'Default' => null, 'CustomSchemaOptions' => ['collation' => null, 'charset' => null]],
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    private function convertStatusOldValuesToBoolean($table): void
+    {
+        $count = $this->getTableRecordCount($table);
+        $batchSize = 10;
+        for ($i = 0; $i < $count; $i += $batchSize) {
+            $result = $this->createQueryBuilder()
+                ->select('id', 'status')
+                ->from($table)
+                ->setFirstResult($i)
+                ->setMaxResults($batchSize)
+                ->executeQuery();
+            foreach ($result->fetchAllAssociative() as $row) {
+                $this->createQueryBuilder()
+                    ->update($table)
+                    ->set('status', ':status')
+                    ->where('id = :id')
+                    ->setParameter('id', $row['id'])
+                    ->setParameter('status', $row['status'] == 'on' ? 1 : 0)
+                    ->executeStatement();
+            }
+            $result->free();
+        }
+    }
+
+    private function getTableRecordCount(string $tableName): int
+    {
+        $count = $this->createQueryBuilder()
+            ->select("COUNT($tableName.id)")
+            ->from($tableName)
+            ->executeQuery()
+            ->fetchOne();
+        Logger::getLogger()->info("`$tableName` record count: $count");
+        return $count;
     }
 
     /**
