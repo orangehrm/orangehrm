cc1b00fc48bf9482424de20a44a3a6ac6be3d2cb
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-02-28 16:50:32 +0530

    OHRM5X-849: Revert email validation regex

--- src/client/src/core/util/validation/rules.ts
+++ src/client/src/core/util/validation/rules.ts
@@ -361,8 +361,7 @@ export const validFileTypes = function (fileTypes: string[]) {
 export const validEmailFormat = function (value: string): boolean | string {
   return (
     !value ||
-    // eslint-disable-next-line no-useless-escape
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    /^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/.test(
       value,
     ) ||
     translate('general.expected_email_address_format_not_matched')
--- src/plugins/orangehrmCorePlugin/Api/V2/Validator/Rules/Email.php
+++ src/plugins/orangehrmCorePlugin/Api/V2/Validator/Rules/Email.php
@@ -24,7 +24,7 @@ class Email extends AbstractRule
     /**
      * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#basic_validation
      */
-    public const EMAIL_REGEX = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+    public const EMAIL_REGEX = '/^[a-zA-Z0-9.!#$%&\'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/';
 
     /**
      * @inheritDoc
