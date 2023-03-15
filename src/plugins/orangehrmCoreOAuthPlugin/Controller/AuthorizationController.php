1b0c3678e9254bba48f3c022c24836923afb90b6
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-03-15 14:55:34 +0530

    OHRM5X-2068: Forward unauthenticated users to login screen instead redirecting

--- src/plugins/orangehrmCoreOAuthPlugin/Controller/AuthorizationController.php
+++ src/plugins/orangehrmCoreOAuthPlugin/Controller/AuthorizationController.php
@@ -20,29 +20,46 @@
 namespace OrangeHRM\OAuth\Controller;
 
 use League\OAuth2\Server\Exception\OAuthServerException;
+use OrangeHRM\Authentication\Auth\User as AuthUser;
+use OrangeHRM\Authentication\Controller\LoginController;
 use OrangeHRM\Config\Config;
 use OrangeHRM\Core\Controller\AbstractVueController;
+use OrangeHRM\Core\Controller\Exception\RequestForwardableException;
+use OrangeHRM\Core\Controller\PublicControllerInterface;
+use OrangeHRM\Core\Traits\Auth\AuthUserTrait;
 use OrangeHRM\Core\Traits\LoggerTrait;
 use OrangeHRM\Core\Vue\Component;
 use OrangeHRM\Core\Vue\Prop;
 use OrangeHRM\CorporateBranding\Traits\ThemeServiceTrait;
 use OrangeHRM\Framework\Http\Request;
+use OrangeHRM\Framework\Services;
 use OrangeHRM\OAuth\Traits\OAuthServerTrait;
 use OrangeHRM\OAuth\Traits\PsrHttpFactoryHelperTrait;
+use Symfony\Component\HttpFoundation\UrlHelper;
 use Throwable;
 
-class AuthorizationController extends AbstractVueController
+class AuthorizationController extends AbstractVueController implements PublicControllerInterface
 {
     use OAuthServerTrait;
     use PsrHttpFactoryHelperTrait;
     use LoggerTrait;
     use ThemeServiceTrait;
+    use AuthUserTrait;
 
     /**
      * @inheritDoc
      */
     public function preRender(Request $request): void
     {
+        if (!$this->getAuthUser()->isAuthenticated()) {
+            /** @var UrlHelper $urlHelper */
+            $urlHelper = $this->getContainer()->get(Services::URL_HELPER);
+            $requestUri = $request->getRequestUri();
+            $redirectUri = $urlHelper->getAbsoluteUrl($requestUri);
+            $this->getAuthUser()->setAttribute(AuthUser::SESSION_TIMEOUT_REDIRECT_URL, $redirectUri);
+            throw new RequestForwardableException(LoginController::class . '::handle');
+        }
+
         $psrRequest = $this->getPsrHttpFactoryHelper()->createPsr7Request($request);
         $component = new Component('oauth-authorize');
         $authRequest = null;
@@ -51,7 +68,7 @@ class AuthorizationController extends AbstractVueController
             $authRequest = $server->validateAuthorizationRequest($psrRequest);
         } catch (OAuthServerException $e) {
             $component->addProp(new Prop('error-type', Prop::TYPE_STRING, $e->getErrorType()));
-            $this->getLogger()->error($e->getMessage());
+            $this->getLogger()->error($e->getMessage(), ['hint' => $e->getHint()]);
             $this->getLogger()->error($e->getTraceAsString());
         } catch (Throwable $e) {
             $component->addProp(new Prop('error-type', Prop::TYPE_STRING, 'server_error'));
