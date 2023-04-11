901aaca5b806ee67c11cfa2fa8d77d574930f207
DulsaraNethmin <nethmin@orangehrm.us.com>
2023-04-11 11:47:03 +0530

    OHRM5X-2051:  Inactive claim event QA bug

--- src/client/src/orangehrmClaimPlugin/components/AddAttachmentModal.vue
+++ src/client/src/orangehrmClaimPlugin/components/AddAttachmentModal.vue
@@ -22,7 +22,7 @@
   <oxd-dialog class="orangehrm-dialog-modal" @update:show="onCancel">
     <div class="orangehrm-modal-header">
       <oxd-text type="card-title">
-        {{ $t('general.add_attachment') }}
+        {{ $t('claim.add_attachment') }}
       </oxd-text>
     </div>
     <oxd-divider />
