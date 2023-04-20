6243f9a3deea303be6c23dcab7accd55922f9789
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-04-20 07:36:50 +0530

    OHRM5X-1995: Improve InstallerCountryListCommand

--- installer/Command/InstallerCountryListCommand.php
+++ installer/Command/InstallerCountryListCommand.php
@@ -22,6 +22,7 @@ namespace OrangeHRM\Installer\Command;
 use OrangeHRM\Installer\Framework\InstallerCommand;
 use OrangeHRM\Installer\Util\InstanceCreationHelper;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InstallerCountryListCommand extends InstallerCommand
@@ -37,6 +38,14 @@ class InstallerCountryListCommand extends InstallerCommand
     /**
      * @inheritDoc
      */
+    protected function configure(): void
+    {
+        $this->addOption('country', 'c', InputOption::VALUE_REQUIRED);
+    }
+
+    /**
+     * @inheritDoc
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $countries = array_combine(
@@ -45,6 +54,19 @@ class InstallerCountryListCommand extends InstallerCommand
         );
         asort($countries);
         $countries = array_map(fn ($country) => strtolower($country), $countries);
+
+        $country = $input->getOption('country');
+        if ($country !== null) {
+            $countries = array_flip($countries);
+            $country = $countries[strtolower($country)] ?? null;
+            if ($country == null) {
+                $this->getIO()->error('Invalid country');
+                return self::FAILURE;
+            }
+            $this->getIO()->writeln($country);
+            return self::SUCCESS;
+        }
+
         $countries = array_map(static function ($k, $v) {
             return " <comment>[$k]</comment> $v";
         }, array_keys($countries), array_values($countries));
