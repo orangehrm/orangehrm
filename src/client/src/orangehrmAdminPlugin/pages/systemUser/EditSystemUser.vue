7fa40cad79503da92c2bcc24eaf33f61ea54681f
ManulMax <manul@orangehrm.us.com>
2023-05-09 02:28:16 +0530

    OHRM5X-2175: fix edited user appear as a disable user issue

--- src/client/src/orangehrmAdminPlugin/pages/systemUser/EditSystemUser.vue
+++ src/client/src/orangehrmAdminPlugin/pages/systemUser/EditSystemUser.vue
@@ -213,7 +213,9 @@ export default {
         .update(this.systemUserId, {
           username: this.user.username.trim(),
           password: this.user.password,
-          status: this.user.status && this.user.status.label === 'Enabled',
+          status:
+            this.user.status &&
+            this.user.status.label === this.$t('general.enabled'),
           userRoleId: this.user.role?.id,
           empNumber: this.user.employee?.id,
           changePassword: this.user.changePassword,
