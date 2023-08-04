f461e451c7c6930ae62437f730cb34957c7bb474
devishke-orange <devishke@orangehrmlive.com>
2023-08-04 08:35:06 +0530

    OHRM5X-2273: changed product version and added migrations

---
+++ installer/Migration/V5_6_0/Migration.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA
+ */
+
+namespace OrangeHRM\Installer\Migration\V5_6_0;
+
+use Doctrine\DBAL\Schema\ForeignKeyConstraint;
+use OrangeHRM\Installer\Util\V1\AbstractMigration;
+
+class Migration extends AbstractMigration
+{
+    /**
+     * @inheritDoc
+     */
+    public function up(): void
+    {
+        $this->getSchemaHelper()->dropForeignKeys('ohrm_i18n_translate', ['langStringId']);
+        $foreignKeyConstraint = new ForeignKeyConstraint(
+            ['lang_string_id'],
+            'ohrm_i18n_lang_string',
+            ['id'],
+            'langStringId',
+            ['onDelete' => 'CASCADE']
+        );
+        $this->getSchemaHelper()->addForeignKey('ohrm_i18n_translate', $foreignKeyConstraint);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getVersion(): string
+    {
+        return '5.6.0';
+    }
+}
