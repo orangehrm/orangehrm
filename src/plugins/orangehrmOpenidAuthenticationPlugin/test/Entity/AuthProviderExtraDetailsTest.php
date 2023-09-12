f75459abc1d4e9143dfa6f498512430f4a5de415
ManulMax <manul@orangehrm.us.com>
2023-09-12 09:29:04 +0530

    OHRM5X-2295: Add test for Entities

--- src/plugins/orangehrmOpenidAuthenticationPlugin/test/Entity/AuthProviderExtraDetailsTest.php
+++ src/plugins/orangehrmOpenidAuthenticationPlugin/test/Entity/AuthProviderExtraDetailsTest.php
@@ -1,8 +1,57 @@
 <?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
 
-namespace OrangeHRM\OpenidAuthentication\test\Entity;
+namespace OrangeHRM\Tests\OpenidAuthentication\Entity;
 
-class AuthProviderExtraDetailsTest
+
+use OrangeHRM\Config\Config;
+use OrangeHRM\Entity\AuthProviderExtraDetails;
+use OrangeHRM\Entity\OpenIdProvider;
+use OrangeHRM\Tests\Util\EntityTestCase;
+use OrangeHRM\Tests\Util\TestDataService;
+
+/**
+ * @group OpenidAuthentication
+ * @group Entity
+ */
+class AuthProviderExtraDetailsTest extends EntityTestCase
 {
+    protected function setUp(): void
+    {
+        $fixture = Config::get(Config::PLUGINS_DIR) . '/orangehrmOpenidAuthenticationPlugin/test/fixtures/AuthProviderExtraDetails.yml';
+        TestDataService::populate($fixture);
+    }
 
+    public function testEntity(): void
+    {
+        $authProviderExtraDetails = new AuthProviderExtraDetails();
+        $authProviderExtraDetails->setOpenIdProvider($this->getReference(OpenIdProvider::class, 1));
+        $authProviderExtraDetails->setProviderType(1);
+        $authProviderExtraDetails->setClientId('123.google.com');
+        $authProviderExtraDetails->setClientSecret('Np3pLf0CvBpDPqXEK');
+        $authProviderExtraDetails->setDeveloperKey('uRwXrFo2W2Gd0');
+
+        $this->assertEquals(1, $authProviderExtraDetails->getProviderType());
+        $this->assertEquals('Google', $authProviderExtraDetails->getOpenIdProvider()->getProviderName());
+        $this->assertEquals('https://google.com/o/8/', $authProviderExtraDetails->getOpenIdProvider()->getProviderUrl());
+        $this->assertEquals('123.google.com', $authProviderExtraDetails->getClientId());
+        $this->assertEquals('Np3pLf0CvBpDPqXEK', $authProviderExtraDetails->getClientSecret());
+        $this->assertEquals('uRwXrFo2W2Gd0', $authProviderExtraDetails->getDeveloperKey());
+    }
 }
--- src/plugins/orangehrmOpenidAuthenticationPlugin/test/Entity/OpenIdUserIdentityTest.php
+++ src/plugins/orangehrmOpenidAuthenticationPlugin/test/Entity/OpenIdUserIdentityTest.php
@@ -1,8 +1,54 @@
 <?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
 
-namespace OrangeHRM\OpenidAuthentication\test\Entity;
+namespace OrangeHRM\Tests\OpenidAuthentication\Entity;
 
-class OpenIdUserIdentityTest
+use OrangeHRM\Config\Config;
+use OrangeHRM\Entity\OpenIdProvider;
+use OrangeHRM\Entity\OpenIdUserIdentity;
+use OrangeHRM\Entity\User;
+use OrangeHRM\Tests\Util\EntityTestCase;
+use OrangeHRM\Tests\Util\TestDataService;
+
+/**
+ * @group OpenidAuthentication
+ * @group Entity
+ */
+class OpenIdUserIdentityTest extends EntityTestCase
 {
 
+    protected function setUp(): void
+    {
+        $fixture = Config::get(Config::PLUGINS_DIR) . '/orangehrmOpenidAuthenticationPlugin/test/fixtures/OpenIdUserIdentity.yml';
+        TestDataService::populate($fixture);
+    }
+
+    public function testEntity(): void
+    {
+        $openIdUserIdentity = new OpenIdUserIdentity();
+        $openIdUserIdentity->setUser($this->getReference(User::class, 1));
+        $openIdUserIdentity->setOpenIdProvider($this->getReference(OpenIdProvider::class, 1));
+        $openIdUserIdentity->setUserIdentity('sample');
+
+        $this->assertEquals('admin', $openIdUserIdentity->getUser()->getUserName());
+        $this->assertEquals('Google', $openIdUserIdentity->getOpenIdProvider()->getProviderName());
+        $this->assertEquals('https://google.com/o/8/', $openIdUserIdentity->getOpenIdProvider()->getProviderUrl());
+        $this->assertEquals('sample', $openIdUserIdentity->getUserIdentity());
+    }
 }
