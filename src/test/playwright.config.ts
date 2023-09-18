68305709f7391d50495c920fd72e5d5db7539612
annatooploox <91067186+annatooploox@users.noreply.github.com>
2023-09-18 12:36:10 +0200

    Setup tests (#2)
    * change folder for package.json to main 'test'
    
    * /gitignore updated plus added github action yml
    
    * update playwright config file
    
    * eslint & pretter added

---
+++ src/test/playwright.config.ts
@@ -0,0 +1,35 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+
+const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: 'desktop-chromium',
+      use: {         
+      headless: false,
+      viewport: { width: 1280, height: 720 },
+      screenshot: 'only-on-failure',
+      video: 'retain-on-failure',
+    },
+    },
+  ],
+  reporter: 'html',
+  outputDir: 'test-results',
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    timeout: 5000,
+
+    toHaveScreenshot: {
+      // An acceptable amount of pixels that could be different, unset by default.
+      maxDiffPixels: 10,
+    },
+
+    toMatchSnapshot: {
+      // An acceptable ratio of pixels that are different to the
+      // total amount of pixels, between 0 and 1.
+      maxDiffPixelRatio: 0.1,
+    },
+  },
+};
+
+export default config;
