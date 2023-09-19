3f30c79c33f36627401e79cddd6e6b1fa10450cf
Anna Jablonska <anna.jablonska@tooploox.com>
2023-09-19 12:38:07 +0200

    add user first step

--- src/test/functional/pages/PimPage.ts
+++ src/test/functional/pages/PimPage.ts
@@ -1,6 +1,27 @@
-import { BasePage } from "./BasePage";
+import { Locator, Page } from 'playwright';
 
-export class PimPage extends BasePage {
+export class PimPage {
+  readonly page: Page;
+  readonly addButton: Locator;
+  readonly nameInput: Locator;
+  readonly middleNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly employeeId: Locator;
 
+  constructor(page: Page) {
+    this.page = page;
+    this.addButton = page.getByRole('button', { name: 'Add' });
+    this.nameInput = page.getByPlaceholder('First Name');
+    this.middleNameInput = page.getByPlaceholder('Middle Name');
+    this.lastNameInput = page.getByPlaceholder('Last Name');
+    this.employeeId = page.locator('form').getByRole('textbox').nth(4);
+  }
 
+  public async addEmployee(): Promise<void> {
+    await this.addButton.click();
+    await this.nameInput.type('name input 1');
+    await this.middleNameInput.type('middle');
+    await this.lastNameInput.type('last name');
+    await this.employeeId.type('123456');
+  }
 }
---
+++ src/test/functional/tests/addUser.spec.ts
@@ -0,0 +1,28 @@
+import { test } from '@playwright/test';
+
+import { userTestData } from '../data';
+import { loginPageLocator } from '../locators';
+import { LoginPage } from '../pages/LoginPage';
+import { PimPage } from '../pages/PimPage';
+
+let loginPage: LoginPage;
+
+test.describe('Adding an employee', () => {
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.initialize();
+    await loginPage.navigateToPIMPage();
+    await loginPage.loginUser(userTestData.name, userTestData.password);
+  });
+
+  test.afterEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.close();
+  });
+
+  test('Add new employee user - full flow', async ({ page }) => {
+    const pimPage = new PimPage(page);
+    await pimPage.addEmployee();
+    await page.waitForTimeout(5000);
+  });
+});
