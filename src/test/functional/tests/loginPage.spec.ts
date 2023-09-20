8677b4e31b10ada0fca3f5a6bc09480be70178d7
Jakub Stawowczyk <jakub.stawowczyk@tooploox.com>
2023-09-20 12:08:59 +0200

    removed creating object from each test and instead creating it once in beforeEach hook

--- src/test/functional/tests/loginPage.spec.ts
+++ src/test/functional/tests/loginPage.spec.ts
@@ -4,25 +4,19 @@ import { normalUserTestData, adminUserTestData } from "../data";
 import { loginPageAssertion } from "../assertions";
 
 test.describe("Login Page", () => {
+  let loginPage: LoginPage;
   test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
+    loginPage = new LoginPage(page);
     await loginPage.initialize();
   });
 
-  test.afterEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.close();
-  });
-
   test("Test Case 1: Login As An Admin", async ({ page }) => {
-    const loginPage = new LoginPage(page);
     await loginPage.navigateToMainPage()
     await loginPage.loginUser(adminUserTestData.userName, adminUserTestData.password)
     await expect(loginPage.userNameAfterLogin).toHaveText(loginPageAssertion.adminName);
   });
 
   test("Test Case 2: Login As A User", async ({ page }) => {
-    const loginPage = new LoginPage(page);
     await loginPage.navigateToMainPage()
     await loginPage.loginUser(normalUserTestData.userName, normalUserTestData.password)
     await expect(loginPage.userNameAfterLogin).toHaveText(loginPageAssertion.userName);
