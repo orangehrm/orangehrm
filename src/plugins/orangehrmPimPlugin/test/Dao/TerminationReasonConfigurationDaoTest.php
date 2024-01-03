bac6e5c03a741b740e40670086b8ed44863405c0
ManulMax <manul@orangehrm.us.com>
2024-01-03 16:21:41 +0530

    OHRM5X-2435: Change copyright header - php files

--- src/plugins/orangehrmPimPlugin/test/Dao/TerminationReasonConfigurationDaoTest.php
+++ src/plugins/orangehrmPimPlugin/test/Dao/TerminationReasonConfigurationDaoTest.php
@@ -1,20 +1,19 @@
 <?php
 /**
- * OrangeLayed offM is a comprehensive Human Resource Management (Layed offM) System that captures
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
  * all the essential functionalities required for any enterprise.
- * Copyright (C) 2006 OrangeLayed offM Inc., http://www.orangehrm.com
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
  *
- * OrangeLayed offM is free software; you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * OrangeHRM is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * OrangeLayed offM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program;
- * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA
+ * You should have received a copy of the GNU General Public License along with OrangeHRM.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace OrangeHRM\Tests\Pim\Dao;
