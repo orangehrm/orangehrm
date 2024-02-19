68840ac014ce492eb7d6d54f3c6614decdddcd31
devishke-orange <devishke@orangehrmlive.com>
2024-02-19 09:33:06 +0530

    OHRM5X-2486: Bump FE testing libraries to latest major version (#1811)

--- src/test/functional/cypress/e2e/integration/feature/admin/empStatus.cy.js
+++ src/test/functional/cypress/e2e/integration/feature/admin/empStatus.cy.js
@@ -82,13 +82,14 @@ describe('Admin - Employment Status', function () {
       cy.task('db:restore', {name: 'empStatus'});
       cy.loginTo(this.user, '/admin/saveEmploymentStatus');
       cy.getOXD('form').within(() => {
-        cy.getOXDInput('Name')
-          .type(this.strings.chars100.text)
-          .isInvalid('Should not exceed 50 characters');
-        cy.getOXDInput('Name').setValue('').isInvalid('Required');
-        cy.getOXDInput('Name')
-          .type(this.strings.chars50.text)
-          .isInvalid('Already exists');
+        cy.getOXDInput('Name').then(($input) => {
+          cy.wrap($input).type(this.strings.chars100.text);
+          cy.wrap($input).isInvalid('Should not exceed 50 characters');
+          cy.wrap($input).setValue('');
+          cy.wrap($input).isInvalid('Required');
+          cy.wrap($input).type(this.strings.chars50.text);
+          cy.wrap($input).isInvalid('Already exists');
+        });
       });
     });
   });
@@ -102,7 +103,10 @@ describe('Admin - Employment Status', function () {
         '.oxd-table-body > :nth-child(1) .oxd-table-cell-actions > :nth-child(2)',
       ).click();
       cy.getOXD('form').within(() => {
-        cy.getOXDInput('Name').clear().type(this.strings.chars30.text);
+        cy.getOXDInput('Name').then(($input) => {
+          cy.wrap($input).clear();
+          cy.wrap($input).type(this.strings.chars30.text);
+        });
         cy.getOXD('button').contains('Save').click();
       });
       cy.wait('@putEmpStatus');
