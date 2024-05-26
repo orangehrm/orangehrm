26590bbc0d82b3fec88a7b2dbaff9035442a6c34
devishke-orange <devishke@orangehrm.com>
2024-05-26 17:40:56 +0530

    OHRM5X-2560: add test cases

---
+++ src/plugins/orangehrmAdminPlugin/test/Api/WorkShiftEmployeeAPITest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OrangeHRM\Tests\Admin\Api;
+
+use OrangeHRM\Admin\Api\WorkShiftEmployeeAPI;
+use OrangeHRM\Framework\Services;
+use OrangeHRM\Tests\Util\EndpointIntegrationTestCase;
+use OrangeHRM\Tests\Util\Integration\TestCaseParams;
+
+/**
+ * @group Admin
+ * @group APIv2
+ */
+class WorkShiftEmployeeAPITest extends EndpointIntegrationTestCase
+{
+    /**
+     * @dataProvider dataProviderForTestGetAll
+     */
+    public function testGetAll(TestCaseParams $testCaseParams): void
+    {
+        $this->populateFixtures('WorkShiftEmployeeAPI.yaml');
+        $this->createKernelWithMockServices([Services::AUTH_USER => $this->getMockAuthUser($testCaseParams)]);
+        $this->registerServices($testCaseParams);
+        $api = $this->getApiEndpointMock(WorkShiftEmployeeAPI::class, $testCaseParams);
+        $this->assertValidTestCase($api, 'getAll', $testCaseParams);
+    }
+
+    public function dataProviderForTestGetAll(): array
+    {
+        return $this->getTestCases('WorkShiftEmployeeTestCases.yaml', 'GetAll');
+    }
+}
