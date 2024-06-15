566e97993d4292ad8e511fd941ad25d39531f5b6
Tharaka Athukorala <tharakaathukorala21@gmail.com>
2024-06-15 22:58:31 +0530

    OHRM5X-2482: Enhanced Developing screen for fixing import errors

--- src/client/src/orangehrmAdminPlugin/components/FixLanguageStringErrorTable.vue
+++ src/client/src/orangehrmAdminPlugin/components/FixLanguageStringErrorTable.vue
@@ -1,3 +1,20 @@
+<!--
+  - OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+  - all the essential functionalities required for any enterprise.
+  - Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+  -
+  - OrangeHRM is free software: you can redistribute it and/or modify it under the terms of
+  - the GNU General Public License as published by the Free Software Foundation, either
+  - version 3 of the License, or (at your option) any later version.
+  -
+  - OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  - without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  - See the GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License along with OrangeHRM.
+  - If not, see <https://www.gnu.org/licenses/>.
+  -->
+
 <template>
   <div class="orangehrm-translation-container">
     <oxd-divider />
@@ -6,7 +23,7 @@
         <oxd-text type="card-title">{{ $t('admin.source_text') }}</oxd-text>
       </oxd-grid-item>
       <oxd-grid-item class="orangehrm-translation-grid-header">
-        <oxd-text type="card-title">Error</oxd-text>
+        <oxd-text type="card-title">{{ $t('admin.error') }}</oxd-text>
       </oxd-grid-item>
       <oxd-grid-item class="orangehrm-translation-grid-header">
         <oxd-text type="card-title">{{ $t('admin.translated_text') }}</oxd-text>
--- src/client/src/orangehrmAdminPlugin/pages/languageImport/FixLanguageStringErrors.vue
+++ src/client/src/orangehrmAdminPlugin/pages/languageImport/FixLanguageStringErrors.vue
@@ -1,3 +1,20 @@
+<!--
+  - OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+  - all the essential functionalities required for any enterprise.
+  - Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+  -
+  - OrangeHRM is free software: you can redistribute it and/or modify it under the terms of
+  - the GNU General Public License as published by the Free Software Foundation, either
+  - version 3 of the License, or (at your option) any later version.
+  -
+  - OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  - without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  - See the GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License along with OrangeHRM.
+  - If not, see <https://www.gnu.org/licenses/>.
+  -->
+
 <template>
   <div class="orangehrm-background-container">
     <div class="orangehrm-paper-container">
@@ -124,8 +141,8 @@ export default {
           method: `PUT`,
           url: `/api/v2/admin/i18n/languages/${props.languageId}/translations/bulk`,
           data: {
-            data: paginatedErrors.value
-              .filter((item) => item.target !== null && item.modified === true)
+            data: paginatedErrors.value.data
+              .filter((item) => item.target !== null && item.modified == true)
               .map((item) => {
                 return {
                   langStringId: item.langStringId,
@@ -137,11 +154,7 @@ export default {
         .then(() => {
           return saveSuccess();
         })
-        .then(() => reloadPage())
-        .catch((error) => {
-          console.error('Error saving translations:', error);
-          isLoading.value = false; // Ensure loading state is reset
-        });
+        .then(() => reloadPage());
     };
 
     return {
