de7511bed255231d8dc3347d6da47b4b513509e9
devishke-orange <devishke@orangehrm.com>
2024-06-27 20:03:36 +0530

    OHRM5X-2482: translation validation test cases

--- src/plugins/orangehrmAdminPlugin/test/Api/I18NTranslationValidationAPITest.php
+++ src/plugins/orangehrmAdminPlugin/test/Api/I18NTranslationValidationAPITest.php
@@ -2,74 +2,71 @@
 
 namespace OrangeHRM\Tests\Admin\Api;
 
-use OrangeHRM\Admin\Api\I18NImportErrorAPI;
+use OrangeHRM\Admin\Api\I18NTranslationValidationAPI;
 use OrangeHRM\Entity\I18NError;
 use OrangeHRM\Entity\I18NGroup;
-use OrangeHRM\Entity\I18NImportError;
 use OrangeHRM\Entity\I18NLangString;
 use OrangeHRM\Entity\I18NLanguage;
-use OrangeHRM\Entity\I18NTranslation;
 use OrangeHRM\Framework\Services;
 use OrangeHRM\Tests\Util\EndpointIntegrationTestCase;
 use OrangeHRM\Tests\Util\Integration\TestCaseParams;
 use OrangeHRM\Tests\Util\TestDataService;
 
-class I18NImportErrorAPITest extends EndpointIntegrationTestCase
+class I18NTranslationValidationAPITest extends EndpointIntegrationTestCase
 {
     protected function setUp(): void
     {
         TestDataService::truncateSpecificTables([
             I18NError::class,
-            I18NImportError::class,
             I18NLangString::class,
-            I18NTranslation::class,
             I18NLanguage::class,
             I18NGroup::class
         ]);
-        $this->populateFixtures('I18NImportErrorAPITest.yml');
+        $this->populateFixtures('I18NTranslationValidationAPITest.yml');
     }
 
     /**
-     * @dataProvider dataProviderForTestGetAll
+     * @dataProvider dataProviderForTestGetOne
      */
-    public function testGetAll(TestCaseParams $testCaseParams): void
+    public function testGetOne(TestCaseParams $testCaseParams): void
     {
         $this->createKernelWithMockServices([Services::AUTH_USER => $this->getMockAuthUser($testCaseParams)]);
         $this->registerServices($testCaseParams);
-        $api = $this->getApiEndpointMock(I18NImportErrorAPI::class, $testCaseParams);
-        $this->assertValidTestCase($api, 'getAll', $testCaseParams);
+        $api = $this->getApiEndpointMock(I18NTranslationValidationAPI::class, $testCaseParams);
+        $this->assertValidTestCase($api, 'getOne', $testCaseParams);
     }
 
-    public function dataProviderForTestGetAll(): array
+    public function dataProviderForTestGetOne(): array
     {
-        return $this->getTestCases('I18NImportErrorAPITestCases.yml', 'GetAll');
+        return $this->getTestCases('I18NTranslationValidationAPITestCases.yml', 'GetOne');
     }
 
-    public function testCreate(): void
+    public function testUpdate(): void
     {
-        $api = new I18NImportErrorAPI($this->getRequest());
+        $api = new I18NTranslationValidationAPI($this->getRequest());
         $this->expectNotImplementedException();
-        $api->create();
+        $api->update();
     }
 
-    public function testGetValidationRuleForCreate(): void
+    public function testGetValidationRuleForUpdate(): void
     {
-        $api = new I18NImportErrorAPI($this->getRequest());
+        $api = new I18NTranslationValidationAPI($this->getRequest());
         $this->expectNotImplementedException();
-        $api->getValidationRuleForCreate();
+        $api->getValidationRuleForUpdate();
     }
 
     public function testDelete(): void
     {
-        $api = new I18NImportErrorAPI($this->getRequest());
+        $api = new I18NTranslationValidationAPI($this->getRequest());
         $this->expectNotImplementedException();
         $api->delete();
     }
 
     public function testGetValidationRuleForDelete(): void
     {
-        $api = new I18NImportErrorAPI($this->getRequest());
+        $api = new I18NTranslationValidationAPI($this->getRequest());
         $this->expectNotImplementedException();
         $api->getValidationRuleForDelete();
     }
+
 }
