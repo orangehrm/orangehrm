4f77ba76312e4bd6afdaf501298ecd4e348bfac2
devishke-orange <devishke@orangehrm.com>
2024-07-19 09:16:15 +0530

    add missing license headers

--- src/plugins/orangehrmAdminPlugin/test/Api/JobCategoryAPITest.php
+++ src/plugins/orangehrmAdminPlugin/test/Api/JobCategoryAPITest.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with OrangeHRM.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 
 namespace OrangeHRM\Tests\Admin\Api;
 
