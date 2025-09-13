b242f6148b3643d0ac8cd73d2a386c57b2c9ddb2
swatibalodi <66731585+swatibalodi@users.noreply.github.com>
2025-09-14 00:16:16 +0530

    Update build_and_run.sh

--- .deeptune/build_and_run.sh
+++ .deeptune/build_and_run.sh
@@ -1,19 +1,24 @@
  #!/bin/bash
- set -e
-# Script to build the Docker image for local development
- # Set environment variables
- DEEPTUNE_APP_ENV="orangehrm"
- DOCKER_IMAGE_NAME="deeptune-${DEEPTUNE_APP_ENV}-base:latest"
- DOCKERFILE_PATH=".deeptune/Dockerfile"
-# Build the Docker image
- echo "Building Docker image..."
+set -e
+
+# Environment setup
+DEEPTUNE_APP_ENV="orangehrm"
+DOCKER_IMAGE_NAME="deeptune-${DEEPTUNE_APP_ENV}-base:latest"
+DOCKERFILE_PATH=".deeptune/Dockerfile"
+
+echo "Building Docker image..."
  docker build -t $DOCKER_IMAGE_NAME -f $DOCKERFILE_PATH . || { echo 
 "Build failed"; exit 1; }
- echo "Build successful"
- # Run the Docker container
- echo "Running Docker container..."
- # Make sure to update these ports to whatever your app needs exposed
- docker run -p 3000:3000 -p 5432:5432 -p 80:80 $DOCKER_IMAGE_NAME
- echo "Container running"
+echo "Build successful"
+
+echo "Running Docker container..."
+docker run -d \
+  --name ${DEEPTUNE_APP_ENV}_container \
+  -p 80:80 \
+  -p 3306:3306 \
+  $DOCKER_IMAGE_NAME
+
+echo "Container is up and running"
+
 
 
