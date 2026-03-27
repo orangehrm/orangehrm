cf4f2dda2c9bf968ec066481a2248aee5bc17479
Rajitha Kumara <rajitha@orangehrm.us.com>
2026-03-27 17:30:11 +0000

    OHRM5X-2633: Update version to 5.8.1

--- src/lib/config/Config.php
+++ src/lib/config/Config.php
@@ -51,7 +51,7 @@ class Config
     public const MODE_DEMO = 'demo';
 
     public const PRODUCT_NAME = 'OrangeHRM OS';
-    public const PRODUCT_VERSION = '5.8';
+    public const PRODUCT_VERSION = '5.8.1';
     public const ORANGEHRM_API_VERSION = '2.7.0';
     public const PRODUCT_MODE = self::MODE_DEV;
     public const REGISTRATION_URL = 'https://ospenguin.orangehrm.com';
